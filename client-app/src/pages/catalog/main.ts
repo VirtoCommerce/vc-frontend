fa564e1a9fcb962dc6da5f42fc47c92c6f9b3be4
Eugeny Tatarincev <et@virtoway.com>
2020-10-13 18:16:55 +0200

    Load user on index page

--- client-app/src/pages/catalog/main.ts
+++ client-app/src/pages/catalog/main.ts
@@ -1,6 +1,8 @@
 import Vue from "vue";
 import i18n from "@i18n";
 import InitializationService from "core/services/initialization.service";
+import profileModule from "libs/authorization/store/profile";
+import { FETCH_PROFILE } from "libs/authorization/store/profile/definitions";
 import cartModule from "libs/shopping-cart/store/cart";
 import AddToCartButton from "libs/shopping-cart/views/add-to-cart-button/index.vue";
 import CartButton from "libs/shopping-cart/views/cart-button/index.vue";
@@ -8,6 +10,7 @@ import store from "store";
 import ActiveOrderSidebar from "./views/active-order-sidebar/index.vue";
 
 store.registerModule("cart", cartModule);
+store.registerModule("profileModule", profileModule);
 
 InitializationService.initializeCommon().then(() => {
   const addToCartElements = document.getElementsByClassName("app-add-item-to-cart");
@@ -29,15 +32,17 @@ InitializationService.initializeCommon().then(() => {
     }).$mount(addToCartEl);
   }
 
-  new Vue({
-    i18n,
-    store,
-    render: h => h(CartButton)
-  }).$mount("#app-cart");
+  store.dispatch(`profileModule/${FETCH_PROFILE}`).then(() => {
+    new Vue({
+      i18n,
+      store,
+      render: h => h(CartButton)
+    }).$mount("#app-cart");
 
-  new Vue({
-    i18n,
-    store,
-    render: h => h(ActiveOrderSidebar)
-  }).$mount("#app-cart-sidebar");
+    new Vue({
+      i18n,
+      store,
+      render: h => h(ActiveOrderSidebar)
+    }).$mount("#app-cart-sidebar");
+  });
 });
