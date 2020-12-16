ce05e58d5bf2d0df95697ea597f1a9939b004551
Filip Kuksov <vectorfield4@gmail.com>
2020-12-16 10:56:43 +0200

    composition api test

--- client-app/src/pages/test/main.ts
+++ client-app/src/pages/test/main.ts
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import { SfLoader } from '@storefront-ui/vue';
 import i18n from "@i18n";
+import SimpleCounter from "@libs/test/views/simple-counter/index.vue";
 import store from "store";
 import InitializationService from "@core/services/initialization.service";
 
@@ -10,4 +11,10 @@ InitializationService.initializeCommon().then(() => {
     store,
     render: h => h(SfLoader)
   }).$mount("#loader");
+
+  new Vue({
+    i18n,
+    store,
+    render: h => h(SimpleCounter)
+  }).$mount("#composition-api-test");
 });
