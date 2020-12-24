788884d84cefbd31bc5f8d8992375e4ed29c76de
Filip Kuksov <vectorfield4@gmail.com>
2020-12-24 16:10:15 +0200

    Add Add to card button

--- client-app/src/libs/cart/index.ts
+++ client-app/src/libs/cart/index.ts
@@ -1,3 +1,5 @@
+import AddToCartButton  from './components/AddToCartButton.vue';
 import CartSidebar  from './components/CartSidebar.vue';
 import useCart from './composables/useCart';
-export { useCart, CartSidebar };
+
+export { useCart, CartSidebar, AddToCartButton };
--- client-app/src/pages/home/main.ts
+++ client-app/src/pages/home/main.ts
@@ -1,9 +1,29 @@
 import Vue from "vue";
 import i18n from "@i18n";
+import { AddToCartButton } from '@libs/cart/index.ts'
 import { HeroSlider } from '@libs/home/index.ts'
 import InitializationService from "@core/services/initialization.service";
 
 InitializationService.initializeCommon().then(() => {
+  const addToCartElements = document.getElementsByClassName("app-add-item-to-cart");
+  for (const addToCartEl of addToCartElements) {
+    const productId = addToCartEl.attributes.getNamedItem("product-id")!.value
+    const title = addToCartEl.attributes.getNamedItem("title")!.value
+    const buyable = addToCartEl.attributes.getNamedItem("text-visible")!.value;
+    new Vue({
+      i18n,
+      render ( h ) {
+        return h(AddToCartButton, {
+          props: {
+            buyable: buyable,
+            productId: productId,
+            title: title
+          }
+        })
+      }
+    }).$mount(addToCartEl);
+  }
+
   new Vue({
     i18n,
     render: h => h(HeroSlider)
