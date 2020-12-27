db6ac15cf1c0dffee67f076dc7639c7f5b309caf
Filip Kuksov <vectorfield4@gmail.com>
2020-12-28 00:38:00 +0200

    Vue account component to composition API

---
+++ client-app/src/pages/account/views/_internal/index.js
@@ -0,0 +1,10 @@
+import Newsletter from "./MyNewsletter.vue";
+import Profile from "./MyProfile.vue";
+import OrderHistory from "./OrderHistory.vue";
+import ShippingDetails from "./ShippingDetails";
+export {
+  Newsletter,
+  Profile,
+  OrderHistory,
+  ShippingDetails,
+};
