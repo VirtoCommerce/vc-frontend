198a2280afc67d2ecc7f36f7d8e059da57b96374
Ilia <ilya.ivanov6@gmail.com>
2021-11-18 13:52:58 +0200

    Remove everything before refreshing line endings

--- client-app/core/utilities/logger/defaultLogger.ts
+++
@@ -1,8 +0,0 @@
-const defaultLogger = {
-  debug: (message: any, ...args) => console.log('[VC][debug]', message, ...args),
-  info: (message: any, ...args) => console.log('[VC][info]', message, ...args),
-  warn: (message: any, ...args) => console.log('[VC][warn]', message, ...args),
-  error: (message: any, ...args) => console.log('[VC][error]', message, ...args)
-};
-
-export default defaultLogger;
--- client-app/pages/account/views/MyAccount/ShippingDetails.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <b>Please implement vendor specific  component in 'components/ShippingDetails' directory</b>
-</template>
-
-<script>
-export default {
-};
-</script>
