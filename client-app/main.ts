3f0340d2e999d727b8a0b0c10d49d711912c28d9
Anton Veretennikov <superiorboroda@gmail.com>
2021-12-01 13:59:19 +0300

    feat: add sign-in page
    - Link from main page.
    - Layout of sign-in.

--- client-app/main.ts
+++ client-app/main.ts
@@ -8,12 +8,14 @@ import Error403 from "./pages/403/403.vue";
 import "@fontsource/lato";
 import "@fontsource/roboto";
 import "@fortawesome/fontawesome-free/css/all.css";
+import SingInPage from "./pages/sign-in/sign-in-page.vue";
 import "../assets/static/styles/index.css";
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: Home },
+    { path: "/signIn", name: "SignIn", component: SingInPage },
     { path: "/500", name: "InternalError", component: Error500 },
     { path: "/403", name: "NoAccess", component: Error403 },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: Error404 },
---
+++ client-app/pages/sign-in/sign-in-page.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="container mx-auto w-4/5 py-5">
+    <div class="grid grid-cols-1 lg:grid-cols-2 lg:gap-24">
+      <div class="my-8 px-4">
+        <h1 class="mb-8 font-semibold uppercase text-4xl">Sign In</h1>
+        <div class="flex space-x-3 px-3 py-2.5 h-11 bg-red-50 rounded-sm">
+          <img class="w-4 h-auto" src="assets/static/images/error-icon.svg" />
+          <span><strong>User</strong> or <strong>password</strong> is incorrect</span>
+        </div>
+        <div class="mt-4">
+          <span class="font-semibold" for>User name</span>
+          <span class="text-red-500">*</span>
+          <input type="text" placeholder="User Name" class="border h-11 border-gray-300 rounded px-3 py-1 w-full" />
+        </div>
+        <div class="mt-4">
+          <span class="font-semibold" for>Password</span>
+          <span class="text-red-500">*</span>
+          <input type="password" placeholder="Password" class="border h-11 border-gray-300 rounded px-3 py-1 w-full" />
+        </div>
+        <a href="/" class="text-blue-700 hover:text-blue-500 text-sm font-medium">Forgot your password?</a>
+        <div class="flex space-x-5 mt-8">
+          <button
+            class="flex-1 md:flex-grow-0 bg-yellow-500 hover:bg-yellow-600 uppercase font-bold rounded text-white px-10 py-2 text-sm"
+          >
+            Login
+          </button>
+          <button
+            class="flex-1 md:flex-grow-0 border-2 border-yellow-500 hover:bg-yellow-600 hover:text-white hover:border-yellow-600 uppercase font-bold rounded text-yellow-500 px-10 py-2 text-sm"
+          >
+            Registration
+          </button>
+        </div>
+      </div>
+      <div class="hidden lg:block">
+        <img class="max-w-md" src="assets/static/images/sign-in-page-image.png" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import useUser from "@/shared/account/composables/useUser";
+const { isAuthenticated, me } = useUser();
+
+// export default {
+//   name: "Test",
+//   created() { },
+//   data() {
+//     return {};
+//   },
+//   props: {},
+//   methods: {},
+// };
+</script>
+
+<style lang="scss" scoped></style>
