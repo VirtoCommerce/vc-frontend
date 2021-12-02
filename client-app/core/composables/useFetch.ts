a20d6d4995031745f1db4c6424514eceef4bb9a9
Ilya <ilya.ivanov6@gmail.com>
2021-12-02 16:42:41 +0200

    feat: rework useFetch composable

--- client-app/core/composables/useFetch.ts
+++ client-app/core/composables/useFetch.ts
@@ -1,41 +1,53 @@
 import { shallowRef, unref } from "vue";
 import { useRouter } from "vue-router";
+import { MaybeRef } from "../helpers/maybeRef";
 
-export function useFetch(url: MaybeRef<string>) {
-  const data = shallowRef<T | undefined>();
+export default function useFetch() {
+  const data = shallowRef<unknown | undefined>();
   const error = shallowRef<Error | undefined>();
   const statusCode = shallowRef<number | null>();
   const router = useRouter();
 
-  fetch(unref(url))
-    .then((result) => {
-      statusCode.value = result.status;
-      return result.json();
-    })
-    .then((result) => {
-      data.value = result;
-    })
-    .catch((e) => {
-      error.value = e;
-      switch (statusCode.value) {
-        case 500:
-          router.push({
-            name: "InternalError",
-          });
-          break;
-        case 403:
-          router.push({
-            name: "NoAccess",
-          });
-          break;
-        default:
-          break;
-      }
+  function innerFetch<TBody, TResult>(url: MaybeRef<string>, method = "POST", body?: TBody): Promise<TResult> {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return new Promise((resolve, reject) => {
+      fetch(unref(url), { headers, method, body: JSON.stringify(body) })
+        .then((result) => {
+          statusCode.value = result.status;
+          return result.json();
+        })
+        .then((result) => {
+          data.value = result;
+          resolve(result);
+        })
+        .catch((e) => {
+          error.value = e;
+          switch (statusCode.value) {
+            case 500:
+              router.push({
+                name: "InternalError",
+              });
+              break;
+            case 403:
+              router.push({
+                name: "NoAccess",
+              });
+              break;
+            default:
+              break;
+          }
+          reject(e);
+        });
     });
+  }
 
   return {
     data,
     error,
     statusCode,
+    innerFetch,
   };
 }
