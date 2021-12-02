a438de766622205a5643df9c948eb72803665dfb
Ilya <ilya.ivanov6@gmail.com>
2021-12-02 11:26:53 +0200

    feat: update color palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./index.html', './client-app/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false,
@@ -6,7 +8,27 @@ module.exports = {
       'lato': ['Lato', 'sans-serif'],
       'roboto': ['Roboto', 'sans-serif']
     },
-    extend: {},
+    extend: {
+      colors: {
+        yellow: {
+          '500': '#F0AD4E'
+        },
+        red: {
+          '500': '#FF4A4A'
+        },
+        cyan: {
+          ...colors.cyan,
+          '500': '#5294AD',
+          '700': '#00739E'
+        },
+        blueGray: {
+          ...colors.blueGray,
+          '300': '#C2C9E0'
+        },
+        trueGray: colors.trueGray,
+        warmGray: colors.warmGray
+      }
+    },
   },
   variants: {
     extend: {},
