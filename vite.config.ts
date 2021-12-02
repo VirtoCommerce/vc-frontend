5946b929a869a0ce66657ae35426d99ee24ecbb8
Anton Veretennikov <superiorboroda@gmail.com>
2021-12-02 17:04:01 +0300

    feat: proxy 'xapi/graphql'

--- vite.config.ts
+++ vite.config.ts
@@ -23,10 +23,8 @@ export default defineConfig({
   },
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:2083/",
-        changeOrigin: false,
-      },
+      "/storefrontapi": "http://localhost:2083",
+      "/xapi": "http://localhost:2083",
     },
   },
 });
