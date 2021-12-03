ad375f278e2e316791234629c70c3d13c13bb93a
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2021-12-03 13:38:00 +0200

    ST-18,ST-19,ST-133: Add pages for errors (#15)
    * feat: 404 page
    
    * ST-269: Add 403 page
    
    * ST-134: Add 500 page
    
    * feat: add useFetch composable
    
    * feat: update polygon bg
    
    * feat: rework useFetch composable
    
    * fix: empty space between footer and content
    
    * fix: remove font-lato on error pages

---
+++ client-app/core/composables/useFetch.ts
@@ -0,0 +1,53 @@
+import { shallowRef, unref } from "vue";
+import { useRouter } from "vue-router";
+import { MaybeRef } from "../helpers/maybeRef";
+
+export default function useFetch() {
+  const data = shallowRef<unknown | undefined>();
+  const error = shallowRef<Error | undefined>();
+  const statusCode = shallowRef<number | null>();
+  const router = useRouter();
+
+  function innerFetch<TBody, TResult>(url: MaybeRef<string>, method = "POST", body?: TBody): Promise<TResult> {
+    const headers = {
+      "Content-Type": "application/json",
+    };
+
+    return new Promise((resolve, reject) => {
+      fetch(unref(url), { headers, method, body: JSON.stringify(body) })
+        .then((result) => {
+          statusCode.value = result.status;
+          return result.json();
+        })
+        .then((result) => {
+          data.value = result;
+          resolve(result);
+        })
+        .catch((e) => {
+          error.value = e;
+          switch (statusCode.value) {
+            case 500:
+              router.push({
+                name: "InternalError",
+              });
+              break;
+            case 403:
+              router.push({
+                name: "NoAccess",
+              });
+              break;
+            default:
+              break;
+          }
+          reject(e);
+        });
+    });
+  }
+
+  return {
+    data,
+    error,
+    statusCode,
+    innerFetch,
+  };
+}
--- client-app/main.ts
+++ client-app/main.ts
@@ -3,6 +3,8 @@ import App from "./App.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "./pages/home/home.vue";
 import Error404 from "./pages/404/404.vue";
+import Error500 from "./pages/500/500.vue";
+import Error403 from "./pages/403/403.vue";
 import "@fontsource/lato";
 import "@fontsource/roboto";
 import "@fortawesome/fontawesome-free/css/all.css";
@@ -12,6 +14,8 @@ const router = createRouter({
   history: createWebHistory(),
   routes: [
     { path: "/", name: "Home", component: Home },
+    { path: "/500", name: "InternalError", component: Error500 },
+    { path: "/403", name: "NoAccess", component: Error403 },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: Error404 },
   ],
 });
