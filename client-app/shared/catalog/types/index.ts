0d4c5b833dd8df56190ac2c90dbac4931d3f91c8
Ilya <ilya.ivanov6@gmail.com>
2021-12-07 10:16:57 +0200

    feat: copy composables and types for category page

---
+++ client-app/shared/catalog/types/index.ts
@@ -0,0 +1,18 @@
+export type CategoryTree = {
+  parent?: string;
+  label?: string;
+  slug?: string;
+  items?: CategoryTree[];
+  isCurrent?: boolean;
+  count?: number;
+  [x: string]: unknown;
+};
+
+export type ProductsSearchParams = {
+  categoryId?: string;
+  term?: string;
+  page?: number;
+  itemsPerPage?: number;
+  sort?: string;
+  [x: string]: any;
+};
