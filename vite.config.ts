144e76458fd1ffe5b4f0b2d6184454ab329d47ad
Yuri A Taranov <me@flanker72.ru>
2021-12-14 22:26:45 +0500

    feat: performance improvements

--- vite.config.ts
+++ vite.config.ts
@@ -14,11 +14,12 @@ export default defineConfig({
   },
   build: {
     outDir: "assets/static/bundle",
+    assetsDir: "./",
     emptyOutDir: true,
-    lib: {
-      entry: path.resolve(__dirname, "client-app/main.ts"),
-      name: "theme",
-      fileName: (format) => `theme.${format}.js`,
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].js",
+      },
     },
   },
   server: {
