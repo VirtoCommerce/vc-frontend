fa5cd84882e683d75350987e365209186ca1ad8a
Yuri A Taranov <me@flanker72.ru>
2021-12-20 20:39:53 +0500

    feat: cart add info dialog

--- client-app/shared/cart/components/cart-add-info.vue
+++ client-app/shared/cart/components/cart-add-info.vue
@@ -37,11 +37,58 @@
                 <i class="fas fa-times text-2xl cursor-pointer" @click="$emit('close')"></i>
               </DialogTitle>
 
-              <div class="">Table</div>
+              <div class="hidden lg:block">
+                <table class="w-full">
+                  <thead class="border-b border-gray-200">
+                    <tr>
+                      <th class="px-5 py-3 text-sm font-bold">Product</th>
+                      <th class="px-5 py-3 text-sm font-bold text-center">Quantity</th>
+                      <th class="px-5 py-3 text-sm font-bold text-right">Total</th>
+                    </tr>
+                  </thead>
+                  <tbody class="border-b border-gray-200">
+                    <tr>
+                      <td class="px-5 py-3">
+                        <div class="flex items-center">
+                          <img
+                            class="border object-contain rounded-sm"
+                            :src="lineItem.imageUrl || '/static/images/common/no-image.svg'"
+                            width="72"
+                            height="72"
+                          />
+                          <div class="ml-4 font-bold text-blue-700">{{ lineItem.name }}</div>
+                        </div>
+                      </td>
+                      <td class="px-5 py-3 text-center">{{ lineItem.quantity }}</td>
+                      <td class="px-5 py-3 text-right text-green-700 font-bold">
+                        {{ lineItem.extendedPrice?.formattedAmount }}
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+              <div class="block lg:hidden">
+                <div class="flex items-center border-b border-gray-200 p-5">
+                  <img
+                    class="border object-contain rounded-sm"
+                    :src="lineItem.imageUrl || '/static/images/common/no-image.svg'"
+                    width="72"
+                    height="72"
+                  />
+                  <div class="ml-4 font-bold text-blue-700">{{ lineItem.name }}</div>
+                </div>
 
-              <div
-                class="mt-4 border-t border-gray-200 px-5 py-4 flex items-center justify-between lg:justify-end space-x-4"
-              >
+                <div class="flex items-center justify-between px-5 py-3">
+                  <div>
+                    Quantity: <span class="font-bold">{{ lineItem.quantity }}</span>
+                  </div>
+                  <div>
+                    Total: <span class="font-bold text-green-700">{{ lineItem.extendedPrice?.formattedAmount }}</span>
+                  </div>
+                </div>
+              </div>
+
+              <div class="px-5 py-4 flex items-center justify-between lg:justify-end space-x-4">
                 <button
                   class="w-1/2 lg:w-auto inline-flex items-center justify-center lg:px-4 h-9 font-roboto-condensed text-base font-bold border-2 border-yellow-500 text-yellow-500 hover:bg-yellow-500 hover:text-white rounded focus:outline-none"
                   @click="$emit('close')"
@@ -63,7 +110,9 @@
 </template>
 
 <script setup lang="ts">
+import { PropType } from "vue";
 import { TransitionRoot, TransitionChild, Dialog, DialogOverlay, DialogTitle } from "@headlessui/vue";
+import { LineItemType } from "@/core/api/graphql/types";
 
 defineProps({
   isOpen: {
@@ -72,7 +121,7 @@ defineProps({
   },
 
   lineItem: {
-    type: Object,
+    type: Object as PropType<LineItemType>,
     required: true,
   },
 });
