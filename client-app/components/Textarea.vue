7a12dfab3e8fe636b02d16c151572415f3d06176
Ilya <ilya.ivanov6@gmail.com>
2021-12-20 15:13:17 +0200

    feat: remove commented code in textarea component

--- client-app/components/Textarea.vue
+++ client-app/components/Textarea.vue
@@ -35,16 +35,11 @@ const props = defineProps({
   },
 });
 const maxLength = toRef(props, "maxLength");
-//const textarea = toRef(props, "modelValue");
 const text = computed({
   get: () => props.modelValue,
   set: (newValue) => emit("update:modelValue", newValue),
 });
 const symbolsCount = computed(() => text.value.length + "/" + maxLength.value);
-// const onInput = (e: Event) => {
-//   const newValue = (e.target as HTMLInputElement).value;
-//   emit("update:modelValue", newValue);
-// };
 const emit = defineEmits(["update:modelValue"]);
 </script>
 
