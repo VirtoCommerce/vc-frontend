01e816141269f8878702db1113e7bbd97a7e46f6
Anton Veretennikov <superiorboroda@gmail.com>
2021-12-23 22:39:41 +0300

    feat: use dompurify for sanitizing

--- client-app/components/atoms/markdown-render/markdown-render.vue
+++ client-app/components/atoms/markdown-render/markdown-render.vue
@@ -5,10 +5,11 @@
 <script setup lang="ts">
 import { computed } from "vue";
 import { marked } from "marked";
+import DOMPurify from "dompurify";
 
 const props = defineProps<{ src: string }>();
 
-const markdown = computed(() => marked(props.src, { sanitize: true }));
+const markdown = computed(() => DOMPurify.sanitize(marked(props.src), { USE_PROFILES: { html: true } }));
 </script>
 
 <style lang="scss" scoped></style>
