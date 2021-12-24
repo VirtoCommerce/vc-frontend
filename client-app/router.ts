c1e58295959ba12fc7080c9f499fbb70526dee6f
Ilya <ilya.ivanov6@gmail.com>
2021-12-24 09:43:04 +0200

    fix: remove trailing slash in checkout route

--- client-app/router.ts
+++ client-app/router.ts
@@ -24,7 +24,7 @@ const router = createRouter({
     { path: "/sign-in", name: "SignIn", component: SingInPage },
     { path: "/catalog", name: "CatalogRoot", component: Catalog },
     { path: "/catalog/:categoryKey", name: "Catalog", component: Catalog },
-    { path: "/checkout/", name: "Checkout", component: Checkout },
+    { path: "/checkout", name: "Checkout", component: Checkout },
     { path: "/500", name: "InternalError", component: Error500 },
     { path: "/403", name: "NoAccess", component: Error403 },
     { path: "/:pathMatch(.*)*", name: "NotFound", component: Error404 },
