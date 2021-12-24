355ec7fb9a5385809259ff8f58bfecf04bd251d9
Ilya <ilya.ivanov6@gmail.com>
2021-12-24 10:36:23 +0200

    fix: use twoColumn for empty-cart, fix mobile

--- client-app/shared/checkout/components/empty-cart.vue
+++ client-app/shared/checkout/components/empty-cart.vue
@@ -1,24 +1,25 @@
 <template>
-  <div class="polygon-bg flex-grow">
-    <div class="mx-auto w-4/5 lg:w-3/5 pt-5 md:pb-32">
-      <div class="flex justify-between md:space-x-16">
-        <div class="flex flex-col py-12 justify-center">
-          <h2 class="text-black-800 text-2xl md:text-4xl font-bold mb-8">Your cart is empty</h2>
-          <router-link
-            to="/catalog"
-            tag="button"
-            class="bg-yellow-500 hover:bg-yellow-600 w-48 uppercase font-bold text-center rounded text-white py-3 text-base whitespace-nowrap font-roboto"
-            >Continue shopping</router-link
-          >
-        </div>
-        <div class="hidden md:block py-5">
-          <img src="/assets/static/images/errors/emptyCart.webp" alt="Page not found" class="w-full max-w-md" />
-        </div>
+  <TwoColumn class="max-w-screen-lg">
+    <template #left>
+      <div class="flex flex-col h-full py-12 justify-center items-center lg:items-start">
+        <img src="/assets/static/images/errors/emptyCartMobile.webp" alt="emptyCartMobile" class="lg:hidden mb-8" />
+        <h2 class="text-black-800 text-2xl md:text-4xl font-bold mb-8">Your cart is empty</h2>
+        <router-link
+          to="/catalog"
+          tag="button"
+          class="bg-yellow-500 hover:bg-yellow-600 w-48 uppercase font-bold text-center rounded text-white py-3 text-base whitespace-nowrap font-roboto"
+          >Continue shopping</router-link
+        >
       </div>
-    </div>
-  </div>
+    </template>
+    <template #right>
+      <img src="/assets/static/images/errors/emptyCart.webp" alt="emptyCart" class="w-full max-w-md" />
+    </template>
+  </TwoColumn>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { TwoColumn } from "@/shared/layout";
+</script>
 
 <style scoped></style>
