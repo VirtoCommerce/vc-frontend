c14b372c8b7db26a88f82ddec4bae02939d1f91d
Ilya <ilya.ivanov6@gmail.com>
2022-01-12 09:34:03 +0100

    fix: add scope and remove prefix

--- client-app/shared/account/components/account-navigation-link.vue
+++ client-app/shared/account/components/account-navigation-link.vue
@@ -19,46 +19,37 @@ defineProps({
 });
 </script>
 
-<style>
+<style scoped>
 /* Icons */
 .icon > div {
   background-color: #808080;
 }
 .dashboard-icon > div {
   mask: url(/assets/static/images/dashboard/icons/grid.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/grid.svg) no-repeat center / contain;
 }
 .profile-icon > div {
   mask: url(/assets/static/images/dashboard/icons/profile.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/profile.svg) no-repeat center / contain;
 }
 .addresses-icon > div {
   mask: url(/assets/static/images/dashboard/icons/building.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/building.svg) no-repeat center / contain;
 }
 .password-icon > div {
   mask: url(/assets/static/images/dashboard/icons/password.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/password.svg) no-repeat center / contain;
 }
 .orders-icon > div {
   mask: url(/assets/static/images/dashboard/icons/orders.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/orders.svg) no-repeat center / contain;
 }
 .list-icon > div {
   mask: url(/assets/static/images/dashboard/icons/list.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/list.svg) no-repeat center / contain;
 }
 .checkout-icon > div {
   mask: url(/assets/static/images/dashboard/icons/cart.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/cart.svg) no-repeat center / contain;
 }
 .company-icon > div {
   mask: url(/assets/static/images/dashboard/icons/company.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/company.svg) no-repeat center / contain;
 }
 .company-members-icon > div {
   mask: url(/assets/static/images/dashboard/icons/members.svg) no-repeat center / contain;
-  -webkit-mask: url(/assets/static/images/dashboard/icons/members.svg) no-repeat center / contain;
 }
 
 /* Active/hover state styles */
