bfc583a4259da04722d933170f99023cc2b7d9c0
Yuri A Taranov <me@flanker72.ru>
2022-01-27 11:13:55 +0500

    feat: image component

---
+++ client-app/components/atoms/image/image.vue
@@ -0,0 +1,30 @@
+<template>
+  <img :src="src" :alt="alt" @error="setFallbackSrc" />
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  src: {
+    type: String,
+    required: true,
+  },
+
+  alt: {
+    type: String,
+    default: undefined,
+  },
+
+  fallbackSrc: {
+    type: String,
+    default: "/static/images/no-image.png",
+  },
+});
+
+/**
+ * Set fallback source when image loading error occures.
+ * @param event Generic event
+ */
+function setFallbackSrc(event: Event): void {
+  (event.target as HTMLImageElement).src = props.fallbackSrc;
+}
+</script>
