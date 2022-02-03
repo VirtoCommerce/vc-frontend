fe909e7726f82358b929cf270bbe7c7c8045036d
Yuri A Taranov <me@flanker72.ru>
2022-02-03 12:11:33 +0500

    fix: z-index on header was too low

--- client-app/shared/layout/components/header/header.vue
+++ client-app/shared/layout/components/header/header.vue
@@ -9,7 +9,7 @@
   </div>
 
   <!-- Mobile header -->
-  <div class="lg:hidden fixed z-10 bg-white w-full border-b shadow-md">
+  <div class="lg:hidden fixed z-40 bg-white w-full border-b shadow-md">
     <div class="px-6 flex justify-between items-center h-14">
       <router-link to="/"><VcImage src="/static/images/common/logo.svg" class="h-9" /></router-link>
       <i class="fas fa-bars text-2xl text-yellow-500" @click="mobileMenuVisible = true"></i>
