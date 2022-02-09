0c8d1189db73ef81e045f9f9eb8e5dabc25a6e90
basilkot <basilkot@gmail.com>
2022-02-09 19:15:01 +0200

    feat: fix style import

--- client-app/builder-preview.ts
+++ client-app/builder-preview.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "@fortawesome/fontawesome-free/css/all.css";
-import "../assets/static/styles/index.css";
+import "./assets/styles/index.css";
 
 import builderPreview from "./builder-preview/BuilderPreview";
 
