8882d51776d973c2cd3b81b9ce2c62441ef942bf
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2022-02-10 11:06:04 +0100

    feat: add order details page (#50)
    * feat: add order details page
    
    * fix: build problem
    
    * feat: create composable for userOrder
    
    * feat: add getDiscountAmmount function
    
    * feat: remove unused import
    
    * feat: add computed properties for addresses

--- client-app/core/api/graphql/account/index.ts
+++ client-app/core/api/graphql/account/index.ts
@@ -4,3 +4,4 @@ export { default as getMe } from "./queries/getMe";
 export { default as getMyAddresses } from "./queries/getMyAddresses";
 export { default as getDefaultShippingAddress } from "./queries/getDefaultShippingAddress";
 export { default as getMyOrders } from "./queries/getMyOrders";
+export { default as getMyOrder } from "./queries/getMyOrder";
