0e8762dea21e1d12f0652028c38a83bdb498d42a
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2022-02-18 09:23:35 +0100

    ST-903: Add vc-table component (#56)
    * feat: add vc-table component and use it everywhere
    
    * feat: add comment for workaround
    
    * feat: add navigation button for dashboard card

--- client-app/pages/account/orders.vue
+++ client-app/pages/account/orders.vue
@@ -13,48 +13,52 @@
             <h2 class="text-gray-800 text-3xl font-bold uppercase">Orders</h2>
           </div>
           <div class="flex flex-col bg-white shadow-sm" :class="{ 'rounded border': !isMobile }">
-            <!-- View Table -->
-            <!-- TODO: make a component to view the addresses table -->
-            <!-- Mobile table view -->
-            <template v-if="isMobile">
-              <div v-if="orders.length">
-                <TableMobileItem v-for="order in orders" :key="order.id" :item="order" class="overflow-auto cursor-pointer" @click="openOrderDetails(order.id)">
-                  <div class="grid grid-cols-2 p-6 gap-y-4 border-b border-gray-200">
-                    <div class="flex flex-col">
-                      <span class="text-sm text-gray-400"> Order number </span>
-                      <span class="pr-4 font-extrabold overflow-hidden overflow-ellipsis">
-                        {{ order.number }}
-                      </span>
-                    </div>
-
-                    <div class="flex flex-col justify-center items-end">
-                      <TableStatusBadge :status="order.status"></TableStatusBadge>
-                    </div>
-
-                    <div class="flex flex-col">
-                      <span class="text-sm text-gray-400">Date</span>
-                      <span class="overflow-hidden overflow-ellipsis">
-                        {{ moment(order?.createdDate).format("YYYY-MM-DD") }}
-                      </span>
-                    </div>
-
-                    <div class="flex flex-col">
-                      <span class="text-sm text-gray-400">Email</span>
-                      <span class="overflow-hidden overflow-ellipsis">{{ order.total?.formattedAmount }}</span>
-                    </div>
+            <VcTable
+              :loading="ordersLoading"
+              :columns="columns"
+              :items="orders"
+              :sort="sort"
+              :pages="pages"
+              :page="page"
+              @itemClick="openOrderDetails"
+              @headerClick="applySorting"
+              @pageChanged="onPageChange"
+            >
+              <template #mobile-item="itemData">
+                <div class="grid grid-cols-2 p-6 gap-y-4 border-b border-gray-200 cursor-pointer">
+                  <div class="flex flex-col">
+                    <span class="text-sm text-gray-400"> Order number </span>
+                    <span class="pr-4 font-extrabold overflow-hidden overflow-ellipsis">
+                      {{ itemData.item.number }}
+                    </span>
                   </div>
-                </TableMobileItem>
-              </div>
 
-              <div v-else-if="!ordersLoading" class="flex items-center justify-center space-x-10 p-5">
-                <img src="/static/images/account/icons/no-addresses.svg" alt="No orders" />
-                <div class="flex flex-col space-y-2">
-                  <span class="text-base">There are no orders yet</span>
+                  <div class="flex flex-col justify-center items-end">
+                    <TableStatusBadge :status="itemData.item.status"></TableStatusBadge>
+                  </div>
+
+                  <div class="flex flex-col">
+                    <span class="text-sm text-gray-400">Date</span>
+                    <span class="overflow-hidden overflow-ellipsis">
+                      {{ moment(itemData.item?.createdDate).format("YYYY-MM-DD") }}
+                    </span>
+                  </div>
+
+                  <div class="flex flex-col">
+                    <span class="text-sm text-gray-400">Total</span>
+                    <span class="overflow-hidden overflow-ellipsis">{{ itemData.item.total?.formattedAmount }}</span>
+                  </div>
                 </div>
-              </div>
-
-              <!-- Grid Skeleton -->
-              <div v-else>
+              </template>
+              <template #mobile-empty>
+                <div class="flex items-center justify-center space-x-10 p-5">
+                  <img src="/static/images/account/icons/no-addresses.svg" alt="No orders" />
+                  <div class="flex flex-col space-y-2">
+                    <span class="text-base">There are no orders yet</span>
+                  </div>
+                </div>
+              </template>
+              <template #mobile-skeleton>
                 <div v-for="i of itemsPerPage" :key="i" class="grid grid-cols-2 p-6 gap-y-4 border-b border-gray-200">
                   <div class="flex flex-col">
                     <span class="text-sm text-gray-400">Order number</span>
@@ -76,68 +80,13 @@
                     <div class="h-6 bg-gray-200 animate-pulse"></div>
                   </div>
                 </div>
-              </div>
-            </template>
-
-            <!-- Desktop table view -->
-            <table v-else class="table-fixed text-sm text-left w-full">
-              <thead class="border-b border-gray-200">
-                <tr>
-                  <th class="py-3 px-5 font-extrabold cursor-pointer" @click="applySorting('number')">
-                    Order number
-                    <i
-                      class="fas fa-caret-down ml-2"
-                      v-if="sort.column === 'number' && sort.direction === sortDescending"
-                    ></i>
-                    <i
-                      class="fas fa-caret-up ml-2"
-                      v-if="sort.column === 'number' && sort.direction === sortAscending"
-                    ></i>
-                  </th>
-                  <th class="py-3 px-5 font-extrabold">Purchase order</th>
-                  <th class="py-3 px-5 font-extrabold">Invoice</th>
-                  <th class="py-3 px-5 font-extrabold cursor-pointer" @click="applySorting('createdDate')">
-                    Date
-                    <i
-                      class="fas fa-caret-down ml-2"
-                      v-if="sort.column === 'createdDate' && sort.direction === sortDescending"
-                    ></i>
-                    <i
-                      class="fas fa-caret-up ml-2"
-                      v-if="sort.column === 'createdDate' && sort.direction === sortAscending"
-                    ></i>
-                  </th>
-                  <th class="py-3 px-5 font-extrabold text-center cursor-pointer" @click="applySorting('status')">
-                    Status
-                    <i
-                      class="fas fa-caret-down ml-2"
-                      v-if="sort.column === 'status' && sort.direction === sortDescending"
-                    ></i>
-                    <i
-                      class="fas fa-caret-up ml-2"
-                      v-if="sort.column === 'status' && sort.direction === sortAscending"
-                    ></i>
-                  </th>
-                  <th class="py-3 px-5 font-extrabold text-right cursor-pointer" @click="applySorting('total')">
-                    Total
-                    <i
-                      class="fas fa-caret-down ml-2"
-                      v-if="sort.column === 'total' && sort.direction === sortDescending"
-                    ></i>
-                    <i
-                      class="fas fa-caret-up ml-2"
-                      v-if="sort.column === 'total' && sort.direction === sortAscending"
-                    ></i>
-                  </th>
-                </tr>
-              </thead>
-
-              <tbody v-if="orders.length">
+              </template>
+              <template #desktop-body>
                 <tr
                   v-for="order in orders"
                   :key="order.id"
                   class="even:bg-gray-50 hover:bg-gray-200 cursor-pointer"
-                  @click="openOrderDetails(order.id)"
+                  @click="openOrderDetails(order)"
                 >
                   <td class="p-5 overflow-hidden overflow-ellipsis">
                     {{ order.number }}
@@ -154,9 +103,8 @@
                   </td>
                   <td class="p-5 overflow-hidden overflow-ellipsis text-right">{{ order.total?.formattedAmount }}</td>
                 </tr>
-              </tbody>
-
-              <tbody v-else-if="!ordersLoading">
+              </template>
+              <template #desktop-empty>
                 <!-- Workaround for using colspan -->
                 <tr>
                   <td></td>
@@ -176,10 +124,8 @@
                     </div>
                   </td>
                 </tr>
-              </tbody>
-
-              <!-- Table Skeleton -->
-              <tbody v-else>
+              </template>
+              <template #desktop-skeleton>
                 <tr v-for="i of itemsPerPage" :key="i" class="even:bg-gray-50">
                   <td class="p-5">
                     <div class="h-6 bg-gray-200 animate-pulse"></div>
@@ -200,17 +146,8 @@
                     <div class="h-6 bg-gray-200 animate-pulse"></div>
                   </td>
                 </tr>
-              </tbody>
-            </table>
-
-            <VcPagination
-              v-if="pages > 1"
-              v-model:page="page"
-              :pages="pages"
-              class="self-start"
-              :class="[isMobile ? 'px-6 py-10' : 'pb-5 px-5 mt-5']"
-              @update:page="loadOrders"
-            ></VcPagination>
+              </template>
+            </VcTable>
           </div>
         </div>
       </div>
@@ -219,14 +156,15 @@
 </template>
 
 <script setup lang="ts">
-import { TableMobileItem, TableStatusBadge, VcPagination } from "@/components";
+import { ITableColumn, TableStatusBadge, VcTable } from "@/components";
 import { AccountNavigation } from "@/shared/account";
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { sortAscending, sortDescending } from "@/core/constants";
 import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
 import useUserOrders from "@/shared/account/composables/useUserOrders";
 import moment from "moment";
 import { useRouter } from "vue-router";
+import { CustomerOrderType } from "@/core/api/graphql/types";
 
 const breakpoints = useBreakpoints(breakpointsTailwind);
 const { loading: ordersLoading, orders, loadOrders, sort, pages, itemsPerPage, page } = useUserOrders();
@@ -235,11 +173,16 @@ const isMobile = breakpoints.smaller("md");
 
 const router = useRouter();
 
-const openOrderDetails = (orderId: string) => {
-  router.push({ name: "OrderDetails", params: { id: orderId } });
+const openOrderDetails = (item: CustomerOrderType) => {
+  router.push({ name: "OrderDetails", params: { id: item.id } });
 };
 
-async function applySorting(column: string): Promise<void> {
+const onPageChange = async (newPage: number) => {
+  page.value = newPage;
+  await loadOrders();
+};
+
+const applySorting = async (column: string) => {
   if (sort.value.column === column) {
     sort.value.direction = sort.value.direction === sortDescending ? sortAscending : sortDescending;
   } else {
@@ -249,11 +192,44 @@ async function applySorting(column: string): Promise<void> {
 
   page.value = 1;
   await loadOrders();
-}
+};
 
 onMounted(async () => {
   await loadOrders();
 });
+
+const columns = ref<ITableColumn[]>([
+  {
+    id: "number",
+    title: "Order number",
+    sortable: true,
+  },
+  {
+    id: "purchaseOrder",
+    title: "Purchase order",
+  },
+  {
+    id: "invoice",
+    title: "Invoice",
+  },
+  {
+    id: "createdDate",
+    title: "Date",
+    sortable: true,
+  },
+  {
+    id: "status",
+    title: "Status",
+    sortable: true,
+    titlePosition: "text-center",
+  },
+  {
+    id: "total",
+    title: "Total",
+    sortable: true,
+    titlePosition: "text-right",
+  },
+]);
 </script>
 
 <style scoped>
