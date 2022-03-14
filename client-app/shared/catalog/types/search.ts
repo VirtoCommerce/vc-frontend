bd53467af14cb0541049afaa4f684a00aa6aead0
Artem Makarov <halfelkan@mail.ru>
2022-03-14 17:55:37 +0300

    refactor: removed "useProductsSearchParams"

--- client-app/shared/catalog/types/search.ts
+++ client-app/shared/catalog/types/search.ts
@@ -23,30 +23,3 @@ export type ProductsFilter = {
   type: "term" | "range";
   values: ProductsFilterValue[];
 };
-
-export type UrlParamKeys = {
-  [key in keyof Omit<
-    ProductsSearchParams,
-    "categoryId" | "fuzzy" | "fuzzyLevel" | "productIds"
-  > as `${key}Key`]?: string;
-};
-
-export type UseProductsSearchParamsOptions = {
-  sortList?: string[];
-  defaultSortBy?: string;
-  itemsPerPageList?: number[];
-  defaultItemsPerPage?: number;
-  urlParamKeys?: UrlParamKeys;
-};
-
-export type FromRouteQueryOptions = {
-  urlParamKeys: Required<UrlParamKeys>;
-  sortList?: string[];
-  defaultSortBy?: string;
-  itemsPerPageList?: number[];
-  defaultItemsPerPage?: number;
-};
-
-export type ToRouteQueryOptions = {
-  urlParamKeys: Required<UrlParamKeys>;
-};
