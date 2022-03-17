fceb7e64fdec1434054d654b38de4bfa7367d3dc
Yuri A Taranov <me@flanker72.ru>
2022-03-17 15:17:27 +0500

    chore: docs reference added

--- client-app/core/plugins/config.ts
+++ client-app/core/plugins/config.ts
@@ -5,6 +5,8 @@ export default {
   install: (app: App, options: IThemeConfigPreset) => {
     app.config.globalProperties.$cfg = options;
     app.provide("cfg", options);
+
+    // Set CSS variables to use as TailwindCSS arbitrary values: https://tailwindcss.com/docs/adding-custom-styles#using-arbitrary-values
     Object.entries(options)
       .filter(([key]) => /^color/.test(key))
       .forEach(([key, value]) => {
