ca601ca8dba3983f270da2e716e4d3517efc72c5
Anton Veretennikov <superiorboroda@gmail.com>
2022-03-17 16:17:01 +0300

    feat: localize home page

--- client-app/pages/home/home.vue
+++ client-app/pages/home/home.vue
@@ -3,21 +3,24 @@
   <div class="banner flex items-center">
     <div class="container mx-auto flex flex-col lg:flex-row items-center space-y-10 lg:space-x-24 p-6 md:p-12">
       <div v-if="!isAuthenticated" class="w-full lg:w-2/5 bg-white shadow-lg rounded p-6 md:p-10">
-        <h1 class="uppercase tracking-wide text-3xl lg:text-4xl font-bold mb-8">Sign in</h1>
+        <h1
+          class="uppercase tracking-wide text-3xl lg:text-4xl font-bold mb-8"
+          v-t="'pages.home.sign_in_form_title'"
+        ></h1>
         <SignInForm :grow-buttons="true" @succeeded="onSignIn" />
       </div>
       <div
         class="w-full lg:w-3/5 text-white font-bold filter drop-shadow-lg text-center lg:text-left text-3xl md:text-5xl select-none"
       >
-        <div class="uppercase mb-8 leading-tight">We can help you<br />to make the right<br />and wise decisions</div>
+        <div class="uppercase mb-8 leading-tight" v-html="$t('pages.home.main_banner_block.message')"></div>
         <div
           class="flex flex-col md:flex-row items-center justify-center lg:justify-start space-y-2 md:space-y-0 md:space-x-7 text-xl md:text-2xl"
         >
-          <div>Result oriented</div>
+          <div v-t="'pages.home.main_banner_block.key_feature_1'"></div>
           <div class="w-2.5 h-2.5 bg-[color:var(--color-primary)] rounded-full"></div>
-          <div>Trustworthy</div>
+          <div v-t="'pages.home.main_banner_block.key_feature_2'"></div>
           <div class="w-2.5 h-2.5 bg-[color:var(--color-primary)] rounded-full"></div>
-          <div>Reliable</div>
+          <div v-t="'pages.home.main_banner_block.key_feature_3'"></div>
         </div>
       </div>
     </div>
@@ -26,25 +29,35 @@
   <!-- Main content -->
   <div class="main">
     <div class="container mx-auto pt-32 pb-40 px-6 md:px-12">
-      <div class="font-bold text-3xl text-center px-6">Professional purchases have never been that easy</div>
+      <div class="font-bold text-3xl text-center px-6" v-t="'pages.home.feature_descriptions_block.title'"></div>
       <div class="flex flex-wrap justify-between mx-auto mt-24">
         <div class="w-full md:w-1/2 lg:w-1/4 md:mt-44 mb-24 md:mb-0">
           <VcImage src="/static/images/home/hexa-1.webp" class="mx-auto w-full" />
-          <div class="px-8 font-bold text-2xl md:text-xl text-center mt-5">Corporate account management</div>
+          <div
+            class="px-8 font-bold text-2xl md:text-xl text-center mt-5"
+            v-t="'pages.home.feature_descriptions_block.feature_1'"
+          ></div>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/4 mb-24 md:mb-0">
           <VcImage src="/static/images/home/hexa-2.webp" class="mx-auto w-full" />
-          <div class="px-8 font-bold text-2xl md:text-xl text-center mt-5">Quote management price negotiation</div>
+          <div
+            class="px-8 font-bold text-2xl md:text-xl text-center mt-5"
+            v-t="'pages.home.feature_descriptions_block.feature_2'"
+          ></div>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/4 md:mt-44 mb-24 md:mb-0">
           <VcImage src="/static/images/home/hexa-3.webp" class="mx-auto w-full" />
-          <div class="px-8 font-bold text-2xl md:text-xl text-center mt-5">Multiple stores, channels and regions</div>
+          <div
+            class="px-8 font-bold text-2xl md:text-xl text-center mt-5"
+            v-t="'pages.home.feature_descriptions_block.feature_3'"
+          ></div>
         </div>
         <div class="w-full md:w-1/2 lg:w-1/4 mb-24 md:mb-0">
           <VcImage src="/static/images/home/hexa-4.webp" class="mx-auto w-full" />
-          <div class="px-8 font-bold text-2xl md:text-xl text-center mt-5">
-            B2B personalization and price management
-          </div>
+          <div
+            class="px-8 font-bold text-2xl md:text-xl text-center mt-5"
+            v-t="'pages.home.feature_descriptions_block.feature_4'"
+          ></div>
         </div>
       </div>
     </div>
@@ -53,19 +66,21 @@
   <!-- CTA -->
   <div class="py-6 lg:py-10 bg-[color:var(--color-primary)]">
     <div class="container mx-auto flex flex-col lg:flex-row items-center space-y-2 lg:space-x-10 px-6 md:px-12">
-      <div class="uppercase font-extrabold text-3xl text-white whitespace-nowrap">subscribe now!</div>
-      <div class="text-base font-medium leading-tight text-white max-w-max lg:max-w-min">
-        Your&nbsp;connection&nbsp;to&nbsp;what's new&nbsp;at&nbsp;B2B-store.
-      </div>
+      <div
+        class="uppercase font-extrabold text-3xl text-white whitespace-nowrap"
+        v-t="'pages.home.subscription_block.subscribe_now'"
+      ></div>
+      <div class="text-base font-medium leading-tight text-white max-w-max lg:max-w-min"></div>
       <div class="flex flex-grow w-full space-x-6">
         <input
           class="bg-white rounded h-11 flex-grow shadow-inner px-4 min-w-0 outline-none"
-          placeholder="Enter E-mail"
+          :placeholder="$t('pages.home.subscription_block.email_placeholder')"
         />
         <!-- todo: use VcButton -->
-        <button class="bg-white hover:bg-gray-200 px-6 uppercase text-sm font-roboto font-bold h-11 rounded shadow-md">
-          Subscribe
-        </button>
+        <button
+          class="bg-white hover:bg-gray-200 px-6 uppercase text-sm font-roboto font-bold h-11 rounded shadow-md"
+          v-t="'pages.home.subscription_block.subscribe_button'"
+        ></button>
       </div>
     </div>
   </div>
