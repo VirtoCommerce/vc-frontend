dc575eb4a8b4fa29a5f98fb2161ff6cfeed10f0f
Anton Veretennikov <superiorboroda@gmail.com>
2022-03-18 19:31:26 +0300

    fix: add removed files

---
+++ client-app/shared/checkout/components/add-or-update-address-dialog.vue
@@ -0,0 +1,70 @@
+<template>
+  <VcPopup title="Select address" modal-width="max-w-5xl" :hide-actions="true">
+    <template #default="{ close }">
+      <AddressForm
+        :model-value="editableAddress"
+        :countries="countries"
+        class="px-6 py-4"
+        required-email
+        required-city
+        @save="saveAddress"
+      >
+        <template #append="{ dirty }">
+          <div class="flex space-x-4 pb-3 pt-7 sm:pb-4 sm:pt-4 sm:float-right">
+            <VcButton
+              kind="secondary"
+              size="md"
+              class="uppercase w-1/2 sm:px-5"
+              is-outline
+              @click="
+                editableAddress = null;
+                close();
+              "
+            >
+              Cancel
+            </VcButton>
+
+            <VcButton size="md" :is-disabled="!dirty" class="uppercase w-1/2 sm:px-5" is-submit>
+              {{ editableAddress ? "Save" : "Create" }}
+            </VcButton>
+          </div>
+        </template>
+      </AddressForm>
+    </template>
+  </VcPopup>
+</template>
+
+<script setup lang="ts">
+import { MemberAddressType } from "@/core/api/graphql/types";
+import { useCountries } from "@/core/composables";
+import { AddressForm } from "@/shared/account";
+import { onMounted, PropType, ref, Ref, watchEffect } from "vue";
+import { VcPopup, VcButton } from "@/components";
+import { clone } from "lodash";
+
+const props = defineProps({
+  address: {
+    type: Object as PropType<MemberAddressType>,
+    default: null,
+  },
+});
+
+const { countries, loadCountries } = useCountries();
+const editableAddress: Ref<MemberAddressType | null> = ref(null);
+
+const emit = defineEmits(["result"]);
+
+onMounted(async () => {
+  if (!countries.value.length) {
+    await loadCountries();
+  }
+});
+
+function saveAddress(address: MemberAddressType) {
+  emit("result", address);
+}
+
+watchEffect(() => {
+  editableAddress.value = clone(props.address);
+});
+</script>
