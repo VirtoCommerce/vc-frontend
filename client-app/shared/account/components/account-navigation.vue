fea20184e97cfc8e215edc95b762c02e40923b7e
Anton Veretennikov <superiorboroda@gmail.com>
2022-03-21 13:09:01 +0300

    fix: fix localization bugs

--- client-app/shared/account/components/account-navigation.vue
+++ client-app/shared/account/components/account-navigation.vue
@@ -3,32 +3,32 @@
     <div class="flex flex-col">
       <AccountNavigationLink
         to="/account/dashboard"
-        :text="$t('shared.account.navigation.dashboard')"
+        :text="$t('shared.account.navigation.links.dashboard')"
         class="dashboard-icon"
       ></AccountNavigationLink>
       <AccountNavigationLink
         to="/account/profile"
-        :text="$t('shared.account.navigation.profile')"
+        :text="$t('shared.account.navigation.links.profile')"
         class="profile-icon"
       ></AccountNavigationLink>
       <AccountNavigationLink
         to="/account/addresses"
-        :text="$t('shared.account.navigation.addresses')"
+        :text="$t('shared.account.navigation.links.addresses')"
         class="addresses-icon"
       ></AccountNavigationLink>
       <AccountNavigationLink
         to="/account/orders"
-        :text="$t('shared.account.navigation.orders')"
+        :text="$t('shared.account.navigation.links.orders')"
         class="orders-icon"
       ></AccountNavigationLink>
       <AccountNavigationLink
         to="/list"
-        :text="$t('shared.account.navigation.your_list')"
+        :text="$t('shared.account.navigation.links.your_list')"
         class="list-icon"
       ></AccountNavigationLink>
       <AccountNavigationLink
         to="/account/checkout-defaults"
-        :text="$t('shared.account.navigation.checkout_defaults')"
+        :text="$t('shared.account.navigation.links.checkout_defaults')"
         class="checkout-icon"
       ></AccountNavigationLink>
     </div>
