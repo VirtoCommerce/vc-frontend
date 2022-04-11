19daa199644446f84a35ab91b912faa6b131fd59
Artem Makarov <halfelkan@mail.ru>
2022-04-11 10:10:40 +0300

    ST-893: Displaying currently applied filters on the category page (#104)
    * feat: added VcChip component
    
    * feat(ST-893): displaying currently applied filters on category page

--- client-app/pages/_ui-kit/ui-kit.vue
+++ client-app/pages/_ui-kit/ui-kit.vue
@@ -10,22 +10,22 @@
           <div class="space-y-4">
             <div class="flex flex-wrap items-center gap-4">
               <VcButton size="sm" class="uppercase px-4" autofocus>primary</VcButton>
-              <VcButton kind="secondary" size="sm" class="uppercase px-4">secondary</VcButton>
-              <VcButton kind="success" size="sm" class="uppercase px-4">success</VcButton>
-              <VcButton kind="warning" size="sm" class="uppercase px-4">warning</VcButton>
-              <VcButton kind="danger" size="sm" class="uppercase px-4">danger</VcButton>
-              <VcButton size="sm" class="uppercase px-4" is-disabled>disabled</VcButton>
-              <VcButton size="sm" class="uppercase px-4" is-waiting>loading</VcButton>
+              <VcButton size="sm" kind="secondary" class="uppercase px-4">secondary</VcButton>
+              <VcButton size="sm" kind="success" class="uppercase px-4">success</VcButton>
+              <VcButton size="sm" kind="warning" class="uppercase px-4">warning</VcButton>
+              <VcButton size="sm" kind="danger" class="uppercase px-4">danger</VcButton>
+              <VcButton size="sm" kind="danger" class="uppercase px-4" is-disabled>disabled</VcButton>
+              <VcButton size="sm" kind="danger" class="uppercase px-4" is-waiting>loading</VcButton>
             </div>
 
             <div class="flex flex-wrap items-center gap-4">
               <VcButton size="sm" class="uppercase px-4" is-outline>primary</VcButton>
-              <VcButton kind="secondary" size="sm" class="uppercase px-4" is-outline>secondary</VcButton>
-              <VcButton kind="success" size="sm" class="uppercase px-4" is-outline>success</VcButton>
-              <VcButton kind="warning" size="sm" class="uppercase px-4" is-outline>warning</VcButton>
-              <VcButton kind="danger" size="sm" class="uppercase px-4" is-outline>danger</VcButton>
-              <VcButton size="sm" class="uppercase px-4" is-disabled is-outline>disabled</VcButton>
-              <VcButton size="sm" class="uppercase px-4" is-waiting is-outline>loading</VcButton>
+              <VcButton size="sm" kind="secondary" class="uppercase px-4" is-outline>secondary</VcButton>
+              <VcButton size="sm" kind="success" class="uppercase px-4" is-outline>success</VcButton>
+              <VcButton size="sm" kind="warning" class="uppercase px-4" is-outline>warning</VcButton>
+              <VcButton size="sm" kind="danger" class="uppercase px-4" is-outline>danger</VcButton>
+              <VcButton size="sm" kind="danger" class="uppercase px-4" is-disabled is-outline>disabled</VcButton>
+              <VcButton size="sm" kind="danger" class="uppercase px-4" is-waiting is-outline>loading</VcButton>
             </div>
           </div>
 
@@ -37,8 +37,8 @@
               <VcButton kind="success" class="uppercase px-4">success</VcButton>
               <VcButton kind="warning" class="uppercase px-4">warning</VcButton>
               <VcButton kind="danger" class="uppercase px-4">danger</VcButton>
-              <VcButton class="uppercase px-4" is-disabled>disabled</VcButton>
-              <VcButton class="uppercase px-4" is-waiting>loading</VcButton>
+              <VcButton kind="danger" class="uppercase px-4" is-disabled>disabled</VcButton>
+              <VcButton kind="danger" class="uppercase px-4" is-waiting>loading</VcButton>
             </div>
 
             <div class="flex flex-wrap items-center gap-4">
@@ -47,8 +47,8 @@
               <VcButton kind="success" class="uppercase px-4" is-outline>success</VcButton>
               <VcButton kind="warning" class="uppercase px-4" is-outline>warning</VcButton>
               <VcButton kind="danger" class="uppercase px-4" is-outline>danger</VcButton>
-              <VcButton class="uppercase px-4" is-disabled is-outline>disabled</VcButton>
-              <VcButton class="uppercase px-4" is-waiting is-outline>loading</VcButton>
+              <VcButton kind="danger" class="uppercase px-4" is-disabled is-outline>disabled</VcButton>
+              <VcButton kind="danger" class="uppercase px-4" is-waiting is-outline>loading</VcButton>
             </div>
           </div>
 
@@ -56,22 +56,50 @@
           <div class="space-y-4">
             <div class="flex flex-wrap items-center gap-4">
               <VcButton size="lg" class="uppercase px-4">primary</VcButton>
-              <VcButton kind="secondary" size="lg" class="uppercase px-4">secondary</VcButton>
-              <VcButton kind="success" size="lg" class="uppercase px-4">success</VcButton>
-              <VcButton kind="warning" size="lg" class="uppercase px-4">warning</VcButton>
-              <VcButton kind="danger" size="lg" class="uppercase px-4">danger</VcButton>
-              <VcButton size="lg" class="uppercase px-4" is-disabled>disabled</VcButton>
-              <VcButton size="lg" class="uppercase px-4" is-waiting>loading</VcButton>
+              <VcButton size="lg" kind="secondary" class="uppercase px-4">secondary</VcButton>
+              <VcButton size="lg" kind="success" class="uppercase px-4">success</VcButton>
+              <VcButton size="lg" kind="warning" class="uppercase px-4">warning</VcButton>
+              <VcButton size="lg" kind="danger" class="uppercase px-4">danger</VcButton>
+              <VcButton size="lg" kind="danger" class="uppercase px-4" is-disabled>disabled</VcButton>
+              <VcButton size="lg" kind="danger" class="uppercase px-4" is-waiting>loading</VcButton>
             </div>
 
             <div class="flex flex-wrap items-center gap-4">
               <VcButton size="lg" class="uppercase px-4" is-outline>primary</VcButton>
-              <VcButton kind="secondary" size="lg" class="uppercase px-4" is-outline>secondary</VcButton>
-              <VcButton kind="success" size="lg" class="uppercase px-4" is-outline>success</VcButton>
-              <VcButton kind="warning" size="lg" class="uppercase px-4" is-outline>warning</VcButton>
-              <VcButton kind="danger" size="lg" class="uppercase px-4" is-outline>danger</VcButton>
-              <VcButton size="lg" class="uppercase px-4" is-disabled is-outline>disabled</VcButton>
-              <VcButton size="lg" class="uppercase px-4" is-waiting is-outline>loading</VcButton>
+              <VcButton size="lg" kind="secondary" class="uppercase px-4" is-outline>secondary</VcButton>
+              <VcButton size="lg" kind="success" class="uppercase px-4" is-outline>success</VcButton>
+              <VcButton size="lg" kind="warning" class="uppercase px-4" is-outline>warning</VcButton>
+              <VcButton size="lg" kind="danger" class="uppercase px-4" is-outline>danger</VcButton>
+              <VcButton size="lg" kind="danger" class="uppercase px-4" is-disabled is-outline>disabled</VcButton>
+              <VcButton size="lg" kind="danger" class="uppercase px-4" is-waiting is-outline>loading</VcButton>
+            </div>
+          </div>
+
+          <!-- Custom colors -->
+          <div class="space-y-4">
+            <div class="flex flex-wrap items-center gap-4">
+              <VcButton class="uppercase px-4 [--color-primary:blue] [--color-primary-hover:green]">
+                Custom color
+              </VcButton>
+
+              <VcButton class="uppercase px-4 [--color-primary:blue] [--color-primary-hover:green]" is-outline>
+                Custom color
+              </VcButton>
+
+              <VcButton
+                class="uppercase px-4 [--color-primary:mediumaquamarine] [--color-primary-hover:teal] !text-black hover:!text-gray-50"
+              >
+                Custom color
+              </VcButton>
+
+              <VcButton
+                class="uppercase px-4 [--color-primary:mediumaquamarine] [--color-primary-hover:teal] !text-black hover:!text-red-700 hover:!bg-transparent"
+                is-outline
+              >
+                Custom color
+              </VcButton>
+
+              <span>— &nbsp; Custom colors</span>
             </div>
           </div>
 
@@ -104,10 +132,102 @@
         </div>
       </section>
       <!-- endregion Buttons -->
+
+      <!-- region Chips -->
+      <section class="bg-white md:rounded px-6 pt-5 pb-6">
+        <header class="text-2xl text-gray-600 font-bold font-roboto mb-5">Chips</header>
+
+        <div class="space-y-8">
+          <!-- Small -->
+          <div class="space-y-4">
+            <div class="flex flex-wrap items-center gap-4">
+              <VcChip size="sm" clickable>Primary Clickable</VcChip>
+              <VcChip size="sm" variant="secondary" is-draggable>Secondary Draggable</VcChip>
+              <VcChip size="sm" variant="success" closable @close="alert('Chip closing event')">
+                Success Closable
+              </VcChip>
+              <VcChip size="sm" variant="warning">Warning</VcChip>
+              <VcChip size="sm" variant="danger">Danger</VcChip>
+              <VcChip size="sm" variant="danger" is-disabled>Disabled</VcChip>
+              <VcChip size="sm" variant="danger" is-disabled closable>Disabled Closable</VcChip>
+            </div>
+
+            <div class="flex flex-wrap items-center gap-4">
+              <VcChip size="sm" is-outline clickable>Primary Clickable</VcChip>
+              <VcChip size="sm" variant="secondary" is-outline is-draggable>Secondary Draggable</VcChip>
+              <VcChip size="sm" variant="success" is-outline closable @close="alert('Chip closing event')">
+                Success Closable
+              </VcChip>
+              <VcChip size="sm" variant="warning" is-outline>Warning</VcChip>
+              <VcChip size="sm" variant="danger" is-outline>Danger</VcChip>
+              <VcChip size="sm" variant="danger" is-disabled is-outline>Disabled</VcChip>
+              <VcChip size="sm" variant="danger" is-disabled is-outline closable>Disabled Closable</VcChip>
+            </div>
+          </div>
+
+          <!-- Medium -->
+          <div class="space-y-4">
+            <div class="flex flex-wrap items-center gap-4">
+              <VcChip clickable>Primary Clickable</VcChip>
+              <VcChip variant="secondary" is-draggable>Secondary Draggable</VcChip>
+              <VcChip variant="success" closable @close="alert('Chip closing event')"> Success Closable</VcChip>
+              <VcChip variant="warning">Warning</VcChip>
+              <VcChip variant="danger">Danger</VcChip>
+              <VcChip variant="danger" is-disabled>Disabled</VcChip>
+              <VcChip variant="danger" is-disabled closable>Disabled Closable</VcChip>
+            </div>
+
+            <div class="flex flex-wrap items-center gap-4">
+              <VcChip is-outline clickable>Primary Clickable</VcChip>
+              <VcChip variant="secondary" is-outline is-draggable>Secondary Draggable</VcChip>
+              <VcChip variant="success" is-outline closable @close="alert('Chip closing event')">
+                Success Closable
+              </VcChip>
+              <VcChip variant="warning" is-outline>Warning</VcChip>
+              <VcChip variant="danger" is-outline>Danger</VcChip>
+              <VcChip variant="danger" is-disabled is-outline>Disabled</VcChip>
+              <VcChip variant="danger" is-disabled is-outline closable>Disabled Closable</VcChip>
+            </div>
+          </div>
+
+          <!-- Large -->
+          <div class="space-y-4">
+            <div class="flex flex-wrap items-center gap-4">
+              <VcChip size="lg" clickable>Primary Clickable</VcChip>
+              <VcChip size="lg" variant="secondary" is-draggable>Secondary Draggable</VcChip>
+              <VcChip size="lg" variant="success" closable @close="alert('Chip closing event')">
+                Success Closable
+              </VcChip>
+              <VcChip size="lg" variant="warning">Warning</VcChip>
+              <VcChip size="lg" variant="danger">Danger</VcChip>
+              <VcChip size="lg" variant="danger" is-disabled>Disabled</VcChip>
+              <VcChip size="lg" variant="danger" is-disabled closable>Disabled Closable</VcChip>
+            </div>
+
+            <div class="flex flex-wrap items-center gap-4">
+              <VcChip size="lg" is-outline clickable>Primary Clickable</VcChip>
+              <VcChip size="lg" variant="secondary" is-outline is-draggable>Secondary Draggable</VcChip>
+              <VcChip size="lg" variant="success" is-outline closable @close="alert('Chip closing event')">
+                Success Closable
+              </VcChip>
+              <VcChip size="lg" variant="warning" is-outline>Warning</VcChip>
+              <VcChip size="lg" variant="danger" is-outline>Danger</VcChip>
+              <VcChip size="lg" variant="danger" is-disabled is-outline>Disabled</VcChip>
+              <VcChip size="lg" variant="danger" is-disabled is-outline closable>Disabled Closable</VcChip>
+            </div>
+          </div>
+          <!-- -->
+        </div>
+      </section>
+      <!-- endregion Chips -->
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { VcButton } from "@/components";
+import { VcButton, VcChip } from "@/components";
+
+function alert(message?: any) {
+  window.alert(message);
+}
 </script>
