c54bd318ff166ee3ebff7fef067e3d0d8c6a14a5
Anton Veretennikov <superiorboroda@gmail.com>
2022-04-13 14:21:56 +0300

    fix: fix all-products menu initiations

--- client-app/core/utilities/menu/index.ts
+++ client-app/core/utilities/menu/index.ts
@@ -4,6 +4,6 @@ import { MenuLinkType } from "@/core/api/graphql/types";
 export const menu: Record<string, MenuLinkType[]> = {};
 
 export async function initMenu(cultureName: string): Promise<Record<string, MenuLinkType[]>> {
-  (await getMenus(cultureName)).forEach((item) => (menu[`${item.name}`] = item.items || []));
+  (await getMenus({ cultureName })).forEach((item) => (menu[`${item.name}`] = item.items || []));
   return menu;
 }
