8ff428f336b2c3392f124f1769a1584a733fe8c1
Anton Veretennikov <superiorboroda@gmail.com>
2022-04-13 10:03:33 +0300

    fix: fix duplicated mobile menu button. update de localization to actual

--- client-app/shared/layout/components/header/_internal/mobile-menu.vue
+++ client-app/shared/layout/components/header/_internal/mobile-menu.vue
@@ -9,10 +9,6 @@
       <!-- Language block -->
       <LanguageSelector v-if="$context.availLanguages && $context.availLanguages.length > 1" />
       <i class="fas fa-times text-2xl text-[color:var(--color-primary)]" @click="$emit('close')"></i>
-
-      <button class="appearance-none py-2 px-4 -mr-4" @click="$emit('close')">
-        <i class="fas fa-times text-2xl text-[color:var(--color-primary)]" />
-      </button>
     </header>
 
     <!-- region Children links section -->
@@ -185,10 +181,9 @@ import { useI18n } from "vue-i18n";
 import { VcImage, VcRadioButton } from "@/components";
 import { useCart } from "@/shared/cart";
 import { useUser } from "@/shared/account";
-import { LanguageSelector } from "@/shared/layout";
+import { LanguageSelector, MenuLink, useNavigations } from "@/shared/layout";
 import { useCompareProducts } from "@/shared/compare";
 import { useCurrency } from "@core/composables";
-import { MenuLink, useNavigations } from "@/shared/layout";
 import MobileMenuLink from "./mobile-menu-link.vue";
 
 defineEmits(["close"]);
