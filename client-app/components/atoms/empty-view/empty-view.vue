aaca38fc5d229ec96708ffeba8f49d99a588bd52
Ilya <ilya.ivanov6@gmail.com>
2022-05-06 12:42:00 +0200

    fix: style fixes for empty-view component

--- client-app/components/atoms/empty-view/empty-view.vue
+++ client-app/components/atoms/empty-view/empty-view.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="flex grow flex-col items-center justify-center space-y-5" :class="{ 'h-96': isMobile }">
     <slot name="icon"></slot>
-    <p class="text-xl">
-      {{ text }}
-    </p>
+    <slot name="text">
+      <p class="text-xl">
+        {{ text }}
+      </p>
+    </slot>
     <slot name="button"></slot>
   </div>
 </template>
