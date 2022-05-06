1ef16223e8a564616685c3be269aa7651c0c33dd
Ilya <ilya.ivanov6@gmail.com>
2022-05-06 12:37:23 +0200

    fix: class style usage

--- client-app/components/atoms/empty-view/empty-view.vue
+++ client-app/components/atoms/empty-view/empty-view.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex grow flex-col items-center justify-center space-y-5" :class="isMobile && 'h-96'">
+  <div class="flex grow flex-col items-center justify-center space-y-5" :class="{ 'h-96': isMobile }">
     <slot name="icon"></slot>
     <p class="text-xl">
       {{ text }}
