7f0dd5c27d9a91228ee44617c1bf8e01686b703b
Yuri A Taranov <me@flanker72.ru>
2022-05-11 16:31:44 +0500

    chore: vc-image component

--- client-app/components/atoms/image/image.vue
+++ client-app/components/atoms/image/image.vue
@@ -1,30 +1,70 @@
 <template>
-  <img :src="preparedSrc" :alt="alt" :loading="lazy ? 'lazy' : null" :class="additionalClass" @error="setFallbackSrc" />
+  <img
+    :src="preparedSrc"
+    :alt="alt"
+    :loading="lazy || isLazy ? 'lazy' : null"
+    :class="[$attrs.class, { [fallbackClass]: preparedSrc === fallbackSrc }]"
+    @click="!isDisabled && $emit('click', $event)"
+    @error="setFallbackSrc"
+  />
 </template>
 
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import { computed } from "vue";
 import { appendSuffixToFilename } from "@core/utilities";
 
 const props = defineProps({
-  lazy: Boolean,
-
+  /**
+   * Image source.
+   */
   src: {
     type: String,
-    default: "",
+    required: true,
   },
 
-  alt: {
-    type: String,
-    default: null,
-  },
-
+  /**
+   * Image fallback source.
+   */
   fallbackSrc: {
     type: String,
     default: "/static/images/common/no-image.svg",
   },
 
   /**
+   * Additional classes added to fallback image.
+   */
+  fallbackClass: {
+    type: String,
+    default: "!object-scale-down !object-center",
+  },
+
+  /**
+   * Image alt text.
+   */
+  alt: {
+    type: String,
+    default: undefined,
+  },
+
+  /**
+   * Image lazy loading.
+   * @deprecated see isLazy property.
+   */
+  lazy: {
+    type: Boolean,
+    default: false,
+  },
+
+  /**
+   * Image lazy loading.
+   */
+  isLazy: {
+    type: Boolean,
+    default: false,
+  },
+
+  /**
+   * Image size passed to previewer service.
    * First you need to generate thumbnails in admin panel, section "Thumbnails" (vc-module-image-tools).
    * You can also set up suffixes there.
    * @see https://github.com/VirtoCommerce/vc-module-image-tools
@@ -32,10 +72,19 @@ const props = defineProps({
   sizeSuffix: {
     type: String,
     validator: (value: string) => ["sm", "md", "lg"].includes(value),
+    default: undefined,
+  },
+
+  /**
+   * Image disabled state. Prevents click event emission.
+   */
+  isDisabled: {
+    type: Boolean,
+    default: false,
   },
 });
 
-const additionalClass = ref("");
+const emit = defineEmits(["click", "error"]);
 
 const preparedSrc = computed<string>(() => {
   const result = props.sizeSuffix ? appendSuffixToFilename(props.src, `_${props.sizeSuffix}`) : props.src;
@@ -44,12 +93,18 @@ const preparedSrc = computed<string>(() => {
 
 /**
  * Set fallback source when image loading error occures.
- * @param event Generic event
+ * @param event Generic event.
  */
 function setFallbackSrc(event: Event): void {
-  (event.target as HTMLImageElement).src = props.fallbackSrc;
-  additionalClass.value = "object-scale-down object-center";
+  if ((event.target as HTMLImageElement).src !== props.fallbackSrc) {
+    (event.target as HTMLImageElement).src = props.fallbackSrc;
+    emit("error", event);
+  }
 }
+</script>
 
-watch(preparedSrc, () => (additionalClass.value = ""));
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+};
 </script>
