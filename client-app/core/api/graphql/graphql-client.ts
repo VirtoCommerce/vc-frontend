2084e0ea4665019b62b53123d43a76537bb27613
Anton Veretennikov <superiorboroda@gmail.com>
2022-05-19 14:20:29 +0300

    feat: use onError handler to check every response

--- client-app/core/api/graphql/graphql-client.ts
+++ client-app/core/api/graphql/graphql-client.ts
@@ -1,11 +1,23 @@
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
+import { onError } from "apollo-link-error";
 const fetchPolicy = "no-cache";
+
+const httpLink = new HttpLink({ uri: `/xapi/graphql` });
+
+const errorHandler = onError(({ networkError, response }) => {
+  if (networkError) {
+    // if (networkError.statusCode === 401) location.href = '/auth/login'
+    // if (networkError.statusCode === 403 && networkError.result.message === 'ACCOUNT_SUSPENDED' ) location.href = '/suspended'
+    console.log(response);
+    console.log(networkError);
+  }
+});
 
 const graphqlClient = new ApolloClient({
   // Provide required constructor fields
-  link: new HttpLink({ uri: `/xapi/graphql` }),
+  link: httpLink,
   cache: new InMemoryCache({
     freezeResults: true,
     addTypename: false,
