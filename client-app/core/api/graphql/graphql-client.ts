5c3ead4917462d6c02d0de26e460cba776b0ef56
Anton Veretennikov <superiorboroda@gmail.com>
2022-05-19 15:37:11 +0300

    feat: redirect to Home if unauth

--- client-app/core/api/graphql/graphql-client.ts
+++ client-app/core/api/graphql/graphql-client.ts
@@ -2,22 +2,24 @@ import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloClient } from "apollo-client";
 import { HttpLink } from "apollo-link-http";
 import { onError } from "apollo-link-error";
+import _ from "lodash";
+
 const fetchPolicy = "no-cache";
 
 const httpLink = new HttpLink({ uri: `/xapi/graphql` });
 
-const errorHandler = onError(({ networkError, response }) => {
-  if (networkError) {
-    // if (networkError.statusCode === 401) location.href = '/auth/login'
-    // if (networkError.statusCode === 403 && networkError.result.message === 'ACCOUNT_SUSPENDED' ) location.href = '/suspended'
-    console.log(response);
-    console.log(networkError);
+const errorHandler = onError(({ graphQLErrors }) => {
+  if (graphQLErrors && graphQLErrors.length) {
+    const unauthorizedError = _.find(graphQLErrors, (x) => x.extensions?.code === "Unauthorized");
+    if (unauthorizedError) {
+      location.href = "/";
+    }
   }
 });
 
 const graphqlClient = new ApolloClient({
   // Provide required constructor fields
-  link: httpLink,
+  link: errorHandler.concat(httpLink),
   cache: new InMemoryCache({
     freezeResults: true,
     addTypename: false,
