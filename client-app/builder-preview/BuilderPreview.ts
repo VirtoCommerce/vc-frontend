aadd5a52d306bdff8f13d8fed0462b29c2eac71d
basilkot <basilkot@gmail.com>
2022-05-26 16:10:31 +0200

    feat: remove comments

--- client-app/builder-preview/BuilderPreview.ts
+++ client-app/builder-preview/BuilderPreview.ts
@@ -1,32 +1,17 @@
 import { App } from "vue";
-import Vue from "vue";
-import { useRouter } from "vue-router";
-
 import { useTemplate } from "./pages/useTemplate";
-
-// const element: any = document?.getElementById('app');
-// const app: any = element?.['__vue_app__'];
-
-// if (app) {
-//   app.use({
-//     install: function(Vue: any, options: any) {
-
-//     }
-//   });
-// }
-
 
 export default {
   install: (app: App, options: any) => {
     console.log("install buidler preview plugin");
     console.log(app);
-    options.router.push('/demo-page');
+    options.router.push("/demo-page");
 
     window.addEventListener("message", (event: MessageEvent) => {
-      if (event.data.source === 'builder') {
+      if (event.data.source === "builder") {
         console.log("received message from template builder", event);
-        if (event.data.type === 'changed') {
-          const { template } = useTemplate('');
+        if (event.data.type === "changed") {
+          const { template } = useTemplate("");
           const source = template.value;
           const index = source.content.findIndex((x: any) => x.id === event.data.model.id);
           if (index !== -1) {
@@ -34,14 +19,10 @@ export default {
             const newSection = { ...section, ...event.data.model };
             template.value = {
               ...source,
-              content: [
-                ...source.content.slice(0, index),
-                newSection,
-                ...source.content.slice(index + 1)
-              ]
+              content: [...source.content.slice(0, index), newSection, ...source.content.slice(index + 1)],
             };
           }
-        } else if (event.data.type === 'navigate') {
+        } else if (event.data.type === "navigate") {
           console.log("navigate to", event.data.url);
           options.router.push(event.data.url);
         }
