26d2595953c6c8190f412afa48524b0e7392bd4f
basilkot <basilkot@gmail.com>
2022-06-07 18:47:38 +0200

    fix: remove commented code

--- client-app/builder-preview/BuilderPreview.ts
+++ client-app/builder-preview/BuilderPreview.ts
@@ -13,16 +13,6 @@ export default {
         if (event.data.type === "changed") {
           const { template } = useTemplate("");
           template.value = event.data.model.template;
-          // const source = template.value;
-          // const index = source.content.findIndex((x: any) => x.id === event.data.model.id);
-          // if (index !== -1) {
-          //   const section = source.content[index];
-          //   const newSection = { ...section, ...event.data.model };
-          //   template.value = {
-          //     ...source,
-          //     content: [...source.content.slice(0, index), newSection, ...source.content.slice(index + 1)],
-          //   };
-          // }
         } else if (event.data.type === "navigate") {
           console.log("navigate to", event.data.url);
           options.router.push(event.data.url);
