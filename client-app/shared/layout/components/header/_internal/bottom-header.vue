1592d538181e432672b817d32ad373a9c4a17e2c
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2022-06-08 13:07:40 +0900

    remove unused

--- client-app/shared/layout/components/header/_internal/bottom-header.vue
+++ client-app/shared/layout/components/header/_internal/bottom-header.vue
@@ -67,7 +67,7 @@
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
+import { ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { VcImage } from "@/components";
 import Catalog from "./catalog.vue";
