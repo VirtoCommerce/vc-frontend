01cee2ff25c3a76a5289fdd4f520505f1f421998
Yuri A Taranov <me@flanker72.ru>
2022-06-08 21:40:14 +0500

    feat: page builder integration

---
+++ client-app/components/pages/matcher.vue
@@ -0,0 +1,94 @@
+<template>
+  <component v-if="seoInfo?.entity?.objectType === 'Category'" :is="Category" :category-seo-urls="seoInfo?.entity?.slug" />
+  <component v-else-if="seoInfo?.entity?.objectType === 'CatalogProduct'" :is="Product" :product-id="seoInfo?.entity?.objectId" />
+  <component v-else-if="seoInfo?.page" :is="PageBuilder" :settings="seoInfo?.page?.settings" :content="seoInfo?.page?.content" />
+  <NotFound v-else-if="!loading" />
+</template>
+
+<script setup lang="ts">
+import Category from "@/pages/category/category.vue";
+import Product from "@/pages/product/product.vue";
+import PageBuilder from "@/pages/builder/builder.vue";
+import NotFound from "@/pages/404/404.vue";
+
+import { PropType, ref } from "vue";
+import { asyncComputed } from "@vueuse/core";
+import { useFetch, useLanguages } from "@/core/composables";
+
+type TEntityInfo = {
+  id: string;
+  objectId: string;
+  objectType: string;
+  slug: string;
+  isActive: boolean;
+  language: {
+    cultureName: string;
+    threeLetterLanguageName: string;
+    twoLetterLanguageName: string;
+    twoLetterRegionName: string;
+    threeLetterRegionName: string;
+  };
+};
+
+type TPageInfo = {
+  settings: {
+    name: string;
+    permalink: string;
+  };
+  content: any[];
+}
+
+type TResult = {
+  entity?: TEntityInfo;
+  page?: TPageInfo;
+}
+
+const props = defineProps({
+  pathMatch: {
+    type: Array as PropType<string[]>,
+    default: () => [],
+  },
+});
+
+const { innerFetch } = useFetch();
+const { currentLanguage } = useLanguages();
+
+const loading = ref(true);
+
+const seoInfo = asyncComputed<TResult | undefined>(
+  async () => {
+    const slug = props.pathMatch[props.pathMatch?.length - 1];
+
+    if (!slug) {
+      return;
+    }
+
+    try {
+      // Load page by slug
+      const page = await innerFetch<TPageInfo>(`/storefrontapi/content/pages`, "POST", { permalink: `/${slug}`});
+
+      if (page) {
+        return {
+          page,
+        };
+      }
+    } catch (err) {
+      // If no page found load entity by slug
+      const resultItems = await innerFetch<TEntityInfo[]>(`/storefrontapi/seoInfos/${slug}`);
+
+      if (resultItems && resultItems.length) {
+        const item = resultItems.find(
+          (item) => item.isActive && item.language.cultureName === currentLanguage.value!.cultureName && item.slug === slug
+        );
+        if (item) {
+          return {
+            entity: item,
+          }
+        }
+      }
+    }
+  },
+  undefined,
+  loading
+);
+</script>
---
+++ client-app/pages/builder/builder.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <component v-for="item in content" :key="item.id" :is="item.type" :model="item" />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType } from "vue";
+
+type TPageContent = {
+  id: String;
+  name: String;
+  type: String;
+}
+
+defineProps({
+  settings: {
+    type: Object,
+    default: () => ({}),
+  },
+
+  content: {
+    type: Array as PropType<TPageContent[]>,
+    default: () => [{}],
+  },
+});
+</script>
