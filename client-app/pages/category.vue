37119143bc283d4c296d730690f2c00caffbd75e
Anton Veretennikov <superiorboroda@gmail.com>
2022-06-17 17:12:23 +0300

    refactor: move category selector to component

--- client-app/pages/category.vue
+++ client-app/pages/category.vue
@@ -61,44 +61,7 @@
 
         <!-- Sidebar -->
         <div v-else ref="sidebarElement" class="flex flex-col gap-4 lg:gap-5 lg:w-1/4 xl:w-1/5 flex-shrink-0">
-          <!-- category selector -->
-          <VcCard :title="selectedCategory?.parent?.label">
-            <template v-if="selectedCategory?.parent" #header>
-              <router-link :to="'/' + selectedCategory?.parent?.slug" class="text-sm">
-                <i class="fas fa-chevron-left text-[color:var(--color-primary)] cursor-pointer"></i>
-                <span class="font-bold ml-2">
-                  {{ selectedCategory?.parent?.label }}
-                </span>
-              </router-link>
-            </template>
-            <div class="text-sm">
-              <div v-if="selectedCategory?.items?.length">
-                <div class="uppercase font-extrabold mb-2">{{ selectedCategory?.label }}</div>
-                <div class="pl-4 flex flex-col">
-                  <router-link
-                    class="hover:bg-gray-100 pl-2"
-                    v-for="category in selectedCategory?.items"
-                    :key="category.id"
-                    :to="'/' + category.slug"
-                    >{{ category.label }}</router-link
-                  >
-                </div>
-              </div>
-              <div v-else-if="selectedCategory?.parent">
-                <div class="uppercase font-extrabold mb-2">{{ selectedCategory?.parent?.label }}</div>
-                <div class="pl-2 flex flex-col">
-                  <router-link
-                    class="hover:bg-gray-100 pl-2"
-                    v-for="category in selectedCategory?.parent?.items"
-                    :key="category.id"
-                    :class="{ 'font-bold': category.id === selectedCategory.id }"
-                    :to="'/' + category.slug"
-                    >{{ category.label }}</router-link
-                  >
-                </div>
-              </div>
-            </div>
-          </VcCard>
+          <CategorySelector :selected-category="selectedCategory"></CategorySelector>
 
           <ProductsFiltersSidebar
             :keyword="keywordQueryParam"
@@ -302,6 +265,7 @@ import {
   ProductsFacet,
   ProductsFacetValue,
   ProductsFiltersSidebar,
+  CategorySelector,
 } from "@/shared/catalog";
 import { AddToCart } from "@/shared/cart";
 import { useElementVisibility, useRouteQueryParam } from "@core/composables";
