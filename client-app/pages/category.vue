9a81ca25a3fda92ef1f22c419b29b6c8a100aa20
Anton Veretennikov <superiorboroda@gmail.com>
2022-06-17 17:05:28 +0300

    feat: make layout

--- client-app/pages/category.vue
+++ client-app/pages/category.vue
@@ -64,23 +64,33 @@
           <!-- category selector -->
           <VcCard :title="selectedCategory?.parent?.label">
             <template v-if="selectedCategory?.parent" #header>
-              <router-link :to="'/' + selectedCategory?.parent?.slug">{{
-                selectedCategory?.parent?.label
-              }}</router-link>
+              <router-link :to="'/' + selectedCategory?.parent?.slug" class="text-sm">
+                <i class="fas fa-chevron-left text-[color:var(--color-primary)] cursor-pointer"></i>
+                <span class="font-bold ml-2">
+                  {{ selectedCategory?.parent?.label }}
+                </span>
+              </router-link>
             </template>
-            <div v-if="selectedCategory?.items?.length">
-              <div class="uppercase font-bold">{{ selectedCategory?.label }}</div>
-              <div class="pl-2">
-                <div class="v" v-for="category in selectedCategory?.items" :key="category.id">
-                  <router-link :to="'/' + category.slug">{{ category.label }}</router-link>
+            <div class="text-sm">
+              <div v-if="selectedCategory?.items?.length">
+                <div class="uppercase font-extrabold mb-2">{{ selectedCategory?.label }}</div>
+                <div class="pl-4 flex flex-col">
+                  <router-link
+                    class="hover:bg-gray-100 pl-2"
+                    v-for="category in selectedCategory?.items"
+                    :key="category.id"
+                    :to="'/' + category.slug"
+                    >{{ category.label }}</router-link
+                  >
                 </div>
               </div>
-            </div>
-            <div v-else-if="selectedCategory?.parent">
-              <div class="uppercase font-bold">{{ selectedCategory?.parent?.label }}</div>
-              <div class="pl-2">
-                <div class="v" v-for="category in selectedCategory?.parent?.items" :key="category.id">
+              <div v-else-if="selectedCategory?.parent">
+                <div class="uppercase font-extrabold mb-2">{{ selectedCategory?.parent?.label }}</div>
+                <div class="pl-2 flex flex-col">
                   <router-link
+                    class="hover:bg-gray-100 pl-2"
+                    v-for="category in selectedCategory?.parent?.items"
+                    :key="category.id"
                     :class="{ 'font-bold': category.id === selectedCategory.id }"
                     :to="'/' + category.slug"
                     >{{ category.label }}</router-link
