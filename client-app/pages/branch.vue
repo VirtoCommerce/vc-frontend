1953513ae93ed351c7ef5a26caac1de2c0a02ddc
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2022-06-21 18:41:30 +0900

    desktop-started

--- client-app/pages/branch.vue
+++ client-app/pages/branch.vue
@@ -1,23 +1,27 @@
 <template>
-  <div class="flex-grow px-[2rem] pt-3.5 pb-7 bg-[color:var(--color-body-bg)] shadow-inner">
-    <h1 class="-ml-0.5 text-[1.75rem] uppercase tracking-wide" v-t="data.branch.title"></h1>
+  <div class="flex-grow px-[2rem] pt-3.5 pb-7 bg-[color:var(--color-body-bg)] shadow-inner lg:px-[4.688rem] lg:pt-4">
+    <h1 class="-ml-0.5 text-[1.75rem] uppercase tracking-wide lg:text-[2.125rem]" v-t="data.branch.title"></h1>
 
-    <div class="mt-4">
-      <div>
-        <div class="py-5 px-[1.4rem] rounded border bg-white text-base drop-shadow-[1px_2px_8px_rgba(0,0,0,0.05)]">
-          <div class="flex flex-wrap mt-0.5">
-            <div class="mb-1 mr-1 font-bold">{{ $t("pages.branch.address") }}</div>
-            <div class="mb-1" v-t="data.branch.address"></div>
+    <div class="mt-4 lg:flex lg:items-start lg:mt-5">
+      <div class="lg:flex-grow">
+        <div
+          class="py-5 px-[1.4rem] rounded border bg-white text-base drop-shadow-[1px_2px_8px_rgba(0,0,0,0.05)] lg:px-8 lg:pt-6 lg:pb-5 lg:rounded-none lg:border-0"
+        >
+          <div class="mt-0.5 mb-1 lg:mb-0.5">
+            <div class="mb-1 mr-1 font-bold lg:mb-2">{{ $t("pages.branch.address") }}</div>
+            <div class="" v-t="data.branch.address"></div>
           </div>
 
-          <div class="flex flex-wrap">
-            <div class="mb-1 mr-1 font-bold">{{ $t("pages.branch.phone") }}</div>
-            <div class="mb-1" v-t="data.branch.phone"></div>
+          <div class="flex flex-wrap mb-1">
+            <div class="mr-1 font-bold">{{ $t("pages.branch.phone") }}</div>
+            <div class="" v-t="data.branch.phone"></div>
           </div>
         </div>
 
-        <div class="mt-6 rounded border bg-white text-base drop-shadow-[1px_2px_8px_rgba(0,0,0,0.05)]">
-          <div class="py-4 px-6 border-b">
+        <div
+          class="mt-6 rounded border bg-white text-base drop-shadow-[1px_2px_8px_rgba(0,0,0,0.05)] lg:mt-0 lg:border-0 lg:rounded-none"
+        >
+          <div class="py-4 px-6 border-b lg:border-b-0">
             <h2 class="uppercase text-[1.188rem] font-bold tracking-wide">Branch hours</h2>
           </div>
           <div class="m-[1.35rem] text-[0.938rem] border rounded" id="branchHours">
@@ -63,14 +67,16 @@
         </div>
       </div>
 
-      <div class="mt-6 rounded border bg-white text-base drop-shadow-[1px_2px_8px_rgba(0,0,0,0.05)]">
-        <div class="py-4 px-6 border-b">
+      <div
+        class="mt-6 rounded border bg-white text-base drop-shadow-[1px_2px_8px_rgba(0,0,0,0.05)] lg:mt-0 lg:ml-8 lg:min-w-[21.125rem]"
+      >
+        <div class="py-4 px-6 border-b lg:pt-5 lg:pb-4">
           <h2 class="uppercase text-[1.188rem] font-bold tracking-wide">Other branches</h2>
         </div>
-        <div class="pt-5 pb-7 px-11">
+        <div class="pt-5 pb-7 px-11 lg:pt-4 lg:px-12 lg:pb-9">
           <ul class="list-disc space-y-7">
             <li class="text-[#F0AD4E] marker:text-[1.25rem]" v-for="branch in data.otherBranches">
-              <div class="mb-1 text-[#00739E] text-[1.063rem] font-extrabold" v-t="branch.title"></div>
+              <div class="mb-1 text-[#00739E] text-[1.063rem] font-extrabold lg:text-base" v-t="branch.title"></div>
               <div class="text-[color:var(--color-body-text)] text-sm" v-t="branch.address"></div>
               <div class="text-[color:var(--color-body-text)] text-sm">
                 <b>Phone:</b> <span v-t="branch.phone"></span>
