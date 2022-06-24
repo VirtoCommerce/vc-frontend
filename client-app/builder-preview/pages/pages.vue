589145d0569f1c5f405bba0e05e53ec1401cdf90
basilkot <basilkot@gmail.com>
2022-06-24 16:05:28 +0200

    fix: hide invisible blocks

--- client-app/builder-preview/pages/pages.vue
+++ client-app/builder-preview/pages/pages.vue
@@ -2,7 +2,9 @@
 <template>
   <div class="max-w-screen-2xl px-5 md:px-12 mx-auto">
     <div class="block py-8" v-for="block in template.content" :key="block.id">
-      <component :is="block.type" :model="block" />
+      <div v-if="!block.hidden">
+        <component :is="block.type" :model="block" />
+      </div>
     </div>
   </div>
 </template>
