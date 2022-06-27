db668dbc35927adf5f976b587aba60739ece4d09
basilkot <basilkot@gmail.com>
2022-06-27 14:02:22 +0200

    fix: change sections settings

--- client-app/builder-preview/pages/pages.vue
+++ client-app/builder-preview/pages/pages.vue
@@ -1,11 +1,11 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 <template>
   <div class="max-w-screen-2xl px-5 md:px-12 mx-auto">
-    <div class="block py-8" v-for="block in template.content" :key="block.id">
-      <div v-if="!block.hidden">
+    <template v-for="block in template.content" :key="block.id">
+      <div class="block py-8" v-if="!block.hidden">
         <component :is="block.type" :model="block" />
       </div>
-    </div>
+    </template>
   </div>
 </template>
 
