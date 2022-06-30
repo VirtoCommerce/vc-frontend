d09205604764baf57fa0cfe2e89c58ea4ad22274
Ilya <ilya.ivanov6@gmail.com>
2022-06-30 12:49:08 +0200

    feat: change symbols counter behavior

--- client-app/ui-kit/components/atoms/text-area/vc-text-area.vue
+++ client-app/ui-kit/components/atoms/text-area/vc-text-area.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col space-y-1">
+  <div>
     <textarea
       v-model="text"
       :rows="rows"
@@ -9,7 +9,13 @@
       class="rounded w-full leading-tight p-3 border focus:border-gray-400 outline-none"
       :class="$attrs.class"
     />
-    <p v-if="counter" class="text-xs text-[color:var(--color-danger)] self-end">{{ symbolsCount }}</p>
+    <p
+      v-if="counter"
+      class="text-xs float-right"
+      :class="symbolsLeft >= 10 ? 'text-gray-300' : 'text-[color:var(--color-danger)]'"
+    >
+      {{ symbolsCount }}
+    </p>
   </div>
 </template>
 
@@ -55,4 +61,5 @@ const text = computed<string>({
 });
 
 const symbolsCount = computed(() => `${text.value.length}/${props.maxLength}`);
+const symbolsLeft = computed(() => props.maxLength - text.value.length);
 </script>
