67c92f38128ceefa96db6050fc4353248d251cf1
Ilya Litvinenko <ilya.ivanov6@gmail.com>
2022-07-04 10:49:31 +0200

    ST-1400: Change textarea counter (#212)
    * feat: change textarea counter
    
    * feat: change symbols counter behavior
    
    * feat: use div instead of p tag
    
    * fix: use text-right

--- client-app/ui-kit/components/atoms/text-area/vc-text-area.vue
+++ client-app/ui-kit/components/atoms/text-area/vc-text-area.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative">
+  <div>
     <textarea
       v-model="text"
       :rows="rows"
@@ -9,7 +9,13 @@
       class="rounded w-full leading-tight p-3 border focus:border-gray-400 outline-none"
       :class="$attrs.class"
     />
-    <p v-if="counter" class="absolute text-xs text-gray-300 bottom-3.5 right-3">{{ symbolsCount }}</p>
+    <div
+      v-if="counter"
+      class="text-xs text-right"
+      :class="symbolsLeft >= 10 ? 'text-gray-300' : 'text-[color:var(--color-danger)]'"
+    >
+      {{ symbolsCount }}
+    </div>
   </div>
 </template>
 
@@ -55,4 +61,5 @@ const text = computed<string>({
 });
 
 const symbolsCount = computed(() => `${text.value.length}/${props.maxLength}`);
+const symbolsLeft = computed(() => props.maxLength - text.value.length);
 </script>
