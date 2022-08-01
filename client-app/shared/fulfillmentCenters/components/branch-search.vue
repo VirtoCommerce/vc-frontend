666d97524e80047e000c56603b72460de670ab21
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2022-08-01 14:55:43 +0900

    remove search button && change locales

--- client-app/shared/fulfillmentCenters/components/branch-search.vue
+++ client-app/shared/fulfillmentCenters/components/branch-search.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex items-center">
     <div
-      class="h-11 sm:h-auto flex-grow flex items-center mr-2 pl-4 pr-3 sm:pr-1 rounded border border-[color:var(--color-branch-dialog-input-border)] bg-white"
+      class="h-11 sm:h-auto flex-grow flex items-center pl-4 pr-2.5 sm:pr-0.5 rounded border border-[color:var(--color-branch-dialog-input-border)] bg-white"
     >
       <input
         class="h-full sm:h-7 w-full placeholder:text-[color:var(--color-branch-dialog-input-text)] outline-none bg-transparent text-sm sm:text-xs"
@@ -11,7 +11,7 @@
         @input="onInput($event)"
       />
       <button
-        class="flex items-center justify-center w-5 h-5"
+        class="flex items-center justify-center w-6 h-5 outline-none"
         :disabled="!modelValue.length"
         @click="emit('update:input', '')"
       >
@@ -25,9 +25,6 @@
         </svg>
       </button>
     </div>
-    <button class="w-11 h-11 sm:w-7 sm:h-7 rounded shadow-t-mds bg-primary sm:bg-white hover:bg-slate-50">
-      <i class="fas fa-search text-white sm:text-primary text-xl sm:text-base" />
-    </button>
   </div>
 </template>
 <script setup lang="ts">
