f21383b549b0e0bebff3923914a8ef95e1b13255
basilkot <basilkot@gmail.com>
2022-08-03 17:45:59 +0200

    fix: remove console.log

--- client-app/builder-preview/BuilderPreview.ts
+++ client-app/builder-preview/BuilderPreview.ts
@@ -8,13 +8,11 @@ export default {
     options.router.push("/");
 
     window.addEventListener("message", (event: MessageEvent) => {
-      console.log("received some message", event);
       if (event.origin !== document.location.origin) {
         // note: it can be cause of some problems. investigate it.
         return;
       }
       if (event.data.source === "builder") {
-        console.log("received message from template builder", event);
         if (event.data.type === "changed") {
           useStaticPage(event.data.model.template);
         } else if (event.data.type === "navigate") {
