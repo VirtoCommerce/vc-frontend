25fa5bc2d8c4656e5dde704fa114efdee9c60b4c
basilkot <basilkot@gmail.com>
2022-08-05 14:35:12 +0200

    fix: check msg origin

--- client-app/builder-preview/BuilderPreview.ts
+++ client-app/builder-preview/BuilderPreview.ts
@@ -3,12 +3,17 @@ import { useStaticPage } from "@/core/composables";
 
 export default {
   install: (_app: App, options: any) => {
+    console.log("install buidler preview plugin");
     const bodyEl = document.getElementsByTagName("body").item(0);
     if (bodyEl) {
       bodyEl.style.visibility = "hidden";
     }
 
     window.addEventListener("message", (event: MessageEvent) => {
+      if (event.origin !== document.location.origin) {
+        // note: it can be cause of some problems. investigate it.
+        return;
+      }
       if (event.data.source === "builder") {
         if (bodyEl) {
           bodyEl.style.visibility = "visible";
@@ -17,6 +22,7 @@ export default {
           useStaticPage(event.data.model.template);
         } else if (event.data.type === "navigate") {
           if (event.data.url !== options.router.currentRoute.fullPath) {
+            console.log("navigate to", event.data.url);
             options.router.push(event.data.url);
           }
         }
@@ -24,5 +30,7 @@ export default {
     });
 
     window.parent.postMessage({ source: "preview", type: "loaded" }, window.location.origin);
+
+    // todo: add ability to send messages when something occurs in storefront, for example react to navigate event
   },
 };
