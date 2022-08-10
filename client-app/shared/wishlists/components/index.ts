223533748de7b4a1f70833ccde72bc4e1c022e4e
Artem Makarov <halfelkan@mail.ru>
2022-08-10 10:21:13 +0300

    fix(ST-2738): deleted product in the wish list (#264)

--- client-app/shared/wishlists/components/index.ts
+++ client-app/shared/wishlists/components/index.ts
@@ -5,5 +5,6 @@ export { default as DeleteWishlistsDialog } from "./delete-wishlist-dialog.vue";
 export { default as WishlistCard } from "./wishlist-card.vue";
 export { default as WishlistCardSkeleton } from "./wishlist-card-skeleton.vue";
 export { default as WishlistProductItem } from "./wishlist-product-item.vue";
+export { default as WishlistProductItemSkeleton } from "./wishlist-product-item-skeleton.vue";
 export { default as DeleteWishlistProductDialog } from "./delete-wishlist-product.vue";
 export { default as UnsuccessfulCreateWishlistDialog } from "./unsuccessful-create-list-dialog.vue";
