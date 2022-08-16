d63b55a426ddd665e8ab5a01de36c9b362108ace
Andrew Orlov <ao@virtoway.com>
2022-08-16 16:19:09 +0200

    fix(st-2943): implement add or update company address dialog

--- client-app/pages/company/info.vue
+++ client-app/pages/company/info.vue
@@ -5,8 +5,6 @@
       <h2 class="text-gray-800 text-3xl font-bold uppercase" v-t="'pages.company.info.title'" />
     </div>
 
-    <button @click="openSaveCompanyAddressDialog()">Add company address</button>
-
     <!-- TODO: Add content
     <div class="flex flex-col bg-white shadow-sm md:rounded md:border"></div>
     -->
@@ -16,23 +14,12 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
 import { usePageHead } from "@/core/composables";
-import { usePopup } from "@/shared/popup";
-import { AddOrUpdateCompanyAddressDialog } from "@/shared/company";
-import { AddressType } from "@/core/types";
 
 const { t } = useI18n();
-const { openPopup } = usePopup();
 
 usePageHead({
   title: t("pages.company.info.meta.title"),
 });
 
-function openSaveCompanyAddressDialog(address?: AddressType): void {
-  openPopup({
-    component: AddOrUpdateCompanyAddressDialog,
-    props: {
-      address,
-    },
-  });
-}
+// openSaveCompanyAddressDialog(address?: AddressType): void
 </script>
--- client-app/shared/company/components/add-or-update-company-address-dialog.vue
+++ client-app/shared/company/components/add-or-update-company-address-dialog.vue
@@ -8,92 +8,193 @@
       )
     "
   >
-    <div class="px-6 py-5 space-y-4">
-      <h3 class="text-xl font-extrabold uppercase">
-        {{ $t("shared.company.add_or_update_address_dialog.content_title") }}
-      </h3>
+    <template #default="{ close }">
+      <form class="px-6 py-5 space-y-4" @submit.prevent="save">
+        <h3 class="text-xl font-extrabold uppercase">
+          {{ $t("shared.company.add_or_update_address_dialog.content_title") }}
+        </h3>
 
-      <VcInput
-        v-model="payload.description"
-        :label="$t('shared.company.add_or_update_address_dialog.description_field_label')"
-        class="flex flex-col flex-1"
-      />
-
-      <div class="flex space-x-5">
-        <VcSelect
-          v-model="payload.countryCode"
-          :items="countries"
-          :label="$t('shared.company.add_or_update_address_dialog.country_field_label')"
+        <VcInput
+          v-model="description"
+          :maxlength="128"
+          :error-message="errors.description"
+          :label="$t('shared.company.add_or_update_address_dialog.description_field_label')"
           class="flex-1"
-          size="lg"
-          is-required
-          text-field="name"
+        />
+
+        <div class="md:flex md:space-x-5">
+          <VcSelect
+            v-model="country"
+            :items="countries"
+            :error-message="errors.countryCode"
+            :label="$t('shared.company.add_or_update_address_dialog.country_field_label')"
+            class="flex-1"
+            size="lg"
+            is-required
+            text-field="name"
+          />
+
+          <VcInput
+            v-model="postalCode"
+            :maxlength="32"
+            :error-message="errors.postalCode"
+            :label="$t('shared.company.add_or_update_address_dialog.postal_code_field_label')"
+            class="flex-1"
+            is-required
+          />
+        </div>
+
+        <div class="md:flex md:space-x-5">
+          <VcSelect
+            v-model="region"
+            :items="regions"
+            :error-message="errors.regionId"
+            :is-required="!!regions.length"
+            :is-disabled="!regions.length"
+            :label="$t('shared.company.add_or_update_address_dialog.region_field_label')"
+            class="flex-1"
+            size="lg"
+            text-field="name"
+          />
+
+          <VcInput
+            v-model="city"
+            :maxlength="128"
+            :error-message="errors.city"
+            :label="$t('shared.company.add_or_update_address_dialog.city_field_label')"
+            class="flex-1"
+            is-required
+          />
+        </div>
+
+        <VcInput
+          v-model="line1"
+          :maxlength="128"
+          :error-message="errors.line1"
+          :label="$t('shared.company.add_or_update_address_dialog.line_1_field_label')"
+          class="flex-1"
+          @click="close"
         />
 
         <VcInput
-          v-model="postalCode"
-          :label="$t('shared.company.add_or_update_address_dialog.postal_code_field_label')"
+          v-model="line2"
+          :maxlength="128"
+          :error-message="errors.line2"
+          :label="$t('shared.company.add_or_update_address_dialog.line_2_field_label')"
           class="flex-1"
-          is-required
         />
-      </div>
+      </form>
+    </template>
 
-      <div class="flex space-x-5">
-        <VcSelect
-          v-model="region"
-          :items="regions"
-          :label="$t('shared.company.add_or_update_address_dialog.region_field_label')"
-          class="flex-1"
-          size="lg"
-          text-field="name"
-        />
+    <template #actions="{ close }">
+      <VcButton class="uppercase w-32 sm:flex-grow" kind="secondary" is-outline @click="close">
+        {{ $t("shared.company.add_or_update_address_dialog.cancel_button_label") }}
+      </VcButton>
 
-        <VcInput
-          v-model="city"
-          :label="$t('shared.company.add_or_update_address_dialog.city_field_label')"
-          class="flex-1"
-          is-required
-        />
-      </div>
-
-      <VcInput
-        v-model="line1"
-        :label="$t('shared.company.add_or_update_address_dialog.line_1_field_label')"
-        class="flex-1"
-      />
-
-      <VcInput
-        v-model="line2"
-        :label="$t('shared.company.add_or_update_address_dialog.line_2_field_label')"
-        class="flex-1"
-      />
-
-      <VcCheckbox v-model="isDefault">
-        <span>{{ $t("shared.company.add_or_update_address_dialog.set_as_default_label") }}</span>
-      </VcCheckbox>
-    </div>
-
-    <template #actions></template>
+      <VcButton class="uppercase w-32 sm:flex-grow" :is-disabled="!meta.dirty || !meta.valid">
+        {{ $t("shared.company.add_or_update_address_dialog.save_button_label") }}
+      </VcButton>
+    </template>
   </VcPopup>
 </template>
 
 <script setup lang="ts">
 import { useCountries } from "@/core/composables";
-import { onMounted, PropType } from "vue";
-import VcCheckbox from "@/ui-kit/components/atoms/checkbox/vc-checkbox.vue";
-import { MemberAddressType } from "@/xapi/types";
+import { onMounted, PropType, ref, Ref, computed, watch } from "vue";
+import { CountryRegionType, CountryType, MemberAddressType } from "@/xapi/types";
+import { clone } from "lodash";
+import { useField, useForm } from "vee-validate";
+import * as yup from "yup";
+import { getAddressName, Logger } from "@/core/utilities";
 
-defineProps({
+const props = defineProps({
   address: {
-    type: Object as PropType<MemberAddressType>,
+    type: Object as PropType<MemberAddressType | null>,
+    default: null,
   },
 });
 
 const { countries, loadCountries } = useCountries();
+
+const _emptyAddress: Readonly<MemberAddressType> = {
+  isDefault: false,
+  postalCode: "",
+  countryCode: "",
+  countryName: "",
+  regionId: "",
+  regionName: "",
+  city: "",
+  line1: "",
+  line2: "",
+  description: "",
+  // FIXME: The values may be NULL. Incorrect behavior of the "dirty" variable
+};
+
+const emit = defineEmits<{
+  (event: "update:modelValue", address: MemberAddressType): void;
+  (event: "save", address: MemberAddressType): void;
+}>();
+
+const initialValues: Ref<MemberAddressType> = ref(clone(props.address || _emptyAddress));
+
+const { meta, errors, setValues, handleSubmit } = useForm({ initialValues });
+
+const country = computed<CountryType | undefined>({
+  get: () => countries.value.find((item) => countryCode.value === item.id),
+  set: (value?: CountryType) => {
+    countryCode.value = value?.id ?? "";
+    countryName.value = value?.name ?? "";
+    regionId.value = "";
+    regionName.value = "";
+  },
+});
+
+const regions = computed<CountryRegionType[]>(() => country.value?.regions ?? []);
+
+const region = computed<CountryRegionType | undefined>({
+  get: () => regions.value.find((item) => regionId.value === item.id),
+  set: (value?: CountryRegionType) => {
+    regionId.value = value?.id ?? "";
+    regionName.value = value?.name ?? "";
+  },
+});
+
+const regionRules = computed(() => {
+  let rules = yup.string().nullable();
+  if (regions.value.length) {
+    rules = rules.required();
+  }
+  return rules;
+});
+
+const { value: description } = useField<string>("description", yup.string().max(128).nullable());
+const { value: city } = useField<string>("city", yup.string().max(128).required().nullable());
+const { value: postalCode } = useField<string>("postalCode", yup.string().max(32).required().nullable());
+const { value: countryCode } = useField<string>("countryCode", yup.string().required().nullable());
+const { value: countryName } = useField<string>("countryName", yup.string().max(128).nullable());
+const { value: regionName } = useField<string>("regionName", yup.string().max(128).nullable());
+const { value: regionId } = useField<string>("regionId", regionRules);
+const { value: line1 } = useField<string>("line1", yup.string().max(128).nullable());
+const { value: line2 } = useField<string>("line2", yup.string().max(128).nullable());
 
 onMounted(async () => {
   if (!countries.value.length) {
     await loadCountries();
   }
 });
+
+watch(
+  () => props.address,
+  (value: MemberAddressType | null) => {
+    initialValues.value = clone(value || _emptyAddress);
+    setValues(initialValues.value);
+  },
+  { deep: true }
+);
+
+const save = handleSubmit((address) => {
+  const newAddress: MemberAddressType = { ...address, name: getAddressName(address) };
+  emit("update:modelValue", newAddress);
+  emit("save", newAddress);
+}, Logger.debug);
 </script>
