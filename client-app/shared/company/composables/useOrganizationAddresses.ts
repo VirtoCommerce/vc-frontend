9462d0ed9f24724a2de5e5d36ba379f9793215ec
Artem Makarov <halfelkan@mail.ru>
2022-08-17 10:13:10 +0300

    feat(ST-2972): company address deletion functionality (#268)

--- client-app/shared/company/composables/useOrganizationAddresses.ts
+++ client-app/shared/company/composables/useOrganizationAddresses.ts
@@ -1,37 +1,31 @@
-import { computed, readonly, ref, shallowRef } from "vue";
-import { getSortingExpression, Logger } from "@/core/utilities";
+import { computed, readonly, ref, shallowRef, unref } from "vue";
+import { getSortingExpression, Logger, toInputAddress } from "@/core/utilities";
 import { ISortInfo } from "@/core/types";
-import { MemberAddressType } from "@/xapi/types";
+import { InputMemberAddressType, MemberAddressType } from "@/xapi/types";
 import { getOrganizationAddresses } from "@/xapi/graphql/organization";
+import { deleteMemberAddresses } from "@/xapi/graphql/account";
 import { SORT_DESCENDING } from "@/core/constants";
+import { MaybeRef } from "@vueuse/core";
 
-const DEFAULT_ITEMS_PER_PAGE = 10;
-
-export default function useOrganizationAddresses() {
+export default function useOrganizationAddresses(organizationId: MaybeRef<string>) {
   const loading = ref(false);
   const addresses = shallowRef<MemberAddressType[]>([]);
-  const itemsPerPage = ref(DEFAULT_ITEMS_PER_PAGE);
-  const pages = ref(0);
-  const page = ref(1);
   const sort = ref<ISortInfo>({
     column: "createdDate",
     direction: SORT_DESCENDING,
   });
 
-  async function fetchAddresses(organizationId: string) {
+  async function fetchAddresses() {
     try {
       loading.value = true;
 
       const sortingExpression = getSortingExpression(sort.value);
 
-      const { items = [], totalCount = 0 } = await getOrganizationAddresses(organizationId, {
+      const { items = [] } = await getOrganizationAddresses(unref(organizationId), {
         sort: sortingExpression,
-        first: itemsPerPage.value,
-        after: String((page.value - 1) * itemsPerPage.value),
       });
 
       addresses.value = items;
-      pages.value = Math.ceil(totalCount / itemsPerPage.value);
     } catch (e) {
       Logger.error(`${useOrganizationAddresses.name}.${fetchAddresses.name}`, e);
       throw e;
@@ -40,13 +34,28 @@ export default function useOrganizationAddresses() {
     }
   }
 
+  async function removeAddresses(items: MemberAddressType[]): Promise<void> {
+    loading.value = true;
+
+    const inputAddresses: InputMemberAddressType[] = items.map(toInputAddress);
+
+    try {
+      await deleteMemberAddresses(inputAddresses, unref(organizationId));
+    } catch (e) {
+      Logger.error(`${useOrganizationAddresses.name}.${removeAddresses.name}`, e);
+      throw e;
+    } finally {
+      loading.value = false;
+    }
+
+    await fetchAddresses();
+  }
+
   return {
-    fetchAddresses,
-    itemsPerPage,
-    page,
     sort,
+    fetchAddresses,
+    removeAddresses,
     loading: readonly(loading),
     addresses: computed(() => addresses.value),
-    pages: readonly(pages),
   };
 }
