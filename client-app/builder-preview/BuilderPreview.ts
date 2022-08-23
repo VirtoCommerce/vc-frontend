51b999da0839163df74f876b96fdb150f02b4ef6
Basil Kotov <basilkot@gmail.com>
2022-08-23 13:29:01 +0200

    feat: preview theme preferences (#270)
    * feat: preview theme preferences
    
    * fix: remove commented code

--- client-app/builder-preview/BuilderPreview.ts
+++ client-app/builder-preview/BuilderPreview.ts
@@ -2,7 +2,7 @@ import { App } from "vue";
 import { useStaticPage } from "@/core/composables";
 
 export default {
-  install: (_app: App, options: any) => {
+  install: (app: App, options: any) => {
     const bodyEl = document.getElementsByTagName("body").item(0);
     if (bodyEl) {
       bodyEl.style.visibility = "hidden";
@@ -10,6 +10,7 @@ export default {
     window.addEventListener("message", (event: MessageEvent) => {
       if (event.origin !== document.location.origin || event.data.source !== "builder") {
         // note: it can be cause of some problems. investigate it.
+        console.log("cancel message");
         return;
       }
       if (bodyEl) {
@@ -22,6 +23,18 @@ export default {
         case "navigate":
           options.router.push(event.data.url);
           break;
+        case "settings": {
+          const keys = Object.entries(event.data.settings);
+          keys.forEach(([key, value]) => {
+            app.config.globalProperties.$cfg[key] = value;
+          });
+          keys
+            .filter(([key]) => /^color/.test(key))
+            .forEach(([key, value]) => {
+              document.documentElement.style.setProperty(`--${key.replace(/_/g, "-")}`, value as string);
+            });
+          break;
+        }
       }
     });
     window.parent.postMessage({ source: "preview", type: "loaded" }, window.location.origin);
