a1edb362c2c933228bad227ea3b45b2d8c5d5f7e
Andrew Orlov <andrew.orlov@outlook.com>
2022-08-26 14:29:17 +0200

    feat(st-2627): format totals based on current culture (#277)
    * feat(st-2627): format totals based on current culture
    
    * fix(st-2627): refactor
    
    * fix(st-2627): refactor
    
    * fix(st-2627): refactor
    
    * fix(st-2627): fix product total issue for empty quantity field case

--- client-app/shared/checkout/components/order-summary.vue
+++ client-app/shared/checkout/components/order-summary.vue
@@ -16,25 +16,39 @@
         </div>
         <div class="py-2 border-t border-b font-normal text-base">
           <div class="flex justify-between" @click="discountsCollapsed = !discountsCollapsed">
-            <span class="flex items-center" :class="{ 'cursor-pointer': cart.discounts && cart.discounts.length > 0 }">
+            <span class="flex items-center" :class="{ 'cursor-pointer': hasDiscounts }">
               {{ $t("shared.checkout.order_summary.discount_label") }}
               <i
-                v-if="cart.discounts && cart.discounts.length > 0 && !discountsCollapsed"
+                v-if="hasDiscounts && !discountsCollapsed"
                 class="fas fa-caret-down ml-1 text-[color:var(--color-primary)]"
               ></i
               ><i
-                v-if="cart.discounts && cart.discounts.length > 0 && discountsCollapsed"
+                v-if="hasDiscounts && discountsCollapsed"
                 class="fas fa-caret-up ml-1 text-[color:var(--color-primary)]"
               ></i>
             </span>
             <span>{{ cart.discountTotal?.amount > 0 ? "-" : "" }}<VcPriceDisplay :value="cart.discountTotal" /></span>
           </div>
-          <div v-if="cart.discounts && cart.discounts.length > 0 && discountsCollapsed">
-            <ul v-for="(discount, index) in cart.discounts" :key="index" class="list-disc pl-5 text-gray-400">
-              <li>
+          <div v-if="hasDiscounts && discountsCollapsed">
+            <ul class="list-disc pl-5 text-gray-400">
+              <li v-for="(discount, index) in cart.discounts" :key="index">
                 <div class="flex justify-between items-center">
-                  <span class="text-sm">{{ discount?.description }}</span>
-                  <span>{{ cart.currency?.symbol }}{{ getDiscountAmmount(discount) }}</span>
+                  <span class="text-sm">{{ discount.description }}</span>
+                  <VcTotalDisplay
+                    :amount="-getDiscountAmmount(discount)"
+                    :currency-code="currentCurrency.code"
+                    :culture-name="currentLanguage.cultureName"
+                  />
+                </div>
+              </li>
+              <li v-if="lineItemsDiscountTotal > 0">
+                <div class="flex justify-between items-center">
+                  <span class="text-sm">{{ $t("common.labels.line_items") }}</span>
+                  <VcTotalDisplay
+                    :amount="-lineItemsDiscountTotal"
+                    :currency-code="currentCurrency.code"
+                    :culture-name="currentLanguage.cultureName"
+                  />
                 </div>
               </li>
             </ul>
@@ -60,21 +74,39 @@
 </template>
 
 <script setup lang="ts">
-import { CartType, CustomerOrderType, DiscountType, OrderDiscountType } from "@/xapi/types";
-import { PropType, ref } from "vue";
+import {
+  CartType,
+  CustomerOrderType,
+  DiscountType,
+  LineItemType,
+  OrderDiscountType,
+  OrderLineItemType,
+} from "@/xapi/types";
+import { computed, PropType, ref } from "vue";
+import _ from "lodash";
+import { useCurrency, useLanguages } from "@/core/composables";
 
-defineProps({
+const props = defineProps({
   cart: {
     type: Object as PropType<CartType | CustomerOrderType | Record<string, never>>,
     required: true,
   },
 });
 
+const { currentLanguage } = useLanguages();
+const { currentCurrency } = useCurrency();
+
 const discountsCollapsed = ref(false);
 
 const getDiscountAmmount = (discount: DiscountType | OrderDiscountType) => {
   return typeof discount?.amount === "object" && discount?.amount !== null ? discount?.amount.amount : discount?.amount;
 };
+
+const lineItemsDiscountTotal = computed(() =>
+  _.sumBy(props.cart.items, (item: LineItemType | OrderLineItemType) => item.discountTotal?.amount || 0)
+);
+
+const hasDiscounts = computed(() => props.cart.discounts?.length || lineItemsDiscountTotal.value > 0);
 </script>
 
 <style scoped></style>
---
+++ client-app/ui-kit/components/atoms/total-display/vc-total-display.vue
@@ -0,0 +1,29 @@
+<template>
+  <span>{{ formattedAmount }}</span>
+</template>
+
+<script lang="ts" setup>
+import { computed } from "vue";
+
+const props = defineProps({
+  amount: {
+    type: Number,
+    required: true,
+  },
+  cultureName: {
+    type: String,
+    required: true,
+  },
+  currencyCode: {
+    type: String,
+    required: true,
+  },
+});
+
+const formattedAmount = computed(() =>
+  new Intl.NumberFormat(props.cultureName, {
+    currency: props.currencyCode,
+    style: "currency",
+  }).format(props.amount)
+);
+</script>
