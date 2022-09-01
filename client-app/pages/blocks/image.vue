664ad325c7ed220af53179b31acd061f7996051e
basilkot <basilkot@gmail.com>
2022-09-01 15:26:46 +0200

    feat: add new sections

--- client-app/pages/blocks/image.vue
+++ client-app/pages/blocks/image.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    <img :src="model.image" />
+  <div class="pt-6 pb-16 lg:py-24">
+    <div class="w-full max-w-screen-2xl mx-auto px-5 md:px-12">
+      <img :src="model.image" />
+    </div>
   </div>
 </template>
 
---
+++ client-app/pages/blocks/subscribe-form.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="py-10 lg:py-24" :class="model.background">
+    <div class="w-full max-w-screen-xl mx-auto px-14 lg:px-32">
+      <div class="font-bold text-2xl lg:text-4xl mb-4 text-center">{{ model.title }}</div>
+      <div class="font-bold text-lg mb-8 text-center">{{ model.subtitle }}</div>
+      <VcInput class="max-w-lg mx-auto" :placeholder="model.placeholder" />
+      <div class="text-center">
+        <div class="mt-4">
+          <VcCheckbox class="inline-flex">
+            <span v-html="model.checkboxText"></span>
+          </VcCheckbox>
+        </div>
+
+        <VcButton class="uppercase px-12 mt-10">
+          {{ model.buttonText }}
+        </VcButton>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  model: {
+    type: Object,
+    required: true,
+  },
+  settings: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
