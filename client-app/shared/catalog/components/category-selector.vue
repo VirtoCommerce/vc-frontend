0e1ed9d57775ae12b28a9f2145415734e69bf352
Artem Makarov <am.work.acc@gmail.com>
2022-09-23 11:51:16 +0300

    fix: category selector

--- client-app/shared/catalog/components/category-selector.vue
+++ client-app/shared/catalog/components/category-selector.vue
@@ -1,34 +1,44 @@
 <template>
   <!-- category selector -->
-  <VcFilterCard v-if="!loading || selectedCategory" :withHeader="!!backCategory">
+  <VcFilterCard v-if="!loading || selectedCategory" :with-header="!!backCategory">
     <template #header>
-      <router-link :to="getCategoryRoute(backCategory!)" class="pl-0.5 text-[color:var(--color-filter-card-header)]">
+      <router-link :to="backCategoryLink" class="pl-0.5 text-[color:var(--color-filter-card-header)]">
         <i class="fas fa-chevron-left text-[color:var(--color-primary)]"></i>
         <span class="font-bold ml-1.5">
           {{ backCategory?.label }}
         </span>
       </router-link>
     </template>
+
     <template v-if="selectedCategory">
-      <div class="uppercase font-extrabold -mt-0.5 mb-1 text-13-title text-[color:var(--color-filter-card-header)]">
-        {{ categoryLabel }}
-      </div>
-      <div class="-mr-1.5 pl-5 pb-1.5 flex flex-col text-sm">
+      <div class="uppercase font-extrabold -mt-1 mb-0.5 text-13-title text-[color:var(--color-filter-card-header)]">
+        <span v-if="selectedCategory.items?.length" class="block py-0.5">
+          {{ category?.label }}
+        </span>
+
         <router-link
-          class="hover:bg-gray-100 py-0.5 mt-0.5 transition-colors truncate"
-          v-for="category in categoryItems"
-          :key="category.id"
-          :class="{
-            'font-bold text-[color:var(--color-filter-card-header)]': category.id === selectedCategory.id,
-            'text-[color:var(--color-category-selector-link)]': category.id !== selectedCategory.id,
-          }"
-          :to="categoriesRoutes[category.id!]"
+          v-else
+          :to="categoryLink"
+          class="block py-0.5 px-1.5 -mx-1.5 rounded-sm hover:bg-gray-100 transition-colors"
         >
-          {{ category.label }}
+          {{ category?.label }}
+        </router-link>
+      </div>
+
+      <div class="pl-5 pb-1.5 flex flex-col text-sm">
+        <router-link
+          v-for="(item, index) in categoryItems"
+          :to="categoriesRoutes[item.id!]"
+          :key="index"
+          :class="{ 'font-bold': item.id === selectedCategory.id }"
+          class="hover:bg-gray-100 py-0.5 px-1.5 -mx-1.5 mt-0.5 rounded-sm transition-colors truncate text-[color:var(--color-category-selector-link)]"
+        >
+          {{ item.label }}
         </router-link>
       </div>
     </template>
   </VcFilterCard>
+
   <!-- skeleton -->
   <template v-else>
     <VcFilterCardSkeleton>
@@ -48,6 +58,7 @@ const props = defineProps({
     type: Object as PropType<CategoryTree>,
     required: false,
   },
+
   loading: {
     type: Boolean,
     default: false,
@@ -62,11 +73,15 @@ const categoryItems = computed(() =>
 
 const categoriesRoutes = useCategoriesRoutes(categoryItems);
 
-const categoryLabel = computed(() =>
-  selectedCategory.value?.items?.length ? selectedCategory.value?.label : selectedCategory.value?.parent!.label
+const category = computed(() =>
+  selectedCategory.value?.items?.length ? selectedCategory.value : selectedCategory.value?.parent
 );
+
+const categoryLink = computed(() => (category.value ? getCategoryRoute(category.value) : undefined));
 
 const backCategory = computed(() =>
   selectedCategory.value?.items?.length ? selectedCategory.value.parent : selectedCategory.value?.parent?.parent
 );
+
+const backCategoryLink = computed(() => (backCategory.value ? getCategoryRoute(backCategory.value) : undefined));
 </script>
