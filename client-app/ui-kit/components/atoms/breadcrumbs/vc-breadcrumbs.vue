e93e84f0f56e6b1c12d9776dfd61bc5ad2ed5ebf
basilkot <basilkot@gmail.com>
2022-09-30 09:47:01 +0600

    fix: ui bugs in blocks

--- client-app/ui-kit/components/atoms/breadcrumbs/vc-breadcrumbs.vue
+++ client-app/ui-kit/components/atoms/breadcrumbs/vc-breadcrumbs.vue
@@ -7,21 +7,21 @@
             <router-link
               :to="item.route"
               class="font-medium text-[color:var(--color-link)] hover:text-[color:var(--color-link-hover)]"
-              >{{ item.title }}</router-link
+              >{{ unref(item.title) }}</router-link
             >
           </li>
           <li><span class="mx-2 text-gray-300">/</span></li>
         </template>
 
         <!-- Last breadcrumbs item -->
-        <li v-else class="font-medium text-gray-500">{{ item.title }}</li>
+        <li v-else class="font-medium text-gray-500">{{ unref(item.title) }}</li>
       </template>
     </ol>
   </nav>
 </template>
 
 <script setup lang="ts">
-import { PropType } from "vue";
+import { PropType, unref } from "vue";
 
 defineProps({
   items: {
