1fb257826641083a85827259ac437791287e3856
Artem Makarov <am.work.acc@gmail.com>
2022-10-05 11:19:57 +0300

    fix(checkout defaults): add migration

--- client-app/shared/account/composables/useUserCheckoutDefaults.ts
+++ client-app/shared/account/composables/useUserCheckoutDefaults.ts
@@ -1,6 +1,7 @@
 import { useUser } from ".";
 import { CheckoutDefaults } from "./../types/index";
 import { Logger } from "@/core/utilities";
+import { PaymentMethodType, ShippingMethodType } from "@/xapi/types";
 
 export default function useUserCheckoutDefaults() {
   const { user } = useUser();
@@ -8,6 +9,36 @@ export default function useUserCheckoutDefaults() {
 
   function setUserCheckoutDefaults(defaults: CheckoutDefaults) {
     localStorage.setItem(`${keyPrefix}${user.value.id}`, JSON.stringify(defaults));
+  }
+
+  /**
+   * Migration to another data type
+   * @deprecated
+   * @todo Remove in the first quarter of 2023
+   */
+  function migration(
+    defaults:
+      | (CheckoutDefaults & /* deprecated type */ {
+          shippingMethod?: ShippingMethodType;
+          paymentMethod?: PaymentMethodType;
+        })
+      | null
+  ): CheckoutDefaults | null {
+    const { shippingMethod, paymentMethod, deliveryMethod } = defaults || {};
+
+    if (!shippingMethod && !paymentMethod) {
+      return defaults;
+    }
+
+    const newDefaults: CheckoutDefaults = {
+      deliveryMethod,
+      shippingMethodId: shippingMethod?.id,
+      paymentMethodCode: paymentMethod?.code,
+    };
+
+    setUserCheckoutDefaults(newDefaults);
+
+    return newDefaults;
   }
 
   function getUserCheckoutDefaults(): CheckoutDefaults | null {
@@ -23,7 +54,7 @@ export default function useUserCheckoutDefaults() {
       }
     }
 
-    return result;
+    return migration(result);
   }
 
   return {
