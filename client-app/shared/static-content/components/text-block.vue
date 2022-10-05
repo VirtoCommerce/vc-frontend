67316953df9e2e8296b268dbc34a281f32319324
Basil Kotov <basilkot@gmail.com>
2022-10-05 16:05:52 +0200

    fix: typography for text block (#321)
    * fix: typography for text block
    
    * fix: isolate text block css
    
    * fix: remove extra tag
    
    * fix: remove warnings

---
+++ client-app/shared/static-content/components/text-block.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="pt-6 pb-16 lg:py-24 text-block">
+    <div class="w-full max-w-screen-2xl mx-auto px-5 md:px-12">
+      <h1 class="text-2xl mb-8">{{ model.header }}</h1>
+      <div class="text-lg block-content" v-html="model.text"></div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  model: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss">
+.text-block {
+  .block-content {
+    p {
+      margin-bottom: 1.5rem;
+    }
+    ul {
+      list-style: disc inside;
+    }
+  }
+}
+</style>
