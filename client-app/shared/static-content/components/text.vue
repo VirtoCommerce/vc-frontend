e41e4ced64af557ddea73483b25b0acd7aed6a2e
basilkot <basilkot@gmail.com>
2022-10-05 19:12:10 +0600

    fix: typography for text block

--- client-app/shared/static-content/components/text.vue
+++ client-app/shared/static-content/components/text.vue
@@ -2,6 +2,7 @@
   <div class="pt-6 pb-16 lg:py-24">
     <div class="w-full max-w-screen-2xl mx-auto px-5 md:px-12">
       <h1 class="text-2xl">{{ model.header }}</h1>
+      <hr />
       <div class="text-lg" v-html="model.text"></div>
     </div>
   </div>
@@ -15,3 +16,17 @@ defineProps({
   },
 });
 </script>
+
+<style lang="scss">
+.text-2xl {
+  margin-bottom: 2rem;
+}
+.text-lg {
+  p {
+    margin-bottom: 1.5rem;
+  }
+  ul {
+    list-style: disc inside;
+  }
+}
+</style>
