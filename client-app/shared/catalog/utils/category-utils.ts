6be93987c612537a0940f9d9408d6c016e10ab95
Artem Makarov <am.work.acc@gmail.com>
2022-10-11 09:37:25 +0300

    fix: category page fixes

--- client-app/shared/catalog/utils/category-utils.ts
+++ client-app/shared/catalog/utils/category-utils.ts
@@ -5,3 +5,25 @@ import { RouteLocationRaw } from "vue-router";
 export function getCategoryRoute(category: Category | CategoryTree): RouteLocationRaw {
   return category.slug ? `/${category.slug}` : { name: "Category", params: { categoryId: category.id } };
 }
+
+export function searchCategoryByKey(
+  categoryTreeItem: CategoryTree,
+  key: keyof CategoryTree,
+  value: any
+): CategoryTree | undefined {
+  if (categoryTreeItem[key] === value) {
+    return categoryTreeItem;
+  }
+
+  const items = categoryTreeItem.items ?? [];
+
+  for (const item of items) {
+    const category = searchCategoryByKey(item, key, value);
+
+    if (category) {
+      return category;
+    }
+  }
+
+  return undefined;
+}
