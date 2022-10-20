d272aa7deca3faaf54ed3a1be6303315f5e834bc
Andrew Orlov <ao@virtoway.com>
2022-10-21 01:28:42 +0200

    feat(3375): quote list page

---
+++ client-app/shared/account/composables/useUserQuotes.ts
@@ -0,0 +1,42 @@
+import { Logger } from "@/core/utilities";
+import { getQuotes } from "@/xapi/graphql/quotes";
+import { QuoteType } from "@/xapi/types";
+import { Ref, ref, shallowRef } from "vue";
+
+const DEFAULT_ITEMS_PER_PAGE = 10;
+
+export default () => {
+  const quotes: Ref<QuoteType[]> = shallowRef<QuoteType[]>([]);
+  const fetching: Ref<boolean> = ref(false);
+  const itemsPerPage: Ref<number> = ref(DEFAULT_ITEMS_PER_PAGE);
+  const pages: Ref<number> = ref(0);
+  const page: Ref<number> = ref(1);
+
+  async function fetchQuotes(): Promise<void> {
+    fetching.value = true;
+
+    try {
+      const response = await getQuotes({
+        first: itemsPerPage.value,
+        after: String((page.value - 1) * itemsPerPage.value),
+      });
+
+      quotes.value = response.items ?? [];
+      pages.value = Math.ceil((response.totalCount ?? 0) / itemsPerPage.value);
+    } catch (e) {
+      Logger.error("useUserQuotes.fetchQuotes", e);
+      throw e;
+    } finally {
+      fetching.value = false;
+    }
+  }
+
+  return {
+    quotes,
+    fetching,
+    itemsPerPage,
+    pages,
+    page,
+    fetchQuotes,
+  };
+};
