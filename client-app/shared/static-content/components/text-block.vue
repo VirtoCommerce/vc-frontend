73bf15ce76cbce31a5af922789811fd36bdca566
Basil Kotov <basilkot@gmail.com>
2022-11-21 13:34:55 +0600

    feat: change text block content type to markdown (#361)

--- client-app/shared/static-content/components/text-block.vue
+++ client-app/shared/static-content/components/text-block.vue
@@ -2,17 +2,23 @@
   <div class="pt-6 pb-16 text-block">
     <div class="w-full max-w-screen-2xl mx-auto px-5 md:px-12">
       <component :is="model.heading" class="text-2xl">{{ model.title }}</component>
-      <div class="text-lg block-content" v-html="model.text"></div>
+      <div class="text-lg block-content" v-html="contentValue"></div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-defineProps({
+import { computed } from "vue";
+
+const props = defineProps({
   model: {
     type: Object,
     required: true,
   },
+});
+
+const contentValue = computed(() => {
+  return props.model.text.html || props.model.text;
 });
 </script>
 
