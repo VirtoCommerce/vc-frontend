c7922c07333f411999ae7c9dc090f7a69903dc50
basilkot <basilkot@gmail.com>
2022-12-08 18:41:52 +0600

    fix: item key as index

--- client-app/shared/static-content/components/demo-product-list.vue
+++ client-app/shared/static-content/components/demo-product-list.vue
@@ -2,11 +2,11 @@
   <div class="pt-6 pb-16">
     <div class="w-full max-w-screen-2xl mx-auto px-5 md:px-12">
       <h2 class="text-2xl text-center mb-4">{{ model.title }}</h2>
-      <VcMarkdownRender :src="model.content" class="text-lg text-center"></VcMarkdownRender>
+      <VcMarkdownRender :src="model.content" class="text-lg text-center" />
     </div>
     <div class="flex flex-row justify-center space-x-4">
-      <div v-for="item in model.products" :key="item.product?.id" :product="item">
-        <div v-show="item.product" class="flex flex-col w-48">
+      <template v-for="(item, index) in model.products" :key="index">
+        <div v-if="item.product" class="flex flex-col w-48">
           <VcImage
             :src="item.product.imgSrc"
             :alt="item.product.name"
@@ -18,7 +18,7 @@
             <div class="whitespace-nowrap">{{ item.product.prices[0].list.formattedAmount }}</div>
           </div>
         </div>
-      </div>
+      </template>
     </div>
   </div>
 </template>
