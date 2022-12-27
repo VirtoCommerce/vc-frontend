8cb550ab57934e39c2e134a5d37e14955ae53ccb
vkrashenko <vkrashenko@me.com>
2022-12-27 14:20:59 +0300

    Add same address for quotes

--- client-app/pages/account/edit-quote.vue
+++ client-app/pages/account/edit-quote.vue
@@ -230,7 +230,7 @@ function openAddressSelectionDialog(addressType: AddressType.Billing | AddressTy
       ),
 
       onResult(selectedAddress: MemberAddressType): void {
-        const quoteAddress = convertToType<QuoteAddressType>(selectedAddress);
+        const quoteAddress = cloneDeep(convertToType<QuoteAddressType>(selectedAddress));
         quoteAddress.addressType = addressType;
         setQuoteAddress(quoteAddress);
         closePopup();
