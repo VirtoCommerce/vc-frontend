badb2ae25272124f205733697fb20efa250f43a2
Artem Makarov <am.work.acc@gmail.com>
2022-12-30 19:14:39 +0300

    refactor: cart line items component

--- client-app/shared/cart/components/index.ts
+++ client-app/shared/cart/components/index.ts
@@ -1,4 +1,5 @@
 export { default as AddBulkItemsToCartResultsPopup } from "./add-bulk-items-to-cart-results-popup.vue";
 export { default as AddToCart } from "./add-to-cart.vue";
+export { default as CartLineItem } from "./cart-line-item.vue";
 export { default as CartLineItems } from "./cart-line-items.vue";
 export { default as ClearCartDialog } from "./clear-cart-dialog.vue";
