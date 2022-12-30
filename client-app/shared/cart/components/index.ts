4cea6b76e0e9b4975689adba357219f5fdd1f85f
Artem Makarov <am.work.acc@gmail.com>
2022-12-30 16:00:29 +0300

    refactor: placing cart page components

--- client-app/shared/cart/components/index.ts
+++ client-app/shared/cart/components/index.ts
@@ -1,2 +1,4 @@
-export { default as AddToCart } from "./add-to-cart.vue";
 export { default as AddBulkItemsToCartResultsPopup } from "./add-bulk-items-to-cart-results-popup.vue";
+export { default as AddToCart } from "./add-to-cart.vue";
+export { default as CartLineItems } from "./cart-line-items.vue";
+export { default as ClearCartDialog } from "./clear-cart-dialog.vue";
