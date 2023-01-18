6bdd2ad3747fef601ad89ec5e4e8c42fa97388ee
Andrew Orlov <ao@virtoway.com>
2023-01-18 10:45:09 +0200

    fix: clear up the code

--- client-app/core/composables/useCatalogMenu.ts
+++ client-app/core/composables/useCatalogMenu.ts
@@ -1,5 +1,5 @@
 import { computed, readonly, ref, shallowRef } from "vue";
-import { Category, childCategories, ChildCategoriesQueryResponseType } from "@/xapi";
+import { childCategories, ChildCategoriesQueryResponseType } from "@/xapi";
 import { Logger } from "@/core";
 
 const loading = ref(true);
