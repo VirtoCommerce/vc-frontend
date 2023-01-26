d60d4898b3ca24e7a63cffa5702f144b15784a6e
alexander.kurilin <alexander.kurilin@virtoway.com>
2023-01-26 09:38:48 +0200

    fix: markup

--- client-app/ui-kit/components/atoms/vendor/vc-vendor.vue
+++ client-app/ui-kit/components/atoms/vendor/vc-vendor.vue
@@ -3,8 +3,8 @@
     <svg height="16" width="16">
       <use href="/static/icons/vendor.svg#icon"></use>
     </svg>
-    <span class="ml-1.5 font-bold uppercase text-16 md:leading-5"
-      >{{ $t("common.labels.vendor") }}: {{ value?.name }}
+    <span class="ml-1.5 font-bold uppercase text-16 md:leading-5">
+      {{ $t("common.labels.vendor") }}: {{ value?.name }}
     </span>
   </div>
 </template>
