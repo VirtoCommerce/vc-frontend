b66116d3af00f5f95eb6205883a5d54ff3902eff
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2023-02-08 14:56:59 +0900

    refactor: remove unused

--- client-app/shared/checkout/components/index.ts
+++ client-app/shared/checkout/components/index.ts
@@ -1,7 +1,6 @@
 export { default as AcceptedGifts } from "./accepted-gifts.vue";
 export { default as AddOrUpdateAddressModal } from "./add-or-update-address-modal.vue";
 export { default as BillingDetailsSection } from "./billing-details-section.vue";
-export { default as CheckoutLabeledBlock } from "./checkout-labeled-block.vue";
 export { default as OrderCommentSection } from "./order-comment-section.vue";
 export { default as OrderSummary } from "./order-summary.vue";
 export { default as SelectAddressModal } from "./select-address-modal.vue";
