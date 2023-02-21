8c255e6cb0b375aea61c2e15857cb6ef930f40d3
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2023-02-21 17:47:22 +0900

    fix: remove unused

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,7 +16,7 @@ module.exports = {
       files: "*.stories.ts",
     },
   ],
-  staticDirs: ['../client-app/public'],
+  staticDirs: ["../client-app/public"],
   addons: ["@storybook/addon-links", "@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/vue3",
   core: {
@@ -33,24 +33,5 @@ module.exports = {
       shouldExtractLiteralValuesFromEnum: true,
       propFilter: (prop) => (prop.parent ? !/node_modules/.test(prop.parent.fileName) : true),
     },
-  },
-  webpackFinal: async (config) => {
-    config.module.rules.push({
-      test: /\.css$/i,
-      use: [
-        {
-          loader: "postcss-loader",
-          options: { implementation: require.resolve("postcss") },
-        },
-      ],
-      include: path.resolve(__dirname, "../"),
-    });
-
-    config.module.rules.push({
-      test: /\.json$/,
-      loader: "json-loader",
-    });
-    // Return the altered config
-    return config;
   },
 };
