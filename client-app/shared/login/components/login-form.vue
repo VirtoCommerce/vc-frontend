49822c2a28f07d70e129b91bef2ed56c277041c1
basilkot <basilkot@gmail.com>
2023-03-10 13:49:53 +0100

    fix: disable eslint rule for v-html in login form

--- client-app/shared/login/components/login-form.vue
+++ client-app/shared/login/components/login-form.vue
@@ -8,6 +8,7 @@
       <div
         class="w-full select-none text-center text-3xl font-bold text-white drop-shadow-lg md:text-5xl lg:w-3/5 lg:text-left"
       >
+        <!-- eslint-disable vue/no-v-html -->
         <div class="mb-8 uppercase leading-tight" v-html="$t('pages.home.main_banner_block.message')"></div>
         <div
           class="flex flex-col items-center justify-center space-y-2 text-xl md:flex-row md:space-y-0 md:space-x-7 md:text-2xl lg:justify-start"
