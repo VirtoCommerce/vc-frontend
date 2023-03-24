b54d2a9a390a1e08c8eb9a6f6f948e4d3b78efe9
Andrew Orlov <andrew.orlov@outlook.com>
2023-03-24 13:29:23 +0200

    feat: add or update address modal on account addresses page (#487)
    * feat: implement add or update address modal for account addresses page
    
    * fix: refactor
    
    * fix: refactor

--- client-app/ui-kit/components/organisms/index.ts
+++ client-app/ui-kit/components/organisms/index.ts
@@ -2,6 +2,7 @@ export { default as VcConfirmationDialog } from "./confirmation-dialog/vc-confir
 export { default as VcPopup } from "./popup/vc-popup.vue";
 export { default as VcSlidingActions } from "./sliding-actions/vc-sliding-actions.vue";
 export { default as VcTable } from "./table/vc-table.vue";
+export { default as VcAddOrUpdateAddressModal } from "./add-or-update-address-modal/vc-add-or-update-address-modal.vue";
 
 // FIXME: Remove private exports
 export { default as TableStatusBadge } from "./table/_internal/table-status-badge/table-status-badge.vue";
