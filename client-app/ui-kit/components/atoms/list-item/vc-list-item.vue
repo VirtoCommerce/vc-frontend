d649dc013ebbb229c3c0fcd43225c8919c1d7929
Aleksandr Vishniakov <av@virtoway.com>
2023-03-29 17:37:02 +0200

    feat: vc-list

---
+++ client-app/ui-kit/components/atoms/list-item/vc-list-item.vue
@@ -0,0 +1,21 @@
+<template>
+  <li class="marker:text-xl marker:text-primary">
+    <slot name="title">
+      <div class="font-extrabold">{{ title }}</div>
+    </slot>
+    <slot>
+      <div class="text-sm font-medium">
+        {{ description }}
+      </div>
+    </slot>
+  </li>
+</template>
+
+<script setup lang="ts">
+interface IProps {
+  title?: string;
+  description?: string;
+}
+
+defineProps<IProps>();
+</script>
