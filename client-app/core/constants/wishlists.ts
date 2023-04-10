275d519c34c5892fbb89a52e39877a708f22f468
Aleksandr Vishniakov <av@virtoway.com>
2023-04-10 08:20:27 +0200

    refactor: files structure for search and related (#475)
    * refactor: files structure for search and related
    
    * fix: imports

---
+++ client-app/core/constants/wishlists.ts
@@ -0,0 +1,1 @@
+export const DEFAULT_WISHLIST_LIMIT = 10;
---
+++ client-app/core/utilities/search/sorting.ts
@@ -0,0 +1,20 @@
+import { DEFAULT_SORT_INFO, SORT_ASCENDING, SORT_DESCENDING } from "@/core/constants/search";
+import type { ISortInfo } from "@/core/types/sorting";
+
+export function getSortingExpression(sort: ISortInfo): string {
+  return `${sort.column}:${sort.direction}`;
+}
+
+export function getSortInfoFromStringExpression(sortInfo: string): ISortInfo {
+  const splitted: string[] = sortInfo.split(":");
+  return splitted.length > 1
+    ? {
+        column: splitted[0],
+        direction: splitted[1],
+      }
+    : DEFAULT_SORT_INFO;
+}
+
+export function toggleSortDirection(currentDirection: string): string {
+  return currentDirection === SORT_ASCENDING ? SORT_DESCENDING : SORT_ASCENDING;
+}
