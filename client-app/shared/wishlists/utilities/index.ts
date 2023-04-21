ab84f89b2dd54270c1dbbfc8025f99c7248ad472
Artem Makarov <am.work.acc@gmail.com>
2023-04-21 10:10:03 +0300

    fix: boolean value of product property on order, quote, wishlist and compare page (#524)

--- client-app/shared/wishlists/utilities/index.ts
+++ client-app/shared/wishlists/utilities/index.ts
@@ -1,13 +1,14 @@
-import { getProductRoute } from "@/core/utilities";
+import { getProductRoute, getPropertiesGroupedByName } from "@/core/utilities";
 import type { LineItemType } from "@/xapi/types";
 
+/** @deprecated Use `prepareLineItem` function */
 export function extendWishListItem(item: LineItemType, countInCart?: number) {
   return {
     ...item,
     extended: {
       isProductExists: !!item.product,
       route: getProductRoute(item.product?.id ?? "", item.product?.slug),
-      displayProperties: item.product?.properties?.slice(0, 3) || [],
+      displayProperties: Object.values(getPropertiesGroupedByName(item.product?.properties ?? [])).slice(0, 3),
       countInCart,
     },
   };
