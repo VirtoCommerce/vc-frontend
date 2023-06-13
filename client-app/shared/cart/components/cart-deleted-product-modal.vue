3b67e60aebbca9ed44653e2c431f9e81ab835ae3
Andrew Orlov <andrew.orlov@outlook.com>
2023-06-13 09:33:23 +0200

    feat: show modal when a customer tries to add deleted products to quote from cart (#597)
    * feat: show modal when a customer tries to add deleted products to quote from cart
    
    * fix: refactor

---
+++ client-app/shared/cart/components/cart-deleted-product-modal.vue
@@ -0,0 +1,21 @@
+<template>
+  <VcPopup :title="$t('common.titles.remove_products')" variant="danger">
+    <template #actions="{ close }">
+      <div class="mx-auto flex space-x-5">
+        <VcButton class="mx-auto inline-flex px-10 uppercase" @click="close()">
+          {{ $t("common.buttons.ok") }}
+        </VcButton>
+      </div>
+    </template>
+
+    <div class="flex items-center justify-center px-5 py-10">
+      <VcIcon name="x-circle" size="xxl" class="mr-4 text-[color:var(--color-danger)]" />
+
+      <span class="text-lg">
+        {{ $t("common.messages.cart_contains_deleted_products") }}
+      </span>
+    </div>
+  </VcPopup>
+</template>
+
+<script setup lang="ts"></script>
