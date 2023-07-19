34343a5ab29ca3e6e49cae7e1927397bb7b2f2e9
NaMax66 <Maxim.Naidovich@virtoway.com>
2023-07-19 12:35:00 +0200

    refactor: form layout

--- client-app/shared/account/components/change-password-form.vue
+++ client-app/shared/account/components/change-password-form.vue
@@ -1,53 +1,56 @@
 <template>
   <form @submit="onSubmit">
-    <div class="px-8 pb-8 pt-5">
-      <VcInput
-        v-model="oldPassword"
-        class="mb-4 w-full"
-        :label="$t('shared.account.change_password_form.current_pass_label')"
-        :placeholder="$t('shared.account.change_password_form.current_pass_placeholder')"
-        type="password"
-        required
-        :message="errors.oldPassword"
-        :error="!!errors.oldPassword"
-        autocomplete="off"
-        :maxlength="MAX_PASS_LENGTH"
-      />
+    <VcInput
+      v-model="oldPassword"
+      class="mb-4"
+      :label="$t('shared.account.change_password_form.current_pass_label')"
+      :placeholder="$t('shared.account.change_password_form.current_pass_placeholder')"
+      type="password"
+      required
+      :message="errors.oldPassword"
+      :error="!!errors.oldPassword"
+      autocomplete="off"
+      :maxlength="MAX_PASS_LENGTH"
+    />
 
-      <VcInput
-        v-model="newPassword"
-        class="mb-4 w-full"
-        :label="$t('shared.account.change_password_form.new_pass_label')"
-        :placeholder="$t('shared.account.change_password_form.new_pass_placeholder')"
-        type="password"
-        required
-        :message="errors.newPassword"
-        :error="!!errors.newPassword"
-        autocomplete="off"
-        :maxlength="MAX_PASS_LENGTH"
-      />
+    <VcInput
+      v-model="newPassword"
+      class="mb-4"
+      :label="$t('shared.account.change_password_form.new_pass_label')"
+      :placeholder="$t('shared.account.change_password_form.new_pass_placeholder')"
+      type="password"
+      required
+      :message="errors.newPassword"
+      :error="!!errors.newPassword"
+      autocomplete="off"
+      :maxlength="MAX_PASS_LENGTH"
+    />
 
-      <VcInput
-        v-model="confirmPassword"
-        class="mb-4 w-full"
-        :label="$t('shared.account.change_password_form.confirm_new_pass_label')"
-        :placeholder="$t('shared.account.change_password_form.confirm_new_pass_placeholder')"
-        type="password"
-        required
-        :message="errors.confirmPassword"
-        :error="!!errors.confirmPassword"
-        autocomplete="off"
-        :maxlength="MAX_PASS_LENGTH"
-      />
+    <VcInput
+      v-model="confirmPassword"
+      class="mb-4"
+      :label="$t('shared.account.change_password_form.confirm_new_pass_label')"
+      :placeholder="$t('shared.account.change_password_form.confirm_new_pass_placeholder')"
+      type="password"
+      required
+      :message="errors.confirmPassword"
+      :error="!!errors.confirmPassword"
+      autocomplete="off"
+      :maxlength="MAX_PASS_LENGTH"
+    />
+    <div class="mt-6 lg:mt-8">
+      <PasswordTips v-if="passwordRequirements" :requirements="passwordRequirements" />
 
       <VcAlert v-for="error in commonErrors" :key="error" color="danger" class="my-4 text-xs" icon>
         {{ error }}
       </VcAlert>
 
-      <PasswordTips v-if="passwordRequirements" :requirements="passwordRequirements" />
-    </div>
-    <div class="flex justify-end gap-x-2 px-8 py-4.5">
-      <VcButton class="w-28" size="sm" type="submit" :loading="loading" :disabled="!meta.valid || meta.pending">
+      <VcButton
+        :disabled="!meta.valid || meta.pending"
+        :loading="loading"
+        type="submit"
+        class="mt-6 w-full lg:mt-3 lg:w-48"
+      >
         {{ $t("shared.account.change_password_form.submit_btn") }}
       </VcButton>
     </div>
