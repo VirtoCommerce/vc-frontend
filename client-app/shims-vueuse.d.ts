889e37ae01ff10e6d7a3ab5a0aa35bb0ddc09f33
Aleksandr Vishniakov <av@virtoway.com>
2023-08-09 19:11:06 +0200

    fix: add quotes

--- client-app/shims-vueuse.d.ts
+++ client-app/shims-vueuse.d.ts
@@ -3,6 +3,6 @@ import "@vueuse/core";
 export {};
 
 declare module "@vueuse/core" {
-  /** @deprecated Use `BREAKPOINTS` from `import { BREAKPOINTS } from @/core/constants` instead */
+  /** @deprecated Use `BREAKPOINTS` from `import { BREAKPOINTS } from "@/core/constants"` instead */
   declare const breakpointsTailwind;
 }
