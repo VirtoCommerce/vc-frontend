9f2aa5606fb61e6788fc871bc71408b84b25adda
Aleksandr Vishniakov <av@virtoway.com>
2023-08-10 10:30:20 +0200

    fix: remove not working deprecation

--- client-app/shims-vueuse.d.ts
+++
@@ -1,8 +0,0 @@
-import "@vueuse/core";
-
-export {};
-
-declare module "@vueuse/core" {
-  /** @deprecated Use `BREAKPOINTS` from `import { BREAKPOINTS } from "@/core/constants"` instead */
-  declare const breakpointsTailwind;
-}
