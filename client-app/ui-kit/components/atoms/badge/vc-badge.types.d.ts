2ea0b8839e0627f79710c6a6e258e5239c70a2bc
Aleksandr Vishniakov <av@virtoway.com>
2023-08-25 09:27:53 +0200

    fix: badge colors

--- client-app/ui-kit/components/atoms/badge/vc-badge.types.d.ts
+++ client-app/ui-kit/components/atoms/badge/vc-badge.types.d.ts
@@ -1,6 +1,13 @@
-import type { ColorType } from "@/core/enums";
+import type { Colors } from "@/core/enums";
 
 declare global {
-  type VcBadgeColorType = Omit<ColorType, "additional">;
+  type VcBadgeColorType =
+    | Colors.primary
+    | Colors.secondary
+    | Colors.neutral
+    | Colors.info
+    | Colors.success
+    | Colors.warning
+    | Colors.danger;
   type VcBadgeVariantType = "solid" | "solid-light" | "outline" | "outline-dark";
 }
