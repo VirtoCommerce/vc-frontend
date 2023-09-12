2122dbea8f6565b0e400b08822c8ff6d46c28da1
NaMax66 <Maxim.Naidovich@virtoway.com>
2023-09-12 13:20:24 +0200

    feat: breadcrumbs show logic

--- client-app/pages/static-page.vue
+++ client-app/pages/static-page.vue
@@ -4,7 +4,7 @@
       v-if="!template.settings?.hideBreadcrumbs || template.settings?.header"
       class="mx-auto mt-7 w-full max-w-screen-2xl px-5 pb-5 md:px-12 lg:pb-10"
     >
-      <VcBreadcrumbs class="mb-3" :items="breadcrumbs"></VcBreadcrumbs>
+      <VcBreadcrumbs v-if="!template.settings?.hideBreadcrumbs" class="mb-3" :items="breadcrumbs"></VcBreadcrumbs>
       <h1 v-if="template.settings?.header" class="text-2xl font-bold uppercase md:text-4xl">
         {{ template.settings.header }}
       </h1>
