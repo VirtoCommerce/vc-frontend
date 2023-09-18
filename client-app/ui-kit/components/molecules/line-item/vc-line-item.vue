45c315bc2b1571cee80c29b1c8850095a1ca236b
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2023-09-18 14:48:25 +0900

    fix(line-item): remove button layout (#748)

--- client-app/ui-kit/components/molecules/line-item/vc-line-item.vue
+++ client-app/ui-kit/components/molecules/line-item/vc-line-item.vue
@@ -56,19 +56,12 @@
           <slot />
         </div>
 
-        <VcButton
-          v-if="removable"
-          class="vc-line-item__remove-button"
-          color="secondary"
-          size="xs"
-          variant="outline"
-          icon
-          :disabled="disabled"
-          @click="$emit('remove')"
-        >
-          <VcIcon v-if="removed" class="text-[--color-success-500]" name="reset" />
-          <VcIcon v-else class="text-[--color-danger-500]" name="delete-2" />
-        </VcButton>
+        <div v-if="removable" class="vc-line-item__remove-button">
+          <VcButton color="secondary" size="xs" variant="outline" icon :disabled="disabled" @click="$emit('remove')">
+            <VcIcon v-if="removed" class="text-[--color-success-500]" name="reset" />
+            <VcIcon v-else class="text-[--color-danger-500]" name="delete-2" />
+          </VcButton>
+        </div>
       </div>
     </div>
 
