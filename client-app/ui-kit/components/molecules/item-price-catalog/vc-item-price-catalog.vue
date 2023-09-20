eaa968f8d8db48315642b38b4a801552fc0a7424
Andrew Orlow <ao@virtoway.com>
2023-09-20 09:28:12 +0200

    feat: show min variation price for product with variations at catalog page

--- client-app/ui-kit/components/molecules/item-price-catalog/vc-item-price-catalog.vue
+++ client-app/ui-kit/components/molecules/item-price-catalog/vc-item-price-catalog.vue
@@ -1,7 +1,7 @@
 <template>
-  <div v-if="variationsMinPrice" class="flex items-end gap-1.5">
-    <div v-t="'common.suffixes.from'" class="pb-0.5 text-14 text-[color:var(--color-price-from)] lg:text-xs"></div>
-    <VcPriceDisplayCatalog :value="variationsMinPrice" class="text-xl font-bold" />
+  <div v-if="hasVariations" class="flex items-end gap-1.5">
+    <div v-t="'common.suffixes.from'" class="pb-0.5 text-14 text-[color:var(--color-price-from)] lg:text-xs" />
+    <VcPriceDisplayCatalog :value="value?.actual" class="text-xl font-bold" />
   </div>
   <div v-else-if="value?.list?.amount > value?.actual?.amount" class="flex flex-wrap items-end gap-1.5">
     <VcPriceDisplayCatalog :class="priceColorClass" :value="value?.actual" class="text-xl font-bold" />
@@ -17,32 +17,15 @@
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
-import type { MoneyType, PriceType, VariationType } from "@/core/api/graphql/types";
+import type { MoneyType, PriceType } from "@/core/api/graphql/types";
 
 interface IProps {
   value?: PriceType | { list: MoneyType; actual: MoneyType };
-  variations?: VariationType[];
+  hasVariations?: boolean;
   priceColorClass?: string;
 }
 
-const props = withDefaults(defineProps<IProps>(), {
+withDefaults(defineProps<IProps>(), {
   priceColorClass: "text-[color:var(--color-price-current)]",
-});
-
-const variationsMinPrice = computed(() => {
-  if (props.variations?.length) {
-    let min = props.variations[0].price?.actual;
-    let tmp;
-    for (let i = 1; i <= props.variations.length - 1; i++) {
-      tmp = props.variations[i].price?.actual;
-      if (tmp?.amount < min?.amount) {
-        min = tmp;
-      }
-    }
-    return min;
-  }
-
-  return null;
 });
 </script>
