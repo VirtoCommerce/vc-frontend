f5521288e62cb5281141c28cf29d52e04461067a
Maksim Naidovich <Maxim.Naidovich@virtoway.com>
2023-09-25 13:02:37 +0200

    PageBuilder. Add "alt" attribute to the "Call to action with image" section (#760)
    * refactor: props
    
    * feat: add image description
    
    * fix: type naming
    
    * feat: add multiline

--- client-app/shared/static-content/components/call-to-action-with-image.vue
+++ client-app/shared/static-content/components/call-to-action-with-image.vue
@@ -1,13 +1,13 @@
 <template>
-  <div class="pb-16 pt-6 lg:py-24" :class="model.background">
+  <div class="pb-16 pt-6 lg:py-24" :class="background">
     <div class="mx-auto w-full max-w-screen-2xl px-5 md:px-12">
       <div class="grid grid-cols-1 items-center gap-x-20 gap-y-10 lg:grid-cols-2">
-        <div :class="[model.imagePosition === 'right' ? 'order-2 lg:order-1' : 'order-1 lg:order-2']">
-          <div class="mb-4 text-2xl font-bold lg:text-5xl">{{ model.title }}</div>
-          <div class="text-lg">{{ model.subtitle }}</div>
-          <div v-if="model.buttons && model.buttons.length" class="mt-6 flex space-x-6 lg:mt-14">
+        <div :class="[imagePosition === 'right' ? 'order-2 lg:order-1' : 'order-1 lg:order-2']">
+          <div class="mb-4 text-2xl font-bold lg:text-5xl">{{ title }}</div>
+          <div class="text-lg">{{ subtitle }}</div>
+          <div v-if="buttons?.length" class="mt-6 flex space-x-6 lg:mt-14">
             <VcButton
-              v-for="(item, index) in model.buttons"
+              v-for="(item, index) in buttons"
               :key="index"
               v-bind="getLinkAttr(item.link)"
               class="flex-1 lg:flex-none"
@@ -18,9 +18,9 @@
         </div>
         <div
           class="aspect-video rounded bg-gray-200"
-          :class="[model.imagePosition === 'right' ? 'order-1 lg:order-2' : 'order-2 lg:order-1']"
+          :class="[imagePosition === 'right' ? 'order-1 lg:order-2' : 'order-2 lg:order-1']"
         >
-          <img :src="model.image" alt="" />
+          <img :src="image" :alt="imageDescription" />
         </div>
       </div>
     </div>
@@ -30,17 +30,24 @@
 <script setup lang="ts">
 import type { RouteLocationRaw } from "vue-router";
 
-defineProps({
-  model: {
-    type: Object,
-    required: true,
-  },
+type ImagePositionType = "left" | "right";
+type ButtonType = {
+  label: string;
+  link: string;
+};
 
-  settings: {
-    type: Object,
-    required: true,
-  },
-});
+interface IProps {
+  id: string;
+  background?: string;
+  title?: string;
+  subtitle?: string;
+  image?: string;
+  imageDescription?: string;
+  imagePosition?: ImagePositionType;
+  buttons?: Array<ButtonType>;
+}
+
+defineProps<IProps>();
 
 type LinkAttrType = { to: RouteLocationRaw } | { link: string } | object;
 const getLinkAttr = (link?: string): LinkAttrType => {
