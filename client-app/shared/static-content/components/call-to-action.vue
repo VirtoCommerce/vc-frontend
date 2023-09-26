f4f258bac162bbeba6e65cce5ff5044fdbe29da9
NaMax66 <Maxim.Naidovich@virtoway.com>
2023-09-26 16:24:43 +0200

    fix: absolute link in CTA

--- client-app/shared/static-content/components/call-to-action.vue
+++ client-app/shared/static-content/components/call-to-action.vue
@@ -1,10 +1,15 @@
 <template>
-  <div class="bg-gray-800 py-10 lg:py-24">
+  <div class="py-10 lg:py-24" :class="background">
     <div class="mx-auto w-full max-w-screen-xl px-5 md:px-12">
       <div class="flex flex-col items-center justify-between space-y-10 lg:block">
-        <div class="text-center text-2xl font-bold text-white lg:text-3xl">{{ model.title }}</div>
+        <div class="text-center text-2xl font-bold text-white lg:text-3xl">{{ title }}</div>
         <div class="flex w-full items-center justify-center space-x-6">
-          <VcButton v-for="(item, index) in model.buttons" :key="index" :to="item.link" class="flex-1 lg:flex-none">
+          <VcButton
+            v-for="(item, index) in buttons"
+            :key="index"
+            v-bind="getLinkAttr(item.link)"
+            class="flex-1 lg:flex-none"
+          >
             {{ item.label }}
           </VcButton>
         </div>
@@ -14,15 +19,32 @@
 </template>
 
 <script setup lang="ts">
-defineProps({
-  model: {
-    type: Object,
-    required: true,
-  },
+import type { RouteLocationRaw } from "vue-router";
 
-  settings: {
-    type: Object,
-    required: true,
-  },
-});
+type ButtonType = {
+  label: string;
+  link: string;
+};
+
+interface IProps {
+  id: string;
+  background?: string;
+  title?: string;
+  buttons?: Array<ButtonType>;
+}
+
+defineProps<IProps>();
+
+type LinkAttrType = { to: RouteLocationRaw } | { link: string } | object;
+
+const getLinkAttr = (link?: string): LinkAttrType => {
+  if (link) {
+    if (link.startsWith("/")) {
+      return { to: link };
+    } else {
+      return { externalLink: link };
+    }
+  }
+  return {};
+};
 </script>
