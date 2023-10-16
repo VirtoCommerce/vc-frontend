390336f02e5cfe43af5896ab3807c6f478228734
NaMax66 <Maxim.Naidovich@virtoway.com>
2023-10-16 13:07:13 +0200

    refactor: format date for input

--- client-app/ui-kit/components/atoms/date-selector/vc-date-selector.vue
+++ client-app/ui-kit/components/atoms/date-selector/vc-date-selector.vue
@@ -4,7 +4,7 @@
     <span v-if="isRequired" class="text-[color:var(--color-danger)]">*</span>
   </div>
   <input
-    :value="dateToIsoDateString(props.modelValue)"
+    :value="formatDate"
     :disabled="isDisabled"
     :name="name"
     class="box-border h-11 w-full min-w-0 appearance-none rounded border border-gray-300 bg-white p-3 text-base leading-none outline-none focus:border-gray-400"
@@ -15,7 +15,7 @@
 </template>
 
 <script setup lang="ts">
-import { dateToIsoDateString } from "@/ui-kit/utilities";
+import { computed } from "vue";
 
 const emit = defineEmits<{
   (e: "update:modelValue", value: Date | undefined): void;
@@ -31,6 +31,18 @@ interface IProps {
   modelValue?: Date;
   errorMessage?: string;
 }
+
+const formatDate = computed(() => {
+  if (!props.modelValue) {
+    return "";
+  }
+
+  const year = props.modelValue.getFullYear();
+  const month = (props.modelValue.getMonth() + 1).toString().padStart(2, "0");
+  const day = props.modelValue.getDate().toString().padStart(2, "0");
+
+  return `${year}-${month}-${day}`;
+});
 
 function change(event: Event): void {
   const newValue: string = (event.target as HTMLInputElement).value;
--- client-app/ui-kit/utilities/index.ts
+++
@@ -1,4 +0,0 @@
-export function dateToIsoDateString(date: Date | undefined) {
-  const lastDateSymbolIndex = 10;
-  return date?.toISOString().substring(0, lastDateSymbolIndex);
-}
