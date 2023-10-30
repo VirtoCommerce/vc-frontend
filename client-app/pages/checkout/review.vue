126fb1f25ef7cff8a4a72f3cb7657afe00be972a
Aleksandr Vishniakov <av@virtoway.com>
2023-10-30 02:36:41 +0200

    fix: avoid page breaks on print

--- client-app/pages/checkout/review.vue
+++ client-app/pages/checkout/review.vue
@@ -22,7 +22,7 @@
       <div class="divide-y print:divide-y-0 lg:divide-y-0">
         <!-- Shipping details -->
         <div v-if="!allItemsAreDigital" class="mt-6 flex flex-col gap-6 md:mt-8 lg:flex-row lg:gap-8">
-          <div class="lg:w-3/5">
+          <div class="print:break-inside-avoid lg:w-3/5">
             <VcLabel>
               {{ $t("shared.checkout.shipping_details_section.labels.shipping_address") }}
             </VcLabel>
@@ -58,7 +58,7 @@
 
         <!-- Payment details -->
         <div class="mb-2 mt-6 flex flex-col gap-6 pt-5 md:mt-8 lg:flex-row lg:gap-8 lg:pt-0">
-          <div class="lg:w-3/5">
+          <div class="print:break-inside-avoid lg:w-3/5">
             <VcLabel>
               {{ $t("shared.checkout.billing_details_section.labels.billing_address") }}
             </VcLabel>
