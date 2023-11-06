6c9fa60d0c9bfc304fb26235b154d409bf584fd3
NaMax66 <Maxim.Naidovich@virtoway.com>
2023-11-06 17:09:35 +0100

    feat: add route

---
+++ client-app/pages/account/change-password.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <div class="mx-5 flex items-center justify-between lg:mx-0">
+      <h2 v-t="'pages.account.change_password.title'" class="text-3xl font-bold uppercase text-gray-800" />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
