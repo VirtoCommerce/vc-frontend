5ce3724cf5146b7231af6d2e7e07264bbe2e82d8
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2023-11-07 18:29:33 +0900

    fix: add stories

--- client-app/ui-kit/components/organisms/pagination/vc-pagination.stories.ts
+++ client-app/ui-kit/components/organisms/pagination/vc-pagination.stories.ts
@@ -12,8 +12,62 @@ const Template: StoryFn<typeof VcPagination> = (args) => ({
   template: '<VcPagination v-bind="args" />',
 });
 
-export const Basic = Template.bind({});
-Basic.args = {
-  page: 11,
+export const Page1 = Template.bind({});
+Page1.args = {
+  page: 1,
   pages: 147,
+};
+
+export const Page5 = Template.bind({});
+Page5.args = {
+  page: 5,
+  pages: 147,
+};
+
+export const Page6 = Template.bind({});
+Page6.args = {
+  page: 6,
+  pages: 147,
+};
+
+export const Page55 = Template.bind({});
+Page55.args = {
+  page: 55,
+  pages: 147,
+};
+
+export const Page142 = Template.bind({});
+Page142.args = {
+  page: 142,
+  pages: 147,
+};
+
+export const Page143 = Template.bind({});
+Page143.args = {
+  page: 143,
+  pages: 147,
+};
+
+export const Page147 = Template.bind({});
+Page147.args = {
+  page: 147,
+  pages: 147,
+};
+
+export const FewPages = Template.bind({});
+FewPages.args = {
+  page: 1,
+  pages: 9,
+};
+
+export const OnePage = Template.bind({});
+OnePage.args = {
+  page: 1,
+  pages: 1,
+};
+
+export const ThreePages = Template.bind({});
+ThreePages.args = {
+  page: 2,
+  pages: 3,
 };
