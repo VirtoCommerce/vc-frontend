f7d5c41eed4d3a512cb278f5711953fd833149fc
NaMax66 <Maxim.Naidovich@virtoway.com>
2023-11-07 12:52:33 +0100

    refactor: remove change password from profile

--- client-app/pages/account/profile.vue
+++ client-app/pages/account/profile.vue
@@ -45,65 +45,6 @@
           disabled
         />
 
-        <div class="my-5 flex items-center">
-          <svg width="54" height="54" class="-ml-0.5 mr-2 text-[color:var(--color-primary)]">
-            <use href="/static/images/polygon-key.svg#main" />
-          </svg>
-
-          <h3
-            v-t="'pages.account.profile.change_password_title'"
-            class="text-xl font-extrabold uppercase text-gray-800"
-          />
-        </div>
-
-        <VcInput
-          v-model="oldPassword"
-          :label="$t('shared.account.change_password_form.current_pass_label')"
-          :placeholder="$t('shared.account.change_password_form.current_pass_placeholder')"
-          :disabled="isSubmitting"
-          :maxlength="64"
-          :message="errors.oldPassword"
-          :error="!!errors.oldPassword"
-          autocomplete="off"
-          type="password"
-          name="oldPassword"
-          class="mb-5"
-        />
-
-        <VcInput
-          :model-value="newPassword"
-          :label="$t('shared.account.change_password_form.new_pass_label')"
-          :placeholder="$t('shared.account.change_password_form.new_pass_placeholder')"
-          :disabled="isSubmitting"
-          :maxlength="64"
-          :required="!!oldPassword"
-          :message="errors.newPassword"
-          :error="!!errors.newPassword"
-          type="password"
-          name="newPassword"
-          class="mb-5"
-          autocomplete="new-password"
-          @update:model-value="newPassword = trimPassword(newPassword, $event)"
-        />
-
-        <VcInput
-          :model-value="confirmNewPassword"
-          :label="$t('shared.account.change_password_form.confirm_new_pass_label')"
-          :placeholder="$t('shared.account.change_password_form.confirm_new_pass_placeholder')"
-          :disabled="isSubmitting"
-          :maxlength="64"
-          :required="!!oldPassword"
-          :message="errors.confirmNewPassword"
-          :error="!!errors.confirmNewPassword"
-          type="password"
-          name="confirmNewPassword"
-          class="mb-5"
-          autocomplete="off"
-          @update:model-value="confirmNewPassword = trimPassword(confirmNewPassword, $event)"
-        />
-
-        <PasswordTips v-if="passwordRequirements" :requirements="passwordRequirements" />
-
         <VcAlert
           v-for="error in commonErrors"
           :key="error"
@@ -134,19 +75,17 @@
 
 <script setup lang="ts">
 import { toTypedSchema } from "@vee-validate/yup";
-import { whenever } from "@vueuse/core";
 import { useField, useForm } from "vee-validate";
 import { computed, ref } from "vue";
 import { useI18n } from "vue-i18n";
-import { object, ref as yupRef, string } from "yup";
+import { object, string } from "yup";
 import { useIdentityErrorTranslator, usePageHead } from "@/core/composables";
-import { PasswordTips, ProfileUpdateSuccessDialog, usePasswordRequirements, useUser } from "@/shared/account";
+import { ProfileUpdateSuccessDialog, useUser } from "@/shared/account";
 import { usePopup } from "@/shared/popup";
 import type { IdentityErrorType } from "@/core/api/graphql/types";
 
 const { t } = useI18n();
-const { user, updateUser, changePassword } = useUser();
-const { passwordRequirements, fetchPasswordRequirements } = usePasswordRequirements();
+const { user, updateUser } = useUser();
 const { openPopup } = usePopup();
 
 usePageHead({
@@ -158,17 +97,6 @@ const validationSchema = toTypedSchema(
     firstName: string().required().max(64),
     lastName: string().required().max(64),
     email: string(),
-    oldPassword: string(),
-    newPassword: string().when("oldPassword", {
-      is: (value: string) => !!value,
-      then: (stringSchema) =>
-        stringSchema.required().notOneOf([yupRef("oldPassword")], t("common.messages.password_new_same_old")),
-    }),
-    confirmNewPassword: string().when("oldPassword", {
-      is: (value: string) => !!value,
-      then: (stringSchema) =>
-        stringSchema.required().oneOf([yupRef("newPassword")], t("common.messages.passwords_do_not_match")),
-    }),
   }),
 );
 
@@ -176,9 +104,6 @@ const initialValues = computed(() => ({
   firstName: user.value.contact?.firstName,
   lastName: user.value.contact?.lastName,
   email: user.value.email,
-  oldPassword: "",
-  newPassword: "",
-  confirmNewPassword: "",
 }));
 
 const { errors, isSubmitting, meta, handleSubmit, resetForm } = useForm({
@@ -189,9 +114,6 @@ const { errors, isSubmitting, meta, handleSubmit, resetForm } = useForm({
 const { value: firstName } = useField<string>("firstName");
 const { value: lastName } = useField<string>("lastName");
 const { value: email } = useField<string>("email");
-const { value: oldPassword } = useField<string>("oldPassword");
-const { value: newPassword } = useField<string>("newPassword");
-const { value: confirmNewPassword } = useField<string>("confirmNewPassword");
 
 const commonErrors = ref<string[]>([]);
 const showErrors = (responseErrors: IdentityErrorType[]) => {
@@ -209,20 +131,6 @@ const getIdentityErrorTranslation = useIdentityErrorTranslator();
 const onSubmit = handleSubmit(async (data) => {
   const results: boolean[] = [];
   commonErrors.value = [];
-
-  if (data.newPassword && data.oldPassword) {
-    const userPasswordUpdateResult = await changePassword({
-      userId: user.value.id,
-      newPassword: data.newPassword,
-      oldPassword: data.oldPassword,
-    });
-
-    results.push(userPasswordUpdateResult.succeeded);
-    if (userPasswordUpdateResult.errors?.length) {
-      showErrors(userPasswordUpdateResult.errors);
-    }
-  }
-
   if (
     (data.firstName && initialValues.value.firstName !== data.firstName) ||
     (data.lastName && initialValues.value.lastName !== data.lastName)
@@ -247,20 +155,4 @@ const onSubmit = handleSubmit(async (data) => {
     });
   }
 });
-
-function trimPassword(oldValue: string, newValue: string | undefined): string {
-  return oldPassword.value ? newValue?.trim() ?? "" : oldValue;
-}
-
-whenever(
-  () => !oldPassword.value,
-  () => {
-    newPassword.value = "";
-    confirmNewPassword.value = "";
-  },
-);
-
-if (!passwordRequirements.value) {
-  fetchPasswordRequirements();
-}
 </script>
