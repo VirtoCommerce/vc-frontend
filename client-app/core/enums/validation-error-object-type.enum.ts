faff0653e2c8ddd97a7ab2370040ce6678f5ad28
Andrew Orlow <ao@virtoway.com>
2023-12-06 08:28:14 +0200

    fix: prevent checkout/place order only for selected cart items with validation errors

---
+++ client-app/core/enums/validation-error-object-type.enum.ts
@@ -0,0 +1,7 @@
+export enum ValidationErrorObjectType {
+  CartProduct = "CartProduct",
+  CatalogProduct = "CatalogProduct",
+  LineItem = "LineItem",
+  Payment = "Payment",
+  Shipment = "Shipment",
+}
