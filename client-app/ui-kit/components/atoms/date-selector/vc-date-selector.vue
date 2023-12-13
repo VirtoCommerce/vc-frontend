e34b2c24c38d8c641dc0081916c9517b8b5d3b87
Aleksandr Vishniakov <av@virtoway.com>
2023-12-13 09:18:47 +0200

    refactor: use useVModel

--- client-app/ui-kit/components/atoms/date-selector/vc-date-selector.vue
+++ client-app/ui-kit/components/atoms/date-selector/vc-date-selector.vue
@@ -4,22 +4,21 @@
     <span v-if="isRequired" class="text-[color:var(--color-danger)]">*</span>
   </div>
   <input
-    :value="modelValue"
+    v-model="dateOnly"
     :disabled="isDisabled"
     :name="name"
     class="box-border h-11 w-full min-w-0 appearance-none rounded border border-gray-300 bg-white p-3 text-base leading-none outline-none focus:border-gray-400"
     type="date"
-    @change="emit('update:modelValue', ($event.target as HTMLInputElement).value)"
   />
   <div v-if="errorMessage" class="text-xs text-[color:var(--color-danger)]">{{ errorMessage }}</div>
 </template>
 
 <script setup lang="ts">
-const emit = defineEmits<{
-  (e: "update:modelValue", value: string | undefined): void;
-}>();
+import { useVModel } from "@vueuse/core";
 
-defineProps<IProps>();
+interface IEmits {
+  (e: "update:modelValue", value: string | undefined): void;
+}
 
 interface IProps {
   label?: string;
@@ -29,4 +28,9 @@ interface IProps {
   modelValue?: string;
   errorMessage?: string;
 }
+
+const emit = defineEmits<IEmits>();
+const props = defineProps<IProps>();
+
+const dateOnly = useVModel(props, "modelValue", emit);
 </script>
