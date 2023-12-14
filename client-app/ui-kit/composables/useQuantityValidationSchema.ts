a22b11594862973a606f7d2e23845277e66144ed
Andrew Orlow <ao@virtoway.com>
2023-12-14 06:50:48 +0200

    fix: refactor

--- client-app/ui-kit/composables/useQuantityValidationSchema.ts
+++ client-app/ui-kit/composables/useQuantityValidationSchema.ts
@@ -3,16 +3,38 @@ import { useI18n } from "vue-i18n";
 import { number } from "yup";
 import type { NumberSchema } from "yup";
 
+const { t } = useI18n();
+
+const maxLimit = 999999;
+
+function setMinMaxForSchema(
+  schema: NumberSchema,
+  payload: { minQuantity?: number; maxQuantity?: number },
+): NumberSchema {
+  if (payload.minQuantity && payload.maxQuantity) {
+    return schema.test(
+      "minMaxValue",
+      t("shared.cart.add_to_cart.errors.min_max", [payload.minQuantity, payload.maxQuantity]),
+      (value) => !!value && value >= payload.minQuantity! && value <= payload.maxQuantity!,
+    );
+  }
+
+  if (payload.minQuantity) {
+    return schema.min(payload.minQuantity, t("shared.cart.add_to_cart.errors.min", [payload.minQuantity]));
+  }
+
+  if (payload.maxQuantity) {
+    return schema.max(payload.maxQuantity, t("shared.cart.add_to_cart.errors.max", [payload.maxQuantity]));
+  }
+
+  return schema.max(maxLimit, t("shared.cart.add_to_cart.errors.max", [maxLimit]));
+}
+
 export function useQuantityValidationSchema(payload: {
   minQuantity?: number;
   maxQuantity?: number;
   availableQuantity?: number;
 }) {
-  const { t } = useI18n();
-
-  const maxLimit = 999999;
-
-  // eslint-disable-next-line sonarjs/cognitive-complexity
   const quantitySchema = computed<NumberSchema>(() =>
     number()
       .typeError(t("shared.cart.add_to_cart.errors.enter_correct_number_message"))
@@ -38,7 +60,14 @@ export function useQuantityValidationSchema(payload: {
           );
         }
 
-        if (payload.minQuantity && payload.maxQuantity) {
+        if (payload.minQuantity || payload.maxQuantity) {
+          return setMinMaxForSchema(schema, {
+            minQuantity: payload.minQuantity,
+            maxQuantity: payload.maxQuantity,
+          });
+        }
+
+        /*if (payload.minQuantity && payload.maxQuantity) {
           return schema.test(
             "minMaxValue",
             t("shared.cart.add_to_cart.errors.min_max", [payload.minQuantity, payload.maxQuantity]),
@@ -52,7 +81,7 @@ export function useQuantityValidationSchema(payload: {
 
         if (payload.maxQuantity) {
           return schema.max(payload.maxQuantity, t("shared.cart.add_to_cart.errors.max", [payload.maxQuantity]));
-        }
+        }*/
 
         return schema.max(maxLimit, t("shared.cart.add_to_cart.errors.max", [maxLimit]));
       }),
