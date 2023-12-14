7d33aeee6438f470c294ce29ec8169ca23d5577e
Aleksandr Vishniakov <av@virtoway.com>
2023-12-14 15:50:53 +0200

    fix: docs

--- client-app/core/utilities/date/index.ts
+++ client-app/core/utilities/date/index.ts
@@ -1,11 +1,19 @@
-/** Converts ISO 8601 date-only to full date */
+/**
+ * Converts ISO 8601 date-only to full date
+ * @param dateOnly ISO 8601 date without time
+ * @returns ISO 8601 date with time
+ */
 export function toStartDateFilterValue(dateOnly?: string): string | undefined {
   if (dateOnly) {
     return toDateObject(dateOnly).toISOString();
   }
 }
 
-/** Adds one day and converts ISO 8610 date-only to full date */
+/**
+ * Adds one day and converts ISO 8610 date-only to full date
+ * @param dateOnly ISO 8601 date without time
+ * @returns ISO 8601 date with time + 1 day (so from [13 Dec to 14 Dec) means full day of 13 Dec)
+ */
 export function toEndDateFilterValue(dateOnly?: string): string | undefined {
   if (dateOnly) {
     const date = toDateObject(dateOnly);
