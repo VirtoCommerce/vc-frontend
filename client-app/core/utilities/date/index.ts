1eebcdbe2e7c6f94203d64dec4431805ae84556d
Aleksandr Vishniakov <av@virtoway.com>
2023-12-20 11:11:35 +0200

    fix: use correct time zone shift & end time for order filters (#893)
    https://virtocommerce.atlassian.net/browse/ST-5571
    
    ---------
    
    Co-authored-by: NaMax66 <Maxim.Naidovich@virtoway.com>

---
+++ client-app/core/utilities/date/index.ts
@@ -0,0 +1,33 @@
+/**
+ * Converts ISO 8601 date-only (YYYY-MM-DD) to full date (YYYY-MM-DDTHH:mm:ss.sssZ)
+ * @param dateOnly ISO 8601 date without time (YYYY-MM-DD)
+ * @returns ISO 8601 date with time (YYYY-MM-DDTHH:mm:ss.sssZ)
+ */
+export function toStartDateFilterValue(dateOnly?: string): string | undefined {
+  if (dateOnly) {
+    return toDateObject(dateOnly).toISOString();
+  }
+}
+
+/**
+ * Converts ISO 8610 date-only (YYYY-MM-DD) to full date (YYYY-MM-DDTHH:mm:ss.sssZ) and adds 1 day without 1 millisecond
+ * @param dateOnly ISO 8601 date without time (YYYY-MM-DD)
+ * @returns ISO 8601 date with time (YYYY-MM-DDTHH:mm:ss.sssZ) + 1 day - 1 millisecond (means full day of 13 Dec)
+ */
+export function toEndDateFilterValue(dateOnly?: string): string | undefined {
+  if (dateOnly) {
+    let date = toDateObject(dateOnly);
+    date.setDate(date.getDate() + 1);
+    date = new Date(date.getTime() - 1);
+    return date.toISOString();
+  }
+}
+
+function toLocalMidnight(dateOnly: string): string {
+  return `${dateOnly}T00:00:00.000`;
+}
+
+function toDateObject(dateOnly: string): Date {
+  const timestamp = Date.parse(toLocalMidnight(dateOnly));
+  return new Date(timestamp);
+}
--- client-app/ui-kit/components/atoms/date-selector/vc-date-selector.vue
+++ client-app/ui-kit/components/atoms/date-selector/vc-date-selector.vue
@@ -4,49 +4,33 @@
     <span v-if="isRequired" class="text-[color:var(--color-danger)]">*</span>
   </div>
   <input
-    :value="formatDate"
+    v-model="dateOnly"
     :disabled="isDisabled"
     :name="name"
     class="box-border h-11 w-full min-w-0 appearance-none rounded border border-gray-300 bg-white p-3 text-base leading-none outline-none focus:border-gray-400"
     type="date"
-    @change="change"
   />
   <div v-if="errorMessage" class="text-xs text-[color:var(--color-danger)]">{{ errorMessage }}</div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
+import { useVModel } from "@vueuse/core";
 
-const emit = defineEmits<{
-  (e: "update:modelValue", value: Date | undefined): void;
-}>();
-
-const props = defineProps<IProps>();
+interface IEmits {
+  (e: "update:modelValue", value: string | undefined): void;
+}
 
 interface IProps {
   label?: string;
   name?: string;
   isRequired?: boolean;
   isDisabled?: boolean;
-  modelValue?: Date;
+  modelValue?: string;
   errorMessage?: string;
 }
 
-const formatDate = computed(() => {
-  if (!props.modelValue) {
-    return "";
-  }
+const emit = defineEmits<IEmits>();
+const props = defineProps<IProps>();
 
-  const year = props.modelValue.getFullYear();
-  const month = (props.modelValue.getMonth() + 1).toString().padStart(2, "0");
-  const day = props.modelValue.getDate().toString().padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-});
-
-function change(event: Event): void {
-  const newValue: string = (event.target as HTMLInputElement).value;
-  const dateValue = newValue ? new Date(Date.parse(newValue)) : undefined;
-  emit("update:modelValue", dateValue);
-}
+const dateOnly = useVModel(props, "modelValue", emit);
 </script>
