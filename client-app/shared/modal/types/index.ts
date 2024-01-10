c30c9de6b88c839f30491d22a3c7df2c5dd03921
Andrew Orlow <ao@virtoway.com>
2024-01-10 11:07:33 +0200

    fix: fix major sonar issues

--- client-app/shared/modal/types/index.ts
+++ client-app/shared/modal/types/index.ts
@@ -5,7 +5,7 @@ export type CloseModalHandleType = () => void;
 export interface IModal {
   id?: string;
   component: Component | "VcConfirmationModal";
-  // FIXME: https://virtocommerce.atlassian.net/browse/ST-5119
+  // TODO: https://virtocommerce.atlassian.net/browse/ST-5119
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   props?: Record<string, any>;
 }
