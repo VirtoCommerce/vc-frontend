b05e8bebf3a437b9fa94961ed4397e78593fd1c8
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-01-18 14:26:21 +0100

    feat: refactor

--- client-app/core/composables/useThemeContext.ts
+++ client-app/core/composables/useThemeContext.ts
@@ -1,34 +1,27 @@
 import { computed, ref } from "vue";
 import { getStoreSettings } from "@/core/api/graphql/settings";
-import type { IThemeConfig, IThemeConfigPreset, IThemeContext } from "../types";
-import type { GetStoreSettingsQuery } from "@/core/api/graphql/types";
-
+import type { IThemeConfig, IThemeContext } from "../types";
 const themeContext = ref<IThemeContext>();
 
 export function useThemeContext() {
-  function fetchThemeContext() {
-    let data: GetStoreSettingsQuery["store"];
-    let settings: IThemeConfigPreset;
+  async function fetchThemeContext() {
+    const [storeSettings, themeSettings] = await Promise.all([
+      getStoreSettings("B2B-store"),
+      import("../../../config/settings_data.json") as Promise<IThemeConfig>,
+    ]);
 
-    const context = getStoreSettings("B2B-store").then((res) => {
-      data = res;
-    });
-    const config = import("../../../config/settings_data.json").then((res) => {
-      const allSettings = res as IThemeConfig;
-      settings =
-        typeof allSettings.current === "string" ? allSettings.presets[allSettings.current] : allSettings.current;
-    });
+    if (!storeSettings || !themeSettings) {
+      return;
+    }
 
-    return Promise.all([config, context]).then(() => {
-      if (!data) {
-        return;
-      }
-      themeContext.value = {
-        ...data,
-        settings,
-        settingsFromPlatform: data.settings,
-      };
-    });
+    const themeConfig =
+      typeof themeSettings.current === "string" ? themeSettings.presets[themeSettings.current] : themeSettings.current;
+
+    themeContext.value = {
+      ...storeSettings,
+      settings: themeConfig,
+      settingsFromPlatform: storeSettings.settings,
+    };
   }
 
   return {
