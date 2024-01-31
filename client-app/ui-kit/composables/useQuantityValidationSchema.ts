801bac51de6a65a3b778fc8601b8d76512603860
Andrew Orlov <andrew.orlov@outlook.com>
2024-01-31 12:43:17 +0200

    feat: actualize wishlist line items quantity validation schema (#880)
    * feat: actualize wishlist line items quantity validation schema
    
    * fix: validation schema for wishlist items quantity fields
    
    * fix: add a sample unit test
    
    * fix: add i18n mock for unit tests
    
    * fix: unit tests
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: added mocks to not-to-dev-dep exclusions
    
    * fix: unit tests ci
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: method naming
    
    * fix: refactor
    
    * fix: refactoring
    
    * fix: refactor validation schema composable
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: refactor
    
    * fix: comparison for available quantity
    
    * fix: make composable schema signature reactive, add one more unit test
    
    * fix: available quantity is less that minimum quantity case is incorrect at all
    
    * fix: update validation schema
    
    ---------
    
    Co-authored-by: Aleksandr Vishniakov <av@virtoway.com>

--- client-app/ui-kit/composables/useQuantityValidationSchema.ts
+++ client-app/ui-kit/composables/useQuantityValidationSchema.ts
@@ -1,34 +1,103 @@
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import { number } from "yup";
+import { LINE_ITEM_QUANTITY_LIMIT } from "@/core/constants";
+import type { Ref } from "vue";
 import type { NumberSchema } from "yup";
 
-export function useQuantityValidationSchema(minQuantity?: number, maxQuantity?: number) {
+export function useQuantityValidationSchema(payload: {
+  minQuantity?: Ref<number | undefined>;
+  maxQuantity?: Ref<number | undefined>;
+  availableQuantity?: Ref<number | undefined>;
+}) {
   const { t } = useI18n();
+
+  const { availableQuantity, minQuantity, maxQuantity } = payload;
+
+  function setAvailabilityForSchema(schema: NumberSchema): NumberSchema {
+    if (availableQuantity?.value && minQuantity?.value && minQuantity.value < availableQuantity.value) {
+      return schema.test(
+        "minMaxValue",
+        t("shared.cart.add_to_cart.errors.min_max", [minQuantity.value, availableQuantity.value]),
+        (value) => !!value && value >= minQuantity.value! && value <= availableQuantity.value!,
+      );
+    }
+
+    if (availableQuantity?.value && minQuantity?.value && minQuantity.value > availableQuantity.value) {
+      return schema.test(
+        "incorrectMinValue",
+        t("shared.cart.add_to_cart.errors.min_not_available", [minQuantity.value]),
+        () => false,
+      );
+    }
+
+    if (availableQuantity?.value && minQuantity?.value && minQuantity.value === availableQuantity.value) {
+      return schema.test(
+        "minValueEqualsAvailableQty",
+        t("shared.cart.add_to_cart.errors.min", [minQuantity.value]),
+        (value) => !!value && value >= minQuantity.value! && value <= availableQuantity.value!,
+      );
+    }
+
+    if (availableQuantity?.value && maxQuantity?.value && maxQuantity.value <= availableQuantity.value) {
+      return schema.max(maxQuantity.value, t("shared.cart.add_to_cart.errors.max", [maxQuantity.value]));
+    }
+
+    if (availableQuantity?.value && maxQuantity?.value && maxQuantity.value > availableQuantity.value) {
+      return schema.max(availableQuantity.value, t("shared.cart.add_to_cart.errors.max", [availableQuantity.value]));
+    }
+
+    if (availableQuantity?.value) {
+      return schema.max(availableQuantity.value, t("shared.cart.add_to_cart.errors.max", [availableQuantity.value]));
+    }
+
+    return schema.max(LINE_ITEM_QUANTITY_LIMIT, t("shared.cart.add_to_cart.errors.max", [LINE_ITEM_QUANTITY_LIMIT]));
+  }
 
   const quantitySchema = computed<NumberSchema>(() =>
     number()
       .typeError(t("shared.cart.add_to_cart.errors.enter_correct_number_message"))
-      .integer()
       .positive()
+      .integer()
       .withMutation((schema) => {
-        if (minQuantity && maxQuantity) {
+        if (
+          availableQuantity?.value &&
+          minQuantity?.value &&
+          maxQuantity?.value &&
+          availableQuantity.value >= minQuantity.value &&
+          availableQuantity.value >= maxQuantity.value
+        ) {
           return schema.test(
             "minMaxValue",
-            t("shared.cart.add_to_cart.errors.min_max", [minQuantity, maxQuantity]),
-            (value) => !!value && value >= minQuantity && value <= maxQuantity,
+            t("shared.cart.add_to_cart.errors.min_max", [minQuantity.value, maxQuantity.value]),
+            (value) => !!value && value >= minQuantity.value! && value <= maxQuantity.value!,
           );
         }
 
-        if (minQuantity) {
-          return schema.min(minQuantity, t("shared.cart.add_to_cart.errors.min", [minQuantity]));
+        if (availableQuantity?.value) {
+          return setAvailabilityForSchema(schema);
         }
 
-        if (maxQuantity) {
-          return schema.max(maxQuantity, t("shared.cart.add_to_cart.errors.max", [maxQuantity]));
+        if (minQuantity?.value && maxQuantity?.value) {
+          return schema.test(
+            "minMaxValue",
+            t("shared.cart.add_to_cart.errors.min_max", [minQuantity.value, maxQuantity.value]),
+            (value) => !!value && value >= minQuantity.value! && value <= maxQuantity.value!,
+          );
         }
 
-        return schema;
+        if (minQuantity?.value) {
+          return schema.min(minQuantity.value, t("shared.cart.add_to_cart.errors.max", [minQuantity.value]));
+        }
+
+        if (maxQuantity?.value) {
+          return schema.max(maxQuantity.value, t("shared.cart.add_to_cart.errors.max", [maxQuantity.value]));
+        }
+
+        return schema.max(
+          LINE_ITEM_QUANTITY_LIMIT,
+          t("shared.cart.add_to_cart.errors.max", [LINE_ITEM_QUANTITY_LIMIT]),
+        );
       }),
   );
 
