17af6495dce741d4893da6b122b39dd51e0a4134
Aleksandr Vishniakov <av@virtoway.com>
2024-02-12 11:57:36 +0200

    fix: storybook stories & addons (#948)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-363
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-1.50.0-pr-948-e950-e95053c5.zip

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -44,7 +44,7 @@ const preview: Preview = {
     controls: {
       matchers: {
         color: /(background|color)$/i,
-        date: /Date$/,
+        date: /Date$/i,
       },
       expanded: true,
     },
