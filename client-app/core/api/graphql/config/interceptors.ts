c3beeffc99b4e4d8f4f6251b85e5929f375a8199
Aleksandr Vishniakov <av@virtoway.com>
2024-02-21 11:03:06 +0200

    fix: better tree-shaking

--- client-app/core/api/graphql/config/interceptors.ts
+++ client-app/core/api/graphql/config/interceptors.ts
@@ -1,10 +1,12 @@
 import { useGlobalInterceptors } from "@/core/api/common";
 
-const { onRequest, onResponse } = useGlobalInterceptors();
+export const apolloFetch = (() => {
+  const { onRequest, onResponse } = useGlobalInterceptors();
 
-export const apolloFetch = async (input: string | Request | URL, init?: RequestInit) => {
-  onRequest.value.forEach((intercept) => intercept(input, init));
-  const response = await fetch(input, init);
-  onResponse.value.forEach((intercept) => intercept(response));
-  return response;
-};
+  return async (input: string | Request | URL, init?: RequestInit) => {
+    onRequest.value.forEach((intercept) => intercept(input, init));
+    const response = await fetch(input, init);
+    onResponse.value.forEach((intercept) => intercept(response));
+    return response;
+  };
+})();
