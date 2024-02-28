c089cba2196b27bb243a28c8c476be98f04778d0
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-02-28 16:59:33 +0900

    fix: checkout payment paddings

--- client-app/pages/checkout/payment.vue
+++ client-app/pages/checkout/payment.vue
@@ -1,34 +1,39 @@
 <template>
   <VcLayoutWithRightSidebar v-if="placedOrder" is-sidebar-sticky>
     <VcWidget size="lg">
-      <div class="flex flex-row items-center justify-between space-x-3 p-4 shadow-lg md:p-6">
-        <div class="min-w-0 truncate">
-          <VcImage
-            :src="payment?.paymentMethod?.logoUrl"
-            class="mr-3.5 inline-block size-8 object-center md:size-9"
-            lazy
+      <template #default-container>
+        <div class="flex flex-row items-center justify-between space-x-3 p-4 shadow-lg md:p-6">
+          <div class="min-w-0 truncate">
+            <VcImage
+              :src="payment?.paymentMethod?.logoUrl"
+              class="mr-3.5 inline-block size-8 object-center md:size-9"
+              lazy
+            />
+
+            <span>{{ $t(`common.methods.payment_by_code.${payment?.paymentMethod?.code}`) }}</span>
+          </div>
+        </div>
+
+        <div class="p-5 md:p-6">
+          <PaymentProcessingRedirection
+            v-if="paymentMethodType === PaymentActionType.Redirection"
+            :order="placedOrder"
           />
 
-          <span>{{ $t(`common.methods.payment_by_code.${payment?.paymentMethod?.code}`) }}</span>
+          <PaymentProcessingAuthorizeNet
+            v-else-if="paymentTypeName === 'AuthorizeNetPaymentMethod'"
+            :order="placedOrder"
+            @success="onPaymentResult(true)"
+            @fail="onPaymentResult(false)"
+          />
+          <PaymentProcessingSkyflow
+            v-else-if="paymentTypeName === 'SkyflowPaymentMethod'"
+            :order="placedOrder"
+            @success="onPaymentResult(true)"
+            @fail="onPaymentResult(false)"
+          />
         </div>
-      </div>
-
-      <div class="p-5 md:p-6">
-        <PaymentProcessingRedirection v-if="paymentMethodType === PaymentActionType.Redirection" :order="placedOrder" />
-
-        <PaymentProcessingAuthorizeNet
-          v-else-if="paymentTypeName === 'AuthorizeNetPaymentMethod'"
-          :order="placedOrder"
-          @success="onPaymentResult(true)"
-          @fail="onPaymentResult(false)"
-        />
-        <PaymentProcessingSkyflow
-          v-else-if="paymentTypeName === 'SkyflowPaymentMethod'"
-          :order="placedOrder"
-          @success="onPaymentResult(true)"
-          @fail="onPaymentResult(false)"
-        />
-      </div>
+      </template>
     </VcWidget>
 
     <template #sidebar>
