db15850531c327d6bcda11da3ad3bdb25d34b2db
Aleksandr Vishniakov <av@virtoway.com>
2024-02-29 19:59:54 +0200

    chore: visualize bundle map

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from "node:url";
 import path from "path";
 import graphql from "@rollup/plugin-graphql";
 import vue from "@vitejs/plugin-vue";
+import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig, loadEnv, splitVendorChunkPlugin } from "vite";
 import { checker } from "vite-plugin-checker";
 import mkcert from "vite-plugin-mkcert";
@@ -66,6 +67,14 @@ export default defineConfig(({ command, mode }): UserConfig => {
           })
         : undefined,
       splitVendorChunkPlugin(),
+      process.env.GENERATE_BUNDLE_MAP
+        ? visualizer({
+            filename: path.resolve(__dirname, "artifacts/bundle-map.html"),
+            brotliSize: true,
+            gzipSize: true,
+            sourcemap: true,
+          })
+        : undefined,
     ],
     resolve: {
       alias: {
@@ -83,7 +92,6 @@ export default defineConfig(({ command, mode }): UserConfig => {
       emptyOutDir: true,
       cssCodeSplit: false,
       sourcemap: true,
-      reportCompressedSize: false,
       rollupOptions: {
         input: {
           main: path.resolve(__dirname, "index.html"),
