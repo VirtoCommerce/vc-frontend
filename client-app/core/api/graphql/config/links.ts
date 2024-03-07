f873d50d90048d01a887849d77477d68788a0862
Aleksandr Vishniakov <av@virtoway.com>
2024-03-07 02:51:34 +0200

    feat: configure web sockets

--- client-app/core/api/graphql/config/links.ts
+++ client-app/core/api/graphql/config/links.ts
@@ -1,11 +1,18 @@
-import { HttpLink, from } from "@apollo/client/core";
+import { HttpLink, from, split } from "@apollo/client/core";
 import { removeTypenameFromVariables } from "@apollo/client/link/remove-typename";
+import { WebSocketLink } from "@apollo/client/link/ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+import { SubscriptionClient } from "subscriptions-transport-ws";
 import { cartLink } from "@/core/api/graphql/cart/links";
 import { errorHandlerLink } from "@/core/api/graphql/config/error-handler";
 import { apolloFetch } from "@/core/api/graphql/config/interceptors";
 import { API_URL } from "@/core/api/graphql/consts";
 
 const httpLink = new HttpLink({ uri: API_URL, fetch: apolloFetch });
+const wsLink = new WebSocketLink(
+  new SubscriptionClient(`wss://${location.host}${API_URL}`, { minTimeout: 0, reconnect: true }),
+);
+
 const sharedLink = from([removeTypenameFromVariables(), errorHandlerLink]);
 
 export const deprecatedLink = from([sharedLink, httpLink]);
@@ -21,5 +28,13 @@ export const link = from([
   sharedLink,
   // Add conditional links here
   cartLink,
-  httpLink,
+  // Send only subscriptions through web sockets
+  split(
+    ({ query }) => {
+      const definition = getMainDefinition(query);
+      return definition.kind === "OperationDefinition" && definition.operation === "subscription";
+    },
+    wsLink,
+    httpLink,
+  ),
 ]);
