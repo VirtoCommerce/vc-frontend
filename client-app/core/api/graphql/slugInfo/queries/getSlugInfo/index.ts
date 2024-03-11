388308d1ee3a8b20108b1154a0fce6f3f1784a8e
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-03-11 14:09:01 +0100

    fix: 404 return btn

--- client-app/core/api/graphql/slugInfo/queries/getSlugInfo/index.ts
+++ client-app/core/api/graphql/slugInfo/queries/getSlugInfo/index.ts
@@ -4,5 +4,5 @@ import type { QuerySlugInfoArgs } from "@/core/api/graphql/types";
 import type { MaybeRefOrGetter } from "vue";
 
 export function getSlugInfo(payload: MaybeRefOrGetter<QuerySlugInfoArgs>) {
-  return useQuery(GetSlugInfoDocument, payload);
+  return useQuery(GetSlugInfoDocument, payload, { fetchPolicy: "cache-and-network" });
 }
