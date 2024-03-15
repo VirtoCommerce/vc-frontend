d5003b3d66c56b145748020aae1bf204fa495228
Andrew Orlow <ao@virtoway.com>
2024-03-15 10:48:06 +0200

    fix: markup

--- client-app/shared/payment/components/payment-processing-skyflow.vue
+++ client-app/shared/payment/components/payment-processing-skyflow.vue
@@ -37,11 +37,13 @@
       </template>
 
       <template #item="{ item }">
-        <VcImage
+        <VcIcon v-if="item.skyflowId.length" class="text-neutral" name="credit-card" size="xl" />
+
+        <!--<VcImage
           v-if="item.skyflowId.length"
           class="size-12 rounded-sm"
           src="/static/icons/payment-methods/credit-card.svg"
-        />
+        />-->
 
         <VcIcon v-else class="size-12 text-success" name="plus-circle-outlined" />
 
