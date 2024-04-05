c445b4a3f15ceb31f2ae956ea918f7e7b11025cf
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-04-05 17:41:55 +0200

    feat: add table

--- client-app/pages/account/notifications.vue
+++ client-app/pages/account/notifications.vue
@@ -3,19 +3,41 @@
     <VcTypography tag="h1">
       {{ $t("shared.account.navigation.links.notifications") }}
     </VcTypography>
-
-    <!-- Skeletons -->
-    <div v-if="loading" class="flex flex-col divide-y lg:space-y-3 lg:divide-none">Sceleton</div>
-    <div v-else></div>
+    <VcWidget>
+      <VcTable hide-default-header :items="items" :loading="loading">
+        <template #desktop-body>
+          <tr v-for="item in items" :key="item.id" class="border-y" :class="{ 'bg-info-50': !item.isRead }">
+            <td class="w-7 py-6 pl-4 pr-1">
+              <div class="flex align-middle">
+                <VcBadge v-if="!item.isRead" color="info" rounded />
+              </div>
+            </td>
+            <VcMarkdownRender :src="item.shortMessage" class="py-6" />
+            <td class="w-24 px-4 py-6">
+              {{ $d(item.createdDate) }}
+            </td>
+          </tr>
+        </template>
+      </VcTable>
+    </VcWidget>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
 import { usePushMessages } from "@/shared/push-messages/composables/usePushMessages";
+
 const showUnreadOnly = ref(false);
 
-const { loading } = usePushMessages(showUnreadOnly);
+const { loading, items } = usePushMessages(showUnreadOnly);
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.vc-typography--variant--h1 {
+  @apply normal-case;
+}
+
+.vc-widget__slot {
+  @apply p-0;
+}
+</style>
