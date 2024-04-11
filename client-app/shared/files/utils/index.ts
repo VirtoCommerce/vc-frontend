50bfdd5bf070f05467003e95fe81044636fc1091
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-04-11 13:55:54 +0200

    feat: useFetch in downloadFile

--- client-app/shared/files/utils/index.ts
+++ client-app/shared/files/utils/index.ts
@@ -1,28 +1,16 @@
-import { useAuth } from "@/core/composables";
+import { toValue } from "vue";
+import { useFetch } from "@/core/api/common";
 
-const { headers } = useAuth();
 export async function downloadFile(fileUrl: string, fileName: string) {
   try {
-    const response = await fetch(fileUrl, {
-      method: "GET",
-      headers: {
-        Authorization: headers.value?.Authorization || "",
-      },
-    });
-
-    if (response.ok) {
-      const blob = await response.blob();
-      const url = window.URL.createObjectURL(blob);
-      const link = document.createElement("a");
-      link.href = url;
-
-      link.setAttribute("download", fileName);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-    } else {
-      console.error("Request error:", response?.statusText);
-    }
+    const { data } = await useFetch(fileUrl).blob();
+    const url = window.URL.createObjectURL(toValue(data) as Blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", fileName);
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   } catch (error) {
     console.error("Request error:", error);
   }
