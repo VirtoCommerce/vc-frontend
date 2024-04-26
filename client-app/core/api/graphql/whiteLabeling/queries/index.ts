080172d6bbfd3c83c808ef80d73906e747fcef25
Andrew Orlow <ao@virtoway.com>
2024-04-26 11:29:57 +0200

    feat: white labeling footer links

--- client-app/core/api/graphql/whiteLabeling/queries/index.ts
+++ client-app/core/api/graphql/whiteLabeling/queries/index.ts
@@ -1,12 +1,54 @@
 import { useLazyQuery } from "@vue/apollo-composable";
-import { GetWhiteLabelingSettingsDocument } from "@/core/api/graphql/types";
+import { gql } from "graphql-tag";
 import { globals } from "@/core/globals";
+import type { DocumentNode } from "graphql";
+
+function getFooterLinksTreeString(level: number): string {
+  return level > 0
+    ? `
+      childItems {
+        title
+        url
+        priority
+        ${getFooterLinksTreeString(level - 1)}
+      }
+    `
+    : "";
+}
+
+function getQueryDocument(maxLevel: number): DocumentNode {
+  return gql`
+    query WhiteLabelingSettings($storeId: String, $userId: String, $cultureName: String, $organizationId: String) {
+      whiteLabelingSettings(
+        storeId: $storeId
+        userId: $userId
+        cultureName: $cultureName
+        organizationId: $organizationId
+      ) {
+        logoUrl
+        secondaryLogoUrl
+        favicons {
+          rel
+          type
+          sizes
+          href
+        }
+        footerLinks {
+          title
+          url
+          priority
+          ${getFooterLinksTreeString(maxLevel)}
+        }
+      }
+    }
+  `;
+}
 
 export function useGetWhiteLabelingSettings(organizationId: string) {
   const { storeId, userId, cultureName } = globals;
 
   return useLazyQuery(
-    GetWhiteLabelingSettingsDocument,
+    getQueryDocument(1),
     { organizationId, storeId, userId, cultureName },
     {
       notifyOnNetworkStatusChange: true,
