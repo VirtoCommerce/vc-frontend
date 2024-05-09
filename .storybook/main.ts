dd94bf0730fe8b286099eb95e3755696a1918175
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-05-09 14:10:41 +0900

    fix: build

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -4,7 +4,7 @@ import type { StorybookConfig } from "@storybook/vue3-vite";
 
 const config: StorybookConfig = {
   stories: ["../client-app/**/*.stories.ts"],
-  staticDirs: ["../storybook-styles"],
+  staticDirs: [{ from: "../storybook-styles/dist", to: "/assets" }],
   addons: [
     "@storybook/addon-viewport",
     "@storybook/addon-links",
