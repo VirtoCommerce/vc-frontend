2c1033a3d1504553c4685dd8352fbefca6c65a1a
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-05-20 20:59:24 +0800

    feat: distinguish environments

---
+++ client-app/shared/layout/components/environmentBadge/environment-badge.vue
@@ -0,0 +1,23 @@
+<template>
+  <div v-if="!!envName" :class="`fixed top-0 z-[21] flex h-1 w-full justify-center bg-[--color-${badgeColor}-500]`">
+    <VcBadge class="h-[22px] min-w-17 items-center rounded-b-md" :color="badgeColor">
+      {{ $t(`env.${envName}`) }}
+    </VcBadge>
+  </div>
+</template>
+
+<script setup lang="ts">
+// import { useThemeContext } from "@/core/composables";
+import { COLORS } from "@/core/constants";
+
+const badgesColors: Record<string, VcBadgeColorType> = {
+  dev: COLORS.secondary,
+  qa: COLORS.info,
+  demo: COLORS.success,
+};
+
+// const { themeContext } = useThemeContext();
+// const envName = themeContext.value.storeSettings?.envName || "dev";
+const envName = "dev";
+const badgeColor: VcBadgeColorType = badgesColors[envName];
+</script>
