81de300fbd098174663ebd57411309eabb6fba8d
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-05-22 13:35:38 +0900

    feat: implement new gifts to checkout/review/acceptedGifts

--- client-app/shared/checkout/components/accepted-gifts.vue
+++ client-app/shared/checkout/components/accepted-gifts.vue
@@ -1,30 +1,12 @@
 <template>
-  <VcWidget :title="$t('shared.checkout.accepted_gifts.title')" prepend-icon="gift" size="lg">
-    <div class="divide-y rounded border">
-      <div class="flex items-center px-6 py-2 text-sm font-bold">
-        <div v-t="'shared.checkout.accepted_gifts.gift_label'" class="grow"></div>
-        <div v-t="'shared.checkout.accepted_gifts.quantity_label'" class="ml-3 w-max"></div>
-        <div class="hidden xl:block xl:w-[6.5rem]"></div>
-      </div>
-
-      <div v-for="gift in items" :key="gift.id" class="flex items-center px-6 py-3">
-        <VcImage :src="gift.imageUrl" class="mr-4 size-[60px] shrink-0 rounded border" lazy />
-
-        <span class="grow text-sm font-bold lg:text-13">
-          {{ gift.name }}
-        </span>
-
-        <div class="ml-3 w-max">
-          <input
-            :disabled="true"
-            class="h-8 w-[5.5rem] rounded border text-center text-sm disabled:bg-gray-100 disabled:text-gray-400"
-            :value="gift.quantity"
-          />
-        </div>
-
-        <div class="hidden xl:block xl:w-[6.5rem]"></div>
-      </div>
-    </div>
+  <VcWidget :title="$t('shared.cart.gifts_section.title')" prepend-icon="gift" size="lg">
+    <VcLineItems :with-header="false">
+      <template #line-items>
+        <VcLineItem v-for="gift in items" :key="gift.id" with-image :name="gift.name" :image-url="gift.imageUrl">
+          <VcQuantity v-model="gift.quantity" disabled />
+        </VcLineItem>
+      </template>
+    </VcLineItems>
   </VcWidget>
 </template>
 
