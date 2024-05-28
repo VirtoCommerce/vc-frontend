4ea04ce90056203440d1b52dd58c62530981fb0c
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-05-28 14:42:48 +0800

    fix: return and fix some optimistic update, add todo

--- client-app/core/api/graphql/push-messages/mutations/markAllPushMessagesRead/index.ts
+++ client-app/core/api/graphql/push-messages/mutations/markAllPushMessagesRead/index.ts
@@ -1,9 +1,35 @@
 import { useMutation } from "@/core/api/graphql/composables/useMutation";
 import { MarkAllPushMessagesReadDocument, OperationNames } from "@/core/api/graphql/types";
+import type { GetPushMessagesQuery } from "@/core/api/graphql/types";
 
 export function useMarkAllPushMessagesRead() {
   return useMutation(MarkAllPushMessagesReadDocument, {
-    // TODO: Implement optimistic response and cache update
+    optimisticResponse: {
+      markAllPushMessagesRead: true,
+    },
+    updateQueries: {
+      [OperationNames.Query.GetPushMessages]: (previousQueryResult, { mutationResult }) => {
+        const pushMessagesQueryResult = previousQueryResult as GetPushMessagesQuery;
+        if (mutationResult.data?.markAllPushMessagesRead) {
+          return {
+            ...pushMessagesQueryResult,
+            pushMessages: {
+              items: pushMessagesQueryResult.pushMessages?.items?.map((pushMessage) => ({
+                ...pushMessage,
+                isRead: true,
+              })),
+              totalCount: pushMessagesQueryResult.pushMessages?.items?.length,
+            },
+            unreadCount: {
+              totalCount: 0,
+            },
+          } satisfies GetPushMessagesQuery;
+        } else {
+          return { ...pushMessagesQueryResult };
+        }
+      },
+    },
+    // Just in case we did something wrong in cache
     refetchQueries: [OperationNames.Query.GetPushMessages],
   });
 }
--- client-app/core/api/graphql/push-messages/mutations/markAllPushMessagesUnread/index.ts
+++ client-app/core/api/graphql/push-messages/mutations/markAllPushMessagesUnread/index.ts
@@ -1,9 +1,36 @@
 import { useMutation } from "@/core/api/graphql/composables/useMutation";
 import { MarkAllPushMessagesUnreadDocument, OperationNames } from "@/core/api/graphql/types";
+import type { GetPushMessagesQuery } from "@/core/api/graphql/types";
 
 export function useMarkAllPushMessagesUnread() {
   return useMutation(MarkAllPushMessagesUnreadDocument, {
-    // TODO: Implement optimistic response and cache update
+    optimisticResponse: {
+      markAllPushMessagesUnread: true,
+    },
+    // TODO: Refactor updateQueries to use update
+    updateQueries: {
+      [OperationNames.Query.GetPushMessages]: (previousQueryResult, { mutationResult }) => {
+        const pushMessagesQueryResult = previousQueryResult as GetPushMessagesQuery;
+        if (mutationResult.data?.markAllPushMessagesUnread) {
+          return {
+            ...pushMessagesQueryResult,
+            pushMessages: {
+              items: pushMessagesQueryResult.pushMessages?.items?.map((pushMessage) => ({
+                ...pushMessage,
+                isRead: false,
+              })),
+              totalCount: pushMessagesQueryResult.pushMessages?.items?.length ?? 0,
+            },
+            unreadCount: {
+              totalCount: pushMessagesQueryResult.pushMessages?.items?.length ?? 0,
+            },
+          } satisfies GetPushMessagesQuery;
+        } else {
+          return { ...pushMessagesQueryResult };
+        }
+      },
+    },
+    // Just in case we did something wrong in cache
     refetchQueries: [OperationNames.Query.GetPushMessages],
   });
 }
