928aa4b1f56d218c9c71a2d7b208599fe25a4708
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-06-05 21:01:28 +0800

    feat: web push notifications

--- client-app/shared/push-messages/composables/useWebPushNotifications.ts
+++ client-app/shared/push-messages/composables/useWebPushNotifications.ts
@@ -2,20 +2,17 @@ import { provideApolloClient } from "@vue/apollo-composable";
 import { createGlobalState } from "@vueuse/core";
 import { initializeApp } from "firebase/app";
 import { isSupported, getMessaging, getToken, deleteToken, onMessage } from "firebase/messaging";
+import { watch, computed } from "vue";
 import { apolloClient } from "@/core/api/graphql";
 import { useAddFcmToken } from "@/core/api/graphql/push-messages/mutations/addFcmToken";
 import { useDeleteFcmToken } from "@/core/api/graphql/push-messages/mutations/deleteFcmToken";
-import { useThemeContext } from "@/core/composables/useThemeContext";
+import { useGetFcmSettings } from "@/core/api/graphql/push-messages/queries/getFcmSettings";
 import { useWhiteLabeling } from "@/core/composables/useWhiteLabeling";
 import { Logger } from "@/core/utilities";
 import { useUser } from "@/shared/account/composables/useUser";
 import type { FcmSettingsType } from "@/core/api/graphql/types";
 import type { Messaging } from "firebase/messaging";
 
-const MODULE_KEYS = {
-  ID: "VirtoCommerce.PushMessages",
-  FCM_SETTINGS: "PushMessages.FcmSettings",
-};
 const REGISTRATION_SCOPE = "/firebase-cloud-messaging-push-scope";
 const DEFAULT_ICON_URL = "/static/icons/favicon-32x32.png";
 const PREFERRED_ICON_PROPERTIES = { type: "image/png", sizes: "32x32" };
@@ -31,7 +28,6 @@ function _useWebPushNotifications() {
   const { isAuthenticated } = useUser();
   const { mutate: addFcmTokenMutation } = useAddFcmToken();
   const { mutate: deleteFcmTokenMutation } = useDeleteFcmToken();
-  const { modulesSettings } = useThemeContext();
 
   const icon =
     favIcons.value?.find(
@@ -54,24 +50,26 @@ function _useWebPushNotifications() {
       vapidKey: "BE1iGYmRsbdI1FB-qTiIUvGrWvC91GCfnXSHtfzcPLgXfQ28fbpKFlD8YlbI7w-aHVepW3Ih17oRB53ceYNaM6k",
     };
 
-    const moduleSettings = modulesSettings?.value?.find(({ moduleId }) => moduleId === MODULE_KEYS.ID) ?? {
-      settings: [],
-    };
-    const fcmSettings =
-      (moduleSettings?.settings?.find(({ name }) => name === MODULE_KEYS.FCM_SETTINGS)
-        ?.value as unknown as FcmSettingsType) ?? firebaseConfig;
-
+    const { result, loading } = useGetFcmSettings();
+    await new Promise((resolve) => {
+      watch(
+        () => loading.value,
+        (value) => !value && resolve(value),
+      );
+    });
+    const fcmSettings = computed(() => (result.value?.fcmSettings ?? {}) as FcmSettingsType);
+    console.log(fcmSettings.value);
     if (initialized) {
-      await getFcmToken(messaging!, fcmSettings.vapidKey);
+      await getFcmToken(messaging!, fcmSettings.value?.vapidKey);
       return;
     }
 
     const firebaseApp = initializeApp(firebaseConfig);
     messaging = getMessaging(firebaseApp);
 
-    await getFcmToken(messaging, fcmSettings.vapidKey);
+    await getFcmToken(messaging, fcmSettings.value.vapidKey);
     const serviceWorkerRegistration = await navigator.serviceWorker.getRegistration(REGISTRATION_SCOPE);
-    serviceWorkerRegistration?.active?.postMessage({ type: "initialize", config: firebaseConfig, icon });
+    serviceWorkerRegistration?.active?.postMessage({ type: "initialize", config: fcmSettings.value, icon });
     initialized = true;
 
     onMessage(messaging, (payload) => {
