6ca2f999ac566b9cb1681c00266c5e3a5981d68a
Andrew Orlov <Andrew.Orlov@virtoworks.com>
2024-06-06 15:31:57 +0200

    fix: apply contact language and currency on sign in

--- client-app/shared/account/composables/useSignMeIn.ts
+++ client-app/shared/account/composables/useSignMeIn.ts
@@ -1,7 +1,8 @@
 import { syncRefs, useAsyncState } from "@vueuse/core";
 import { ref, toValue } from "vue";
 import { useGetMeQuery, useMergeCartMutation } from "@/core/api/graphql";
-import { useAuth } from "@/core/composables";
+import { useCurrency, useLanguages } from "@/core/composables";
+import { useAuth } from "@/core/composables/useAuth";
 import { TabsType, openReturnUrl, useBroadcast } from "@/shared/broadcast";
 import { useShortCart } from "@/shared/cart/composables";
 import type { IdentityErrorType } from "@/core/api/graphql/types";
@@ -14,6 +15,8 @@ export function useSignMeIn(payload: MaybeRefOrGetter<SignMeIn>) {
   const { cart } = useShortCart();
   const { result: me, load: getMe } = useGetMeQuery();
   const { mutate: mergeCart } = useMergeCartMutation();
+  const { supportedLanguages, saveLocaleAndReload } = useLanguages();
+  const { supportedCurrencies, saveCurrencyCodeAndReload } = useCurrency();
 
   const { isLoading: loading, execute: signIn } = useAsyncState(
     async () => {
@@ -22,6 +25,26 @@ export function useSignMeIn(payload: MaybeRefOrGetter<SignMeIn>) {
 
       await getMe();
       await mergeCart({ command: { userId: me.value!.me!.id, secondCartId: cart.value!.id } });
+
+      if (me.value?.me?.contact?.defaultLanguage) {
+        const contactLanguage = supportedLanguages.value.find(
+          (item) => item.cultureName === me.value!.me!.contact!.defaultLanguage,
+        );
+
+        if (contactLanguage) {
+          saveLocaleAndReload(contactLanguage.twoLetterLanguageName);
+        }
+      }
+
+      if (me.value?.me?.contact?.currencyCode) {
+        const contactCurrency = supportedCurrencies.value.find(
+          (item) => item.code === me.value!.me!.contact!.currencyCode,
+        );
+
+        if (contactCurrency) {
+          saveCurrencyCodeAndReload(contactCurrency.code);
+        }
+      }
 
       broadcast.emit(openReturnUrl, undefined, TabsType.ALL);
     },
