68e1a9afdb2e4e0cd7b6cdc898efffd27a28215a
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-06-07 15:35:28 +0800

    fix: remove unused query

--- client-app/core/api/graphql/push-messages/queries/getFcmSettings/index.ts
+++
@@ -1,6 +0,0 @@
-import { useLazyQuery } from "@vue/apollo-composable";
-import { GetFcmSettingsDocument } from "@/core/api/graphql/types";
-
-export function useGetFcmSettings() {
-  return useLazyQuery(GetFcmSettingsDocument);
-}
