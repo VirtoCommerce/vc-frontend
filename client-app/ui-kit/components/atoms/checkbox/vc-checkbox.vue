6895c67f78109f8e6d7070998e1e7a22be8dbb4b
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-06-10 18:03:50 +0900

    feat: implement VcCheckbox new design (#1124)
    ## Description
    
    ## References
    ### Jira-link:
    <!-- Put link to your task in Jira here -->
    https://virtocommerce.atlassian.net/browse/VCST-939
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-1.60.0-pr-1124-d7f4-d7f46f90.zip

--- client-app/ui-kit/components/atoms/checkbox/vc-checkbox.vue
+++ client-app/ui-kit/components/atoms/checkbox/vc-checkbox.vue
@@ -1,26 +1,42 @@
 <template>
-  <label class="flex select-none items-center" :class="disabled ? 'cursor-not-allowed' : 'cursor-pointer'">
-    <input
-      type="checkbox"
-      :aria-label="name"
-      :name="name"
-      :value="value"
-      :disabled="disabled"
-      :checked="checked"
-      :aria-checked="checked"
-      :class="[
-        disabled
-          ? `border-neutral-200 ${checked ? 'bg-neutral-300' : 'bg-neutral-50'}`
-          : `border-neutral-300 bg-additional-50 checked:bg-primary`,
-      ]"
-      class="form-tick size-5 shrink-0 cursor-pointer appearance-none rounded border-2 checked:border-transparent disabled:cursor-not-allowed"
-      @change="change"
-    />
+  <div
+    :class="[
+      'vc-checkbox',
+      `vc-checkbox--size--${size}`,
+      `vc-checkbox--label--${labelPosition}`,
+      {
+        'vc-checkbox--disabled': disabled,
+        'vc-checkbox--checked': checked,
+      },
+    ]"
+  >
+    <label class="vc-checkbox__container">
+      <input
+        type="checkbox"
+        :aria-label="name"
+        :name="name"
+        :value="value"
+        :disabled="disabled"
+        :checked="checked"
+        :indeterminate="indeterminate"
+        :aria-checked="checked"
+        class="vc-checkbox__input"
+        @change="change"
+      />
 
-    <span v-if="$slots.default" :class="{ 'opacity-60': disabled }" class="ml-2 min-w-0 text-sm">
-      <slot v-bind="{ checked }" />
-    </span>
-  </label>
+      <span v-if="$slots.default" class="vc-checkbox__label">
+        <slot v-bind="{ checked }" />
+      </span>
+    </label>
+
+    <VcInputDetails
+      class="vc-checkbox__details"
+      :show-empty="showEmptyDetails"
+      :message="message"
+      :error="error"
+      :single-line="singleLineMessage"
+    />
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -28,29 +44,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { computed } from "vue";
 
+interface IProps {
+  disabled?: boolean;
+  modelValue?: boolean | any[];
+  name?: string;
+  value?: boolean | string | number | object;
+  indeterminate?: boolean;
+  size?: "sm" | "md";
+  labelPosition?: "left" | "right";
+  showEmptyDetails?: boolean;
+  message?: string;
+  error?: boolean;
+  singleLineMessage?: boolean;
+}
+
 const emit = defineEmits<{
   (event: "update:modelValue", value: boolean | any[]): void;
   (event: "change", value: boolean | any[]): void;
 }>();
 
-const props = defineProps({
-  disabled: Boolean,
-
-  modelValue: {
-    type: [Boolean, Array],
-    default: () => [],
-  },
-
-  name: {
-    type: String,
-    default: null,
-  },
-
-  // only for array of model value
-  value: {
-    type: [Boolean, String, Number, Object],
-    default: undefined,
-  },
+const props = withDefaults(defineProps<IProps>(), {
+  modelValue: () => [],
+  size: "md",
+  labelPosition: "right",
 });
 
 const checked = computed<boolean>(() =>
@@ -81,3 +97,98 @@ function change() {
   }
 }
 </script>
+
+<style lang="scss">
+.vc-checkbox {
+  $disabled: "";
+  $left: "";
+  $right: "";
+
+  @apply select-none;
+
+  &--size {
+    &--sm {
+      --size: 1.125rem;
+
+      @apply text-xs;
+    }
+
+    &--md {
+      --size: 1.25rem;
+
+      @apply text-sm;
+    }
+  }
+
+  &--disabled {
+    $disabled: &;
+  }
+
+  &--label {
+    &--left {
+      $left: &;
+    }
+
+    &--right {
+      $right: &;
+    }
+  }
+
+  &__container {
+    @apply flex items-center cursor-pointer;
+
+    #{$disabled} & {
+      @apply cursor-not-allowed;
+    }
+  }
+
+  &__input {
+    @apply size-[--size] shrink-0 cursor-pointer appearance-none rounded border-2 border-neutral-400 bg-additional-50;
+
+    &:checked {
+      @apply border-none bg-primary bg-no-repeat bg-center bg-[length:120%];
+
+      background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 26 26' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M17.1561 8.43073C17.7304 7.85642 18.6616 7.85642 19.2359 8.43073C19.8102 9.00503 19.8102 9.93615 19.2359 10.5105L12.079 17.6673C11.5047 18.2416 10.5736 18.2416 9.99927 17.6673L6.76398 14.432C6.18968 13.8577 6.18968 12.9266 6.76398 12.3523C7.33828 11.778 8.2694 11.778 8.8437 12.3523L11.0391 14.5477L17.1561 8.43073Z' fill='white'/%3e%3c/svg%3e");
+    }
+
+    &:indeterminate {
+      @apply border-none bg-neutral bg-no-repeat bg-center bg-contain;
+
+      background-image: url("data:image/svg+xml,%3csvg viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'%3e%3cpath d='M4 10a1.5 1.5 0 0 1 1.5-1.5h9a1.5 1.5 0 0 1 1.5 1.5v0a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 4 10z' fill='white'/%3e%3c/svg%3e");
+    }
+
+    &:focus {
+      @apply ring ring-primary-100;
+    }
+
+    &:disabled {
+      @apply bg-neutral-50 cursor-not-allowed;
+
+      &:checked,
+      &:indeterminate {
+        @apply border-neutral-200 bg-neutral-200;
+      }
+    }
+  }
+
+  &__label {
+    @apply min-w-0;
+
+    #{$left} & {
+      @apply order-first me-2;
+    }
+
+    #{$right} & {
+      @apply order-last ms-2;
+    }
+
+    #{$disabled} & {
+      @apply opacity-60;
+    }
+  }
+
+  &__details {
+    @apply min-w-full;
+  }
+}
+</style>
