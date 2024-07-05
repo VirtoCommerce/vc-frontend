9b140a6101ae4803495178b28bc1257960d4ce5e
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-07-05 18:55:16 +0800

    fix: update shared types generation and composable typing

--- shared-types/index.d.ts
+++
@@ -1,3 +0,0 @@
-export type * from "../client-app/core/api/graphql/types.js";
-export type { LoggerType } from "../client-app/core/utilities/logger/logger.type.js";
-export type { ICurrency } from "../client-app/core/types/currency.js";
---
+++ shared-types/index.ts
@@ -0,0 +1,11 @@
+export type {
+  Breadcrumb,
+  CartType,
+  CustomerOrderType,
+  LineItemType,
+  OrderLineItemType,
+  Product,
+  VariationType,
+} from "../client-app/core/api/graphql/types.js";
+export type { LoggerType } from "../client-app/core/utilities/logger/logger.type.js";
+export type { ICurrency } from "../client-app/core/types/currency.js";
