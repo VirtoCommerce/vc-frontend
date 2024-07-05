345a28e60e8f93ce1d7cc92ef539c01534fd20a7
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-07-05 19:33:52 +0800

    fix: shared types ci

--- shared-types/index.ts
+++ shared-types/index.ts
@@ -6,6 +6,7 @@ export type {
   OrderLineItemType,
   Product,
   VariationType,
+  Maybe,
 } from "../client-app/core/api/graphql/types.js";
 export type { LoggerType } from "../client-app/core/utilities/logger/logger.type.js";
 export type { ICurrency } from "../client-app/core/types/currency.js";
