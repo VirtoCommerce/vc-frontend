db5e303fbd8805e64e6a5b00594479ec99042484
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-07-05 22:07:53 +0800

    feat: publish-shared-types

--- shared-types/index.ts
+++ shared-types/index.ts
@@ -4,8 +4,8 @@ export type {
   CustomerOrderType,
   LineItemType,
   OrderLineItemType,
-  VariationType,
   Product,
+  VariationType,
 } from "../client-app/core/api/graphql/types.js";
 export type { LoggerType } from "../client-app/core/utilities/logger/logger.type.js";
 export type { ICurrency } from "../client-app/core/types/currency.js";
