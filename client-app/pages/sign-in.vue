7228fc6f45806f81a6d889748d28ac88d985af77
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-07-09 14:46:29 +0200

    feat: add alwaysShowRight prop

--- client-app/pages/sign-in.vue
+++ client-app/pages/sign-in.vue
@@ -1,30 +1,45 @@
 <template>
-  <TwoColumn class="max-w-screen-xl">
+  <TwoColumn class="max-w-screen-xl" :always-show-right="hasIdentityProviders">
     <template #left>
       <VcTypography tag="h1" class="mb-6 lg:mt-5">
         {{ $t("pages.sign_in.header") }}
       </VcTypography>
 
-      <SignInForm />
+      <SignInForm v-if="hasLoginByEmail" />
     </template>
+    <div v-if="hasIdentityProviders && hasLoginByEmail">OR</div>
     <template #right>
       <VcImage
+        v-if="!hasIdentityProviders"
         :alt="$t('common.labels.background_image')"
-        class="max-w-md"
+        class="hidden max-w-md lg:block"
         src="/static/images/sign-in/sign-in-page-image.webp"
         lazy
       />
+      <div v-else>
+        <p>Google</p>
+        <p>Microsoft</p>
+      </div>
     </template>
   </TwoColumn>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 import { usePageHead } from "@/core/composables";
 import { SignInForm } from "@/shared/account";
 import { TwoColumn } from "@/shared/layout";
 
 const { t } = useI18n();
+
+const hasIdentityProviders = computed(() => {
+  return true;
+});
+
+const hasLoginByEmail = computed(() => {
+  return true;
+});
 
 usePageHead({
   title: t("pages.sign_in.meta.title"),
