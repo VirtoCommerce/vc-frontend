61d9912c1fb837b631caa0f36d6b62b8df3117d4
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-07-12 16:39:06 +0800

    fix: unregister service worker if user is not aithentificated or there is no settings

--- client-app/shared/push-messages/composables/useWebPushNotifications/index.ts
+++ client-app/shared/push-messages/composables/useWebPushNotifications/index.ts
@@ -2,6 +2,7 @@ import { useModuleSettings } from "@/core/composables/useModuleSettings";
 import { useUser } from "@/shared/account/composables/useUser";
 
 const MODULE_ID = "VirtoCommerce.PushMessages";
+const REGISTRATION_SCOPE = "/firebase-cloud-messaging-push-scope";
 
 export function useWebPushNotifications() {
   async function init() {
@@ -9,6 +10,10 @@ export function useWebPushNotifications() {
     const { isAuthenticated } = useUser();
 
     if (isAuthenticated.value === false || !hasModuleSettings.value) {
+      const serviceWorkerRegistration = await navigator.serviceWorker.getRegistration(REGISTRATION_SCOPE);
+      if (serviceWorkerRegistration) {
+        void serviceWorkerRegistration.unregister();
+      }
       return;
     }
 
