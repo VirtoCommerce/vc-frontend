173fb095bba443a310d9c6386d34013a0a8ca313
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-07-22 14:55:30 +0800

    fix: replace comment with a test case

--- client-app/pages/matcher/priorityManager.test.ts
+++ client-app/pages/matcher/priorityManager.test.ts
@@ -159,4 +159,29 @@ describe("getVisiblePreviewer", () => {
 
     expect(getVisiblePreviewer(test7)).toBe("loader");
   });
+
+  it("returns null if the highest priority previewer is 'internal' and has 'empty' state (means 404)", () => {
+    const test8: PreviewerStateType[] = [
+      {
+        id: "internal",
+        priority: 3,
+        state: "empty",
+        isActive: true,
+      },
+      {
+        id: "builderIo",
+        priority: 2,
+        state: "empty",
+        isActive: true,
+      },
+      {
+        id: "slugContent",
+        priority: 1,
+        state: "empty",
+        isActive: true,
+      },
+    ];
+
+    expect(getVisiblePreviewer(test8)).toBe(null);
+  });
 });
