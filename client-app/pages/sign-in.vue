e49eeab6ec51f8002d8f9fae26e3724a87d203d8
artem-dudarev <ad@virtoway.com>
2024-07-23 20:31:09 +0200

    feat: add external sign in

--- client-app/pages/sign-in.vue
+++ client-app/pages/sign-in.vue
@@ -1,6 +1,7 @@
 <template>
   <TwoColumn class="max-w-screen-xl">
     <template #left>
+      <VcButton @click="externalSignIn">Azure</VcButton>
       <VcTypography tag="h1" class="mb-6 lg:mt-5">
         {{ $t("pages.sign_in.header") }}
       </VcTypography>
@@ -21,8 +22,28 @@
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
 import { usePageHead } from "@/core/composables";
+import { globals } from "@/core/globals";
+import { getReturnUrlValue } from "@/core/utilities";
 import { SignInForm } from "@/shared/account";
 import { TwoColumn } from "@/shared/layout";
+
+function externalSignIn() {
+  const origin = location.origin;
+  const returnUrl = getReturnUrlValue() ?? "/";
+
+  const oidcUrl = new URL("/signin-oidc", origin);
+
+  const callbackUrl = new URL("/callback", origin);
+  callbackUrl.searchParams.set("returnUrl", returnUrl);
+
+  const url = new URL("/externalsignin", origin);
+  url.searchParams.set("authenticationType", "AzureAD");
+  url.searchParams.set("oidcUrl", oidcUrl.href);
+  url.searchParams.set("callbackUrl", callbackUrl.href);
+  url.searchParams.set("storeId", globals.storeId);
+
+  location.assign(url);
+}
 
 const { t } = useI18n();
 
