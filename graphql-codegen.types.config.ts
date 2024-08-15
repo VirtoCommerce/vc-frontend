53f75be8f2da6399c2395a6aea6eafe099c00750
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-08-15 13:31:10 +0200

    feat: update config

--- graphql-codegen.types.config.ts
+++ graphql-codegen.types.config.ts
@@ -1,28 +1,9 @@
 /* eslint-disable @typescript-eslint/naming-convention */
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
-// Workaround for missed field in CodegenConfig
-// Types is a copy of not exported types from @graphql-codegen/cli with added field
-
-interface CustomDocumentLoaderOptions {
-  skipGraphQLImport?: boolean;
-}
-
-interface CustomDocumentLoader {
-  [path: string]: CustomDocumentLoaderOptions;
-}
-
-type OperationDocument = CustomDocumentLoader;
-
-type CodegenConfigWorkaround = { documents: OperationDocument | OperationDocument[] };
-
-const graphQLCodegenTypesConfig: CodegenConfig | CodegenConfigWorkaround = {
+const graphQLCodegenTypesConfig: CodegenConfig = {
   schema: `client-app/core/api/graphql/schema.json`,
-  documents: {
-    "client-app/**/*.(graphql|gql)": {
-      skipGraphQLImport: false,
-    },
-  },
+  documents: "client-app/**/*.(graphql|gql)",
   generates: {
     "client-app/core/api/graphql/types.ts": {
       plugins: [
@@ -36,39 +17,47 @@ const graphQLCodegenTypesConfig: CodegenConfig | CodegenConfigWorkaround = {
         "typed-document-node",
         "named-operations-object",
       ],
-      config: {
-        dedupeFragments: true,
-        identifierName: "OperationNames",
-        maybeValue: "T",
-        scalars: {
-          BigInt: "number",
-          Byte: "number",
-          Date: "string",
-          DateOnly: "string",
-          Decimal: "number",
-          DynamicPropertyValue: "string | number | boolean | null",
-          Guid: "string",
-          Half: "number",
-          Long: "number",
-          Milliseconds: "number",
-          ModuleSettingValue: "string | number | boolean | null",
-          OptionalDecimal: "number | undefined",
-          OptionalNullableDecimal: "number | null | undefined",
-          OptionalString: "string | undefined",
-          PropertyValue: "string | number | boolean | null",
-          SByte: "number",
-          Seconds: "number",
-          Short: "number",
-          TimeOnly: "string",
-          UInt: "number",
-          ULong: "number",
-          Uri: "string",
-          UShort: "number",
-        },
-        skipTypename: true,
-        useTypeImports: true,
-      },
     },
+    "client-app/": {
+      preset: "near-operation-file",
+      presetConfig: {
+        baseTypesPath: "core/api/graphql/types.ts",
+      },
+      plugins: ["typescript-operations", "typed-document-node"],
+    },
+  },
+  config: {
+    dedupeFragments: true,
+    identifierName: "OperationNames",
+    maybeValue: "T",
+    scalars: {
+      BigInt: "number",
+      Byte: "number",
+      Date: "string",
+      DateOnly: "string",
+      Decimal: "number",
+      DynamicPropertyValue: "string | number | boolean | null",
+      Guid: "string",
+      Half: "number",
+      Long: "number",
+      Milliseconds: "number",
+      ModuleSettingValue: "string | number | boolean | null",
+      OptionalDecimal: "number | undefined",
+      OptionalNullableDecimal: "number | null | undefined",
+      OptionalString: "string | undefined",
+      PropertyValue: "string | number | boolean | null",
+      SByte: "number",
+      Seconds: "number",
+      Short: "number",
+      TimeOnly: "string",
+      UInt: "number",
+      ULong: "number",
+      Uri: "string",
+      UShort: "number",
+    },
+    skipTypename: true,
+    useTypeImports: true,
+    skipGraphQLImport: true,
   },
 };
 
