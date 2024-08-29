4e99dc2292f594fc50faf2faa1f7903c09c7f9e0
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-08-29 17:50:04 +0900

    fix: module settings

--- client-app/shared/catalog/composables/useRecommendedProducts.ts
+++ client-app/shared/catalog/composables/useRecommendedProducts.ts
@@ -1,18 +1,27 @@
 import { ref, computed, readonly } from "vue";
 import { searchRecommendedProducts } from "@/core/api/graphql/catalog";
+import { useModuleSettings } from "@/core/composables/useModuleSettings";
+import { MODULE_ID_XRECOMMEND } from "@/core/constants/modules";
 import { Logger } from "@/core/utilities";
 import type { RecommendedProductsSearchParamsType } from "@/core/api/graphql/catalog";
 import type { Product } from "@/core/api/graphql/types";
 
+const RECOMMENDATIONS_ENABLED = "XRecommend.RecommendationsEnabled";
+
 export function useRecommendedProducts() {
   const loading = ref(true);
   const recommendedProducts = ref<Product[]>([]);
+  const { hasModuleSettings, isEnabled } = useModuleSettings(MODULE_ID_XRECOMMEND);
 
   async function fetchRecommendedProducts(params: RecommendedProductsSearchParamsType) {
     loading.value = true;
     try {
-      const associations = await searchRecommendedProducts(params);
-      recommendedProducts.value = associations.products ?? [];
+      if (hasModuleSettings.value && isEnabled(RECOMMENDATIONS_ENABLED)) {
+        const associations = await searchRecommendedProducts(params);
+        recommendedProducts.value = associations.products ?? [];
+      } else {
+        recommendedProducts.value = [];
+      }
     } catch (e) {
       Logger.error("useRecommendedProducts.fetchRecommendedProducts", e);
       throw e;
