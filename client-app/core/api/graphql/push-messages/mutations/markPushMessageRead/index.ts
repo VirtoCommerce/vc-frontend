60098e03d794237b63cd2d1effd2eb4eb2c37156
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-08-30 15:01:59 +0800

    chore: replace updateQueries with update (#1258)

--- client-app/core/api/graphql/push-messages/mutations/markPushMessageRead/index.ts
+++ client-app/core/api/graphql/push-messages/mutations/markPushMessageRead/index.ts
@@ -1,35 +1,56 @@
 import { useMutation } from "@/core/api/graphql/composables/useMutation";
+import {
+  PUSH_MESSAGE_CACHE_ID,
+  UNREAD_COUNT_CACHE_ID,
+  UNREAD_COUNT_WITH_HIDDEN_CACHE_ID,
+} from "@/core/constants/notifications";
 import { MarkPushMessageReadDocument, OperationNames, PushMessageFragmentDoc } from "../../types";
-import type { GetPushMessagesQuery } from "../../types";
+import type { PushMessageConnection, PushMessageType } from "../../types";
 
-export function useMarkPushMessageRead(optimistic = true) {
+type UnreadCountType = Pick<PushMessageConnection, "totalCount">;
+
+export function useMarkPushMessageRead() {
   return useMutation(MarkPushMessageReadDocument, {
     optimisticResponse: {
       markPushMessageRead: true,
     },
     update(cache, result, { variables }) {
-      if (result.data?.markPushMessageRead) {
-        cache.updateFragment(
-          {
-            id: `PushMessageType:${variables?.command?.messageId}`,
-            fragment: PushMessageFragmentDoc,
-          },
-          (pushMessage) => ({ ...pushMessage!, isRead: true }),
-        );
+      if (!result.data?.markPushMessageRead) {
+        return;
       }
-    },
-    updateQueries: (!optimistic && {}) || {
-      [OperationNames.Query.GetPushMessages]: (previousQueryResult) => {
-        const pushMessagesQueryResult = previousQueryResult as GetPushMessagesQuery;
-        return {
-          ...pushMessagesQueryResult,
-          unreadCount: {
-            totalCount: pushMessagesQueryResult.unreadCount?.totalCount
-              ? pushMessagesQueryResult.unreadCount.totalCount - 1
-              : 0,
+
+      let message: PushMessageType | null = null;
+
+      cache.updateFragment(
+        {
+          id: `${PUSH_MESSAGE_CACHE_ID}${variables?.command?.messageId}`,
+          fragment: PushMessageFragmentDoc,
+        },
+        (pushMessage) => {
+          message = pushMessage;
+          return pushMessage ? { ...pushMessage, isRead: true } : null;
+        },
+      );
+
+      cache.modify({
+        fields: {
+          [UNREAD_COUNT_CACHE_ID]: (value) => {
+            const unreadCount = value as UnreadCountType;
+            if (message?.isHidden) {
+              return unreadCount;
+            }
+            return {
+              totalCount: unreadCount.totalCount ? unreadCount.totalCount - 1 : 0,
+            };
           },
-        } satisfies GetPushMessagesQuery;
-      },
+          [UNREAD_COUNT_WITH_HIDDEN_CACHE_ID]: (value) => {
+            const unreadCountWithHidden = value as UnreadCountType;
+            return {
+              totalCount: unreadCountWithHidden.totalCount ? unreadCountWithHidden.totalCount - 1 : 0,
+            };
+          },
+        },
+      });
     },
     // Just in case we did something wrong in cache
     refetchQueries: [OperationNames.Query.GetPushMessages],
