d9afc1412f6661d145c1b65c381843533ccf188a
mihey8800 <mihey8800@gmail.com>
2024-09-06 17:21:37 +0700

    fix: yarn version check (#1285)
    feat: added script to check Yarn version
    ## Description
    
    ## References
    ### Jira-link:
    <!-- Put link to your task in Jira here -->
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.5.0-pr-1285-2ebd-2ebd1780.zip

---
+++ scripts/check-yarn-version.js
@@ -0,0 +1,58 @@
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+import { dirname, join } from "path";
+import { fileURLToPath } from "url";
+
+// Recreate __dirname in ES module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Get the installed Yarn version
+function getInstalledYarnVersion() {
+  try {
+    return execSync("yarn --version").toString().trim();
+  } catch (error) {
+    console.error("Error checking installed Yarn version:", error);
+    process.exit(1);
+  }
+}
+
+// Get the required Yarn version from package.json (located in the main project folder)
+function getRequiredYarnVersion() {
+  try {
+    // Resolve the path to the package.json file in the root folder
+    const packageJsonPath = join(__dirname, "../package.json");
+    const packageJson = JSON.parse(readFileSync(packageJsonPath, "utf8"));
+    return packageJson.packageManager.split("@")[1];
+  } catch (error) {
+    console.error(
+      'Error checking Yarn version in package.json. Please specify it like "packageManager": "yarn@4.4.0"',
+      error,
+    );
+    process.exit(1);
+  }
+}
+
+// Compare versions
+function compareYarnVersions(installedVersion, requiredVersion) {
+  const [installedMajor, installedMinor, installedPatch] = installedVersion.split(".").map(Number);
+  const [requiredMajor, requiredMinor, requiredPatch] = requiredVersion.split(".").map(Number);
+
+  if (
+    installedMajor < requiredMajor ||
+    (installedMajor === requiredMajor && installedMinor < requiredMinor) ||
+    (installedMajor === requiredMajor && installedMinor === requiredMinor && installedPatch < requiredPatch)
+  ) {
+    console.error(
+      `Your installed Yarn version (${installedVersion}) is less than the required version (${requiredVersion}). Please upgrade.`,
+    );
+    process.exit(1);
+  } else {
+    console.log(`Installed Yarn version (${installedVersion}) meets the requirement (${requiredVersion}).`);
+  }
+}
+
+// Run the checks
+const installedVersion = getInstalledYarnVersion();
+const requiredVersion = getRequiredYarnVersion();
+compareYarnVersions(installedVersion, requiredVersion);
