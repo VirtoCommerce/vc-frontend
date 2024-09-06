eebcced4c1f7517104328b6af387d24238d72495
Mikhail Lyakhov <mihey8800@gmail.com>
2024-09-06 11:59:10 +0300

    fix: yarn version check feat: added script to check Yarn version 4.1.0 or higher

---
+++ scripts/check-yarn-version.js
@@ -0,0 +1,15 @@
+import { execSync } from "child_process";
+
+try {
+  const version = execSync("yarn --version").toString().trim();
+  const [major, minor] = version.split(".").map(Number);
+
+  // Check if major version is less than 4 or if the minor version is less than 1 when major is 4
+  if (major < 4 || (major === 4 && minor < 1)) {
+    console.error(`Your Yarn version is ${version}. Please upgrade to Yarn v4.1.0 or higher.`);
+    process.exit(1);
+  }
+} catch (error) {
+  console.error("Error checking Yarn version:", error);
+  process.exit(1);
+}
