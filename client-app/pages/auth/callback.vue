89daf44d1fa28c7af39b6c0fcb984ce920b22a17
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-09-10 14:11:08 +0200

    feat: add merge cart during auth callback handling

--- client-app/pages/auth/callback.vue
+++ client-app/pages/auth/callback.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
+import { useMergeCartMutation } from "@/core/api/graphql";
 import { useAuth } from "@/core/composables/useAuth";
 import { Logger } from "@/core/utilities";
+import { useUser } from "@/shared/account";
 import { TabsType, openReturnUrl, useBroadcast } from "@/shared/broadcast";
+import { useShortCart } from "@/shared/cart";
 
 const broadcast = useBroadcast();
 const { externalSignInCallback } = useAuth();
+const { cart } = useShortCart();
+const { mutate: mergeCart } = useMergeCartMutation();
+const { user } = useUser();
 
 async function externalSignIn(): Promise<void> {
   try {
     await externalSignInCallback();
+    if (cart.value?.id) {
+      await mergeCart({ command: { userId: user.value.id, secondCartId: cart.value.id } });
+    }
     broadcast.emit(openReturnUrl, undefined, TabsType.ALL);
   } catch (e) {
     Logger.error(externalSignIn.name, e);
