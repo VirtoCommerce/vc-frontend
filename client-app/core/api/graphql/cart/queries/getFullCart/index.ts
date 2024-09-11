a1425ab0f2fb1b508be4136d4c00554b39f8beae
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-09-11 19:49:31 +0200

    fix: cart infinity loading

--- client-app/core/api/graphql/cart/queries/getFullCart/index.ts
+++ client-app/core/api/graphql/cart/queries/getFullCart/index.ts
@@ -4,7 +4,6 @@ import { GetFullCartDocument } from "@/core/api/graphql/types";
 
 export function useGetFullCartQuery() {
   return useLazyQuery(GetFullCartDocument, useCartQueryVariables(), {
-    notifyOnNetworkStatusChange: true,
     fetchPolicy: "cache-and-network",
     nextFetchPolicy: "cache-first",
     keepPreviousResult: true,
