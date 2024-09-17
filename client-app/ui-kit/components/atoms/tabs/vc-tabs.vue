c9897ed553ec151b50c289889e4cd6707f7add28
Maksim Naidovich <Maxim.Naidovich@virtoway.com>
2024-09-17 12:27:03 +0200

    chore: add generic to vc-tabs (#1302)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-1685
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.6.0-pr-1302-e83b-e83b4756.zip

--- client-app/ui-kit/components/atoms/tabs/vc-tabs.vue
+++ client-app/ui-kit/components/atoms/tabs/vc-tabs.vue
@@ -14,82 +14,67 @@
         class="block appearance-none border-b-[3px] border-transparent pb-2.5 pt-3 font-bold"
       >
         <slot name="item" v-bind="{ item, index }">
-          {{ textField && item ? item[textField] : item }}
+          {{ textField && isItemObject(item) ? item[textField] : item }}
         </slot>
       </span>
     </button>
   </div>
 </template>
 
-<script setup lang="ts">
-/* eslint-disable @typescript-eslint/no-explicit-any */
-import type { PropType } from "vue";
+<script setup lang="ts" generic="T extends ItemType | string | number, U extends string | number">
+export type ItemType = {
+  [key: string | number]: unknown;
+};
+
+type EmitType = T extends ItemType ? T[U] : T;
 
 const emit = defineEmits<{
-  (event: "update:modelValue", value: any): void;
-  (event: "change", value: any): void;
+  (event: "update:modelValue", value: EmitType): void;
+  (event: "change", value: EmitType): void;
 }>();
 
-const props = defineProps({
-  isDisabled: {
-    type: Boolean,
-    default: false,
-  },
-
-  modelValue: {
-    type: [Object, String],
-    default: undefined,
-  },
-
-  items: {
-    type: Array as PropType<any[]>,
-    default: () => [],
-  },
-
+const props = withDefaults(defineProps<IProps>(), {
+  isDisabled: false,
+  items: () => [],
+  size: "md",
+});
+interface IProps {
+  isDisabled?: boolean;
+  modelValue: T extends ItemType ? T[U] : T;
+  items?: readonly T[];
   /**
    * The property of the object that will be displayed in the "selected" and "item" slots.
    * (Optional) Only for objects array of modelValue.
    */
-  textField: {
-    type: String,
-    default: undefined,
-  },
+  textField?: string;
 
   /**
    * An object property passed as a new value for the "modelValue" or "change" events
    * (Optional) Only for objects array of modelValue.
    */
-  valueField: {
-    type: String,
-    default: undefined,
-  },
+  valueField?: U;
+  size?: "sm" | "md" | "lg";
+}
 
-  // TODO: implement resizing
-  size: {
-    type: String as PropType<"sm" | "md" | "lg">,
-    default: "md",
-
-    validator(value: string) {
-      return ["sm", "md", "lg"].includes(value);
-    },
-  },
-});
-
-function isActiveItem(item: any): boolean {
-  const itemValue = props.valueField && item ? item[props.valueField] : item;
+function isActiveItem(item: T): boolean {
+  const itemValue = props.valueField && isItemObject(item) ? item[props.valueField] : item;
   return itemValue === props.modelValue;
 }
 
-function select(item?: any) {
+function select(item: T) {
   if (props.isDisabled) {
     return;
   }
 
-  const newValue = props.valueField && item ? item[props.valueField] : item;
+  const newValue = (props.valueField && isItemObject(item) ? item[props.valueField] : item) as EmitType | undefined;
 
-  if (newValue !== props.modelValue) {
+  if (newValue && newValue !== props.modelValue) {
     emit("update:modelValue", newValue);
     emit("change", newValue);
   }
+}
+
+function isItemObject(item: unknown): item is ItemType {
+  return typeof item === "object" && item !== null;
 }
 </script>
