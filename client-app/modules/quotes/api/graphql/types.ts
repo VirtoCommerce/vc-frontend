294fdbebaf5706780ae352fa57c37f717c90ebbf
Maksim Naidovich <Maxim.Naidovich@virtoway.com>
2024-09-20 13:17:33 +0200

    chore: remove unused "quotesEnabled" and "isSpa" from "getStore" query (#1322)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-1867
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.6.0-pr-1322-6a40-6a404d2a.zip

--- client-app/modules/quotes/api/graphql/types.ts
+++ client-app/modules/quotes/api/graphql/types.ts
@@ -2046,6 +2046,7 @@ export type InputProcessOrderPaymentType = {
 export type InputPushHistoricalEventType = {
   eventType?: InputMaybe<Scalars['String']['input']>;
   productId?: InputMaybe<Scalars['String']['input']>;
+  sessionId?: InputMaybe<Scalars['String']['input']>;
   storeId?: InputMaybe<Scalars['String']['input']>;
 };
 
