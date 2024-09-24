d4a3fededb64d00b8eef4621b6e870c217292e86
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-09-24 15:48:29 +0200

    feat: move links to separate components

--- client-app/shared/layout/components/header/_internal/bottom-header.vue
+++ client-app/shared/layout/components/header/_internal/bottom-header.vue
@@ -35,34 +35,10 @@
 
       <ul class="-mx-2 flex items-center">
         <li v-for="item in desktopMainMenuItems" :key="item.id">
-          <BottomHeaderLink v-if="item.id === 'compare'" :link="item" :count="productsIds.length">
-            {{ item.title }}
-          </BottomHeaderLink>
-
-          <BottomHeaderLink v-else-if="item.id === 'cart'" :link="item" :count="cart?.itemsQuantity">
-            {{ item.title }}
-          </BottomHeaderLink>
-
-          <template v-else-if="item.id === 'push-messages'">
-            <PushMessages v-if="isPushMessagesActive" :offset-options="20">
-              <template #trigger="{ totalCount, unreadCount }">
-                <BottomHeaderLink :link="item" :count="unreadCount">
-                  <template #icon>
-                    <transition :name="unreadCount ? 'shake' : ''" mode="out-in">
-                      <svg v-if="item.icon" :key="totalCount" height="24" width="24" class="mb-0.5 text-primary">
-                        <use :href="item.icon" />
-                      </svg>
-                    </transition>
-                  </template>
-                  {{ item.title }}
-                </BottomHeaderLink>
-              </template>
-            </PushMessages>
-          </template>
-
-          <BottomHeaderLink v-else :link="item">
-            {{ item.title }}
-          </BottomHeaderLink>
+          <LinkCompare v-if="item.id === 'compare'" :item="item" />
+          <LinkCart v-else-if="item.id === 'cart'" :item="item" />
+          <LinkPushMessages v-else-if="item.id === 'push-messages'" :item="item" />
+          <LinkDefault v-else :item="item" />
         </li>
       </ul>
     </nav>
@@ -93,21 +69,18 @@ import { computed, ref, shallowRef } from "vue";
 import { useRouter } from "vue-router";
 import { useNavigations, useWhiteLabeling } from "@/core/composables";
 import { useUser } from "@/shared/account/composables/useUser";
-import { useCompareProducts } from "@/shared/compare";
 import { SearchBar } from "@/shared/layout";
-import { useDeferShortCart } from "@/shared/layout/composables";
-import { isActive as isPushMessagesActive } from "@/shared/push-messages/composables/usePushMessages";
-import BottomHeaderLink from "./bottom-header-link.vue";
 import CatalogMenu from "./catalog-menu.vue";
 import type { StyleValue } from "vue";
-import PushMessages from "@/shared/push-messages/components/push-messages.vue";
+import LinkCart from "@/shared/layout/components/header/_internal/link-components/link-cart.vue";
+import LinkCompare from "@/shared/layout/components/header/_internal/link-components/link-compare.vue";
+import LinkDefault from "@/shared/layout/components/header/_internal/link-components/link-default.vue";
+import LinkPushMessages from "@/shared/layout/components/header/_internal/link-components/link-push-messages.vue";
 
 const router = useRouter();
 const { organization } = useUser();
-const { cart } = useDeferShortCart();
 const { logoUrl } = useWhiteLabeling();
 const { catalogMenuItems, desktopMainMenuItems } = useNavigations();
-const { productsIds } = useCompareProducts();
 
 const bottomHeader = ref<HTMLElement | null>(null);
 const catalogMenuElement = shallowRef<HTMLElement | null>(null);
---
+++ client-app/shared/layout/components/header/_internal/link-components/link-cart.vue
@@ -0,0 +1,14 @@
+<template>
+  <BottomHeaderLink :link="item" :count="cart?.itemsQuantity">
+    {{ item.title }}
+  </BottomHeaderLink>
+</template>
+
+<script setup lang="ts">
+import { useDeferShortCart } from "@/shared/layout";
+import type { ExtendedMenuLinkType } from "@/core/types";
+import BottomHeaderLink from "@/shared/layout/components/header/_internal/bottom-header-link.vue";
+
+defineProps<{ item: ExtendedMenuLinkType }>();
+const { cart } = useDeferShortCart();
+</script>
