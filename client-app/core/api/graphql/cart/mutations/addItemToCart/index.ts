63433d3ee7cd24719f9f16cc0ebb79223be713ce
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-10-07 16:38:23 +0200

    fix: remove unecessary changes

--- client-app/core/api/graphql/cart/mutations/addItemToCart/index.ts
+++ client-app/core/api/graphql/cart/mutations/addItemToCart/index.ts
@@ -1,9 +1,9 @@
 import { useApolloClient } from "@vue/apollo-composable";
 import { useCartMutationVariables } from "@/core/api/graphql/cart/composables";
 import { useMutation } from "@/core/api/graphql/composables";
-import { AddItemDocument, CartItemsFragmentDoc, OperationNames } from "@/core/api/graphql/types";
+import { AddItemDocument, OperationNames } from "@/core/api/graphql/types";
 import { filterActiveQueryNames } from "@/core/api/graphql/utils";
-import type { CartType, CartIdFragment } from "@/core/api/graphql/types";
+import type { CartIdFragment } from "@/core/api/graphql/types";
 import type { MaybeRef } from "vue";
 
 export function useAddItemToCartMutation(cart?: MaybeRef<CartIdFragment | undefined>) {
@@ -12,33 +12,6 @@ export function useAddItemToCartMutation(cart?: MaybeRef<CartIdFragment | undefi
     AddItemDocument,
     useCartMutationVariables(cart, {
       refetchQueries: () => filterActiveQueryNames(client, [OperationNames.Query.GetFullCart]),
-      update: (cache, { data }) => {
-        cache.updateFragment(
-          {
-            id: `CartType:${data?.addItem?.id}`,
-            fragment: CartItemsFragmentDoc,
-            fragmentName: "cartItems",
-          },
-          (cacheData) => {
-            return {
-              ...cacheData,
-              items: data?.addItem?.items || [],
-            };
-          },
-        );
-      },
     }),
   );
-}
-
-/** @deprecated Use {@link useAddItemToCartMutation} instead. */
-export async function addItemToCart(productId: string, quantity: number): Promise<CartType> {
-  const { mutate } = useAddItemToCartMutation();
-  const result = await mutate({
-    command: {
-      productId,
-      quantity,
-    },
-  });
-  return result!.data!.addItem as CartType;
 }
