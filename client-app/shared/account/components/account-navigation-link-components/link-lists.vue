5fb1ba81c4d29da0ce365557d2d382a349c0a3e3
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-10-08 09:35:19 +0200

    fix: return lists fetching

--- client-app/shared/account/components/account-navigation-link-components/link-lists.vue
+++ client-app/shared/account/components/account-navigation-link-components/link-lists.vue
@@ -21,21 +21,25 @@
 </template>
 
 <script setup lang="ts">
-import { computed, toRef } from "vue";
+import { computed, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 import { useWishlists } from "@/shared/wishlists";
 import type { ExtendedMenuLinkType } from "@/core/types";
 import AccountNavigationLink from "@/shared/account/components/account-navigation-link.vue";
-const props = defineProps<IProps>();
 
-const { lists } = useWishlists();
+defineProps<IProps>();
+
+const { lists, fetchWishlists } = useWishlists();
 const isListDetails = computed(() => route.name === "ListDetails");
 
 interface IProps {
   item: ExtendedMenuLinkType;
 }
 
-const item = toRef(props, "item");
-
 const route = useRoute();
+watchEffect(async () => {
+  if (isListDetails.value) {
+    await fetchWishlists();
+  }
+});
 </script>
