f6a88b8d81f96e2551bdd281cdf09909030051b6
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-10-18 18:05:50 +0200

    feat: catalog seo

--- client-app/shared/catalog/composables/useCategorySeo.ts
+++ client-app/shared/catalog/composables/useCategorySeo.ts
@@ -1,6 +1,7 @@
 import { useSeoMeta } from "@unhead/vue";
 import { computed, watchEffect } from "vue";
 import { usePageHead } from "@/core/composables";
+import { globals } from "@/core/globals";
 import { useCategory } from "@/shared/catalog/composables/useCategory";
 import type { Ref } from "vue";
 
@@ -9,9 +10,16 @@ type PropsType = {
   categoryComponentAnchorIsVisible: Ref<boolean>;
 };
 
+const { i18n } = globals;
+
 export function useCategorySeo({ allowSetMeta, categoryComponentAnchorIsVisible }: PropsType) {
-  const { category: currentCategory } = useCategory();
-  const seoTitle = computed(() => currentCategory.value?.seoInfo?.pageTitle || currentCategory.value?.name);
+  const { category: currentCategory, loading: categoryLoading } = useCategory();
+  const seoTitle = computed(
+    () =>
+      currentCategory.value?.seoInfo?.pageTitle ||
+      currentCategory.value?.name ||
+      (categoryLoading.value ? "" : i18n.global.t("pages.catalog.title")),
+  );
   const seoDescription = computed(() => currentCategory.value?.seoInfo?.metaDescription);
   const seoKeywords = computed(() => currentCategory.value?.seoInfo?.metaKeywords);
   const seoImageUrl = computed(() => currentCategory.value?.images?.[0]?.url);
