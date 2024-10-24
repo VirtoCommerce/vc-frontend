01b7715ba3bf4827f3d0b3c931d781555a626aa9
Maksim Naidovich <Maxim.Naidovich@virtoway.com>
2024-10-24 15:31:05 +0200

    feat: [builder.io] add breadcrumbs component (#1380)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-2024
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.8.0-pr-1380-ef36-ef36c84f.zip

---
+++ client-app/shared/static-content/components/breadcrumbs-block.vue
@@ -0,0 +1,17 @@
+<template>
+  <VcBreadcrumbs :items="breadcrumbs" />
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import { useBreadcrumbs } from "@/core/composables";
+import { buildBreadcrumbs } from "@/core/utilities";
+
+interface IProps {
+  breadcrumbs: { title: string; seoPath?: string }[];
+}
+
+const props = defineProps<IProps>();
+
+const breadcrumbs = useBreadcrumbs(computed(() => buildBreadcrumbs(props.breadcrumbs)));
+</script>
