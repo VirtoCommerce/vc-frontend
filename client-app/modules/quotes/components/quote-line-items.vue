ce81f7f4f9f3aedf1c37664bd716c3541c276069
NaMax66 <Maxim.Naidovich@virtoway.com>
2024-10-25 16:07:45 +0200

    feat: refactor quotes

--- client-app/modules/quotes/components/quote-line-items.vue
+++ client-app/modules/quotes/components/quote-line-items.vue
@@ -40,16 +40,12 @@
           class="quote-line-items__quantity"
           :model-value="item.selectedTierPrice?.quantity"
           :name="item.id"
-          :min-quantity="item.product?.minQuantity"
-          :max-quantity="item.product?.maxQuantity"
-          :available-quantity="item.product?.availabilityData?.availableQuantity"
-          :pack-size="item.product?.packSize"
           @update:model-value="$emit('update:item', { itemId: item.id, quantity: $event })"
         />
       </VcLineItem>
     </template>
     <template #after-items>
-      <div v-if="!items.length" class="quote-line-items__no-items">
+      <div v-if="!items?.length" class="quote-line-items__no-items">
         <VcAlert color="warning" size="sm" variant="outline-dark" icon>
           {{ $t("quote_details.no_items_message") }}
         </VcAlert>
@@ -75,13 +71,13 @@ interface IEmits {
 }
 interface IProps {
   readonly?: boolean;
-  items: QuoteItemType[];
+  items?: QuoteItemType[];
 }
 
 const { n } = useI18n();
 
 const normalizedItems = computed(() => {
-  return props.items.map((item) => ({
+  return props.items?.map((item) => ({
     ...item,
     extendedPrice: getTotalPrice(item),
   }));
