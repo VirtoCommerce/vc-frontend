a0a9c5391fcf4906d031cbb7a934866bb993c3d3
Aleksandr Vishniakov <av@virtoway.com>
2024-10-28 16:44:41 +0200

    fix: add overload with correct return

--- client-app/core/utilities/product/index.ts
+++ client-app/core/utilities/product/index.ts
@@ -1,5 +1,6 @@
 import type { RouteLocationRaw } from "vue-router";
 
-export function getProductRoute(productId?: string, productSlug?: string): RouteLocationRaw {
-  return productSlug ? `/${productSlug}` : productId ? { name: "Product", params: { productId } } : "";
+export function getProductRoute(productId: string, productSlug?: string): RouteLocationRaw;
+export function getProductRoute(productId?: string, productSlug?: string): RouteLocationRaw | undefined {
+  return productSlug ? `/${productSlug}` : productId ? { name: "Product", params: { productId } } : undefined;
 }
