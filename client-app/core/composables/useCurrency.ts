54a0661e730a513c3821b080b2e4de1bc0238e7c
Aleksandr Vishniakov <av@virtoway.com>
2024-10-28 14:43:50 +0200

    refactor: revert back unneccessary changes

--- client-app/core/composables/useCurrency.ts
+++ client-app/core/composables/useCurrency.ts
@@ -1,35 +1,35 @@
-import { createGlobalState, useLocalStorage } from "@vueuse/core";
+import { useLocalStorage } from "@vueuse/core";
 import { computed } from "vue";
 import { useUser } from "@/shared/account/composables/useUser";
 import { useThemeContext } from "./useThemeContext";
 import type { ICurrency } from "../types";
 
-function _useCurrency() {
-  const { themeContext } = useThemeContext();
-  const { user } = useUser();
+const { themeContext } = useThemeContext();
+const { user } = useUser();
 
-  const savedCurrencyCode = useLocalStorage<string | null>("currency", "");
+const savedCurrencyCode = useLocalStorage<string | null>("currency", "");
 
-  const defaultCurrency = computed<ICurrency>(() => themeContext.value.defaultCurrency);
-  const supportedCurrencies = computed<ICurrency[]>(() => themeContext.value.availableCurrencies);
-  const contactCurrency = computed(() =>
-    supportedCurrencies.value?.find((item) => item.code === user.value?.contact?.currencyCode),
-  );
-  const currentCurrency = computed<ICurrency>(
-    () =>
-      supportedCurrencies.value?.find((item) => item.code === savedCurrencyCode.value) ||
-      contactCurrency.value ||
-      defaultCurrency.value,
-  );
+const defaultCurrency = computed<ICurrency>(() => themeContext.value.defaultCurrency);
+const supportedCurrencies = computed<ICurrency[]>(() => themeContext.value.availableCurrencies);
+const contactCurrency = computed(() =>
+  supportedCurrencies.value?.find((item) => item.code === user.value?.contact?.currencyCode),
+);
+const currentCurrency = computed<ICurrency>(
+  () =>
+    supportedCurrencies.value?.find((item) => item.code === savedCurrencyCode.value) ||
+    contactCurrency.value ||
+    defaultCurrency.value,
+);
 
-  function saveCurrencyCode(code: string, needToReload: boolean = true) {
-    savedCurrencyCode.value = code;
+function saveCurrencyCode(code: string, needToReload: boolean = true) {
+  savedCurrencyCode.value = code;
 
-    if (needToReload) {
-      location.reload();
-    }
+  if (needToReload) {
+    location.reload();
   }
+}
 
+export function useCurrency() {
   return {
     savedCurrencyCode,
     defaultCurrency,
@@ -38,5 +38,3 @@ function _useCurrency() {
     saveCurrencyCode,
   };
 }
-
-export const useCurrency = createGlobalState(_useCurrency);
