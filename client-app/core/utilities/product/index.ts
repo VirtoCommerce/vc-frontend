fc4f7822521d74e4e0397b448a6860f07373460f
Aleksandr Vishniakov <av@virtoway.com>
2024-10-29 19:33:15 +0200

    fix: sonar issue

--- client-app/core/utilities/product/index.ts
+++ client-app/core/utilities/product/index.ts
@@ -2,5 +2,6 @@ import type { RouteLocationRaw } from "vue-router";
 
 export function getProductRoute(productId: string, productSlug?: string): RouteLocationRaw;
 export function getProductRoute(productId?: string, productSlug?: string): RouteLocationRaw | undefined {
-  return productSlug ? `/${productSlug}` : productId ? { name: "Product", params: { productId } } : undefined;
+  const defaultRoute = productId ? { name: "Product", params: { productId } } : undefined;
+  return productSlug ? `/${productSlug}` : defaultRoute;
 }
