8d83e3073321e8f954618ed212e083e700c8f123
Andrew Orlov <andrew.orlov@outlook.com>
2024-11-01 12:37:39 +0200

    refactor: category component on catalog page

--- client-app/shared/catalog/components/category.vue
+++ client-app/shared/catalog/components/category.vue
@@ -35,7 +35,7 @@
       <div v-if="!hideSidebar && !isMobile && !isHorizontalFilters" class="flex shrink-0 items-start lg:w-60">
         <div ref="filtersElement" class="sticky w-full space-y-5" :style="filtersStyle">
           <CategorySelector
-            v-if="categoryId"
+            v-if="categoryId || isRoot"
             :category="currentCategory"
             :loading="!currentCategory && loadingCategory"
           />
@@ -53,7 +53,7 @@
       <div ref="contentElement" class="w-0 grow">
         <div class="flex">
           <VcTypography tag="h1">
-            <i18n-t v-if="!categoryId" keypath="pages.search.header" tag="span">
+            <i18n-t v-if="!categoryId && !isRoot" keypath="pages.search.header" tag="span">
               <template #keyword>
                 <strong>{{ searchParams.keyword }}</strong>
               </template>
@@ -264,6 +264,7 @@ const viewModes = ["grid", "list"] as const;
 type ViewModeType = (typeof viewModes)[number];
 
 interface IProps {
+  isRoot?: boolean;
   categoryId?: string;
   title?: string;
   hideTotal?: boolean;
@@ -364,7 +365,7 @@ const searchParams = computedEager<ProductsSearchParamsType>(() => ({
   categoryId: props.categoryId,
   itemsPerPage: props.fixedProductsCount || itemsPerPage.value,
   sort: sortQueryParam.value,
-  keyword: props.keyword || (!props.categoryId ? searchQueryParam.value : keywordQueryParam.value),
+  keyword: props.keyword || (!props.categoryId && !props.isRoot ? searchQueryParam.value : keywordQueryParam.value),
   filter: [
     props.filter,
     facetsQueryParam.value,
@@ -444,7 +445,7 @@ whenever(() => !isMobile.value, hideFiltersSidebar);
 watch(
   () => props.categoryId,
   (categoryId) => {
-    if (categoryId) {
+    if (categoryId || props.isRoot) {
       const { zero_price_product_enabled } = themeContext.value.settings;
       const catalog_empty_categories_enabled = getSettingValue(MODULE_XAPI_KEYS.CATALOG_EMPTY_CATEGORIES_ENABLED);
 
