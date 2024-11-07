aa4a56c9ec9ca86bb1227abb644187d30f963743
Andrew Orlov <andrew.orlov@outlook.com>
2024-11-07 09:09:54 +0200

    refactor: use useModuleSettings to check appinsights module settings

--- client-app/core/plugins/applicationInsights.plugin.ts
+++ client-app/core/plugins/applicationInsights.plugin.ts
@@ -1,39 +1,33 @@
-import { computed } from "vue";
 import { AppInsightsPlugin } from "vue3-application-insights";
-import type { IThemeContext } from "../types";
+import { useModuleSettings } from "../composables/useModuleSettings";
+import {
+  APP_INSIGHTS_ENABlE_STATE,
+  APP_INSIGHTS_INSTRUMENTATION_KEY,
+  APP_INSIGHTS_MODULE_ID,
+} from "../constants/modules";
 import type { App, Plugin } from "vue";
 import type { AppInsightsPluginOptions } from "vue3-application-insights";
 
 export const applicationInsightsPlugin: Plugin = {
-  install: (app: App, themeContext: IThemeContext) => {
-    const MODULE_KEYS = {
-      ID: "VirtoCommerce.ApplicationInsights",
-      ENABLE_STATE: "ApplicationInsights.EnableTracking",
-      INSTRUMENTATION_KEY: "ApplicationInsights.InstrumentationKey",
-    };
+  install: (app: App) => {
+    const { getSettingValue, isEnabled } = useModuleSettings(APP_INSIGHTS_MODULE_ID);
 
-    const moduleSettings = computed(() =>
-      themeContext.storeSettings.modules?.find((module) => module.moduleId === MODULE_KEYS.ID),
-    );
-    const moduleEnabled = computed(
-      () => moduleSettings.value?.settings?.find((item) => item.name === MODULE_KEYS.ENABLE_STATE)?.value,
-    );
-    const instrumentationKey = computed(
-      () => moduleSettings.value?.settings?.find((item) => item.name === MODULE_KEYS.INSTRUMENTATION_KEY)?.value,
-    );
+    if (isEnabled(APP_INSIGHTS_ENABlE_STATE)) {
+      const instrumentationKey = getSettingValue(APP_INSIGHTS_INSTRUMENTATION_KEY) as string;
 
-    if (moduleEnabled.value && typeof instrumentationKey.value === "string" && instrumentationKey.value) {
-      const options: AppInsightsPluginOptions = {
-        appInsightsConfig: {
-          config: {
-            instrumentationKey: instrumentationKey.value,
+      if (instrumentationKey) {
+        const options: AppInsightsPluginOptions = {
+          appInsightsConfig: {
+            config: {
+              instrumentationKey,
+            },
           },
-        },
-        trackAppErrors: true,
-        trackInitialPageView: true,
-      };
+          trackAppErrors: true,
+          trackInitialPageView: true,
+        };
 
-      app.use(AppInsightsPlugin, options);
+        app.use(AppInsightsPlugin, options);
+      }
     }
   },
 };
