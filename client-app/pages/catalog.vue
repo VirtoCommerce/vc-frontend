3e715488ac5e254e3fa79b0dc7c55039f9e8b2e4
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-11-12 09:38:50 +0100

    fix: catalog title

--- client-app/pages/catalog.vue
+++ client-app/pages/catalog.vue
@@ -1,24 +1,29 @@
 <template>
-  <Category is-root />
+  <Category :title="title" is-root />
 </template>
 
 <script setup lang="ts">
 import { useSeoMeta } from "@unhead/vue";
-import { watchEffect } from "vue";
+import { computed, watchEffect } from "vue";
 import { useRoute } from "vue-router";
 import { usePageHead } from "@/core/composables";
+import { globals } from "@/core/globals";
 import { useSlugInfo } from "@/shared/common";
 import Category from "@/shared/catalog/components/category.vue";
 
 const route = useRoute();
+const { i18n } = globals;
+
 const { seoInfo } = useSlugInfo(route.path.slice(1));
+const catalogName = i18n.global.t("pages.catalog.title");
+const title = computed(() => seoInfo.value?.pageTitle ?? catalogName);
 
 watchEffect(() => {
   if (!seoInfo.value) {
     return;
   }
   usePageHead({
-    title: seoInfo.value?.pageTitle,
+    title: title.value,
     meta: {
       keywords: seoInfo?.value?.metaKeywords,
       description: seoInfo?.value?.metaDescription,
@@ -26,7 +31,7 @@ watchEffect(() => {
   });
 
   useSeoMeta({
-    ogTitle: seoInfo.value?.pageTitle,
+    ogTitle: title.value,
     ogDescription: seoInfo?.value?.metaDescription,
   });
 });
