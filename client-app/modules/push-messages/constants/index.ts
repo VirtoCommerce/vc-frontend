72b981f0115d17295754dc9d7822f226f8bb5752
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2024-11-14 17:47:02 +0100

    fix: use version in the filename itself

--- client-app/modules/push-messages/constants/index.ts
+++ client-app/modules/push-messages/constants/index.ts
@@ -2,7 +2,7 @@ export const PUSH_MESSAGES_MODULE_ENABLED_KEY = "PushMessages.Enable";
 export const PUSH_MESSAGES_MODULE_FCM_ENABLED_KEY = "PushMessages.FCM.Enable";
 
 export const REGISTRATION_SCOPE = "/firebase-cloud-messaging-push-scope";
-export const SERVICE_WORKER_PATH = "/fcm-service-worker-v1.js";
+export const SERVICE_WORKER_PATH = "/fcm-service-worker-v1.js"; // NOTE: When updating the service worker, make sure to update the file name and this constant accordingly - example: fcm-service-worker-v2.js
 export const DEFAULT_ICON_URL = "/static/icons/favicon-32x32.png";
 export const PREFERRED_ICON_PROPERTIES = { type: "image/png", sizes: "32x32" };
 export const SETTINGS_MAPPING = {
