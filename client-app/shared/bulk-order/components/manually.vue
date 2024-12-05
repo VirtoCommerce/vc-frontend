4ca0334df3c1150dfa3d53d83c9641bb237f5e51
Aleksandr Vishniakov <av@virtoway.com>
2024-12-05 14:57:02 +0200

    refactor: bulk order (#1465)
    ## Description
    Bulk order refactored (redesigned) and made extensible
    
    ![New bulk order
    UI](https://github.com/user-attachments/assets/3fc9128b-c950-48c7-b132-d2e7668e2668)
    
    Based on
    [Quick
    order](https://www.figma.com/design/9DBmmQwGVbYiayQa4JOVC5/%F0%9F%94%B6--STOREFRONT-DRAFT-%E2%80%A2-PART-2?node-id=3149-29402&node-type=canvas&t=SFuSZdiECCapJUrq-0)
    [Quick order with
    quotes](https://www.figma.com/design/9DBmmQwGVbYiayQa4JOVC5/%F0%9F%94%B6--STOREFRONT-DRAFT-%E2%80%A2-PART-2?node-id=3928-26399&node-type=frame&t=SFuSZdiECCapJUrq-0)
    [Quick order
    mobile](https://www.figma.com/design/9DBmmQwGVbYiayQa4JOVC5/%F0%9F%94%B6--STOREFRONT-DRAFT-%E2%80%A2-PART-2?node-id=3311-196126&node-type=canvas&t=SFuSZdiECCapJUrq-0)
    
    ## References
    ### Jira-link:
    <!-- Put link to your task in Jira here -->
    https://virtocommerce.atlassian.net/browse/VCST-2397
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.11.0-pr-1465-4cfc-4cfccca2.zip
    
    ---------
    
    Co-authored-by: Elena Mutykova <56412054+Lenajava1@users.noreply.github.com>

--- client-app/shared/bulk-order/components/manually.vue
+++ client-app/shared/bulk-order/components/manually.vue
@@ -1,76 +1,67 @@
 <template>
-  <section>
-    <h2 class="hidden border-b px-5 py-2 font-black uppercase lg:block">
-      {{ $t("shared.bulk_order.manually.title") }}
-    </h2>
+  <VcWidget :title="!isMobile ? $t('shared.bulk_order.manually.title') : undefined" size="md">
+    <p class="text-sm">
+      {{ $t("shared.bulk_order.manually.subtitle_message_start") }}
+      <router-link :to="{ name: 'Cart' }" class="text-[--link-color] hover:text-[--link-hover-color]">
+        {{ $t("shared.bulk_order.manually.cart_link") }}
+      </router-link>
+      {{ $t("shared.bulk_order.manually.subtitle_message_end") }}
+    </p>
 
-    <div class="p-6 pb-5 pt-4 md:p-5">
-      <p class="text-sm">
-        {{ $t("shared.bulk_order.manually.subtitle_message_start") }}
-        <router-link :to="{ name: 'Cart' }" class="text-[--link-color] hover:text-[--link-hover-color]">
-          {{ $t("shared.bulk_order.manually.cart_link") }}
-        </router-link>
-        {{ $t("shared.bulk_order.manually.subtitle_message_end") }}
-      </p>
-
-      <div class="mb-1.5 mt-3 flex flex-row gap-x-5">
-        <div class="w-full font-bold">
-          {{ $t("shared.bulk_order.manually.product_sku_label") }}
-        </div>
-        <div class="w-1/3 max-w-[164px] font-bold xl:w-1/4">
-          {{ $t("shared.bulk_order.manually.quantity_label") }}
-        </div>
+    <div class="mb-1.5 mt-3 flex flex-row gap-x-5">
+      <div class="w-full font-bold">
+        {{ $t("shared.bulk_order.manually.product_sku_label") }}
       </div>
-
-      <div class="flex flex-col gap-y-4">
-        <div v-for="(item, index) in items" :key="index" class="flex flex-row gap-x-5">
-          <div class="w-full">
-            <VcInput v-model.trim="item.productSku" :disabled="loading" placeholder="SKU" />
-          </div>
-
-          <div class="w-1/3 max-w-[164px] xl:w-1/4">
-            <VcInput
-              v-model="item.quantity"
-              :disabled="loading"
-              :max="maxQuantity"
-              min="1"
-              type="number"
-              placeholder="0"
-              @update:model-value="$nextTick(() => (item.quantity = String(validateQuantity(+$event!) || '')))"
-              @keypress="onKeypress"
-            />
-          </div>
-        </div>
-      </div>
-
-      <div class="mt-4">
-        <button
-          type="button"
-          class="inline-flex appearance-none items-center gap-1.5 py-1.5 md:py-0"
-          @click="increment"
-        >
-          <VcIcon class="fill-primary" name="plus" size="sm" />
-
-          <span class="border-b border-dashed border-current text-[--link-color] hover:text-[--link-hover-color]">
-            {{ $t("shared.bulk_order.manually.add_rows_action_link") }}
-          </span>
-        </button>
-      </div>
-
-      <div class="mb-2 mt-6 flex flex-row flex-wrap justify-between gap-3 md:mb-0 md:flex-nowrap">
-        <VcButton :disabled="!dirty || loading" color="secondary" variant="outline" @click="resetItems">
-          {{ $t("shared.bulk_order.manually.reset_button") }}
-        </VcButton>
-
-        <VcButton :disabled="!valid" :loading="loading" @click="addToCart">
-          {{ $t("shared.bulk_order.manually.add_to_cart_button") }}
-        </VcButton>
+      <div class="w-1/3 max-w-[164px] font-bold xl:w-1/4">
+        {{ $t("shared.bulk_order.manually.quantity_label") }}
       </div>
     </div>
-  </section>
+
+    <div class="flex flex-col gap-y-4">
+      <div v-for="(item, index) in items" :key="index" class="flex flex-row gap-x-5">
+        <div class="w-full">
+          <VcInput v-model.trim="item.productSku" :disabled="loading" placeholder="SKU" />
+        </div>
+
+        <div class="w-1/3 max-w-[164px] xl:w-1/4">
+          <VcInput
+            v-model="item.quantity"
+            :disabled="loading"
+            :max="maxQuantity"
+            min="1"
+            type="number"
+            placeholder="0"
+            @update:model-value="$nextTick(() => (item.quantity = String(validateQuantity(+$event!) || '')))"
+            @keypress="onKeypress"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="mt-4">
+      <button type="button" class="inline-flex appearance-none items-center gap-1.5 py-1.5 md:py-0" @click="increment">
+        <VcIcon class="fill-primary" name="plus" size="sm" />
+
+        <span class="border-b border-dashed border-current text-[--link-color] hover:text-[--link-hover-color]">
+          {{ $t("shared.bulk_order.manually.add_rows_action_link") }}
+        </span>
+      </button>
+    </div>
+
+    <div class="mb-2 mt-6 flex flex-row flex-wrap justify-between gap-3 md:mb-0 md:flex-nowrap">
+      <VcButton :disabled="!dirty || loading" color="secondary" variant="outline" min-width="9rem" @click="resetItems">
+        {{ $t("shared.bulk_order.manually.reset_button") }}
+      </VcButton>
+
+      <VcButton :disabled="!valid" :loading="loading" min-width="9rem" @click="addToCart">
+        {{ $t("shared.bulk_order.manually.add_to_cart_button") }}
+      </VcButton>
+    </div>
+  </VcWidget>
 </template>
 
 <script setup lang="ts">
+import { useBreakpoints, breakpointsTailwind } from "@vueuse/core";
 import { computed, ref } from "vue";
 import { maxQuantity, validateQuantity } from "@/shared/bulk-order/utils";
 import type { InputNewBulkItemType } from "@/core/api/graphql/types";
@@ -88,6 +79,9 @@ defineProps<IProps>();
 interface IProps {
   loading: boolean;
 }
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+const isMobile = breakpoints.smaller("lg");
 
 const items: Ref<InputNewBulkItemExtendedType[]> = ref(createItems(5));
 
