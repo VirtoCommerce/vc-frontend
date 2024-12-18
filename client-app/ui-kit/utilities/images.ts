bc7a05302af29434a8a4753dbe95b988ab65a646
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-12-18 14:25:44 +0900

    feat: move main-banner image

--- client-app/ui-kit/utilities/images.ts
+++ client-app/ui-kit/utilities/images.ts
@@ -2,6 +2,10 @@ export function loadImage(fileName: string): string {
   return new URL(`/client-app/assets/images/${fileName}`, import.meta.url).href;
 }
 
+export function loadIcon(name?: string): string {
+  return new URL(`/client-app/assets/icons/basic/${name}.svg`, import.meta.url).href ?? "";
+}
+
 export function isFilenameOnly(src: string): boolean {
   return /^[^/]+(\.[a-zA-Z0-9]+)?$/.test(src);
 }
