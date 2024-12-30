a1a33be338ce94d3802bc2e758f895a9cd8afb40
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-12-30 14:09:56 +0900

    feat: implement bg-image in vc-container

--- client-app/ui-kit/components/atoms/container/vc-container.vue
+++ client-app/ui-kit/components/atoms/container/vc-container.vue
@@ -7,6 +7,8 @@
       },
     ]"
   >
+    <div v-if="bgImage" class="vc-container__bg" v-html="bgImageRaw"></div>
+
     <div class="vc-container__wrapper">
       <slot />
     </div>
@@ -15,17 +17,20 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import bgImageRaw from "../../../images/main-bg.svg?raw";
 import { isCssVariable } from "../../../utilities";
 
 interface IProps {
   loading?: boolean;
   maxWidth?: string;
   bgColor?: string;
+  bgImage?: boolean;
 }
 
 const props = withDefaults(defineProps<IProps>(), {
   maxWidth: "",
   bgColor: "",
+  bgImage: true,
 });
 
 const _bgColor = computed(() => (isCssVariable(props.bgColor) ? `var(${props.bgColor})` : props.bgColor));
@@ -42,7 +47,7 @@ const _bgColor = computed(() => (isCssVariable(props.bgColor) ? `var(${props.bgC
   --vc-container-width: min(var(--max-width), calc(100vw - var(--vc-container-padding-x) * 2));
   --vc-container-offset: calc((100vw - var(--vc-container-width)) / 2);
 
-  @apply grow bg-[--bg-color] py-6 px-[--vc-container-padding-x];
+  @apply relative grow bg-[--bg-color] py-6 px-[--vc-container-padding-x];
 
   @media (width >= theme("screens.md")) {
     @apply pb-9;
@@ -60,8 +65,20 @@ const _bgColor = computed(() => (isCssVariable(props.bgColor) ? `var(${props.bgC
     @apply pointer-events-none opacity-50;
   }
 
+  &__bg {
+    @apply overflow-hidden absolute inset-0;
+
+    svg {
+      @apply absolute size-[28rem] bottom-[-10rem] right-[-10rem];
+
+      @media (min-width: theme("screens.xl")) {
+        @apply size-[48rem] bottom-[-15rem] right-[-15rem];
+      }
+    }
+  }
+
   &__wrapper {
-    @apply mx-auto max-w-[--max-width];
+    @apply relative mx-auto max-w-[--max-width];
   }
 }
 </style>
