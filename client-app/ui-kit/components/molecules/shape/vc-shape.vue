9365930dbfe000d6b3d153f12ca051c075473db5
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2024-12-30 17:13:11 +0900

    feat: extend vc-shape

--- client-app/ui-kit/components/molecules/shape/vc-shape.vue
+++ client-app/ui-kit/components/molecules/shape/vc-shape.vue
@@ -8,10 +8,13 @@
 
 <script setup lang="ts">
 import { computed, ref, watch } from "vue";
+import { isCssVariable } from "@/ui-kit/utilities";
 
 interface IProps {
   size?: string;
   icon?: string;
+  iconColor?: string;
+  bgColor?: string;
   img?: string;
   mask?: string;
 }
@@ -19,6 +22,8 @@ interface IProps {
 const props = withDefaults(defineProps<IProps>(), {
   mask: "polygon",
   size: "",
+  iconColor: "",
+  bgColor: "",
 });
 
 const iconUrl = ref<string>("");
@@ -27,6 +32,9 @@ const style = computed(() => ({
   backgroundImage: props.img ? `url("${props.img}")` : "none",
   maskImage: iconUrl.value ? `url("${iconUrl.value}")` : "none",
 }));
+
+const _bgColor = computed(() => (isCssVariable(props.bgColor) ? `var(${props.bgColor})` : props.bgColor));
+const _iconColor = computed(() => (isCssVariable(props.iconColor) ? `var(${props.iconColor})` : props.iconColor));
 
 function loadIcon(name?: string) {
   iconUrl.value = new URL(`/client-app/assets/icons/basic/${name}.svg`, import.meta.url).href ?? "";
@@ -44,10 +52,13 @@ watch(
 <style lang="scss">
 .vc-shape {
   --props-size: v-bind(props.size);
+  --props-bg-color: v-bind(_bgColor);
+  --props-icon-color: v-bind(_iconColor);
 
   --size: var(--props-size, var(--vc-shape-size, 2.5rem));
-  --bg-color: var(--vc-shape-bg-color, theme("colors.secondary.500"));
-  --vc-icon-color: var(--vc-shape-color, theme("colors.additional.50"));
+  --bg-color: var(--props-bg-color, var(--vc-shape-bg-color, theme("colors.secondary.500")));
+
+  --vc-icon-color: var(--props-icon-color, var(--vc-shape-color, theme("colors.additional.50")));
   --vc-icon-size: 50%;
 
   @apply relative flex items-center justify-center size-[--size] bg-[--bg-color] bg-cover bg-center;
