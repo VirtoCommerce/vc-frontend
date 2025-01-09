9a55d686460d62cc3b46d6616a9faf7a73945d6b
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-01-09 18:44:36 +0900

    refactor: getIconUrl, getImageUrl

--- client-app/ui-kit/utilities/images.ts
+++ client-app/ui-kit/utilities/images.ts
@@ -2,29 +2,11 @@ import DOMPurify from "dompurify";
 import { Logger } from "@/core/utilities";
 
 export function getImageUrl(fileName?: string): string {
-  if (fileName) {
-    try {
-      return new URL(`/client-app/assets/images/${fileName}`, import.meta.url).href ?? "";
-    } catch (error) {
-      Logger.error(`Failed to load icon: ${fileName}`, error);
-      return "";
-    }
-  } else {
-    return "";
-  }
+  return new URL(`/client-app/assets/images/${fileName}`, import.meta.url).href ?? "";
 }
 
 export function getIconUrl(name?: string) {
-  if (name) {
-    try {
-      return new URL(`../icons/${name}.svg`, import.meta.url).href ?? "";
-    } catch (error) {
-      Logger.error(`Failed to load icon: ${name}`, error);
-      return "";
-    }
-  } else {
-    return "";
-  }
+  return new URL(`../icons/${name}.svg`, import.meta.url).href ?? "";
 }
 
 export async function loadIconRaw(name?: string) {
