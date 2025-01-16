032994ff4ae7c9b674df0254438d446eaeb7ecd4
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-01-16 15:28:39 +0100

    feat: back in stock

---
+++ client-app/modules/back-in-stock/types.ts
@@ -0,0 +1,13 @@
+import type { ISortInfo } from "@/core/types";
+
+export type PaginationType = {
+  page: number;
+  pages: number;
+  itemsPerPage: number;
+};
+
+export type FetchParametersType = {
+  keyword: string;
+  sort: ISortInfo;
+  productIds: string[];
+};
