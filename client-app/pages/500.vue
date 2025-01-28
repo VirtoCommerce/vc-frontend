dc25df894cfa4fd704f8063495c6ed7397afc3b4
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-01-28 16:27:54 +0900

    feat: update 500

--- client-app/pages/500.vue
+++ client-app/pages/500.vue
@@ -1,33 +1,28 @@
 <template>
-  <TwoColumn class="max-w-screen-lg">
-    <template #left>
-      <h1 class="mb-5 text-7xl font-black text-neutral-900 md:text-8xl">
-        {{ $t("pages.500.error_code") }}
-      </h1>
+  <VcEmptyPage ref="page404Anchor" icon="outline-alert" image="servers.jpg">
+    <h1 class="text-5xl font-black text-primary max-sm:order-first lg:mb-5 lg:text-8xl">
+      {{ $t("pages.500.error_code") }}
+    </h1>
 
-      <VcTypography tag="h2" variant="h1" class="mb-2">
-        {{ $t("pages.500.error_text") }}
-      </VcTypography>
+    <VcTypography tag="h2" variant="h1" class="mb-5 max-sm:order-first sm:mb-2">
+      {{ $t("pages.500.error_text") }}
+    </VcTypography>
 
-      <p class="mb-10 text-base leading-relaxed text-neutral-700 md:text-xl">
-        {{ $t("pages.500.message") }}
-      </p>
-      <div>
-        <VcButton to="/" class="w-40">
-          {{ $t("pages.500.home_button") }}
-        </VcButton>
-      </div>
-    </template>
-    <template #right>
-      <VcImage src="500.webp" :alt="$t('pages.500.img_alt')" class="w-full max-w-md" lazy />
-    </template>
-  </TwoColumn>
+    <p class="mb-7 text-base text-neutral-700 lg:text-xl">
+      {{ $t("pages.500.message") }}
+    </p>
+
+    <div>
+      <VcButton to="/" min-width="11rem">
+        {{ $t("pages.500.home_button") }}
+      </VcButton>
+    </div>
+  </VcEmptyPage>
 </template>
 
 <script setup lang="ts">
 import { useI18n } from "vue-i18n";
 import { usePageHead } from "@/core/composables";
-import { TwoColumn } from "@/shared/layout";
 
 const { t } = useI18n();
 
