e6a5f802232ee822f6619738c4e2780cb8f26273
puhkaya <irina.puhkaya@virtoworks.com>
2025-01-30 19:15:04 +0300

    feat: refactor

--- client-app/core/api/graphql/organization/mutations/changeOrganizationLogo/index.ts
+++ client-app/core/api/graphql/organization/mutations/changeOrganizationLogo/index.ts
@@ -1,8 +1,9 @@
 import { graphqlClient } from "../../../client";
 import mutationDocument from "./changeOrganizationLogo.graphql";
+import type { Mutations, MutationsChangeOrganizationLogoArgs } from "@/core/api/graphql/types";
 
 export async function changeOrganizationLogo(organizationId: string, logoUrl: string): Promise<void> {
-  await graphqlClient.mutate({
+  await graphqlClient.mutate<Required<Pick<Mutations, "changeOrganizationLogo">>, MutationsChangeOrganizationLogoArgs>({
     mutation: mutationDocument,
     variables: {
       command: {
