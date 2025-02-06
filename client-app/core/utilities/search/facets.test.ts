3b6832a7849940f688e9f2da683f8d7dce26500c
Artem Dudarev <artem@virtoworks.com>
2025-02-06 18:15:33 +0200

    fix: change range facet bounds to nullable decimal (#1568)

--- client-app/core/utilities/search/facets.test.ts
+++ client-app/core/utilities/search/facets.test.ts
@@ -1,45 +1,33 @@
-import { describe, it, expect } from "vitest";
+import { describe, test, expect } from "vitest";
 import { getFilterExpressionFromFacetRange } from "@/core/utilities";
 
-const over30 = {
-  from: 30,
-  to: 0,
-  includeFrom: true,
-  includeTo: false,
-};
-
-const over100 = {
-  from: 100,
-  to: 0,
-  includeFrom: true,
-  includeTo: false,
-};
-
-const from30To40 = {
-  from: 30,
-  to: 40,
-  includeFrom: true,
-  includeTo: false,
-};
-
-const zeroTo10 = {
-  from: 0,
-  to: 10,
-  includeFrom: false,
-  includeTo: false,
-};
-
 describe("getFilterExpressionFromFacetRange", () => {
-  it("NOT includes 'to' if it zero", () => {
-    expect(getFilterExpressionFromFacetRange(over30)).toEqual("[30 TO)");
-    expect(getFilterExpressionFromFacetRange(over100)).toEqual("[100 TO)");
-  });
+  test.each`
+    from    | to      | includeFrom | includeTo | expectedString
+    ${null} | ${-1.2} | ${true}     | ${false}  | ${"[TO -1.2)"}
+    ${null} | ${0}    | ${true}     | ${false}  | ${"[TO 0)"}
+    ${null} | ${1.2}  | ${true}     | ${false}  | ${"[TO 1.2)"}
+    ${-1.2} | ${null} | ${true}     | ${false}  | ${"[-1.2 TO)"}
+    ${0}    | ${null} | ${true}     | ${false}  | ${"[0 TO)"}
+    ${0}    | ${null} | ${false}    | ${false}  | ${"(0 TO)"}
+    ${1.2}  | ${null} | ${true}     | ${false}  | ${"[1.2 TO)"}
+    ${-1.2} | ${0}    | ${true}     | ${false}  | ${"[-1.2 TO 0)"}
+    ${0}    | ${1.2}  | ${true}     | ${false}  | ${"[0 TO 1.2)"}
+    ${-1.2} | ${1.2}  | ${true}     | ${false}  | ${"[-1.2 TO 1.2)"}
+  `(
+    "{from: $from, to: $to, includeFrom: $includeFrom, includeTo: $includeTo} -> $expectedString",
+    ({ from, to, includeFrom, includeTo, expectedString }) => {
+      /* eslint-disable @typescript-eslint/no-unsafe-assignment */
+      const facetRange = {
+        from,
+        to,
+        includeFrom,
+        includeTo,
+      };
 
-  it("includes 'from' and 'to'", () => {
-    expect(getFilterExpressionFromFacetRange(from30To40)).toEqual("[30 TO 40)");
-  });
+      const actualString = getFilterExpressionFromFacetRange(facetRange);
 
-  it("includes 'from' if from is zero", () => {
-    expect(getFilterExpressionFromFacetRange(zeroTo10)).toEqual("(0 TO 10)");
-  });
+      expect(actualString).toEqual(expectedString);
+    },
+  );
 });
