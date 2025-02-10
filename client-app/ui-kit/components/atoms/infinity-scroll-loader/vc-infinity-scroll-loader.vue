cb678d756cc6657a519dcabae52b11e642c7004d
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-02-10 11:07:23 +0100

    fix: reaching end of list message

--- client-app/ui-kit/components/atoms/infinity-scroll-loader/vc-infinity-scroll-loader.vue
+++ client-app/ui-kit/components/atoms/infinity-scroll-loader/vc-infinity-scroll-loader.vue
@@ -4,7 +4,7 @@
       <VcLoader />
     </slot>
     <slot v-else name="loaded">
-      <VcIcon v-if="isPageLimitReached || pageNumber === pagesCount" class="size-7 fill-primary" name="badge-check" />
+      <VcIcon v-if="isPageLimitReached || pageNumber >= pagesCount" class="size-7 fill-primary" name="badge-check" />
 
       <span v-if="isPageLimitReached">{{ $t("ui_kit.reach_limit.page_limit_filters") }}</span>
       <span v-else-if="pageNumber >= pagesCount">{{ $t("ui_kit.reach_limit.end_list") }}</span>
