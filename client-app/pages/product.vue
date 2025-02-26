fd2b2ad3be3d58e4e58087db87bb9b297930b54d
puhkaya <irina.puhkaya@virtoworks.com>
2025-02-26 11:01:11 +0300

    fix: add ability to move the product description to a separate section

--- client-app/pages/product.vue
+++ client-app/pages/product.vue
@@ -46,6 +46,14 @@
           :model="productInfoSection"
         />
 
+        <component
+          :is="productDescriptionSection && 'type' in productDescriptionSection ? productDescriptionSection.type : ''"
+          v-if="productDescriptionSection && !productDescriptionSection.hidden"
+          :product="product"
+          :model="productDescriptionSection"
+          :is-collapsible="false"
+        ></component>
+
         <ProductConfiguration
           v-if="product.isConfigurable && configuration?.length"
           :product-id="productId"
@@ -230,6 +238,8 @@ const seoUrl = computed(() =>
 const productTemplate = _productTemplate as IPageTemplate;
 
 const productInfoSection = productTemplate?.content?.find((item) => item?.type === "product-info");
+
+const productDescriptionSection = productTemplate?.content?.find((item) => item?.type === "product-description");
 
 const productReviewsSection = productTemplate?.content?.find((item) => item?.type === "product-reviews");
 
