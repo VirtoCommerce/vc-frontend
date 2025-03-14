5dbd6b5e2b43f95cbba7c93c7fe80d63b4bc2bf8
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-03-14 14:15:57 +0900

    feat: implement bopis elements on checkout/shipping

--- client-app/shared/checkout/components/shipping-details-section.vue
+++ client-app/shared/checkout/components/shipping-details-section.vue
@@ -1,61 +1,115 @@
 <template>
   <VcWidget :title="$t('shared.checkout.shipping_details_section.title')" prepend-icon="truck" size="lg">
-    <div class="flex flex-col gap-6 lg:flex-row lg:gap-8">
-      <div class="lg:w-3/5">
-        <VcLabel required>
-          {{ $t("shared.checkout.shipping_details_section.labels.shipping_address") }}
+    <div class="flex flex-col flex-wrap gap-4 xs:flex-row xs:gap-y-6 lg:gap-8">
+      <div>
+        <VcLabel>
+          {{ $t("shared.checkout.shipping_details_section.labels.delivery_option") }}
         </VcLabel>
 
-        <div :class="['grow divide-y rounded border', { 'cursor-not-allowed bg-neutral-50': disabled }]">
-          <AddressSelection
-            :placeholder="$t('shared.checkout.shipping_details_section.links.select_address')"
-            :address="deliveryAddress"
-            :disabled="disabled"
-            class="min-h-18 px-3 py-1.5"
-            @change="onDeliveryAddressChange"
+        <div class="flex min-h-18 items-center gap-2 rounded border p-4">
+          <VcTabSwitch
+            v-model="mode"
+            value="pickup"
+            icon="cube"
+            :label="$t('shared.checkout.shipping_details_section.switchers.pickup')"
+            @change="switchShippingOptions($event)"
+          />
+
+          <VcTabSwitch
+            v-model="mode"
+            value="shipping"
+            icon="truck"
+            :label="$t('shared.checkout.shipping_details_section.switchers.shipping')"
+            @change="switchShippingOptions($event)"
           />
         </div>
       </div>
 
-      <VcSelect
-        :model-value="shipmentMethod"
-        :label="$t('shared.checkout.shipping_details_section.labels.shipping_method')"
-        :items="availableShippingMethods"
-        :disabled="disabled"
-        size="auto"
-        item-size="lg"
-        class="lg:w-2/5"
-        required
-        test-id-dropdown="shipping-method-select"
-        @change="(value) => setShippingMethod(value)"
-      >
-        <template #placeholder>
-          <div class="flex items-center gap-3 p-3 text-sm">
-            <VcImage class="size-12 rounded-sm bg-neutral-100" src="select-shipping.svg" />
+      <template v-if="mode === 'shipping'">
+        <div class="grow">
+          <VcLabel required>
+            {{ $t("shared.checkout.shipping_details_section.labels.shipping_address") }}
+          </VcLabel>
 
-            {{ $t("common.placeholders.select_delivery_method") }}
+          <div
+            :class="[
+              'flex min-h-18 grow flex-col justify-center divide-y rounded border px-3 py-1.5',
+              {
+                'cursor-not-allowed bg-neutral-50': disabled,
+              },
+            ]"
+          >
+            <AddressSelection
+              :placeholder="$t('shared.checkout.shipping_details_section.links.select_address')"
+              :address="deliveryAddress"
+              :disabled="disabled"
+              @change="onDeliveryAddressChange"
+            />
           </div>
-        </template>
+        </div>
 
-        <template #selected="{ item }">
-          <div class="flex items-center gap-3 p-3 text-sm">
+        <VcSelect
+          :model-value="shipmentMethod"
+          :label="$t('shared.checkout.shipping_details_section.labels.shipping_method')"
+          :items="availableShippingMethods"
+          :disabled="disabled"
+          size="auto"
+          item-size="lg"
+          class="lg:w-3/12"
+          required
+          test-id-dropdown="shipping-method-select"
+          @change="(value) => setShippingMethod(value)"
+        >
+          <template #placeholder>
+            <div class="flex items-center gap-3 p-[0.688rem] text-sm">
+              <VcImage class="size-12 rounded-sm bg-neutral-100" src="select-shipping.svg" />
+
+              {{ $t("common.placeholders.select_delivery_method") }}
+            </div>
+          </template>
+
+          <template #selected="{ item }">
+            <div class="flex items-center gap-3 p-[0.688rem] text-sm">
+              <VcImage class="size-12 rounded-sm" :src="item.logoUrl" />
+
+              {{ $t(`common.methods.delivery_by_id.${item.id}`) }}
+            </div>
+          </template>
+
+          <template #item="{ item }">
             <VcImage class="size-12 rounded-sm" :src="item.logoUrl" />
 
             {{ $t(`common.methods.delivery_by_id.${item.id}`) }}
-          </div>
-        </template>
+          </template>
+        </VcSelect>
+      </template>
 
-        <template #item="{ item }">
-          <VcImage class="size-12 rounded-sm" :src="item.logoUrl" />
+      <div v-else class="grow">
+        <VcLabel required>
+          {{ $t("shared.checkout.shipping_details_section.labels.pickup_point") }}
+        </VcLabel>
 
-          {{ $t(`common.methods.delivery_by_id.${item.id}`) }}
-        </template>
-      </VcSelect>
+        <div
+          :class="[
+            'flex min-h-18 grow flex-col justify-center divide-y rounded border px-3 py-1.5',
+            {
+              'cursor-not-allowed bg-neutral-50': disabled,
+            },
+          ]"
+        >
+          <AddressSelection
+            :placeholder="$t('shared.checkout.shipping_details_section.links.select_pickup_point')"
+            :disabled="disabled"
+            @change="onDeliveryAddressChange"
+          />
+        </div>
+      </div>
     </div>
   </VcWidget>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import { useFullCart } from "@/shared/cart";
 import { useCheckout } from "@/shared/checkout/composables";
 import { AddressSelection } from "@/shared/common";
@@ -66,6 +120,12 @@ interface IProps {
 
 defineProps<IProps>();
 
+const mode = ref<"pickup" | "shipping">("shipping");
+
 const { availableShippingMethods } = useFullCart();
 const { deliveryAddress, shipmentMethod, onDeliveryAddressChange, setShippingMethod } = useCheckout();
+
+function switchShippingOptions(_mode: "pickup" | "shipping") {
+  mode.value = _mode;
+}
 </script>
