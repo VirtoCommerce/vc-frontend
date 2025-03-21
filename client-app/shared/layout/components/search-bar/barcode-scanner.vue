871725023c8ad73ef6c3cd2458aa1595f5567331
NaMax66 <Maxim.Naidovich@virtoway.com>
2025-03-21 11:55:26 +0100

    feat: add file selection

--- client-app/shared/layout/components/search-bar/barcode-scanner.vue
+++ client-app/shared/layout/components/search-bar/barcode-scanner.vue
@@ -1,5 +1,12 @@
 <template>
+  <div v-if="isMobile">
+    <input id="image-search" class="hidden" type="file" />
+    <label for="image-search" aria-label="image search">
+      <VcIcon name="barcode" class="mobile-icon" />
+    </label>
+  </div>
   <VcButton
+    v-else
     class="barcode-scanner"
     color="primary"
     variant="no-border"
@@ -11,14 +18,18 @@
 </template>
 
 <script setup lang="ts">
+import { breakpointsTailwind, useBreakpoints } from "@vueuse/core";
 import { useModal } from "@/shared/modal";
 import BarcodeScannerModal from "./barcode-scanner-modal.vue";
+const emit = defineEmits<IEmits>();
+
+const breakpoints = useBreakpoints(breakpointsTailwind);
+
+const isMobile = breakpoints.smaller("md");
 
 interface IEmits {
   (e: "scannedCode", code: string): void;
 }
-const emit = defineEmits<IEmits>();
-
 const { openModal, closeModal } = useModal();
 
 const onBarcodeValueReceived = (codes: string[]) => {
@@ -42,5 +53,9 @@ const openBarcodeScanner = () => {
 <style lang="scss">
 .barcode-scanner {
   @apply m-1.5;
+}
+
+.mobile-icon {
+  @apply h-full m-2 text-[--base-color];
 }
 </style>
