e83dcf5b6bee26ffebdaf06a851f47c6439e6fc3
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-03-24 12:32:39 +0100

    fix: save changes modal buttons

--- client-app/shared/common/components/save-changes-modal.vue
+++ client-app/shared/common/components/save-changes-modal.vue
@@ -3,11 +3,11 @@
     {{ message ?? $t("common.messages.save_changes") }}
 
     <template #actions>
-      <VcButton color="secondary" variant="outline" @click="$emit('close')">
+      <VcButton color="secondary" variant="outline" min-width="8rem" @click="$emit('close')">
         {{ closeButtonText ?? $t("common.buttons.no") }}
       </VcButton>
 
-      <VcButton @click="$emit('confirm')">
+      <VcButton class="ml-auto" min-width="8rem" @click="$emit('confirm')">
         {{ confirmButtonText ?? $t("common.buttons.yes") }}
       </VcButton>
     </template>
