15276aae508ed705d0a00ea6e4032b0f7d4ed675
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-03-27 00:30:01 +0900

    feat: improve vc-typography

--- client-app/ui-kit/components/atoms/typography/vc-typography.vue
+++ client-app/ui-kit/components/atoms/typography/vc-typography.vue
@@ -15,16 +15,25 @@
 
 <script setup lang="ts">
 import { computed } from "vue";
+import { getColorValue } from "../../../utilities";
 
 interface IProps {
   tag?: string;
   variant?: VcTypographyVariantType;
   truncate?: boolean;
+  fontSize?: string;
+  fontWeight?: string;
+  textTransform?: string;
+  color?: string;
 }
 
 const props = withDefaults(defineProps<IProps>(), {
   tag: "p",
   align: "left",
+  fontSize: "",
+  fontWeight: "",
+  textTransform: "",
+  color: "",
 });
 
 const isHeader = computed(() => /^(h[1-6])$/.test(props.tag));
@@ -40,55 +49,87 @@ const _variant = computed(() => {
 
   return "base";
 });
+
+const _color = computed(() => getColorValue(props.color));
 </script>
 
 <style lang="scss">
 .vc-typography {
+  --props-font-size: v-bind(props.fontSize);
+  --props-font-weight: v-bind(props.fontWeight);
+  --props-text-transform: v-bind(props.textTransform);
+  --props-color: v-bind(_color);
+
+  --font-size: var(--props-font-size, var(--vc-typography-font-size));
+  --font-weight: var(--props-font-weight, var(--vc-typography-font-weight));
+  --text-transform: var(--props-text-transform, var(--vc-typography-text-transform));
+  --color: var(--props-color, var(--vc-typography-color, theme("colors.neutral.950")));
+
   @apply empty:hidden;
 
   &--variant {
     &--h1 {
-      @apply text-2xl tracking-wide font-bold uppercase;
+      @apply tracking-wide;
+
+      font-size: var(--font-size, theme("fontSize.2xl"));
+      font-weight: var(--font-weight, 700);
+      text-transform: var(--text-transform, uppercase);
 
       @media (min-width: theme("screens.lg")) {
-        @apply text-3xl;
+        font-size: var(--font-size, theme("fontSize.3xl"));
       }
     }
 
     &--h2 {
-      @apply text-xl tracking-wide font-bold uppercase;
+      @apply tracking-wide;
+
+      font-size: var(--font-size, theme("fontSize.xl"));
+      font-weight: var(--font-weight, 700);
+      text-transform: var(--text-transform, uppercase);
 
       @media (min-width: theme("screens.lg")) {
-        @apply text-2xl;
+        font-size: var(--font-size, theme("fontSize.2xl"));
       }
     }
 
     &--h3 {
-      @apply text-lg tracking-wide font-bold uppercase;
+      @apply tracking-wide;
+
+      font-size: var(--font-size, theme("fontSize.lg"));
+      font-weight: var(--font-weight, 700);
+      text-transform: var(--text-transform, uppercase);
 
       @media (min-width: theme("screens.lg")) {
-        @apply text-xl;
+        font-size: var(--font-size, theme("fontSize.xl"));
       }
     }
 
     &--h4 {
-      @apply text-base font-bold uppercase;
+      font-size: var(--font-size, theme("fontSize.base"));
+      font-weight: var(--font-weight, 700);
+      text-transform: var(--text-transform, uppercase);
 
       @media (min-width: theme("screens.lg")) {
-        @apply text-lg;
+        font-size: var(--font-size, theme("fontSize.lg"));
       }
     }
 
     &--h5 {
-      @apply text-base font-bold uppercase;
+      font-size: var(--font-size, theme("fontSize.base"));
+      font-weight: var(--font-weight, 700);
+      text-transform: var(--text-transform, uppercase);
     }
 
     &--h6 {
-      @apply text-base font-bold uppercase;
+      font-size: var(--font-size, theme("fontSize.base"));
+      font-weight: var(--font-weight, 700);
+      text-transform: var(--text-transform, uppercase);
     }
 
     &--base {
-      @apply text-base;
+      font-size: var(--font-size, theme("fontSize.base"));
+      font-weight: var(--font-weight, 400);
+      text-transform: var(--text-transform, none);
     }
   }
 
