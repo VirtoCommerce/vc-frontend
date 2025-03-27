f705d0c4af04708145f190ed0c9d7a306b059ee1
NaMax66 <Maxim.Naidovich@virtoway.com>
2025-03-27 14:56:39 +0100

    chore: fix cart warnings

---
+++ client-app/pages/matcher/builderIo/console-ignored-errors.ts
@@ -0,0 +1,7 @@
+export const consoleIgnoredErrors = [
+  'Start Failed setting prop "attributes" on <div>',
+  'Failed setting prop "attributes" on <div>',
+  "Extraneous non-props attributes",
+];
+
+export const BUILDER_IO_TRACE_MARKER = "<VpBuilderIo>";
