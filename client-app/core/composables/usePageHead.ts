1ad860a859574b7f38d57056e4454d9f918a09cd
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-04-02 16:45:14 +0200

    fix: eslint errors

--- client-app/core/composables/usePageHead.ts
+++ client-app/core/composables/usePageHead.ts
@@ -4,12 +4,12 @@ import { usePageTitle } from "./usePageTitle";
 import type { IUsePageSeoData } from "../types";
 
 export function usePageHead(data?: IUsePageSeoData) {
-  const { title: builtTitle } = usePageTitle(data.title);
+  const { title: builtTitle } = usePageTitle(data?.title ?? "");
 
   return useHead({
     title: builtTitle,
     meta: () => {
-      if (!data.meta) {
+      if (!data?.meta) {
         return [];
       }
       return Object.entries(data.meta).map(([name, content]) => ({ name, content: unref(content) ?? "" }));
