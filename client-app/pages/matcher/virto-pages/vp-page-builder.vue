d3b178b3c34459eb8025401696b98e768c4eac56
Basil Kotov <basilkot@gmail.com>
2025-04-03 15:03:35 +0100

    feat: use page buidler pages in virto pages

---
+++ client-app/pages/matcher/virto-pages/vp-page-builder.vue
@@ -0,0 +1,57 @@
+<template>
+  <div v-if="pageBuilderContent">
+    <template v-for="item in pageBuilderContent.content">
+      <!-- @deprecated. TODO Keep only v-bind="item". Remove settings and model from all components -->
+      <component
+        :is="item.type"
+        v-if="!item.hidden"
+        :key="item.id"
+        v-bind="item"
+        :model="item"
+        :settings="pageBuilderContent.settings"
+      />
+    </template>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { onBeforeMount, shallowRef } from "vue";
+
+interface IProps {
+  content?: string;
+}
+
+type BlockType = { [key: string]: unknown };
+
+interface IPageBuilderContent {
+  settings: BlockType;
+  content: BlockType[];
+}
+
+const props = defineProps<IProps>();
+
+const canShowContent = shallowRef(false);
+const pageBuilderContent = shallowRef<IPageBuilderContent | null>(null);
+
+function clearState() {
+  pageBuilderContent.value = null;
+  canShowContent.value = false;
+}
+
+onBeforeMount(() => {
+  if (props.content) {
+    trySetContent();
+  } else {
+    clearState();
+  }
+});
+
+function trySetContent() {
+  if (!props.content) {
+    return;
+  }
+  const blocks = <IPageBuilderContent>JSON.parse(props.content);
+  pageBuilderContent.value = blocks;
+  canShowContent.value = true;
+}
+</script>
