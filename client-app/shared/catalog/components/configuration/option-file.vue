4204756480f0e67a1a42fc1b197f4b427d69df73
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-04-04 09:02:21 +0200

    feat: file section for configurable products (#1619)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-2796
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.19.0-pr-1619-61d7-61d7b277.zip

---
+++ client-app/shared/catalog/components/configuration/option-file.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="option-file">
+    <VcFileUploader
+      :files="files"
+      v-bind="fileOptions"
+      removable
+      @add-files="onAddFiles"
+      @remove-files="onRemoveFiles"
+      @download="onFileDownload"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed, toRefs, onMounted } from "vue";
+import { downloadFile, useFiles } from "@/shared/files";
+import { toAttachedFile } from "@/ui-kit/utilities";
+import type { CartConfigurationItemFileType } from "@/core/api/graphql/types";
+import type { DeepReadonly } from "vue";
+
+const emit = defineEmits<IEmits>();
+const props = defineProps<IProps>();
+
+const { value } = toRefs(props);
+
+const DEFAULT_FILES_SCOPE = "product-configuration";
+
+interface IProps {
+  value?: DeepReadonly<CartConfigurationItemFileType[]>;
+}
+
+interface IEmits {
+  (e: "input", value: CartConfigurationItemFileType[]): void;
+}
+
+const initialFiles = computed(
+  () => value.value?.map((file) => toAttachedFile(file.name, file.size, file.contentType, file.url)) ?? [],
+);
+
+const {
+  files,
+  attachedAndUploadedFiles,
+  addFiles,
+  validateFiles,
+  removeFiles,
+  uploadFiles,
+  fetchOptions: fetchFileOptions,
+  options: fileOptions,
+  stopWatchInitialValue,
+} = useFiles(DEFAULT_FILES_SCOPE, initialFiles);
+
+async function onAddFiles(items: INewFile[]) {
+  addFiles(items);
+  validateFiles();
+  await uploadFiles();
+  stopWatchInitialValue();
+  emit("input", attachedAndUploadedFiles.value);
+}
+
+async function onRemoveFiles(filesToRemove: FileType[]) {
+  await removeFiles(filesToRemove);
+  validateFiles();
+  emit("input", attachedAndUploadedFiles.value);
+}
+
+function onFileDownload(file: FileType) {
+  if (file && file.url) {
+    void downloadFile(file.url, file.name);
+  }
+}
+
+onMounted(() => {
+  void fetchFileOptions();
+});
+</script>
