eddc040d8af392e0445fb838bf4ecb1617618dbc
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-04-04 17:40:35 +0900

    fix: rollback ui-kit changes

--- client-app/ui-kit/components/molecules/vendor/vc-vendor.vue
+++ client-app/ui-kit/components/molecules/vendor/vc-vendor.vue
@@ -1,29 +1,51 @@
 <template>
-  <VcChip size="lg" :color="color" variant="outline-dark">
-    {{ _name ? $t("ui_kit.labels.vendor_name", { name: _name }) : $t("ui_kit.labels.not_available") }}
-  </VcChip>
+  <div :class="['vc-vendor', { 'vc-vendor--not-available': !vendor?.name }]">
+    <VcIcon name="vendor" size="sm" class="vc-vendor__icon" />
+
+    <span class="vc-vendor__title">
+      {{ $t("ui_kit.labels.vendor") }}:
+
+      <span class="vc-vendor__name">
+        {{ vendor?.name || $t("ui_kit.labels.not_available") }}
+      </span>
+    </span>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from "vue";
 import type { CommonVendor } from "@/core/api/graphql/types";
 
 interface IProps {
-  name?: string;
-  // @deprecated use `name` instead of `vendor`
   vendor?: CommonVendor;
-  size?: VcChipSizeType;
-  color?: VcChipColorType;
 }
 
-const props = withDefaults(defineProps<IProps>(), {
-  size: "lg",
-  color: "secondary",
-});
+defineProps<IProps>();
 
-if (props.vendor) {
-  console.warn("[UI-kit] VcVendor:", 'The "vendor" prop is deprecated. Please use "name" instead.');
-}
-
-const _name = computed(() => props.name ?? props.vendor?.name);
+console.log("[UI-kit] VcVendor is deprecated. Use VendorName instead.");
 </script>
+
+<style lang="scss">
+.vc-vendor {
+  $notAvailable: "";
+
+  @apply flex gap-x-1.5 items-center min-w-0 text-primary-600;
+
+  &--not-available {
+    $notAvailable: &;
+  }
+
+  &__icon {
+    @apply flex-none fill-primary;
+  }
+
+  &__title {
+    @apply font-bold uppercase text-base truncate;
+  }
+
+  &__name {
+    #{$notAvailable} & {
+      @apply text-neutral-400;
+    }
+  }
+}
+</style>
