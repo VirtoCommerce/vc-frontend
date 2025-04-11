ecb9bbd84c4d432a9b7ac0cff8ce73361e659b2c
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-04-11 19:46:21 +0900

    feat: implement property groups in product details page

--- client-app/shared/catalog/components/product/properties.vue
+++ client-app/shared/catalog/components/product/properties.vue
@@ -3,33 +3,50 @@
     v-if="showPropertiesBlock"
     :title="model.title || $t('shared.catalog.product_details.technical_specs_block_title')"
     icon="adjustments"
+    class="properties"
   >
-    <VcCollapsibleContent max-height="12.5rem">
-      <div class="space-y-4">
-        <VcProperty v-for="property in properties" :key="property.name" :label="property.label!" class="text-base">
-          <span v-if="isHTML(property)" :title="String(property.value)">
-            <VcMarkdownRender :src="String(property.value)" />
-          </span>
+    <div class="space-y-4">
+      <!-- Rating -->
+      <VcProperty
+        v-if="productReviewsEnabled && product.rating"
+        :label="$t('shared.catalog.product_card.product_rating')"
+        class="text-base"
+      >
+        <ProductRating :rating="product.rating" />
+      </VcProperty>
 
-          <span v-else :title="String(property.value)">{{ property.value }}</span>
-        </VcProperty>
+      <!-- Vendor -->
+      <VcProperty v-if="showVendor" :label="$t('shared.catalog.product_details.vendor_label')" class="text-base">
+        <Vendor :vendor="product.vendor!" with-rating />
+      </VcProperty>
+    </div>
 
-        <!-- Rating -->
-        <VcProperty
-          v-if="productReviewsEnabled && product.rating"
-          :label="$t('shared.catalog.product_card.product_rating')"
-          class="text-base"
-        >
-          <ProductRating :rating="product.rating" />
-        </VcProperty>
+    <VcWidget
+      v-for="(item, index) in groupedProperties"
+      :key="index"
+      size="xs"
+      :title="item.group?.localizedName"
+      collapsible
+      :collapsed="index > 0"
+    >
+      <div class="properties__group">
+        <div v-for="property in item.properties" :key="property.id" :label="property.label!" class="properties__prop">
+          <div class="properties__label">
+            {{ property.label }}
+          </div>
 
-        <!-- Vendor -->
-        <VcProperty v-if="showVendor" :label="$t('shared.catalog.product_details.vendor_label')" class="text-base">
-          <Vendor :vendor="product.vendor!" with-rating />
-        </VcProperty>
+          <div class="properties__value">
+            <span v-if="isHTML(property)">
+              <VcMarkdownRender :src="String(property.value)" />
+            </span>
+
+            <span v-else>
+              {{ property.value }}
+            </span>
+          </div>
+        </div>
       </div>
-    </VcCollapsibleContent>
-    <!-- Properties -->
+    </VcWidget>
   </ProductTitledBlock>
 </template>
 
@@ -45,8 +62,13 @@ import {
 } from "@/modules/customer-reviews/constants";
 import { PropertyValueTypes } from "@/modules/quotes/api/graphql/types";
 import { ProductTitledBlock, Vendor } from "@/shared/catalog";
-import type { Product, Property } from "@/core/api/graphql/types";
+import type { Product, Property, PropertyGroup } from "@/core/api/graphql/types";
 import ProductRating from "@/modules/customer-reviews/components/product-rating.vue";
+
+interface IGroupedProperties {
+  group?: PropertyGroup;
+  properties: Property[];
+}
 
 interface IProps {
   product: Product;
@@ -58,6 +80,46 @@ interface IProps {
 
 const props = defineProps<IProps>();
 
+function groupAndSortProperties(source: Property[]): IGroupedProperties[] {
+  const result = new Map<string, IGroupedProperties>();
+  const defaultGroup: PropertyGroup = {
+    id: "ungrouped",
+    name: "Other",
+    priority: Infinity,
+    localizedName: "Other",
+    localizedDescription: "",
+  };
+
+  let hasValid = false;
+
+  for (const prop of source) {
+    const group = prop.group?.id ? prop.group! : defaultGroup;
+
+    if (group !== defaultGroup) {
+      hasValid = true;
+    }
+
+    const entry = result.get(group.id) ?? { group, properties: [] };
+    entry.properties.push(prop);
+    result.set(group.id, entry);
+  }
+
+  if (!hasValid) {
+    return [
+      {
+        properties: source,
+      },
+    ];
+  }
+
+  return Array.from(result.values())
+    .sort((a, b) => (a.group?.priority ?? 0) - (b.group?.priority ?? 0))
+    .map(({ group, properties }) => ({
+      group,
+      properties: properties.sort((a, b) => (a.displayOrder ?? 0) - (b.displayOrder ?? 0)),
+    }));
+}
+
 const { themeContext } = useThemeContext();
 const { isEnabled } = useModuleSettings(CUSTOMER_REVIEWS_MODULE_ID);
 const productReviewsEnabled = isEnabled(CUSTOMER_REVIEWS_ENABLED_KEY);
@@ -65,6 +127,8 @@ const productReviewsEnabled = isEnabled(CUSTOMER_REVIEWS_ENABLED_KEY);
 const properties = computed(() =>
   Object.values(getPropertiesGroupedByName(props.product.properties ?? [], PropertyType.Product)),
 );
+
+const groupedProperties = computed(() => groupAndSortProperties(properties.value));
 
 const showVendor = computed(
   () => themeContext.value?.settings?.vendor_enabled && !props.product.hasVariations && props.product.vendor,
@@ -75,3 +139,19 @@ function isHTML(property: Property): boolean {
   return (property.propertyValueType as PropertyValueTypes) === PropertyValueTypes.Html;
 }
 </script>
+
+<style lang="scss">
+.properties {
+  &__prop {
+    @apply flex items-stretch text-sm rounded even:bg-neutral-100;
+  }
+
+  &__label {
+    @apply w-32 py-2.5 ps-2 text-neutral-600;
+  }
+
+  &__value {
+    @apply grow py-2.5 px-2 text-neutral-950;
+  }
+}
+</style>
