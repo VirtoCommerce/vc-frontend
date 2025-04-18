296a79ab6cdfa16d5f062f571e639646c877d517
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-04-18 15:00:32 +0200

    fix: minor improvements

--- client-app/core/api/graphql/config/links/ws.ts
+++ client-app/core/api/graphql/config/links/ws.ts
@@ -1,7 +1,8 @@
 import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
 import { createClient } from "graphql-ws";
 import { apolloWebSocketConnectionParams } from "@/core/api/graphql/config/interceptors";
-import { WEBSOCKETS_ENDPOINT_TIMEOUT, WEBSOCKETS_ENDPOINT_URL } from "@/core/api/graphql/consts";
+import { WEBSOCKETS_ENDPOINT_TIMEOUT, WEBSOCKETS_ENDPOINT_URL, WEBSOCKETS_KEEP_ALIVE } from "@/core/api/graphql/consts";
+import { Logger } from "@/core/utilities";
 
 export const wsLink = new GraphQLWsLink(
   createClient({
@@ -10,28 +11,28 @@ export const wsLink = new GraphQLWsLink(
     shouldRetry: () => true,
     connectionAckWaitTimeout: WEBSOCKETS_ENDPOINT_TIMEOUT,
     connectionParams: apolloWebSocketConnectionParams,
-    keepAlive: 30000,
+    keepAlive: WEBSOCKETS_KEEP_ALIVE,
     on: {
       connecting: () => {
-        console.info("[WebSocket] Connecting... Attempting to establish connection.");
+        Logger.info("[WebSocket] Connecting... Attempting to establish connection.");
       },
       opened: (_) => {
         const socket = _ as WebSocket;
-        console.info("[WebSocket] Connection opened successfully.", {
+        Logger.info("[WebSocket] Connection opened successfully.", {
           url: socket.url,
           protocol: socket.protocol,
         });
       },
       closed: (_) => {
         const closeEvent = _ as CloseEvent;
-        console.warn("[WebSocket] Connection closed.", {
+        Logger.warn("[WebSocket] Connection closed.", {
           code: closeEvent.code,
           reason: closeEvent.reason || "No reason provided",
           wasClean: closeEvent.wasClean,
         });
       },
       error: (err) => {
-        console.error("[WebSocket] An error occurred.", err);
+        Logger.error("[WebSocket] An error occurred.", err);
       },
     },
   }),
--- client-app/core/api/graphql/consts.ts
+++ client-app/core/api/graphql/consts.ts
@@ -1,5 +1,6 @@
 export const HTTP_ENDPOINT_URL = "/graphql";
 export const WEBSOCKETS_ENDPOINT_URL = `${location.protocol === "https:" ? "wss" : "ws"}://${location.host}${HTTP_ENDPOINT_URL}`;
 export const WEBSOCKETS_ENDPOINT_TIMEOUT = 55 * 1000; // 55 seconds
+export const WEBSOCKETS_KEEP_ALIVE = 30 * 1000; // 30 seconds
 export const DEFAULT_REQUEST_TIMEOUT = 60 * 1000;
 export const ROOT_QUERY_CACHE_ID = "ROOT_QUERY";
