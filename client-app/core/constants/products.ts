29e9b5d3d0bd81fefe41dd9fb3ffcb62e48ec74a
NaMax66 <max.naidovich@gmail.com>
2025-04-24 17:33:54 +0200

    feat: add keys

--- client-app/core/constants/products.ts
+++ client-app/core/constants/products.ts
@@ -1,9 +1,9 @@
 export const PRODUCT_SORTING_LIST = [
-  { id: "", name: "Featured" },
-  { id: "name-ascending", name: "Alphabetically, A-Z" },
-  { id: "name-descending", name: "Alphabetically, Z-A" },
-  { id: "price-ascending", name: "Price, low to high" },
-  { id: "price-descending", name: "Price, high to low" },
-  { id: "createddate-descending", name: "Date, new to old" },
-  { id: "createddate-ascending", name: "Date, old to new" },
+  { id: "", name: "shared.featured" },
+  { id: "name-ascending", name: "shared.alphabetically_a_z" },
+  { id: "name-descending", name: "shared.alphabetically_z_a" },
+  { id: "price-ascending", name: "shared.price_low_to_high" },
+  { id: "price-descending", name: "shared.price_high_to_low" },
+  { id: "createddate-descending", name: "shared.date_new_to_old" },
+  { id: "createddate-ascending", name: "shared.date_old_to_new" },
 ];
