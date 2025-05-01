917a4e66381ffd0793ccb22b5237dec78a805f46
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-01 18:02:14 +0200

    feat: implement outer radius

--- client-app/shared/catalog/components/badges-wrapper.vue
+++ client-app/shared/catalog/components/badges-wrapper.vue
@@ -6,10 +6,32 @@
 
 <style lang="scss">
 .badges-wrapper {
-  @apply absolute -left-1 -top-1 flex items-center gap-2 rounded-br-lg bg-additional-50 z-[1];
+  --corner-radius: 4px;
+
+  @apply absolute -left-px -top-px flex items-center gap-2 rounded-br-lg bg-additional-50 z-[1];
 
   &:not(:empty) {
-    @apply p-1.5;
+    @apply pb-1.5 pr-1.5 pl-px pt-px;
+  }
+
+  &::after,
+  &::before {
+    content: "";
+    @apply absolute size-2;
+
+    background-image: radial-gradient(
+      circle at right bottom,
+      transparent calc(var(--corner-radius) * 2),
+      var(--color-additional-50) calc(var(--corner-radius) * 2)
+    );
+  }
+
+  &::after {
+    @apply -right-2 top-0;
+  }
+
+  &::before {
+    @apply left-0 -bottom-2;
   }
 }
 </style>
