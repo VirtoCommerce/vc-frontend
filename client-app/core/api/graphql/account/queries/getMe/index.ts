e938734c4c324761d14bd2b63bb519a359c86c6b
NaMax66 <max.naidovich@gmail.com>
2025-05-01 15:04:23 +0200

    fix: type UserType

--- client-app/core/api/graphql/account/queries/getMe/index.ts
+++ client-app/core/api/graphql/account/queries/getMe/index.ts
@@ -1,14 +1,16 @@
 import { useLazyQuery } from "@vue/apollo-composable";
 import { GetMeDocument } from "@/core/api/graphql/types";
 import { graphqlClient } from "../../../client";
-import type { Query, UserType } from "@/core/api/graphql/types";
+import type { GetMeQuery, GetMeQueryVariables } from "@/core/api/graphql/types";
+
+export type UserType = GetMeQuery["me"];
 
 export function useGetMeQuery() {
   return useLazyQuery(GetMeDocument);
 }
 
 export async function getMe(userId: string): Promise<UserType> {
-  const { data } = await graphqlClient.query<Required<Pick<Query, "me">>, { userId: string }>({
+  const { data } = await graphqlClient.query<GetMeQuery, GetMeQueryVariables>({
     query: GetMeDocument,
     variables: {
       userId,
