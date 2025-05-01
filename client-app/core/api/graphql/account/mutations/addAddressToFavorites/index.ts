9c013bfb01e4e7befe2cc48a7194daa9692b0da2
NaMax66 <max.naidovich@gmail.com>
2025-05-01 16:01:12 +0200

    fix: add address to favorites

--- client-app/core/api/graphql/account/mutations/addAddressToFavorites/index.ts
+++ client-app/core/api/graphql/account/mutations/addAddressToFavorites/index.ts
@@ -1,10 +1,10 @@
+import { AddAddressToFavoritesDocument } from "@/core/api/graphql/types";
 import { graphqlClient } from "../../../client";
-import addAddressToFavoritesMutation from "./addAddressToFavoritesMutation.graphql";
 import type { AddAddressToFavoritesMutation, AddAddressToFavoritesMutationVariables } from "@/core/api/graphql/types";
 
 export async function addAddressToFavorites(addressId: string): Promise<void> {
   await graphqlClient.mutate<AddAddressToFavoritesMutation, AddAddressToFavoritesMutationVariables>({
-    mutation: addAddressToFavoritesMutation,
+    mutation: AddAddressToFavoritesDocument,
     variables: {
       command: {
         addressId,
