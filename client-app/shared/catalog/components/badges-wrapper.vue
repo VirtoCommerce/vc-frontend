5d9508e86d6b44c8bb7cd21023fef547aa9c7ba9
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-05 15:21:09 +0200

    fix: empty pseudo styles

--- client-app/shared/catalog/components/badges-wrapper.vue
+++ client-app/shared/catalog/components/badges-wrapper.vue
@@ -13,31 +13,26 @@
 
   &:not(:empty) {
     @apply pb-1.5 pr-1.5 pl-px pt-px;
-  }
 
-  &:empty::after,
-  &:empty::before {
-    @apply hidden;
-  }
+    &::after,
+    &::before {
+      content: "";
+      @apply absolute size-2;
 
-  &::after,
-  &::before {
-    content: "";
-    @apply absolute size-2;
+      background-image: radial-gradient(
+        circle at right bottom,
+        transparent var(--decorative-size),
+        var(--color-additional-50) var(--decorative-size)
+      );
+    }
 
-    background-image: radial-gradient(
-      circle at right bottom,
-      transparent var(--decorative-size),
-      var(--color-additional-50) var(--decorative-size)
-    );
-  }
+    &::after {
+      @apply top-0 -right-[var(--decorative-size)];
+    }
 
-  &::after {
-    @apply top-0 -right-[var(--decorative-size)];
-  }
-
-  &::before {
-    @apply left-0 -bottom-[var(--decorative-size)];
+    &::before {
+      @apply left-0 -bottom-[var(--decorative-size)];
+    }
   }
 }
 </style>
