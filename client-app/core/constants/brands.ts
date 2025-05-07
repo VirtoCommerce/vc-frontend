75ca70bc8a9cf0ce3aa21fa476e11d92bbeed248
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-07 16:00:59 +0200

    fix: minor refactor

--- client-app/core/constants/brands.ts
+++ client-app/core/constants/brands.ts
@@ -2,12 +2,8 @@ export const DEFAULT_BRANDS_PER_PAGE = 5;
 
 const LATIN_UPPERCASE_LETTERS = Array.from({ length: 26 }, (_, i) => String.fromCharCode(65 + i));
 
-export const LATIN_UPPERCASE_LETTERS_MAPPING = LATIN_UPPERCASE_LETTERS.reduce(
-  (acc, letter) => {
-    acc[letter] = letter;
-    return acc;
-  },
-  {} as Record<string, string>,
+export const LATIN_UPPERCASE_LETTERS_MAPPING = Object.fromEntries(
+  LATIN_UPPERCASE_LETTERS.map((letter) => [letter, letter]),
 );
 
 export const NAV_INDEX_ITEMS = {
