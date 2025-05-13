8fa5d16786c00ca7e234748c55e332bf021ad6e4
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-13 15:11:42 +0200

    fix: purchase-before and discount badge size

--- client-app/shared/catalog/components/purchased-before-badge.vue
+++ client-app/shared/catalog/components/purchased-before-badge.vue
@@ -1,5 +1,5 @@
 <template>
-  <VcBadge color="info" size="sm" :title="$t('shared.catalog.purchased_before_badge.title')" class="min-h-4 px-1">
+  <VcBadge color="info" size="md" :title="$t('shared.catalog.purchased_before_badge.title')" class="min-h-4.5 px-1">
     <VcIcon name="cart-check" size="xxs" />
   </VcBadge>
 </template>
