9c6c5d46bbef3108a08d9501e6c22692d4419826
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-13 15:17:56 +0200

    fix: purchase-before icon size

--- client-app/shared/catalog/components/purchased-before-badge.vue
+++ client-app/shared/catalog/components/purchased-before-badge.vue
@@ -1,5 +1,5 @@
 <template>
   <VcBadge color="info" size="md" :title="$t('shared.catalog.purchased_before_badge.title')" class="min-h-4.5 px-1">
-    <VcIcon name="cart-check" size="xxs" />
+    <VcIcon name="cart-check" size="xs" />
   </VcBadge>
 </template>
