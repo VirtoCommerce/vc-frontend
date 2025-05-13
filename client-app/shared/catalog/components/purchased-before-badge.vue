15b62551f7d7201332f409c98f4f3df8f1232c64
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-13 15:19:02 +0200

    fix: add data-test-id

--- client-app/shared/catalog/components/purchased-before-badge.vue
+++ client-app/shared/catalog/components/purchased-before-badge.vue
@@ -1,5 +1,11 @@
 <template>
-  <VcBadge color="info" size="md" :title="$t('shared.catalog.purchased_before_badge.title')" class="min-h-4.5 px-1">
+  <VcBadge
+    color="info"
+    size="md"
+    :title="$t('shared.catalog.purchased_before_badge.title')"
+    class="min-h-4.5 px-1"
+    data-testid="purchased-before-badge"
+  >
     <VcIcon name="cart-check" size="xs" />
   </VcBadge>
 </template>
