b8a6ff8a1d22f73e6c644348cfe950067fb239ec
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-05-20 15:06:11 +0200

    chore: enhance extension points mechanism

--- client-app/shared/common/composables/useComponentsRegistry.ts
+++ client-app/shared/common/composables/useComponentsRegistry.ts
@@ -3,34 +3,36 @@ import { defineAsyncComponent, shallowReadonly, shallowRef } from "vue";
 import { IS_DEVELOPMENT } from "@/core/constants";
 import type { ComponentRegistryType } from "@/shared/common/types/components-registry";
 
+const initialComponentRegistry: ComponentRegistryType = {
+  header: {
+    compare: defineAsyncComponent(
+      () => import("@/shared/layout/components/header/_internal/link-components/link-compare.vue"),
+    ),
+    cart: defineAsyncComponent(
+      () => import("@/shared/layout/components/header/_internal/link-components/link-cart.vue"),
+    ),
+  },
+  mobileMenu: {
+    cart: defineAsyncComponent(
+      () => import("@/shared/layout/components/header/_internal/mobile-menu/link-components/link-cart.vue"),
+    ),
+    compare: defineAsyncComponent(
+      () => import("@/shared/layout/components/header/_internal/mobile-menu/link-components/link-compare.vue"),
+    ),
+  },
+  account: {
+    orders: defineAsyncComponent(
+      () => import("@/shared/account/components/account-navigation-link-components/link-orders.vue"),
+    ),
+    lists: defineAsyncComponent(
+      () => import("@/shared/account/components/account-navigation-link-components/link-lists.vue"),
+    ),
+  },
+  mobileHeader: {},
+};
+
 function _useComponentsRegistry() {
-  const componentRegistry = shallowRef<ComponentRegistryType>({
-    header: {
-      compare: defineAsyncComponent(
-        () => import("@/shared/layout/components/header/_internal/link-components/link-compare.vue"),
-      ),
-      cart: defineAsyncComponent(
-        () => import("@/shared/layout/components/header/_internal/link-components/link-cart.vue"),
-      ),
-    },
-    mobileMenu: {
-      cart: defineAsyncComponent(
-        () => import("@/shared/layout/components/header/_internal/mobile-menu/link-components/link-cart.vue"),
-      ),
-      compare: defineAsyncComponent(
-        () => import("@/shared/layout/components/header/_internal/mobile-menu/link-components/link-compare.vue"),
-      ),
-    },
-    account: {
-      orders: defineAsyncComponent(
-        () => import("@/shared/account/components/account-navigation-link-components/link-orders.vue"),
-      ),
-      lists: defineAsyncComponent(
-        () => import("@/shared/account/components/account-navigation-link-components/link-lists.vue"),
-      ),
-    },
-    mobileHeader: {},
-  });
+  const componentRegistry = shallowRef<ComponentRegistryType>(initialComponentRegistry);
 
   function registerComponent<T extends keyof ComponentRegistryType>(
     type: T,
@@ -50,16 +52,23 @@ function _useComponentsRegistry() {
     return shallowReadonly(componentRegistry.value[type]);
   }
 
+  function isComponentRegistered<T extends keyof ComponentRegistryType>(type: T, id: string) {
+    return componentRegistry.value[type][id] !== undefined;
+  }
+
   if (IS_DEVELOPMENT) {
     window.vcComponentsRegistry = {
       components: componentRegistry.value,
       registerComponent,
       getComponents,
       unregisterComponent,
+      isComponentRegistered,
     };
   }
 
   return {
+    components: componentRegistry.value,
+
     headerLinkComponents: shallowReadonly(componentRegistry.value.header),
     mobileLinkComponents: shallowReadonly(componentRegistry.value.mobileMenu),
     accountLinkComponents: shallowReadonly(componentRegistry.value.account),
@@ -68,6 +77,7 @@ function _useComponentsRegistry() {
     getComponents,
     registerComponent,
     unregisterComponent,
+    isComponentRegistered,
   };
 }
 
--- client-app/shared/common/types/window.d.ts
+++ client-app/shared/common/types/window.d.ts
@@ -4,11 +4,6 @@ export {};
 
 declare global {
   interface Window {
-    vcComponentsRegistry?: {
-      components: ReturnType<typeof useComponentsRegistry>["components"];
-      registerComponent: ReturnType<typeof useComponentsRegistry>["registerComponent"];
-      getComponents: ReturnType<typeof useComponentsRegistry>["getComponents"];
-      unregisterComponent: ReturnType<typeof useComponentsRegistry>["unregisterComponent"];
-    };
+    vcComponentsRegistry: Partial<ReturnType<typeof useComponentsRegistry>>;
   }
 }
