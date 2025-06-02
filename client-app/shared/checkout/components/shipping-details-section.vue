47a24a49a61ea8a79896122c5d542d2a46c59c4a
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-06-02 11:23:32 +0200

    fix: only bopis delivery method scenario (#1759)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-3368
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.23.0-pr-1759-0f75-0f751899.zip

--- client-app/shared/checkout/components/shipping-details-section.vue
+++ client-app/shared/checkout/components/shipping-details-section.vue
@@ -1,7 +1,7 @@
 <template>
   <VcWidget :title="$t('shared.checkout.shipping_details_section.title')" prepend-icon="truck" size="lg">
     <div class="flex flex-col flex-wrap gap-4 xs:flex-row xs:gap-y-6 lg:gap-8">
-      <div v-if="hasBOPIS">
+      <div v-if="hasBOPIS && !onlyOneDeliveryMethod">
         <VcLabel>
           {{ $t("shared.checkout.shipping_details_section.labels.delivery_option") }}
         </VcLabel>
@@ -133,7 +133,8 @@ const SHIPPING_OPTIONS = {
 
 type ShippingOptionType = keyof typeof SHIPPING_OPTIONS;
 
-const { deliveryAddress, shipmentMethod, onDeliveryAddressChange, setShippingMethod } = useCheckout();
+const { deliveryAddress, shipmentMethod, onDeliveryAddressChange, setShippingMethod, billingAddressEqualsShipping } =
+  useCheckout();
 
 const mode = ref<ShippingOptionType>(shipmentMethod.value?.code === BOPIS_CODE ? "pickup" : "shipping");
 
@@ -142,21 +143,23 @@ const { hasBOPIS, openSelectAddressModal, loading: isLoadingBopisAddresses, bopi
 
 const shippingMethods = computed(() => availableShippingMethods.value.filter((method) => method.code !== BOPIS_CODE));
 
+const onlyOneDeliveryMethod = computed(() => availableShippingMethods.value.length === 1);
+
 function switchShippingOptions(_mode: ShippingOptionType) {
   mode.value = _mode;
 }
 
 watch(
   mode,
-  (newMode, previousMode) => {
-    if (!previousMode) {
-      return;
-    }
-
+  (newMode) => {
     const shippingMethod = newMode === SHIPPING_OPTIONS.pickup ? bopisMethod.value : shippingMethods.value[0];
 
     if (!shippingMethod || shippingMethod.code === shipment.value?.shipmentMethodCode) {
       return;
+    }
+
+    if (shippingMethod.code === BOPIS_CODE) {
+      billingAddressEqualsShipping.value = false;
     }
 
     void updateShipment({
