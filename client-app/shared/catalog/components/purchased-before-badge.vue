163a1ea73fbe2a3623915dde8b1ff1b9ca004176
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-06-05 14:01:50 +0900

    fix: purchase-sefore-badge

--- client-app/shared/catalog/components/purchased-before-badge.vue
+++ client-app/shared/catalog/components/purchased-before-badge.vue
@@ -5,9 +5,11 @@
     :title="$t('shared.catalog.purchased_before_badge.title')"
     class="purchased-before-badge"
     data-test-id="purchased-before-badge"
+    :square="!showText"
   >
-    <VcIcon name="round-check" square />
-    <span v-if="showText" class="max-w-full truncate">
+    <VcIcon name="round-check" />
+
+    <span v-if="showText">
       {{ $t("shared.catalog.purchased_before_badge.title") }}
     </span>
   </VcBadge>
@@ -15,7 +17,7 @@
 
 <script setup lang="ts">
 interface IProps {
-  size?: "sm" | "md" | "lg";
+  size?: VcBadgeSizeType;
   showText?: boolean;
 }
 
@@ -23,9 +25,3 @@ withDefaults(defineProps<IProps>(), {
   size: "md",
 });
 </script>
-
-<style lang="scss">
-.purchased-before-badge {
-  @apply max-w-full;
-}
-</style>
