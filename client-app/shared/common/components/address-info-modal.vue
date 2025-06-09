08c59d021071eb024235a7849d59b8d0d77518c5
NaMax66 <max.naidovich@gmail.com>
2025-06-09 16:26:02 +0200

    chore: make table wip

--- client-app/shared/common/components/address-info-modal.vue
+++ client-app/shared/common/components/address-info-modal.vue
@@ -1,24 +1,43 @@
 <template>
-  <VcModal :title="$t(`${TRANSLATION_KEYS_ORIGIN}.pick_point_info`)" max-width="40rem">
-    <div v-if="eta">{{ $t(`${TRANSLATION_KEYS_ORIGIN}.eta`) }}: {{ eta }}</div>
+  <VcModal :title="$t(`${TRANSLATION_KEYS_ORIGIN}.pick_point_info`)" max-width="40rem" icon="check">
+    <VcTable hide-default-footer hide-default-header :columns="TABLE_COLUMNS" :items="tableItems">
+      <template #desktop-body>
+        <tbody>
+          <tr v-if="eta">
+            <td>
+              <span class="address-info-modal__label">{{ $t(`${TRANSLATION_KEYS_ORIGIN}.eta`) }}:</span>
+            </td>
+            <td>{{ eta }}</td>
+          </tr>
+          <tr v-if="props.address.email || props.address.phone">
+            <td>
+              <span class="address-info-modal__label">{{ $t(`${TRANSLATION_KEYS_ORIGIN}.questions`) }}</span>
+            </td>
+            <td>
+              <i18n-t :keypath="contactKey">
+                <template #email>
+                  <a class="address-info-modal__link" :href="`mailto:${props.address.email}`">
+                    {{ props.address.email }}
+                  </a>
+                </template>
 
-    <div>
-      {{ $t(`${TRANSLATION_KEYS_ORIGIN}.questions`) }}
-      <i18n-t v-if="props.address.email || props.address.phone" :keypath="contactKey">
-        <template #email>
-          <a class="address-info-modal__link" :href="`mailto:${props.address.email}`">
-            {{ props.address.email }}
-          </a>
-        </template>
-
-        <template #phone>
-          <a class="address-info-modal__link" :href="`tel:${props.address.phone}`">
-            {{ props.address.phone }}
-          </a>
-        </template>
-      </i18n-t>
-    </div>
-    <div>{{ $t(`${TRANSLATION_KEYS_ORIGIN}.address`) }}: {{ addressSentence }}</div>
+                <template #phone>
+                  <a class="address-info-modal__link" :href="`tel:${props.address.phone}`">
+                    {{ props.address.phone }}
+                  </a>
+                </template>
+              </i18n-t>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <span class="address-info-modal__label">{{ $t(`${TRANSLATION_KEYS_ORIGIN}.address`) }}:</span>
+            </td>
+            <td>{{ addressSentence }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </VcTable>
     <template #actions="{ close }">
       <GetDirectionsAction v-if="link" size="md" :link="link" />
 
@@ -60,6 +79,12 @@ const props = defineProps<IProps>();
 
 const TRANSLATION_KEYS_ORIGIN = "pages.account.order_details.bopis";
 
+const TABLE_COLUMNS = [{ id: "label" }, { id: "text" }];
+
+const tableItems = computed(() => {
+  return [];
+});
+
 const contactKey = computed(() => {
   let key = "";
   const email = props.address.email;
@@ -86,6 +111,10 @@ const addressSentence = computed(() => {
 .address-info-modal {
   &__link {
     @apply py-1 text-[--header-top-link-color] hover:text-[--header-top-link-hover-color];
+  }
+
+  &__label {
+    @apply font-bold;
   }
 }
 </style>
