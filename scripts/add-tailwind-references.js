7d2248295d513280d7f69e9ee950d215cebaea4f
NaMax66 <max.naidovich@gmail.com>
2025-06-16 15:32:10 +0200

    chore: remove comments

--- scripts/add-tailwind-references.js
+++ scripts/add-tailwind-references.js
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unsafe-member-access */
-/* eslint-disable @typescript-eslint/no-unsafe-return */
-/* eslint-disable @typescript-eslint/no-unsafe-call */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import { glob } from 'glob';
 import fs from 'fs';
 import path from 'path';
