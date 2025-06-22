9acf6d45890bc41bf71d7380273c83ad27bee8bd
Eugeny Okhrimenko <Eugeny.Okhrimenko@virtoworks.com>
2025-06-22 10:35:14 +0200

    feat: add geo replica support

--- client-app/core/api/graphql/config/links/http.ts
+++ client-app/core/api/graphql/config/links/http.ts
@@ -1,5 +1,15 @@
 import { HttpLink } from "@apollo/client/core";
 import { apolloFetch } from "@/core/api/graphql/config/interceptors";
-import { HTTP_ENDPOINT_URL } from "@/core/api/graphql/consts";
+import { HTTP_ENDPOINT_URL, GQL_QUERY_ENDPOINT_URL, GQL_MUTATION_ENDPOINT_URL } from "@/core/api/graphql/consts";
+import { IS_DEVELOPMENT } from "@/core/constants/environment";
 
 export const httpLink = new HttpLink({ uri: HTTP_ENDPOINT_URL, fetch: apolloFetch });
+
+export const queryHttpLink = new HttpLink({
+  uri: IS_DEVELOPMENT ? HTTP_ENDPOINT_URL : GQL_QUERY_ENDPOINT_URL,
+  fetch: apolloFetch,
+});
+export const mutationHttpLink = new HttpLink({
+  uri: IS_DEVELOPMENT ? HTTP_ENDPOINT_URL : GQL_MUTATION_ENDPOINT_URL,
+  fetch: apolloFetch,
+});
--- client-app/core/api/graphql/config/links/index.ts
+++ client-app/core/api/graphql/config/links/index.ts
@@ -1,13 +1,37 @@
-import { from, split } from "@apollo/client/core";
+import { from, split, ApolloLink } from "@apollo/client/core";
 import { removeTypenameFromVariables } from "@apollo/client/link/remove-typename";
 import { getMainDefinition } from "@apollo/client/utilities";
+import { Kind, OperationTypeNode } from "graphql";
 import { cartLink } from "@/core/api/graphql/cart/links";
 import { errorHandlerLink } from "@/core/api/graphql/config/error-handler";
-import { httpLink } from "./http";
+import { queryHttpLink, mutationHttpLink } from "./http";
 import { timeoutLink } from "./timeout";
 import { wsLink } from "./ws";
+import type { OperationDefinitionNode } from "graphql";
 
-const sharedLink = from([removeTypenameFromVariables(), timeoutLink, errorHandlerLink]);
+// Custom link to add x-operation-type header for query and mutation
+const operationTypeHeaderLink = new ApolloLink((operation, forward) => {
+  const definition = getMainDefinition(operation.query) as OperationDefinitionNode;
+  if (
+    definition.kind === Kind.OPERATION_DEFINITION &&
+    (definition.operation === OperationTypeNode.QUERY || definition.operation === OperationTypeNode.MUTATION)
+  ) {
+    operation.setContext(({ headers = {} }) => ({
+      headers: {
+        ...headers,
+        "x-operation-type": definition.operation,
+      },
+    }));
+  }
+  return forward(operation);
+});
+
+const sharedLink = from([
+  operationTypeHeaderLink, // Inject operation type header
+  removeTypenameFromVariables(),
+  timeoutLink,
+  errorHandlerLink,
+]);
 
 // https://www.apollographql.com/docs/react/api/link/introduction/#composing-a-link-chain
 // Tree:
@@ -33,6 +57,14 @@ export const link = from([
       return definition.kind === "OperationDefinition" && definition.operation === "subscription";
     },
     wsLink,
-    httpLink,
+    split(
+      ({ query }) => {
+        const definition = getMainDefinition(query);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+        return definition.kind === "OperationDefinition" && definition.operation === "query";
+      },
+      queryHttpLink,
+      mutationHttpLink,
+    ),
   ),
 ]);
