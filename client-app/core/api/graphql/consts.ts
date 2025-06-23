271ab2e77bf8a75cbd880f2655ecd7147b4f3079
Eugeny Okhrimenko <Eugeny.Okhrimenko@virtoworks.com>
2025-06-23 13:07:58 +0200

    feat: change urls

--- client-app/core/api/graphql/consts.ts
+++ client-app/core/api/graphql/consts.ts
@@ -1,6 +1,6 @@
 export const HTTP_ENDPOINT_URL = "/graphql";
-export const GQL_QUERY_ENDPOINT_URL = "https://tokyo-gwc.govirto.com/graphql";
-export const GQL_MUTATION_ENDPOINT_URL = "https://tokyo-eus2.govirto.com/graphql";
+export const GQL_QUERY_ENDPOINT_URL = "https://tokyo-gwc-store.govirto.com/graphql";
+export const GQL_MUTATION_ENDPOINT_URL = "https://tokyo-eus2-store.govirto.com/graphql";
 export const WEBSOCKETS_ENDPOINT_URL = `${location.protocol === "https:" ? "wss" : "ws"}://${location.host}${HTTP_ENDPOINT_URL}`;
 export const WEBSOCKETS_ENDPOINT_TIMEOUT = 55 * 1000; // 55 seconds
 export const WEBSOCKETS_KEEP_ALIVE = 30 * 1000; // 30 seconds
