e8b962c889d42219f1257b0d079ec55b3903ff69
Basil Kotov <basilkot@gmail.com>
2025-06-26 15:06:32 +0100

    feat: use redirectUrl

--- client-app/pages/matcher/priorityManager.ts
+++ client-app/pages/matcher/priorityManager.ts
@@ -1,13 +1,19 @@
-export type StateType = "initial" | "loading" | "ready" | "empty";
+export type StateType = "initial" | "loading" | "ready" | "empty" | "redirect";
+
+export type UpdateStateEventArgs = {
+  state: StateType;
+  redirectUrl?: string;
+};
 
 export type PreviewerStateType = {
   id: string;
   priority: number;
   state: StateType;
   isActive: boolean;
+  redirectUrl?: string;
 };
 
-export function getVisiblePreviewer(previewers: PreviewerStateType[]): PreviewerStateType["id"] | null {
+export function getVisiblePreviewer(previewers: PreviewerStateType[]): PreviewerStateType | "loader" | null {
   const activeUnits = previewers
     .filter((el) => el.isActive && el.state !== "empty")
     .sort((a, b) => a.priority - b.priority);
@@ -17,7 +23,13 @@ export function getVisiblePreviewer(previewers: PreviewerStateType[]): Previewer
   }
 
   if (activeUnits[0].state === "ready") {
-    return activeUnits[0].id;
+    return activeUnits[0];
+  }
+
+  const redirectUnit = activeUnits.find((el) => el.state === "redirect");
+  const nonEmptyUnits = activeUnits.filter((el) => el.state !== "redirect");
+  if (!nonEmptyUnits.length && redirectUnit) {
+    return redirectUnit;
   }
 
   return "loader";
