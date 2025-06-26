9f0b8a07a050069f7b536593c6c16faa61322853
NaMax66 <max.naidovich@gmail.com>
2025-06-26 12:23:54 +0200

    chore: refactor and add tests

--- client-app/core/utilities/categories/index.ts
+++ client-app/core/utilities/categories/index.ts
@@ -2,16 +2,18 @@ import type { Category } from "@/core/api/graphql/types";
 import type { RouteLocationRaw, LocationQueryRaw } from "vue-router";
 
 export function getCategoryRoute(category: Pick<Category, "id" | "slug">, query?: LocationQueryRaw): RouteLocationRaw {
+  const queryPart = query && Object.keys(query).length > 0 ? { query } : {};
+
   if (category.slug) {
     return {
       path: `/${category.slug}`,
-      ...(query && { query }),
+      ...queryPart,
     };
   }
 
   return {
     name: "Category",
     params: { categoryId: category.id },
-    ...(query && { query }),
+    ...queryPart,
   };
 }
