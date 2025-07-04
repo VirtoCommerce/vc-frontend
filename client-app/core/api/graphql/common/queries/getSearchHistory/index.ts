f259077a8c91a296479437686953e554c2f16806
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-07-04 15:46:55 +0100

    chore: minor refactor

--- client-app/core/api/graphql/common/queries/getSearchHistory/index.ts
+++ client-app/core/api/graphql/common/queries/getSearchHistory/index.ts
@@ -3,11 +3,13 @@ import { GetSearchHistoryDocument } from "@/core/api/graphql/types";
 import { globals } from "@/core/globals";
 import type { QuerySearchHistoryArgs } from "@/core/api/graphql/types";
 
+const DEFAULT_MAX_COUNT = 10;
+
 export function useGetSearchHistoryQuery(payload?: Partial<QuerySearchHistoryArgs>) {
   const { storeId } = globals;
 
   return useLazyQuery(GetSearchHistoryDocument, {
     storeId: payload?.storeId ?? storeId,
-    maxCount: payload?.maxCount ?? 10,
+    maxCount: payload?.maxCount ?? DEFAULT_MAX_COUNT,
   });
 }
