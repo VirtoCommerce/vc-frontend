1f29751a0daae9d860f9eccb2bc88ca9c1e846f8
NaMax66 <max.naidovich@gmail.com>
2025-07-09 18:07:25 +0200

    feat: refactor set shipment method

--- client-app/shared/checkout/components/shipping-details-section.vue
+++ client-app/shared/checkout/components/shipping-details-section.vue
@@ -60,7 +60,7 @@
           :class="hasBOPIS ? 'lg:w-3/12' : 'lg:w-4/12'"
           required
           test-id-dropdown="shipping-method-select"
-          @change="(value) => setShippingMethod(value)"
+          @change="onShipmentMethodChange"
         >
           <template #placeholder>
             <div class="flex items-center gap-3 p-[0.688rem] text-sm">
@@ -119,6 +119,7 @@ import { useFullCart } from "@/shared/cart";
 import { useCheckout } from "@/shared/checkout/composables";
 import { useBopis, BOPIS_CODE } from "@/shared/checkout/composables/useBopis";
 import { AddressSelection } from "@/shared/common";
+import type { ShippingMethodType } from "@/core/api/graphql/types.ts";
 
 interface IProps {
   disabled?: boolean;
@@ -133,8 +134,7 @@ const SHIPPING_OPTIONS = {
 
 type ShippingOptionType = keyof typeof SHIPPING_OPTIONS;
 
-const { deliveryAddress, shipmentMethod, onDeliveryAddressChange, setShippingMethod, billingAddressEqualsShipping } =
-  useCheckout();
+const { deliveryAddress, shipmentMethod, onDeliveryAddressChange, billingAddressEqualsShipping } = useCheckout();
 
 const { availableShippingMethods, updateShipment, shipment, changing: cartChanging } = useFullCart();
 const { hasBOPIS, openSelectAddressModal, loading: isLoadingBopisAddresses, bopisMethod } = useBopis();
@@ -186,4 +186,13 @@ watch(
     immediate: true,
   },
 );
+
+function onShipmentMethodChange(method: ShippingMethodType) {
+  void updateShipment({
+    id: shipment.value?.id,
+    shipmentMethodCode: method.code,
+    shipmentMethodOption: method.optionName,
+    price: method.price.amount,
+  });
+}
 </script>
