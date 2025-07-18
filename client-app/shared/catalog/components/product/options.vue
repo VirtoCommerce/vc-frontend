7587891faf0c16799d98e5ad51103bc50f81fecd
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-07-18 13:54:46 +0100

    feat: prepare setting and templates to switch between

---
+++ client-app/shared/catalog/components/product/options.vue
@@ -0,0 +1,23 @@
+<template>
+  <ProductTitledBlock :title="model.title" icon="collection">
+    <div>content</div>
+    {{ variations }}
+  </ProductTitledBlock>
+</template>
+
+<script setup lang="ts">
+import { toRef } from "vue";
+import { ProductTitledBlock } from "@/shared/catalog";
+import type { Product } from "@/core/api/graphql/types";
+
+interface IProps {
+  model: {
+    title: string;
+  };
+  variations: Product[];
+  fetchingVariations?: boolean;
+}
+
+const props = defineProps<IProps>();
+const variations = toRef(props, "variations");
+</script>
