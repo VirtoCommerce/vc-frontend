939fce383a6efccf84eedb56a7f078fb278bafa2
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-07-21 13:31:22 +0900

    fix: tag

--- client-app/shared/layout/components/header/_internal/top-header.vue
+++ client-app/shared/layout/components/header/_internal/top-header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div
+  <header
     class="flex h-10 items-center gap-1 bg-[--header-top-bg-color] px-5 text-sm text-[--header-top-text-color] xl:gap-3 xl:px-11"
   >
     <LanguageSelector v-if="$context.availableLanguages && $context.availableLanguages.length > 1" />
