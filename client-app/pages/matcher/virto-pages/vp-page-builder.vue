c78d78e7b57ba8537047f10097744c47f84697bc
Basil Kotov <basilkot@gmail.com>
2025-08-19 15:43:19 +0100

    fix: use breadcrumbs om pb pages

--- client-app/pages/matcher/virto-pages/vp-page-builder.vue
+++ client-app/pages/matcher/virto-pages/vp-page-builder.vue
@@ -1,5 +1,15 @@
 <template>
   <div v-if="pageBuilderContent">
+    <div
+         v-if="!pageBuilderContent.settings?.hideBreadcrumbs || pageBuilderContent.settings?.header"
+         class="mx-auto mt-7 w-full max-w-screen-2xl px-5 pb-5 md:px-12 lg:pb-10">
+      <VcBreadcrumbs v-if="!pageBuilderContent.settings?.hideBreadcrumbs" class="mb-3" :items="breadcrumbs">
+      </VcBreadcrumbs>
+
+      <VcTypography v-if="pageBuilderContent.settings?.header" tag="h1">
+        {{ pageBuilderContent.settings.header }}
+      </VcTypography>
+    </div>
     <template v-for="item in pageBuilderContent.content">
       <component
                  :is="getBlockType(item.type)"
@@ -13,7 +23,8 @@
 </template>
 
 <script setup lang="ts">
-import { onBeforeMount, shallowRef } from "vue";
+import { onBeforeMount, shallowRef, computed, unref } from "vue";
+import { useBreadcrumbs } from "@/core/composables";
 import { getBlockType } from "@/plugins/builder-preview/block-mapping";
 
 interface IProps {
@@ -32,6 +43,8 @@ interface IPageBuilderContent {
 
 const props = defineProps<IProps>();
 
+const templateName = computed(() => unref(pageBuilderContent)?.settings?.name || unref(pageBuilderContent)?.settings?.header || "");
+const breadcrumbs = useBreadcrumbs(() => [{ title: templateName.value }] as IBreadcrumb[]);
 const canShowContent = shallowRef(false);
 const pageBuilderContent = shallowRef<IPageBuilderContent | null>(null);
 
