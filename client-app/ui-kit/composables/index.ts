6f440e369c215349125040293bf23836f811c310
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-09-01 14:28:55 +0100

    feat: refactor

--- client-app/ui-kit/composables/index.ts
+++ client-app/ui-kit/composables/index.ts
@@ -1,5 +1,5 @@
 export * from "./useAttrsOnly";
 export * from "./useComponentId";
+export * from "./useHorizontalScrollSync";
 export * from "./useListeners";
 export * from "./useQuantityValidationSchema";
-export * from "./useScrollSync";
