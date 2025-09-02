93564176d6491cf33eff01e02be1f86b71cc7b2a
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-09-02 15:30:45 +0900

    fix: remove HeadlessUI wrapper in vc-dialog story to stop focus-trap warning

--- client-app/ui-kit/components/atoms/dialog/vc-dialog.stories.ts
+++ client-app/ui-kit/components/atoms/dialog/vc-dialog.stories.ts
@@ -1,4 +1,3 @@
-import { Dialog } from "@headlessui/vue";
 import { VcDialog } from "..";
 import { VcDialogHeader, VcDialogContent, VcDialogFooter } from "../../molecules";
 import type { Meta, StoryFn } from "@storybook/vue3";
@@ -9,16 +8,15 @@ export default {
 } as Meta<typeof VcDialog>;
 
 const Template: StoryFn = (args) => ({
-  components: { Dialog, VcDialog, VcDialogHeader, VcDialogContent, VcDialogFooter },
+  components: { VcDialog, VcDialogHeader, VcDialogContent, VcDialogFooter },
   setup: () => ({ args }),
-  template: `<Dialog v-bind="{ open: true, as: 'div' }">
+  template: `
     <VcDialog v-bind="args">
       <VcDialogHeader>Title</VcDialogHeader>
       <VcDialogContent>Content</VcDialogContent>
-      <VcDialogFooter>
-      </VcDialogFooter>
+      <VcDialogFooter />
     </VcDialog>
-  </Dialog>`,
+  `,
 });
 
 export const Basic = Template.bind({});
