a2906d048653d87fccfa760759b559917a73e570
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-09-17 12:04:20 +0100

    fix: active filters formatting

--- client-app/core/utilities/search/common.ts
+++ client-app/core/utilities/search/common.ts
@@ -2,23 +2,23 @@ import { globals } from "@/core/globals";
 import { isDateString } from "../date";
 
 /**
- * Formats a label based on its type
- * @param label - The label to format
- * @returns A formatted string representing the label
+ * Formats a label or value based on its type
+ * @param labelOrValue - The label or value to format
+ * @returns A formatted string
  */
-export function getFormattedLabel(label: string): string {
+export function getFormattedLabel(labelOrValue: string): string {
   const { d, t } = globals.i18n.global;
 
-  if (isDateString(label)) {
-    return d(new Date(label));
+  if (isDateString(labelOrValue)) {
+    return d(new Date(labelOrValue));
   }
 
-  switch (label.toLowerCase()) {
+  switch (labelOrValue.toLowerCase()) {
     case "true":
       return t("common.labels.true_property");
     case "false":
       return t("common.labels.false_property");
     default:
-      return label;
+      return labelOrValue;
   }
 }
