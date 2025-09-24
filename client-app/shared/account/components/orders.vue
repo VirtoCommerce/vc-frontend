99a449441edcabe696bf63958ea349014ddfd4d6
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-09-24 15:21:32 +0900

    fix: console warnings

--- client-app/shared/account/components/orders.vue
+++ client-app/shared/account/components/orders.vue
@@ -183,7 +183,6 @@
       :hide-default-footer="!withPagination"
       :description="$t('pages.account.orders.meta.table_description')"
       mobile-breakpoint="lg"
-      @item-click="goToOrderDetails"
       @header-click="applySorting"
       @page-changed="changePage"
     >
