ece425b311480b74b19384b067700af74ea7eeea
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-10-08 09:54:14 +0100

    fix: use proper variable for company name in header

--- client-app/shared/layout/components/header/_internal/bottom-header.vue
+++ client-app/shared/layout/components/header/_internal/bottom-header.vue
@@ -12,7 +12,7 @@
       <template v-if="organization">
         <div class="hidden h-6 w-0.5 bg-primary xl:block"></div>
 
-        <div class="hidden max-w-36 text-base italic leading-[18px] text-neutral-800 xl:line-clamp-2">
+        <div class="hidden max-w-36 text-base italic leading-[18px] text-[--header-top-text-color] xl:line-clamp-2">
           {{ organization?.name }}
         </div>
       </template>
