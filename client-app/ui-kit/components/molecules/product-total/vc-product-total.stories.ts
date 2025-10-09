e122c88688ac68a1461316044534ed5b7e1c2b7e
Alex Kurilin <alexander.kurilin@virtoworks.com>
2025-10-09 17:29:48 +0200

    feat(VCST-4072): migrate storybook stories from storyfn to storyobj format

--- client-app/ui-kit/components/molecules/product-total/vc-product-total.stories.ts
+++ client-app/ui-kit/components/molecules/product-total/vc-product-total.stories.ts
@@ -1,34 +1,22 @@
 import { getMoney } from "@/ui-kit/mocks";
 import { VcProductTotal } from "..";
-import type { Meta, StoryFn } from "@storybook/vue3-vite";
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
 
-const ALIGNS = ["start", "end"];
-
-export default {
+const meta: Meta<typeof VcProductTotal> = {
   title: "Components/Molecules/VcProductTotal",
   component: VcProductTotal,
-  argTypes: {
-    align: {
-      control: "inline-radio",
-      options: ALIGNS,
-      type: { name: "string", required: false },
-      table: {
-        type: {
-          summary: ALIGNS.join(" | "),
-        },
-      },
-    },
+  render: (args) => ({
+    setup: () => ({ args }),
+    template: '<VcProductTotal v-bind="args" />',
+  }),
+};
+
+export default meta;
+type StoryType = StoryObj<typeof meta>;
+
+export const Basic: StoryType = {
+  args: {
+    actualPrice: getMoney(1599999999),
+    listPrice: getMoney(1799999999),
   },
-} as Meta<typeof VcProductTotal>;
-
-const Template: StoryFn = (args) => ({
-  components: { VcProductTotal },
-  setup: () => ({ args }),
-  template: '<VcProductTotal v-bind="args" />',
-});
-
-export const Basic = Template.bind({});
-Basic.args = {
-  actualPrice: getMoney(1599999999),
-  listPrice: getMoney(1799999999),
 };
