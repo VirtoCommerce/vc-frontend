91da88e40328884a4f1aed8dfad70f54217746d3
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-10-09 11:15:32 +0100

    fix: adjustments

--- client-app/modules/push-messages/components/link-push-messages.vue
+++ client-app/modules/push-messages/components/link-push-messages.vue
@@ -4,7 +4,7 @@
       <BottomHeaderLink :link="item" :count="unreadCount" v-bind="triggerProps">
         <template #icon>
           <transition :name="unreadCount ? 'shake' : ''" mode="out-in">
-            <VcIcon :key="totalCount" :name="item.icon" class="mb-0.5 fill-primary" :size="24" />
+            <VcIcon :key="totalCount" :name="item.icon" class="mb-0.5 fill-additional-50" :size="24" />
           </transition>
         </template>
 
