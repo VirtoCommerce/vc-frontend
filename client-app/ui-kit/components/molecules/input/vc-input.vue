de6abba9465fe5873ccba5477eaebae57fd11373
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2025-10-14 16:20:49 +0900

    feat(VCST-3832): improve vc-quantity-stepper accessibility (#1974)
    ## Description
    
    ## References
    ### Jira-link:
    <!-- Put link to your task in Jira here -->
    https://virtocommerce.atlassian.net/browse/VCST-3832
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.33.0-pr-1974-d255-d2554d07.zip
    
    ---------
    
    Co-authored-by: Aleksandra-Mitricheva <54628043+Aleksandra-Mitricheva@users.noreply.github.com>

--- client-app/ui-kit/components/molecules/input/vc-input.vue
+++ client-app/ui-kit/components/molecules/input/vc-input.vue
@@ -42,6 +42,7 @@
         :aria-label="ariaLabel ?? label"
         :title="browserTooltip === 'enabled' ? message : ''"
         class="vc-input__input"
+        :tabindex="tabindex"
         :data-test-id="testIdInput"
         @keydown="keyDown($event)"
         @click.stop="inputClick"
@@ -119,6 +120,7 @@ export interface IProps {
   testIdInput?: string;
   aria?: Record<string, string | number | null>;
   disableAutocomplete?: boolean;
+  tabindex?: string | number;
 }
 
 defineOptions({
@@ -135,6 +137,7 @@ const props = withDefaults(defineProps<IProps>(), {
   type: "text",
   size: "md",
   browserTooltip: "disabled",
+  tabindex: 0,
 });
 
 const LIMITED_TYPES: IProps["type"][] = ["number", "date"];
@@ -201,7 +204,7 @@ function clear() {
 // Workaround to fix Safari bug
 function keyDown(event: KeyboardEvent) {
   if (props.type === "number") {
-    const allowedCharacter = /(^\d*$)|(Backspace|Tab|Delete|ArrowLeft|ArrowRight)/;
+    const allowedCharacter = /(^\d*$)|(Backspace|Tab|Delete|ArrowLeft|ArrowRight|ArrowUp|ArrowDown)/;
     if (!allowedCharacter.test(event.key)) {
       event.preventDefault();
     }
