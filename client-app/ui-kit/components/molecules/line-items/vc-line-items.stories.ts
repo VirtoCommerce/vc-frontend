ddb9061ac57682925559883a7a84ff5061742d77
Alex Kurilin <alexander.kurilin@virtoworks.com>
2025-10-15 15:14:24 +0200

    feat(VCST-4083): migrate storybook stories from storyfn to storyobj format

--- client-app/ui-kit/components/molecules/line-items/vc-line-items.stories.ts
+++ client-app/ui-kit/components/molecules/line-items/vc-line-items.stories.ts
@@ -1,9 +1,8 @@
-import { VcLineItems, VcChip } from "..";
+import { VcLineItems } from "..";
 import { preparedLineItemMock1, preparedLineItemMock2 } from "../../../mocks/line-item.mock";
-import { VcAddToCart } from "../../organisms";
-import type { Meta, StoryFn } from "@storybook/vue3-vite";
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
 
-export default {
+const meta: Meta<typeof VcLineItems> = {
   title: "Components/Molecules/VcLineItems",
   component: VcLineItems,
   args: {
@@ -11,130 +10,135 @@ export default {
     disabled: false,
     removable: false,
   },
-} as Meta<typeof VcLineItems>;
+  render: (args) => ({
+    setup: () => ({ args }),
+    template: '<VcLineItems v-bind="args" />',
+  }),
+};
 
-const Template: StoryFn = (args) => ({
-  components: { VcLineItems },
-  setup: () => ({ args }),
-  template: '<VcLineItems v-bind="args" />',
-});
+export default meta;
+type StoryType = StoryObj<typeof meta>;
 
-const TemplateQuantity: StoryFn = (args) => ({
-  components: { VcLineItems, VcChip, VcAddToCart },
-  setup: () => ({ args }),
-  template: `<VcLineItems v-bind="args">
-    <template #titles>
-      <div class="text-end">Quantity</div>
-    </template>
+export const Basic: StoryType = {
+  args: {
+    items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
+  },
+};
 
-    <template #default="{ item }">
-      <VcAddToCart
-        @update:modelValue="$emit('change:itemQuantity', { item, quantity: $event })"
-      />
-    </template>
-  </VcLineItems>`,
-});
+export const NoRoute: StoryType = {
+  args: {
+    items: [preparedLineItemMock1, preparedLineItemMock1, preparedLineItemMock1],
+  },
+};
 
-const TemplateAddToCart: StoryFn = (args) => ({
-  components: { VcLineItems, VcAddToCart },
-  setup: () => ({ args }),
-  template: `<VcLineItems v-bind="args">
-    <template #default="{ item }">
+export const Selectable: StoryType = {
+  args: {
+    items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
+    selectable: true,
+  },
+};
+
+export const Removable: StoryType = {
+  args: {
+    items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
+    removable: true,
+  },
+};
+
+export const WithImage: StoryType = {
+  args: {
+    items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
+    withImage: true,
+    removable: true,
+  },
+};
+
+export const WithImageAndProperties: StoryType = {
+  args: {
+    items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
+    withImage: true,
+    withProperties: true,
+    removable: true,
+  },
+};
+
+export const QuantityInput: StoryType = {
+  args: {
+    items: [
+      {
+        ...preparedLineItemMock2,
+        id: "1",
+        quantity: 4,
+      },
+      {
+        ...preparedLineItemMock2,
+        id: "2",
+        quantity: 1999,
+      },
+      {
+        ...preparedLineItemMock2,
+        id: "3",
+      },
+    ],
+    removable: true,
+    withImage: true,
+    withProperties: true,
+    withPrice: true,
+    withTotal: true,
+  },
+  render: (args) => ({
+    setup: () => ({ args }),
+    template: `<VcLineItems v-bind="args">
+      <template #titles>
+        <div class="text-end">Quantity</div>
+      </template>
+
+      <template #default="{ item }">
         <VcAddToCart
-          :model-value="item.quantity"
-          is-active
-          is-available
-          is-buyable
-          is-in-stock
-          :available-quantity="999999"
+          @update:modelValue="$emit('change:itemQuantity', { item, quantity: $event })"
         />
-
-        <div class="flex gap-1 mt-1.5">
-          <VcChip size="xs" variant="outline-dark" color="neutral" rounded>10 in Cart</VcChip>
-        </div>
-    </template>
-  </VcLineItems>`,
-});
-
-export const Basic = Template.bind({});
-Basic.args = {
-  items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
+      </template>
+    </VcLineItems>`,
+  }),
 };
 
-export const NoRoute = Template.bind({});
-NoRoute.args = {
-  items: [preparedLineItemMock1, preparedLineItemMock1, preparedLineItemMock1],
-};
+export const AddToCart: StoryType = {
+  args: {
+    items: [
+      {
+        ...preparedLineItemMock2,
+        id: "1",
+        quantity: 4,
+      },
+      {
+        ...preparedLineItemMock2,
+        id: "2",
+        quantity: 1999,
+      },
+      {
+        ...preparedLineItemMock2,
+        id: "3",
+      },
+    ],
+    removable: true,
+  },
+  render: (args) => ({
+    setup: () => ({ args }),
+    template: `<VcLineItems v-bind="args">
+      <template #default="{ item }">
+          <VcAddToCart
+            :model-value="item.quantity"
+            is-active
+            is-available
+            is-buyable
+            is-in-stock
+            :available-quantity="999999"
+          />
 
-export const Selectable = Template.bind({});
-Selectable.args = {
-  items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
-  selectable: true,
-};
-
-export const Removable = Template.bind({});
-Removable.args = {
-  items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
-  removable: true,
-};
-
-export const WithImage = Template.bind({});
-WithImage.args = {
-  items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
-  withImage: true,
-  removable: true,
-};
-
-export const WithImageAndProperties = Template.bind({});
-WithImageAndProperties.args = {
-  items: [preparedLineItemMock2, preparedLineItemMock2, preparedLineItemMock2],
-  withImage: true,
-  withProperties: true,
-  removable: true,
-};
-
-export const QuantityInput = TemplateQuantity.bind({});
-QuantityInput.args = {
-  items: [
-    {
-      ...preparedLineItemMock2,
-      id: "1",
-      quantity: 4,
-    },
-    {
-      ...preparedLineItemMock2,
-      id: "2",
-      quantity: 1999,
-    },
-    {
-      ...preparedLineItemMock2,
-      id: "3",
-    },
-  ],
-  removable: true,
-  withImage: true,
-  withProperties: true,
-  withPrice: true,
-  withTotal: true,
-};
-
-export const AddToCart = TemplateAddToCart.bind({});
-AddToCart.args = {
-  items: [
-    {
-      ...preparedLineItemMock2,
-      id: "1",
-      quantity: 4,
-    },
-    {
-      ...preparedLineItemMock2,
-      id: "2",
-      quantity: 1999,
-    },
-    {
-      ...preparedLineItemMock2,
-      id: "3",
-    },
-  ],
-  removable: true,
+          <div class="flex gap-1 mt-1.5">
+            <VcChip size="xs" variant="outline-dark" color="neutral" rounded>10 in Cart</VcChip>
+          </div>
+      </template>
+    </VcLineItems>`,
+  }),
 };
