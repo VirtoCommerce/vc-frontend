6093a369942e17f309c91e1bdd1a1eb005fe423b
Alex Kurilin <alexander.kurilin@virtoworks.com>
2025-10-17 17:13:01 +0200

    fix: add catalog-product route

--- client-app/core/composables/useBrowserTarget.ts
+++ client-app/core/composables/useBrowserTarget.ts
@@ -1,15 +1,17 @@
 import { computed } from "vue";
 import { useRoute } from "vue-router";
 import { useThemeContext } from "@/core/composables";
+import { useSlugInfo } from "@/shared/common";
 import { BrowserTargetType } from "../enums";
 
 export function useBrowserTarget() {
   const defaultBrowserTarget = BrowserTargetType.BLANK;
   const route = useRoute();
+  const { objectType } = useSlugInfo(route.path.slice(1));
   const { themeContext } = useThemeContext();
 
   const browserTarget = computed((): BrowserTargetType => {
-    if (route.name === "Product") {
+    if (route.name === "Product" || objectType.value === "CatalogProduct") {
       return themeContext.value.settings.product_page_browser_target ?? defaultBrowserTarget;
     }
 
