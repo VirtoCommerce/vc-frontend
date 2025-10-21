226301852034851b9397fc3248cd9f322f6cbffe
Ivan Kalachikov <ivan.kalachikov@gmail.com>
2025-10-21 09:05:46 +0100

    fix(VCST-4160): orders and quotes url contains no language code

--- client-app/shared/account/components/orders.vue
+++ client-app/shared/account/components/orders.vue
@@ -497,7 +497,6 @@ function handleOrdersDateFilterChange(dateFilterType: DateFilterType): void {
 
 function goToOrderDetails(order: CustomerOrderType): void {
   const orderRoute = router.resolve({ name: "OrderDetails", params: { orderId: order.id } });
-  console.log(JSON.stringify(orderRoute, null, 2));
 
   if (browserTarget.value === BrowserTargetType.BLANK) {
     window.open(orderRoute.href, "_blank")!.focus();
