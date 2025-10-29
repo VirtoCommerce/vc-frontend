99e0b6bee7d794fc9e890370208f76b32a523f2b
yuskithedeveloper <yuskianywhere@gmail.com>
2025-10-29 13:27:08 -0600

    feat: showing saved for later block for empty cart

--- client-app/pages/cart.vue
+++ client-app/pages/cart.vue
@@ -1,5 +1,5 @@
 <template>
-  <template v-if="!cart?.items?.length && !recentlyBrowsedProducts?.length">
+  <template v-if="!cart?.items?.length && !savedForLaterList?.items?.length && !recentlyBrowsedProducts?.length">
     <VcLoaderOverlay v-if="loading" no-bg />
 
     <VcEmptyPage
