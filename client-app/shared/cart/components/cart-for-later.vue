4706b6c6df87c599efd231acddd2515ec85001cf
yuskithedeveloper <126832118+yuskithedeveloper@users.noreply.github.com>
2025-10-31 06:20:03 -0600

    feat(VCST-3728): show more button in saved for later cart section (#2011)
    ## Description
    "Show more" button added to the "Saved for later" section of the cart
    page
    <img width="1164" height="488" alt="image"
    src="https://github.com/user-attachments/assets/da61dacb-243d-4eaf-b3f0-ce088e71e086"
    />
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-3728
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.34.0-pr-2011-da84-da844f95.zip
    
    ---------
    
    Co-authored-by: Aleksandra-Mitricheva <54628043+Aleksandra-Mitricheva@users.noreply.github.com>

--- client-app/shared/cart/components/cart-for-later.vue
+++ client-app/shared/cart/components/cart-for-later.vue
@@ -1,14 +1,14 @@
 <template>
-  <VcWidget :title="$t('pages.cart.saved_for_later')" prepend-icon="bookmark" size="lg">
+  <VcWidget :title="$t('pages.cart.saved_for_later')" prepend-icon="bookmark" size="lg" class="cart-for-later">
     <template v-slot:append>
       <VcButton variant="outline" :to="{ name: ROUTES.SAVED_FOR_LATER.NAME }" size="sm">
         {{ $t("common.buttons.see_all") }}
       </VcButton>
     </template>
 
-    <VcProductsGrid short>
+    <VcProductsGrid>
       <CartItemForLater
-        v-for="(item, index) in savedForLaterList?.items"
+        v-for="(item, index) in visibleItems"
         :key="index"
         :item="item"
         :saved-for-later-list="savedForLaterList"
@@ -18,11 +18,17 @@
         @add-to-cart="(lineItemId) => $emit('addToCart', lineItemId)"
       />
     </VcProductsGrid>
+
+    <div v-if="maxVisibleItems < (savedForLaterList?.items.length ?? 0)" class="cart-for-later__show-more">
+      <VcButton variant="no-border" append-icon="chevron-down" @click="maxVisibleItems += VISIBLE_ITEMS_STEP">
+        {{ $t("common.buttons.show_more") }}
+      </VcButton>
+    </div>
   </VcWidget>
 </template>
 
 <script setup lang="ts">
-import { computed, toRef } from "vue";
+import { computed, toRef, ref } from "vue";
 import { useI18n } from "vue-i18n";
 import { useAnalytics } from "@/core/composables/useAnalytics";
 import { ROUTES } from "@/router/routes/constants";
@@ -45,6 +51,10 @@ interface IProps {
 
 const savedForLaterList = toRef(props, "savedForLaterList");
 
+const VISIBLE_ITEMS_STEP = 6;
+const maxVisibleItems = ref(6);
+const visibleItems = computed(() => savedForLaterList.value?.items.slice(0, maxVisibleItems.value));
+
 const listProperties = computed(() => ({
   item_list_id: "recently_browsed_products",
   item_list_name: t("pages.cart.recently_browsed_products"),
@@ -61,3 +71,11 @@ function selectItemEvent(item?: SavedForLaterListFragment["items"][number]["prod
   analytics("selectItem", item as Product, listProperties.value);
 }
 </script>
+
+<style lang="scss">
+.cart-for-later {
+  &__show-more {
+    @apply border-t pt-5 text-center;
+  }
+}
+</style>
