953b32a25f865b975a2e719962462d7c31f6a343
Basil Kotov <basilkot@gmail.com>
2025-11-05 11:13:20 +0000

    chore: temporary commit

---
+++ client-app/core/composables/usePayments.ts
@@ -0,0 +1,5 @@
+// class PaymentMethods {
+//   async initialize() {}
+
+//   async authorize() {}
+// }
--- vite.config.ts
+++ vite.config.ts
@@ -98,7 +98,7 @@ export default defineConfig(({ command, mode }): UserConfig => {
       headers: {
         "Content-Security-Policy": "frame-ancestors 'self' https://localhost:5001 https://builder.io;",
         "Cross-Origin-Resource-Policy": "cross-origin",
-        "Cross-Origin-Embedder-Policy": "unsafe-none",
+        "Cross-Origin-Embedder-Policy": "require-corp",
       },
       proxy: {
         "^/api": getProxy(process.env.APP_BACKEND_URL),
