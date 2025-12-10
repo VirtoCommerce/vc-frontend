ff1a1d07ad35790e5b03feac9b49b820a5ac7123
Basil Kotov <basilkot@gmail.com>
2025-12-10 08:50:56 +0000

    fix: place order button disabling

--- client-app/shared/checkout/components/place-order.vue
+++ client-app/shared/checkout/components/place-order.vue
@@ -1,19 +1,25 @@
 <template>
-  <ProceedTo
-    :disabled="hasOnlyUnselectedLineItems || !isValidCheckout || !isCanFinalizePayment"
-    @click="createOrderFromCart"
-  >
+  <ProceedTo :disabled="isDisabled" @click="createOrderFromCart">
     {{ $t("common.buttons.place_order") }}
   </ProceedTo>
 </template>
 
 <script setup lang="ts">
+import { computed } from "vue";
 import { useFullCart } from "@/shared/cart";
 import { useCheckout } from "@/shared/checkout/composables";
 import { usePayment } from "@/shared/payment/composables";
 import ProceedTo from "@/shared/checkout/components/proceed-to.vue";
 
 const { hasOnlyUnselectedLineItems } = useFullCart();
-const { isValidCheckout, createOrderFromCart } = useCheckout();
+const { paymentMethod, isValidCheckout, createOrderFromCart } = useCheckout();
 const { isCanFinalizePayment } = usePayment();
+
+const isDisabled = computed(() => {
+  return (
+    hasOnlyUnselectedLineItems.value ||
+    !isValidCheckout.value ||
+    (paymentMethod.value?.allowCartPayment && !isCanFinalizePayment.value)
+  );
+});
 </script>
