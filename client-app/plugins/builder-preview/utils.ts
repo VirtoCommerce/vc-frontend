d2c0d1849ff4ec9f053086ea33f7b585fcdb8971
Basil Kotov <basilkot@gmail.com>
2025-12-12 17:17:58 +0000

    chore: globalThis instead of window

--- client-app/plugins/builder-preview/utils.ts
+++ client-app/plugins/builder-preview/utils.ts
@@ -1,15 +1,15 @@
 export function getBuilderOrigin() {
-  const urlParams = new URLSearchParams(window.location.search);
+  const urlParams = new URLSearchParams(globalThis.location.search);
   const ep = urlParams.get("ep");
 
   return ep?.replace(/\/$/, "");
 }
 
 export function getPreviewPageId(): string | undefined {
-  const urlParams = new URLSearchParams(window.location.search);
+  const urlParams = new URLSearchParams(globalThis.location.search);
   return urlParams.get("pageId") ?? undefined;
 }
 
 export function isPreviewMode(): boolean {
-  return document.location.pathname === "/designer-preview";
+  return globalThis.location.pathname === "/designer-preview";
 }
