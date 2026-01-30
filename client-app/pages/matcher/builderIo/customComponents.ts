4266b4c57693af5198498ef32b681d9595b4b4ea
Anatolii Vasilev <anatolii.vslvv@gmail.com>
2026-01-30 17:45:04 +0100

    feat: add builder.io configuration for predefined products

--- client-app/pages/matcher/builderIo/customComponents.ts
+++ client-app/pages/matcher/builderIo/customComponents.ts
@@ -202,6 +202,57 @@ export const builderIOComponents: Array<BuilderIOComponentType> = [
     ],
   },
   {
+    name: "Predefined Products",
+    component: ProductsBlock,
+    inputs: [
+      {
+        name: "title",
+        type: "string",
+        defaultValue: "Predefined Products",
+      },
+      {
+        name: "subtitle",
+        type: "string",
+      },
+      {
+        name: "cardType",
+        type: "string",
+        defaultValue: "full",
+        enum: ["full", "short"],
+      },
+      {
+        name: "columnsAmountTablet",
+        type: "string",
+        defaultValue: "3",
+        enum: ["3", "2"],
+      },
+      {
+        name: "columnsAmountDesktop",
+        type: "string",
+        defaultValue: "4",
+        enum: ["4", "3"],
+      },
+      {
+        name: "skus",
+        type: "list",
+        subFields: [
+          {
+            name: "sku",
+            type: "string",
+            friendlyName: "Product SKU",
+          },
+        ],
+        onChange: (options: Map<string, Array<{ sku: string }>>) => {
+          const skus = options.get("skus");
+          if (Array.isArray(skus) && skus.length > 12) {
+            options.set("skus", skus.slice(0, 12));
+            alert("Maximum 12 SKUs allowed");
+          }
+        },
+      },
+    ],
+  },
+  {
     component: Slider,
     name: "Slider",
     inputs: [
