9c7161ce4b588d45d3e1970ad70b7ae19a24ee6b
Alexander Kurilin <46611511+muller39@users.noreply.github.com>
2026-02-10 17:36:37 +0200

    feat(VCST-4537): migrate storybook stories from storyfn to storyobj format (#2149)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-4537
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.42.0-pr-2149-8584-85843c7b.zip
    
    ---------
    
    Co-authored-by: Elena Mutykova <56412054+Lenajava1@users.noreply.github.com>

--- client-app/ui-kit/components/atoms/checkbox-group/vc-checkbox-group.stories.ts
+++ client-app/ui-kit/components/atoms/checkbox-group/vc-checkbox-group.stories.ts
@@ -1,26 +1,72 @@
-import { VcCheckbox, VcCheckboxGroup } from "..";
-import type { Meta, StoryFn } from "@storybook/vue3-vite";
+import { ref } from "vue";
+import { VcCheckboxGroup } from "..";
+import type { Meta, StoryObj } from "@storybook/vue3-vite";
 
-export default {
+const meta: Meta<typeof VcCheckboxGroup> = {
   title: "Components/Atoms/VcCheckboxGroup",
   component: VcCheckboxGroup,
-} as Meta<typeof VcCheckboxGroup>;
+  argTypes: {
+    modelValue: {
+      control: "object",
+      description: "Array of selected checkbox values",
+    },
+  },
+  render: (args) => ({
+    setup: () => {
+      const model = ref<(string | number | object)[]>(args.modelValue ?? []);
+      return { args, model };
+    },
+    template: `<VcCheckboxGroup v-model="model" v-bind="args" class="space-y-3">
+      <VcCheckbox value="1">Checkbox 1</VcCheckbox>
+      <VcCheckbox value="2">Checkbox 2</VcCheckbox>
+      <VcCheckbox value="3">Checkbox 3</VcCheckbox>
+      <VcCheckbox value="4">Checkbox 4</VcCheckbox>
+      <VcCheckbox value="5">Checkbox 5</VcCheckbox>
+    </VcCheckboxGroup>`,
+  }),
+};
 
-const Template: StoryFn = (args) => ({
-  components: { VcCheckbox, VcCheckboxGroup },
-  setup: () => ({ args }),
-  template: `<VcCheckboxGroup v-bind="args" class="space-y-3">
-  <VcCheckbox value="1">Checkbox 1</VcCheckbox>
-  <VcCheckbox value="2">Checkbox 2</VcCheckbox>
-  <VcCheckbox value="3">Checkbox 3</VcCheckbox>
-  <VcCheckbox value="4">Checkbox 4</VcCheckbox>
-  <VcCheckbox value="5">Checkbox 5</VcCheckbox>
-  </VcCheckboxGroup>`,
-});
+export default meta;
+type StoryType = StoryObj<typeof meta>;
 
-export const Basic = Template.bind({});
+export const Basic: StoryType = {
+  args: {
+    modelValue: [],
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `
+          <VcCheckboxGroup>
+            <VcCheckbox value="1">Checkbox 1</VcCheckbox>
+            <VcCheckbox value="2">Checkbox 2</VcCheckbox>
+            <VcCheckbox value="3">Checkbox 3</VcCheckbox>
+            <VcCheckbox value="4">Checkbox 4</VcCheckbox>
+            <VcCheckbox value="5">Checkbox 5</VcCheckbox>
+          </VcCheckboxGroup>
+        `,
+      },
+    },
+  },
+};
 
-export const Selected = Template.bind({});
-Selected.args = {
-  modelValue: ["2", "4", "5"],
+export const Selected: StoryType = {
+  args: {
+    modelValue: ["2", "4", "5"],
+  },
+  parameters: {
+    docs: {
+      source: {
+        code: `
+          <VcCheckboxGroup :model-value="['2', '4', '5']">
+            <VcCheckbox value="1">Checkbox 1</VcCheckbox>
+            <VcCheckbox value="2">Checkbox 2</VcCheckbox>
+            <VcCheckbox value="3">Checkbox 3</VcCheckbox>
+            <VcCheckbox value="4">Checkbox 4</VcCheckbox>
+            <VcCheckbox value="5">Checkbox 5</VcCheckbox>
+          </VcCheckboxGroup>
+        `,
+      },
+    },
+  },
 };
