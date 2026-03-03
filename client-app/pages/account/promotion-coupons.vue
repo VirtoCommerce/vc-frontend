48ecdf2cb5eec6b458911bf6e6d0a3e8360c56e1
Anatolii Vasilev <anatolii.vslvv@gmail.com>
2026-03-03 19:09:48 +0100

    feat: skeletons & empty view

--- client-app/pages/account/promotion-coupons.vue
+++ client-app/pages/account/promotion-coupons.vue
@@ -4,17 +4,24 @@
       {{ $t("pages.account.promotion_coupons.title") }}
     </VcTypography>
 
-    <div v-if="result?.promotionCoupons?.items" class="promotion-coupons__wrapper">
+    <div v-if="loading" class="promotion-coupons__wrapper">
+      <CouponItemSkeleton v-for="item in 5" :key="item" />
+    </div>
+
+    <div v-else-if="result?.promotionCoupons?.items?.length" class="promotion-coupons__wrapper">
       <CouponItem v-for="coupon in result.promotionCoupons.items" :key="coupon.id" :coupon="coupon" />
     </div>
+
+    <VcEmptyView v-else :text="$t('pages.account.promotion_coupons.no_lists')" icon="outline-lists" />
   </div>
 </template>
 
 <script setup lang="ts">
 import { getPromotionCoupons } from "@/core/api/graphql";
 import { CouponItem } from "@/shared/account";
+import CouponItemSkeleton from "@/shared/account/components/coupon-item-skeleton.vue";
 
-const { result } = getPromotionCoupons();
+const { result, loading } = getPromotionCoupons();
 </script>
 
 <style lang="scss">
