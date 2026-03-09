635b934a6b87f2b4d55c5bed1fb81338d86f6213
Anatolii Vasilev <anatolii.vslvv@gmail.com>
2026-03-09 23:22:26 +0100

    feat: simplify layout

--- client-app/shared/account/components/coupon-item-skeleton.vue
+++ client-app/shared/account/components/coupon-item-skeleton.vue
@@ -10,8 +10,6 @@
 
     <div class="coupon-item-skeleton__description">
       <div class="coupon-item-skeleton__line">&nbsp;</div>
-
-      <div class="coupon-item-skeleton__line">&nbsp;</div>
     </div>
 
     <div class="coupon-item-skeleton__end-date">
@@ -21,21 +19,25 @@
     </div>
 
     <div class="coupon-item-skeleton__code">
-      <div class="coupon-item-skeleton__code-inner">
-        <div class="coupon-item-skeleton__line">&nbsp;</div>
-
-        <div class="coupon-item-skeleton__line">&nbsp;</div>
-      </div>
+      <div class="coupon-item-skeleton__line">&nbsp;</div>
     </div>
   </div>
 </template>
 
 <style lang="scss">
 .coupon-item-skeleton {
-  @apply bg-additional-50 rounded-md shadow-md p-4 grid grid-cols-3 lg:grid-cols-12 gap-4 lg:gap-5 animate-pulse;
+  @apply bg-additional-50 rounded-md shadow-md p-4 grid grid-cols-3 gap-4 animate-pulse;
+
+  @media (min-width: theme("screens.lg")) {
+    @apply grid-cols-12 gap-5;
+  }
 
   &__title {
-    @apply order-1 col-span-1 lg:order-none lg:col-span-2 flex items-center justify-center;
+    @apply order-1 col-span-1 flex items-center justify-center;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-2;
+    }
 
     .coupon-item-skeleton__line {
       @apply w-3/4;
@@ -43,7 +45,11 @@
   }
 
   &__name {
-    @apply order-2 col-span-2 lg:order-none lg:col-span-3 flex items-center border-l border-neutral-300 pl-4 lg:border-l-0 lg:pl-0;
+    @apply order-2 col-span-2 flex items-center border-l border-neutral-300 pl-4;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-3 border-l-0 pl-0;
+    }
 
     .coupon-item-skeleton__line {
       @apply w-2/3;
@@ -51,19 +57,31 @@
   }
 
   &__description {
-    @apply order-3 col-span-3 lg:col-span-3 2xl:col-span-4 lg:order-none hidden lg:flex flex-col justify-center;
+    @apply order-3 col-span-3 hidden items-center;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-3 flex;
+    }
+
+    @media (min-width: theme("screens.2xl")) {
+      @apply col-span-4;
+    }
 
     .coupon-item-skeleton__line {
-      @apply w-4/5;
-
-      &:first-child {
-        @apply w-full mb-1;
-      }
+      @apply w-full;
     }
   }
 
   &__end-date {
-    @apply order-last col-span-3 lg:col-span-2 2xl:col-span-1 lg:order-none -mt-2.5 lg:mt-0 flex items-center justify-center text-center lg:flex-col gap-1;
+    @apply order-last col-span-3 -mt-2.5 flex items-center justify-center text-center gap-1;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-2 mt-0 flex-col;
+    }
+
+    @media (min-width: theme("screens.2xl")) {
+      @apply col-span-1;
+    }
 
     .coupon-item-skeleton__line {
       @apply w-10;
@@ -75,18 +93,14 @@
   }
 
   &__code {
-    @apply order-4 col-span-3 lg:order-none lg:col-span-2 flex items-center;
+    @apply order-4 col-span-3 flex items-center;
 
-    &-inner {
-      @apply w-full border border-dashed border-neutral-200 items-center px-2.5 py-3 rounded flex flex-col;
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-2;
+    }
 
-      .coupon-item-skeleton__line {
-        @apply w-3/4;
-
-        &:last-child {
-          @apply w-1/2 mt-1;
-        }
-      }
+    .coupon-item-skeleton__line {
+      @apply w-full;
     }
   }
 
--- client-app/shared/account/components/coupon-item.vue
+++ client-app/shared/account/components/coupon-item.vue
@@ -24,11 +24,11 @@
       <button type="button" class="coupon-item__code-button" @click="copyCoupon(coupon.couponCode?.toUpperCase())">
         <span class="coupon-item__code-value">{{ coupon.couponCode?.toUpperCase() }}</span>
 
-        <span class="coupon-item__code-label coupon-item__code-label--click">
+        <span class="coupon-item__click-label">
           {{ $t("shared.account.promotion_coupons.click_to_copy") }}
         </span>
 
-        <span class="coupon-item__code-label coupon-item__code-label--tap">
+        <span class="coupon-item__tap-label">
           {{ $t("shared.account.promotion_coupons.tap_to_copy") }}
         </span>
       </button>
@@ -57,18 +57,38 @@ async function copyCoupon(text?: string) {
 
 <style lang="scss">
 .coupon-item {
-  @apply bg-additional-50 rounded-md shadow-md p-4 grid grid-cols-3 lg:grid-cols-12 gap-4 lg:gap-5;
+  @apply bg-additional-50 rounded-md shadow-md p-4 grid grid-cols-3 gap-4;
+
+  @media (min-width: theme("screens.lg")) {
+    @apply grid-cols-12 gap-5;
+  }
 
   &__title {
-    @apply order-1 col-span-1 lg:order-none lg:col-span-2 flex items-center justify-center font-bold text-xl text-success-500;
+    @apply order-1 col-span-1 flex items-center justify-center font-bold text-xl text-success-500;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-2;
+    }
   }
 
   &__name {
-    @apply order-2 col-span-2 lg:order-none lg:col-span-3 flex items-center font-bold border-l border-neutral-300 pl-4 lg:border-l-0 lg:pl-0;
+    @apply order-2 col-span-2 flex items-center font-bold border-l border-neutral-300 pl-4;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-3 border-l-0 pl-0;
+    }
   }
 
   &__description {
-    @apply order-3 col-span-3 lg:col-span-3 2xl:col-span-4 lg:order-none hidden lg:flex items-center text-sm;
+    @apply order-3 col-span-3 hidden items-center text-sm;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-3 flex;
+    }
+
+    @media (min-width: theme("screens.2xl")) {
+      @apply col-span-4;
+    }
 
     &-text {
       @apply line-clamp-4;
@@ -76,7 +96,15 @@ async function copyCoupon(text?: string) {
   }
 
   &__end-date {
-    @apply order-last col-span-3 lg:col-span-2 2xl:col-span-1 lg:order-none -mt-2.5 lg:mt-0 flex items-center justify-center text-center lg:flex-col text-xs gap-1;
+    @apply order-last col-span-3 -mt-2.5 flex items-center justify-center text-center text-xs gap-1;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-2 mt-0 flex-col;
+    }
+
+    @media (min-width: theme("screens.2xl")) {
+      @apply col-span-1;
+    }
 
     &-label {
       @apply text-neutral-600;
@@ -88,22 +116,14 @@ async function copyCoupon(text?: string) {
   }
 
   &__code {
-    @apply order-4 col-span-3 lg:order-none lg:col-span-2 flex items-center;
+    @apply order-4 col-span-3 flex items-center;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply order-none col-span-2;
+    }
 
     &-value {
       @apply font-bold uppercase leading-5;
-    }
-
-    &-label {
-      @apply text-xs text-secondary-500;
-
-      &--click {
-        @apply hidden lg:inline;
-      }
-
-      &--tap {
-        @apply lg:hidden;
-      }
     }
 
     &-button {
@@ -112,6 +132,22 @@ async function copyCoupon(text?: string) {
       &:hover {
         @apply bg-secondary-100;
       }
+    }
+  }
+
+  &__click-label {
+    @apply hidden text-xs text-secondary-500;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply inline;
+    }
+  }
+
+  &__tap-label {
+    @apply text-xs text-secondary-500;
+
+    @media (min-width: theme("screens.lg")) {
+      @apply hidden;
     }
   }
 }
