36f99c1a86f250db41cc8febd71e5ac8abe4e0dc
Anatolii Vasilev <49168917+vas11yev1work@users.noreply.github.com>
2026-09-08 14:02:21 +0200

    fix(VCST-5910): mission issues (#2471)
    ## Description
    
    ## References
    ### Jira-link:
    https://virtocommerce.atlassian.net/browse/VCST-5910
    https://virtocommerce.atlassian.net/browse/VCST-5831
    https://virtocommerce.atlassian.net/browse/VCST-5825
    https://virtocommerce.atlassian.net/browse/VCST-5909
    ### Artifact URL:
    
    https://vc3prerelease.blob.core.windows.net/packages/vc-theme-b2b-vue-2.57.0-pr-2471-6ed5-6ed5dc1b.zip

--- client-app/modules/loyalty/composables/useMissionCard.ts
+++ client-app/modules/loyalty/composables/useMissionCard.ts
@@ -72,20 +72,23 @@ function presentPerSkuAny(mission: MissionDataType): ReturnType<MissionPresenter
 }
 
 function presentOrderValue(mission: MissionDataType): ReturnType<MissionPresenterType> {
-  const sum = mission.currentMoneyValue?.formattedAmount ?? "";
+  const target = mission.targetMoneyValue?.formattedAmount ?? "";
+  const sum = (mission.percentage ?? 0) >= 100 ? target : (mission.currentMoneyValue?.formattedAmount ?? "");
 
   return {
     typeLabelKey: `${CARD_I18N}.type_order_value`,
     progressLabelKey: `${CARD_I18N}.progress_spend`,
-    progressParams: { sum },
+    progressParams: { sum, target },
   };
 }
 
 function presentOrderCount(mission: MissionDataType): ReturnType<MissionPresenterType> {
+  const target = mission.targetValue ?? 0;
+
   return {
     typeLabelKey: `${CARD_I18N}.type_orders`,
     progressLabelKey: `${CARD_I18N}.progress_orders`,
-    progressParams: { current: mission.currentValue ?? 0, target: mission.targetValue ?? 0 },
+    progressParams: { current: Math.min(mission.currentValue ?? 0, target), target },
   };
 }
 
@@ -140,7 +143,7 @@ export function useMissionCard(mission: MaybeRefOrGetter<MissionDataType>) {
       title: data.localizedName ?? data.name ?? "",
       bannerUrl: data.bannerUrl ?? "",
       rewardPoints: data.rewardPoints?.amount ?? 0,
-      percent: Math.round((data.percentage ?? 0) * 10) / 10,
+      percent: Math.min(Math.round((data.percentage ?? 0) * 10) / 10, 100),
       progressLabel: progressLabelKey ? t(progressLabelKey, progressParams) : "",
       dateLabel,
       dateSeverity: resolveDateSeverity(data, daysLeft),
