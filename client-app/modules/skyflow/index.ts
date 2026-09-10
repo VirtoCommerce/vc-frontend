95f9d60ef56e2480dcc21d25249dfa1a760bf612
Anatolii Vasilev <anatolii.vslvv@gmail.com>
2026-09-10 09:48:18 +0200

    fix: by review

--- client-app/modules/skyflow/index.ts
+++ client-app/modules/skyflow/index.ts
@@ -1,10 +1,10 @@
 import { defineAsyncComponent } from "vue";
-import { useModules, useNavigations } from "@/core/composables";
+import { useNavigations } from "@/core/composables";
+import { useModuleSettings } from "@/core/composables/useModuleSettings";
 import { useUser } from "@/shared/account/composables";
 import { useExtensionRegistry } from "@/shared/common/composables/extensionRegistry/useExtensionRegistry";
-import { EXTENSION_NAMES } from "@/shared/common/constants/extensionPointsNames.ts";
 import { loadModuleLocale } from "../utils";
-import { MODULE_ID_SKYFLOW, SKYFLOW_PAYMENT_TYPE_NAME } from "./constants";
+import { MODULE_ID_SKYFLOW, SKYFLOW_PAYMENT_EXTENSION, SKYFLOW_PAYMENT_TYPE_NAME } from "./constants";
 import type { MenuType } from "@/core/types";
 import type { I18n } from "@/i18n";
 import type { DeepPartial } from "utility-types";
@@ -13,7 +13,7 @@ import type { Router, RouteRecordRaw } from "vue-router";
 const SavedCreditCards = () => import("./pages/saved-credit-cards.vue");
 const PaymentProcessingSkyflow = defineAsyncComponent(() => import("./components/payment-processing-skyflow.vue"));
 
-const { hasModule } = useModules();
+const { hasModuleSettings } = useModuleSettings(MODULE_ID_SKYFLOW);
 const { mergeMenuSchema } = useNavigations();
 const { register } = useExtensionRegistry();
 
@@ -60,17 +60,21 @@ function renderCondition({ paymentTypeName }: { paymentTypeName: string }) {
 
 export function init(router: Router, i18n: I18n): void {
   const { isAuthenticated } = useUser();
-  const isSkyflowInstalled = hasModule(MODULE_ID_SKYFLOW);
+  const isSkyflowInstalled = hasModuleSettings.value;
 
   if (isSkyflowInstalled) {
     router.addRoute("Account", route);
     void loadModuleLocale(i18n, "skyflow");
 
-    register("paymentPage", EXTENSION_NAMES.paymentPage.skyflowPaymentMethod, {
+    register("paymentPage", SKYFLOW_PAYMENT_EXTENSION, {
       component: PaymentProcessingSkyflow,
       condition: renderCondition,
     });
-    register("orderPaymentPage", EXTENSION_NAMES.orderPaymentPage.skyflowPaymentMethod, {
+    register("orderPaymentPage", SKYFLOW_PAYMENT_EXTENSION, {
+      component: PaymentProcessingSkyflow,
+      condition: renderCondition,
+    });
+    register("cartPayment", SKYFLOW_PAYMENT_EXTENSION, {
       component: PaymentProcessingSkyflow,
       condition: renderCondition,
     });
