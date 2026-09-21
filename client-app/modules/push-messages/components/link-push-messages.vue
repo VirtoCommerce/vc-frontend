c9858f9d4614c367f36aa2796bc6230c601dc1ba
Maiia Diachkovskaia <mayapavlovna@gmail.com>
2026-09-21 14:06:40 +0300

    fix(VCST-6030): render the push-messages header link as a pod

--- client-app/modules/push-messages/components/link-push-messages.vue
+++ client-app/modules/push-messages/components/link-push-messages.vue
@@ -1,15 +1,13 @@
 <template>
   <PushMessages v-if="isPushMessagesActive" :offset-options="20">
     <template #trigger="{ totalCount, unreadCount, triggerProps }">
-      <BottomHeaderLink :link="item" :count="unreadCount" v-bind="triggerProps">
+      <HeaderPod :link="item" :count="unreadCount" v-bind="triggerProps">
         <template #icon>
           <transition :name="unreadCount ? 'shake' : ''" mode="out-in">
-            <VcIcon :key="totalCount" :name="item.icon" class="mb-0.5 text-primary" :size="24" />
+            <VcIcon :key="totalCount" :name="item.icon" :size="20" />
           </transition>
         </template>
-
-        {{ item.title }}
-      </BottomHeaderLink>
+      </HeaderPod>
     </template>
   </PushMessages>
 </template>
@@ -18,7 +16,7 @@
 import { isActive as isPushMessagesActive } from "../composables/usePushMessages";
 import PushMessages from "./push-messages.vue";
 import type { ExtendedMenuLinkType } from "@/core/types";
-import BottomHeaderLink from "@/shared/layout/components/header/_internal/bottom-header-link.vue";
+import HeaderPod from "@/shared/layout/components/header/_internal/header-pod.vue";
 
 defineProps<{ item: ExtendedMenuLinkType }>();
 </script>
